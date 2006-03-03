aa2381b92462d2b8ffcd6b8512a9614c00d7a86b
Jeremy Boynes <jboynes@apache.org>
2006-03-03 15:57:24 +0000

    add a custom loader to allow privileged servlets to be added to an application
    git-svn-id: https://svn.apache.org/repos/asf/incubator/tuscany/java/sca@382877 13f79535-47bb-0310-9956-ffa450edef68

--- tomcat/src/test/java/org/apache/tuscany/tomcat/integration/TomcatIntegrationTestCase.java
+++ tomcat/src/test/java/org/apache/tuscany/tomcat/integration/TomcatIntegrationTestCase.java
@@ -17,11 +17,14 @@
 package org.apache.tuscany.tomcat.integration;
 
 import org.apache.catalina.Valve;
+import org.apache.catalina.Loader;
+import org.apache.catalina.loader.WebappLoader;
 import org.apache.catalina.core.StandardContext;
 import org.apache.catalina.core.StandardWrapper;
 import org.apache.catalina.startup.ContextConfig;
 import org.apache.tuscany.tomcat.TuscanyHost;
 import org.apache.tuscany.tomcat.TuscanyValve;
+import org.apache.tuscany.tomcat.ContainerLoader;
 
 import java.io.File;
 
@@ -32,19 +35,19 @@ public class TomcatIntegrationTestCase extends AbstractTomcatTest {
     protected File app1;
 
     public void testRuntimeIntegration() throws Exception {
+        // create the webapp Context
         StandardContext ctx = new StandardContext();
-
-        // caution: this sets the parent of the webapp loader to the test classloader so it can find TestServlet
-        // anything that relies on the TCCL may not work correctly
-        ClassLoader cl = TestServlet.class.getClassLoader();
-        ctx.setParentClassLoader(cl);
-
         ctx.addLifecycleListener(new ContextConfig());
         ctx.setName("testContext");
         ctx.setDocBase(app1.getAbsolutePath());
+
+        // define our test servlet
+        Loader loader = new ContainerLoader(TestServlet.class.getClassLoader());
         StandardWrapper wrapper = new StandardWrapper();
         wrapper.setServletClass(TestServlet.class.getName());
+        wrapper.setLoader(loader);
         ctx.addChild(wrapper);
+
         host.addChild(ctx);
         boolean found = false;
         for (Valve valve: ctx.getPipeline().getValves()) {
