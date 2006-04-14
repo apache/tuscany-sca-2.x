b9d1e8d7f2ec58ea4288211a9267c4d22b42c6d7
Jeremy Boynes <jboynes@apache.org>
2006-04-14 16:00:44 +0000

    refactor to create a support class for test cases that need an XMLStreamReader
    git-svn-id: https://svn.apache.org/repos/asf/incubator/tuscany/java/sca@394128 13f79535-47bb-0310-9956-ffa450edef68

--- core/src/test/java/org/apache/tuscany/core/loader/StAXLoaderRegistryTestCase.java
+++ core/src/test/java/org/apache/tuscany/core/loader/StAXLoaderRegistryTestCase.java
@@ -19,10 +19,8 @@ package org.apache.tuscany.core.loader;
 import java.util.List;
 import java.util.ArrayList;
 import javax.xml.namespace.QName;
-import javax.xml.namespace.NamespaceContext;
 import javax.xml.stream.XMLStreamReader;
 import javax.xml.stream.XMLStreamException;
-import javax.xml.stream.Location;
 
 import junit.framework.TestCase;
 
@@ -136,187 +134,11 @@ public class StAXLoaderRegistryTestCase extends TestCase {
         }
     }
 
-    public static class MockReader implements XMLStreamReader {
+    private static class MockReader extends MockReaderSupport {
         private QName name;
 
         public QName getName() {
             return name;
-        }
-
-        public Object getProperty(String name) throws IllegalArgumentException {
-            throw new UnsupportedOperationException();
-        }
-
-        public int next() throws XMLStreamException {
-            throw new UnsupportedOperationException();
-        }
-
-        public void require(int i, String name, String name1) throws XMLStreamException {
-            throw new UnsupportedOperationException();
-        }
-
-        public String getElementText() throws XMLStreamException {
-            throw new UnsupportedOperationException();
-        }
-
-        public int nextTag() throws XMLStreamException {
-            throw new UnsupportedOperationException();
-        }
-
-        public boolean hasNext() throws XMLStreamException {
-            throw new UnsupportedOperationException();
-        }
-
-        public void close() throws XMLStreamException {
-            throw new UnsupportedOperationException();
-        }
-
-        public String getNamespaceURI(String name) {
-            throw new UnsupportedOperationException();
-        }
-
-        public boolean isStartElement() {
-            throw new UnsupportedOperationException();
-        }
-
-        public boolean isEndElement() {
-            throw new UnsupportedOperationException();
-        }
-
-        public boolean isCharacters() {
-            throw new UnsupportedOperationException();
-        }
-
-        public boolean isWhiteSpace() {
-            throw new UnsupportedOperationException();
-        }
-
-        public String getAttributeValue(String name, String name1) {
-            throw new UnsupportedOperationException();
-        }
-
-        public int getAttributeCount() {
-            throw new UnsupportedOperationException();
-        }
-
-        public QName getAttributeName(int i) {
-            throw new UnsupportedOperationException();
-        }
-
-        public String getAttributeNamespace(int i) {
-            throw new UnsupportedOperationException();
-        }
-
-        public String getAttributeLocalName(int i) {
-            throw new UnsupportedOperationException();
-        }
-
-        public String getAttributePrefix(int i) {
-            throw new UnsupportedOperationException();
-        }
-
-        public String getAttributeType(int i) {
-            throw new UnsupportedOperationException();
-        }
-
-        public String getAttributeValue(int i) {
-            throw new UnsupportedOperationException();
-        }
-
-        public boolean isAttributeSpecified(int i) {
-            throw new UnsupportedOperationException();
-        }
-
-        public int getNamespaceCount() {
-            throw new UnsupportedOperationException();
-        }
-
-        public String getNamespacePrefix(int i) {
-            throw new UnsupportedOperationException();
-        }
-
-        public String getNamespaceURI(int i) {
-            throw new UnsupportedOperationException();
-        }
-
-        public NamespaceContext getNamespaceContext() {
-            throw new UnsupportedOperationException();
-        }
-
-        public int getEventType() {
-            throw new UnsupportedOperationException();
-        }
-
-        public String getText() {
-            throw new UnsupportedOperationException();
-        }
-
-        public char[] getTextCharacters() {
-            throw new UnsupportedOperationException();
-        }
-
-        public int getTextCharacters(int i, char[] chars, int i1, int i2) throws XMLStreamException {
-            throw new UnsupportedOperationException();
-        }
-
-        public int getTextStart() {
-            throw new UnsupportedOperationException();
-        }
-
-        public int getTextLength() {
-            throw new UnsupportedOperationException();
-        }
-
-        public String getEncoding() {
-            throw new UnsupportedOperationException();
-        }
-
-        public boolean hasText() {
-            throw new UnsupportedOperationException();
-        }
-
-        public Location getLocation() {
-            throw new UnsupportedOperationException();
-        }
-
-        public String getLocalName() {
-            throw new UnsupportedOperationException();
-        }
-
-        public boolean hasName() {
-            throw new UnsupportedOperationException();
-        }
-
-        public String getNamespaceURI() {
-            throw new UnsupportedOperationException();
-        }
-
-        public String getPrefix() {
-            throw new UnsupportedOperationException();
-        }
-
-        public String getVersion() {
-            throw new UnsupportedOperationException();
-        }
-
-        public boolean isStandalone() {
-            throw new UnsupportedOperationException();
-        }
-
-        public boolean standaloneSet() {
-            throw new UnsupportedOperationException();
-        }
-
-        public String getCharacterEncodingScheme() {
-            throw new UnsupportedOperationException();
-        }
-
-        public String getPITarget() {
-            throw new UnsupportedOperationException();
-        }
-
-        public String getPIData() {
-            throw new UnsupportedOperationException();
         }
     }
 }
