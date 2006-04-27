62d9dbabab1c5079ae410c9521971ce6ba8fecf2
Jean Sebastien Delfino <jsdelfino@apache.org>
2006-04-27 03:18:43 +0000

    Moved async non-blocking call support code to sca/core/, moved helloworldasync to samples/helloworld/
    git-svn-id: https://svn.apache.org/repos/asf/incubator/tuscany/java/sca@397375 13f79535-47bb-0310-9956-ffa450edef68

---
+++ core/src/test/java/org/apache/tuscany/core/async/wire/mock/MockHandler.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package org.apache.tuscany.core.async.wire.mock;
+
+import org.apache.tuscany.core.wire.MessageHandler;
+import org.apache.tuscany.core.message.Message;
+
+/**
+ *
+ */
+public class MockHandler implements MessageHandler {
+
+    private int count =0;
+    
+    public boolean processMessage(Message message) {
+        //System.out.println("Invoking handler");
+        count++;
+        return true;
+    }
+    
+    public int getCount(){
+        return count;
+    }
+}
