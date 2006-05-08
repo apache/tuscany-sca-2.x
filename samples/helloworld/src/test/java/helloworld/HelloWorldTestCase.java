c8b681a4362cc806f5e2da0e4086947e185580f5
Jean Sebastien Delfino <jsdelfino@apache.org>
2006-05-08 06:07:10 +0000

    TUSCANY-159 - cleanup old packages, move test case to the helloworld package
    git-svn-id: https://svn.apache.org/repos/asf/incubator/tuscany/java/sca@404939 13f79535-47bb-0310-9956-ffa450edef68

--- samples/helloworld/src/test/java/helloworld/HelloWorldTestCase.java
+++ samples/helloworld/src/test/java/helloworld/HelloWorldTestCase.java
@@ -14,7 +14,7 @@
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
  */
-package org.apache.tuscany.samples.helloworld;
+package helloworld;
 
 import helloworld.HelloWorldService;
 import junit.framework.TestCase;
