3940a4dad1846ecfebb42c93ee48e571890f8130
Jeremy Boynes <jboynes@apache.org>
2006-05-25 20:05:19 +0000

    apply patch from Raymond Feng for skeleton loaders
    git-svn-id: https://svn.apache.org/repos/asf/incubator/tuscany/sandbox/jboynes/sca@409453 13f79535-47bb-0310-9956-ffa450edef68

---
+++ containers/container.java/src/main/java/org/apache/tuscany/container/java/JavaImplementationLoader.java
@@ -0,0 +1,33 @@
+package org.apache.tuscany.container.java;
+
+import javax.xml.namespace.QName;
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.XMLStreamReader;
+
+import org.apache.tuscany.core.loader.AssemblyConstants;
+import org.apache.tuscany.core.loader.StAXUtil;
+import org.apache.tuscany.spi.deployer.DeploymentContext;
+import org.apache.tuscany.spi.extension.LoaderExtension;
+import org.apache.tuscany.spi.loader.LoaderException;
+import org.apache.tuscany.spi.model.ModelObject;
+
+public class JavaImplementationLoader extends LoaderExtension {
+    public static final QName IMPLEMENTATION_JAVA = new QName(AssemblyConstants.SCA_NAMESPACE, "implementation.java");
+    
+    @Override
+    protected QName getXMLType() {
+        return IMPLEMENTATION_JAVA;
+    }
+
+    public ModelObject load(XMLStreamReader reader, DeploymentContext deploymentContext) throws XMLStreamException, LoaderException {
+        assert IMPLEMENTATION_JAVA.equals(reader.getName());
+        JavaImplementation implementation = new JavaImplementation();
+        String implClass = reader.getAttributeValue(null, "class");
+        Class<?> implementationClass = StAXUtil.loadClass(implClass, deploymentContext.getClassLoader());
+        implementation.setImplementationClass(implementationClass);
+        registry.loadComponentType(implementation, deploymentContext);
+        StAXUtil.skipToEndElement(reader);
+        return implementation;
+    }
+
+}
