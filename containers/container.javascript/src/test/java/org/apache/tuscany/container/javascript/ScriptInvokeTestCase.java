2797ead9cf1d1464400e71cc0be1406e8d30d8d7
Anthony Elder <antelder@apache.org>
2006-07-28 11:19:18 +0000

    Start migrating the old JavaScript container to the new trunk code. This is based on the new groovy container with all the groovy specific bits changed to use rhino. Still work to do and some issues which I'll discuss on the dev list. No e4x support yet
    git-svn-id: https://svn.apache.org/repos/asf/incubator/tuscany/java/sca@426478 13f79535-47bb-0310-9956-ffa450edef68

---
+++ containers/container.javascript/src/test/java/org/apache/tuscany/container/javascript/ScriptInvokeTestCase.java
@@ -0,0 +1,43 @@
+package org.apache.tuscany.container.javascript;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import junit.framework.TestCase;
+
+import org.apache.tuscany.container.javascript.mock.Greeting;
+import org.apache.tuscany.container.javascript.rhino.RhinoScript;
+import org.apache.tuscany.core.component.scope.ModuleScopeContainer;
+import org.apache.tuscany.test.ArtifactFactory;
+
+/**
+ * Tests for invoker JavaScriptComponents
+ */
+public class ScriptInvokeTestCase extends TestCase {
+
+    private static final String SCRIPT = "function greet(name) { return name }";
+
+    private RhinoScript rhinoScript;
+
+    /**
+     * Tests the invocation of a Groovy "script" as opposed to a class
+     */
+    public void testBasicScriptInvocation() throws Exception {
+        ModuleScopeContainer scope = new ModuleScopeContainer(null);
+        scope.start();
+        List<Class<?>> services = new ArrayList<Class<?>>();
+        services.add(Greeting.class);
+        JavaScriptComponent<Greeting> context = new JavaScriptComponent<Greeting>("source", rhinoScript, services, new HashMap<String, Object>(),
+                null, scope, ArtifactFactory.createWireService());
+        scope.register(context);
+        Greeting object = (Greeting) context.getServiceInstance();
+        assertEquals("foo", object.greet("foo"));
+        scope.stop();
+    }
+
+    protected void setUp() throws Exception {
+        super.setUp();
+        rhinoScript = new RhinoScript("test", SCRIPT);
+    }
+}
