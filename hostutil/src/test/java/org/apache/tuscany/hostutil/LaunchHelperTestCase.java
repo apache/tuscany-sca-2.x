6cfc43808809b3dc31dd3030e32ec5439dd8e123
Jeremy Boynes <jboynes@apache.org>
2006-08-14 15:05:02 +0000

    add a support module for host environments
    git-svn-id: https://svn.apache.org/repos/asf/incubator/tuscany/java/sca@431365 13f79535-47bb-0310-9956-ffa450edef68

---
+++ hostutil/src/test/java/org/apache/tuscany/hostutil/LaunchHelperTestCase.java
@@ -0,0 +1,57 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.    
+ */
+package org.apache.tuscany.hostutil;
+
+import junit.framework.TestCase;
+
+/**
+ * @version $Rev$ $Date$
+ */
+public class LaunchHelperTestCase extends TestCase {
+    private TestBean instance;
+
+    public void testSetPrimitive() {
+        // fixme this does not work yet
+//        LaunchHelper.setProperty(instance, "int", 12);
+//        assertEquals(12, instance.anInt);
+    }
+
+    public void testSetObject() {
+        LaunchHelper.setProperty(instance, "string", "Hello");
+        assertEquals("Hello", instance.aString);
+    }
+
+    protected void setUp() throws Exception {
+        super.setUp();
+        instance = new TestBean();
+    }
+
+    private static class TestBean {
+        private int anInt;
+        private String aString;
+
+        public void setInt(int anInt) {
+            this.anInt = anInt;
+        }
+
+        public void setString(String aString) {
+            this.aString = aString;
+        }
+    }
+}
