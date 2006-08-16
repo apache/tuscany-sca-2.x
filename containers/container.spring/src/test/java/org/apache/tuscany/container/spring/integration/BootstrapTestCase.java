a3a5d22455b0a7b11da45eca92b14318c828ebe4
James F. Marino <jmarino@apache.org>
2006-08-16 03:25:06 +0000

    add Javadoc
    git-svn-id: https://svn.apache.org/repos/asf/incubator/tuscany/java/sca@431787 13f79535-47bb-0310-9956-ffa450edef68

--- containers/container.spring/src/test/java/org/apache/tuscany/container/spring/integration/BootstrapTestCase.java
+++ containers/container.spring/src/test/java/org/apache/tuscany/container/spring/integration/BootstrapTestCase.java
@@ -10,6 +10,11 @@ import org.apache.tuscany.container.spring.mock.TestBean;
 import org.apache.tuscany.test.SCATestCase;
 
 /**
+ * Bootstraps a simple scenario where a service can invoke a Spring bean. This test case is intended to be temporary and
+ * replaced when the SPI test harness is finished.
+ * <p/>
+ * <bold>PLEASE DO NOT EMULATE</bold>
+ *
  * @version $Rev$ $Date$
  */
 public class BootstrapTestCase extends SCATestCase {
