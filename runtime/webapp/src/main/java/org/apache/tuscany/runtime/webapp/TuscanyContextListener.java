6c8ee39ed62f1b3e807a907e919200ca829b7741
Jeremy Boynes <jboynes@apache.org>
2006-08-17 21:38:42 +0000

    new context listener with isolated classloader
    git-svn-id: https://svn.apache.org/repos/asf/incubator/tuscany/java/sca@432397 13f79535-47bb-0310-9956-ffa450edef68

--- runtime/webapp/src/main/java/org/apache/tuscany/runtime/webapp/TuscanyContextListener.java
+++ runtime/webapp/src/main/java/org/apache/tuscany/runtime/webapp/TuscanyContextListener.java
@@ -18,8 +18,15 @@
  */
 package org.apache.tuscany.runtime.webapp;
 
+import java.util.Set;
+import java.net.URL;
+import java.net.MalformedURLException;
+import java.net.URLClassLoader;
+import java.beans.Beans;
+import java.io.IOException;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
+import javax.servlet.ServletContext;
 
 /**
  * Launcher for runtime environment that loads info from servlet context params.
@@ -31,13 +38,67 @@ import javax.servlet.ServletContextListener;
  */
 public class TuscanyContextListener implements ServletContextListener {
     /**
-     * Servlet context-param name for user-specified system SCDL path.
+     * Name of the context parameter that defines the directory containing bootstrap jars.
      */
-    public static final String SYSTEM_SCDL_PATH_PARAM = "systemScdlPath";
+    public static final String BOOTDIR_PARAM = "tuscany.bootDir";
+
+    /**
+     * Name of the class to load to launch the runtime.
+     */
+    public static final String LAUNCHER_PARAM = "tuscany.launcherClass";
+
+    private ServletContextListener runtime;
 
     public void contextInitialized(ServletContextEvent servletContextEvent) {
+        ServletContext servletContext = servletContextEvent.getServletContext();
+        ClassLoader bootClassLoader = getBootClassLoader(servletContext);
+
+        runtime = getLauncher(servletContext, bootClassLoader);
+
+        runtime.contextInitialized(servletContextEvent);
     }
 
     public void contextDestroyed(ServletContextEvent servletContextEvent) {
+        if (runtime != null) {
+            runtime.contextDestroyed(servletContextEvent);
+        }
+    }
+
+    protected ClassLoader getBootClassLoader(ServletContext servletContext) {
+        ClassLoader contextClassLoader = Thread.currentThread().getContextClassLoader();
+        String bootDirName = servletContext.getInitParameter(BOOTDIR_PARAM);
+        if (bootDirName == null) {
+            bootDirName = "/WEB-INF/tuscany/boot/";
+        }
+        Set paths = servletContext.getResourcePaths(bootDirName);
+        if (paths == null) {
+            // nothing in boot directory, assume everything is in the webapp classloader
+            return contextClassLoader;
+        }
+        URL[] urls = new URL[paths.size()];
+        int i =0;
+        for (Object path : paths) {
+            try {
+                urls[i++] = servletContext.getResource((String) path);
+            } catch (MalformedURLException e) {
+                throw new AssertionError("getResourcePaths returned an invalid path");
+            }
+        }
+        return new URLClassLoader(urls, contextClassLoader);
+    }
+
+    protected ServletContextListener getLauncher(ServletContext servletContext, ClassLoader bootClassLoader) {
+        String launcherClass = servletContext.getInitParameter(LAUNCHER_PARAM);
+        if (launcherClass == null) {
+            launcherClass = "org.apache.tuscany.core.launcher.ServletLauncherListener";
+        }
+
+        try {
+            return (ServletContextListener) Beans.instantiate(bootClassLoader, launcherClass);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
