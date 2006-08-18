800eb6a3a054d36b7c84a2bdd87094e3e4d190f8
James F. Marino <jmarino@apache.org>
2006-08-18 07:24:12 +0000

    initial commit of OSGi host for Joel, Tuscany-610
    git-svn-id: https://svn.apache.org/repos/asf/incubator/tuscany/java/sca@432508 13f79535-47bb-0310-9956-ffa450edef68

---
+++ runtime/osgi/src/main/java/org/apache/tuscany/osgi/binding/NoRemoteServiceException.java
@@ -0,0 +1,39 @@
+/*
+ *
+ * Copyright 2006 The Apache Software Foundation or its licensors as applicable
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package org.apache.tuscany.osgi.binding;
+
+import org.osgi.framework.BundleException;
+
+/**
+ * FIXME class not used
+ *
+ * @version $Rev: 424773 $ $Date: 2006-07-23 14:14:09 -0400 (Sun, 23 Jul 2006) $
+ */
+public class NoRemoteServiceException extends BundleException {
+
+    public NoRemoteServiceException(String message) {
+        super(message);
+    }
+
+    public NoRemoteServiceException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public NoRemoteServiceException(Throwable cause) {
+        super("", cause);
+    }
+}
---
+++ runtime/osgi/src/main/java/org/apache/tuscany/osgi/binding/OSGiBindingBuilder.java
@@ -0,0 +1,79 @@
+/*
+ *
+ * Copyright 2006 The Apache Software Foundation or its licensors as applicable
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package org.apache.tuscany.osgi.binding;
+
+import org.osoa.sca.annotations.Constructor;
+
+import org.apache.tuscany.spi.QualifiedName;
+import org.apache.tuscany.spi.annotation.Autowire;
+import org.apache.tuscany.spi.component.CompositeComponent;
+import org.apache.tuscany.spi.component.SCAObject;
+import org.apache.tuscany.spi.deployer.DeploymentContext;
+import org.apache.tuscany.spi.extension.BindingBuilderExtension;
+import org.apache.tuscany.spi.model.BoundReferenceDefinition;
+import org.apache.tuscany.spi.model.BoundServiceDefinition;
+
+import org.apache.tuscany.osgi.OSGiHost;
+
+/**
+ * Builds a Service or Reference for an OSGi binding.
+ *
+ * @version $Rev: 424773 $ $Date: 2006-07-23 14:14:09 -0400 (Sun, 23 Jul 2006) $
+ */
+public class OSGiBindingBuilder extends BindingBuilderExtension<OSGiBinding> {
+
+    OSGiHost host;
+
+    @Constructor
+    public OSGiBindingBuilder(@Autowire OSGiHost host) {
+        this.host = host;
+    }
+
+    protected Class<OSGiBinding> getBindingType() {
+        return OSGiBinding.class;
+    }
+
+    @SuppressWarnings("unchecked")
+    public SCAObject build(CompositeComponent parent,
+                           BoundServiceDefinition<OSGiBinding> boundServiceDefinition,
+                           DeploymentContext deploymentContext) {
+        String name = boundServiceDefinition.getName();
+        Class<? extends Object> service = getServiceInterface(boundServiceDefinition);
+        String uri = boundServiceDefinition.getBinding().getURI();
+        String osgiServiceName = boundServiceDefinition.getBinding().getService();
+        Class<?> interfaze = boundServiceDefinition.getServiceContract().getInterfaceClass();
+        QualifiedName targetName = new QualifiedName(boundServiceDefinition.getTarget().getPath());
+
+        return new OSGiService(name, parent, wireService, osgiServiceName, service, host);
+    }
+
+    @SuppressWarnings("unchecked")
+    public OSGiReference build(CompositeComponent parent,
+                               BoundReferenceDefinition<OSGiBinding> boundReferenceDefinition,
+                               DeploymentContext deploymentContext) {
+        String name = boundReferenceDefinition.getName();
+        String uri = boundReferenceDefinition.getBinding().getURI();
+        Class<?> interfaze = boundReferenceDefinition.getServiceContract().getInterfaceClass();
+        return new OSGiReference(name, interfaze, parent, wireService, uri);
+    }
+
+    @SuppressWarnings({"unchecked"})
+    protected Class<? extends Object> getServiceInterface(BoundServiceDefinition<OSGiBinding> boundServiceDefinition) {
+        return (Class<?>) boundServiceDefinition.getServiceContract().getInterfaceClass();
+    }
+
+}
