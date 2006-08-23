6ffcf335744716b99257aa2419dcacc7281697a3
James F. Marino <jmarino@apache.org>
2006-08-23 17:58:54 +0000

    commit Jervis' patch Tuscany-661 for /sca
    git-svn-id: https://svn.apache.org/repos/asf/incubator/tuscany/java/sca@434106 13f79535-47bb-0310-9956-ffa450edef68

--- bindings/binding.celtix/src/main/java/org/apache/tuscany/binding/celtix/CeltixBindingBuilder.java
+++ bindings/binding.celtix/src/main/java/org/apache/tuscany/binding/celtix/CeltixBindingBuilder.java
@@ -6,15 +6,15 @@
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
  * with the License.  You may obtain a copy of the License at
- * 
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
  * KIND, either express or implied.  See the License for the
  * specific language governing permissions and limitations
- * under the License.    
+ * under the License.
  */
 package org.apache.tuscany.binding.celtix;
 
@@ -32,7 +32,7 @@ import org.apache.tuscany.spi.model.BoundServiceDefinition;
 import org.objectweb.celtix.Bus;
 
 /**
- * Builds a {@link Service} or {@link org.apache.tuscany.spi.component.Reference} configured 
+ * Builds a {@link Service} or {@link org.apache.tuscany.spi.component.Reference} configured
  * with the Celtix binding
  *
  * @version $Rev$ $Date$
@@ -44,8 +44,6 @@ public class CeltixBindingBuilder extends BindingBuilderExtension<WebServiceBind
     public SCAObject build(CompositeComponent parent,
                            BoundServiceDefinition<WebServiceBinding> boundServiceDefinition,
                            DeploymentContext deploymentContext) {
-        //FIXME: CeltixService needs an instance of BusService. How to get BusService wired in 
-        //and where BusService is created?
         WebServiceBinding wsBinding = boundServiceDefinition.getBinding();
         if (bus == null) {
             bus = getBus(wsBinding.getWSDLDefinitionRegistry());
@@ -78,13 +76,13 @@ public class CeltixBindingBuilder extends BindingBuilderExtension<WebServiceBind
     protected Class<WebServiceBinding> getBindingType() {
         return WebServiceBinding.class;
     }
-    
+
     private Bus getBus(WSDLDefinitionRegistry wsdlDefinitionRegistry) {
         Bus celtixBus = null;
         try {
             Map<String, Object> properties = new WeakHashMap<String, Object>();
             properties.put("celtix.WSDLManager", new TuscanyWSDLManager(wsdlDefinitionRegistry));
-            bus = Bus.init(new String[0], properties);
+            celtixBus = Bus.init(new String[0], properties);
         } catch (Exception e) {
             e.printStackTrace();
         }
--- bindings/binding.celtix/src/main/java/org/apache/tuscany/binding/celtix/WebServiceBindingLoader.java
+++ bindings/binding.celtix/src/main/java/org/apache/tuscany/binding/celtix/WebServiceBindingLoader.java
@@ -6,31 +6,37 @@
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
  * with the License.  You may obtain a copy of the License at
- * 
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
  * KIND, either express or implied.  See the License for the
  * specific language governing permissions and limitations
- * under the License.    
+ * under the License.
  */
 package org.apache.tuscany.binding.celtix;
 
+
+import java.io.IOException;
+import java.net.URL;
 import java.util.Collection;
+import java.util.LinkedList;
 import java.util.List;
 import javax.wsdl.Definition;
 import javax.wsdl.Port;
 import javax.wsdl.Service;
+import javax.wsdl.WSDLException;
+import javax.wsdl.factory.WSDLFactory;
+import javax.wsdl.xml.WSDLReader;
 import javax.xml.namespace.QName;
 import javax.xml.stream.XMLStreamException;
 import javax.xml.stream.XMLStreamReader;
 
-import static org.osoa.sca.Version.XML_NAMESPACE_1_0;
-import org.osoa.sca.annotations.Constructor;
-import org.osoa.sca.annotations.Scope;
-
+import org.apache.tuscany.idl.wsdl.WSDLDefinitionRegistry;
+import org.apache.tuscany.idl.wsdl.WSDLDefinitionRegistryImpl;
+import org.apache.tuscany.idl.wsdl.WSDLDefinitionRegistryImpl.Monitor;
 import org.apache.tuscany.spi.annotation.Autowire;
 import org.apache.tuscany.spi.component.CompositeComponent;
 import org.apache.tuscany.spi.deployer.DeploymentContext;
@@ -38,7 +44,10 @@ import org.apache.tuscany.spi.extension.LoaderExtension;
 import org.apache.tuscany.spi.loader.LoaderException;
 import org.apache.tuscany.spi.loader.LoaderRegistry;
 
-import org.apache.tuscany.idl.wsdl.WSDLDefinitionRegistry;
+import org.osoa.sca.annotations.Constructor;
+import org.osoa.sca.annotations.Scope;
+import org.xml.sax.InputSource;
+import static org.osoa.sca.Version.XML_NAMESPACE_1_0;
 
 /**
  * Parses a <code>WebServiceBinding</code> entry in an assembly XML file
@@ -51,11 +60,28 @@ public class WebServiceBindingLoader extends LoaderExtension<WebServiceBinding> 
 
     protected WSDLDefinitionRegistry wsdlRegistry;
 
-    @Constructor({"registry", "wsdlregistry"})
+    @Constructor({"registry", "wsdlregistry" })
     public WebServiceBindingLoader(@Autowire LoaderRegistry registry,
-                                   @Autowire WSDLDefinitionRegistry wsdlRegistry) {
+                                   @Autowire WSDLDefinitionRegistry theWsdlRegistry) {
         super(registry);
-        this.wsdlRegistry = wsdlRegistry;
+        this.wsdlRegistry = theWsdlRegistry;
+
+        //FIXME:  this is a  hack, WSDLDefinitionRegistry should not be created here
+        if (wsdlRegistry == null) {
+            try {
+                wsdlRegistry = new WSDLDefinitionRegistryImpl();
+                Monitor monitor = new Monitor() {
+                    public void readingWSDL(String namespace, URL location) {
+                    }
+                    public void cachingDefinition(String namespace, URL location) {
+                    }
+                };
+
+                ((WSDLDefinitionRegistryImpl)wsdlRegistry).setMonitor(monitor);
+            } catch (javax.wsdl.WSDLException e) {
+                //do nothing
+            }
+        }
     }
 
     public QName getXMLType() {
@@ -66,12 +92,22 @@ public class WebServiceBindingLoader extends LoaderExtension<WebServiceBinding> 
                                   XMLStreamReader reader,
                                   DeploymentContext deploymentContext)
         throws XMLStreamException, LoaderException {
-        String uri = reader.getAttributeValue(null, "uri");
-        String portURI = reader.getAttributeValue(null, "port");
-        return createBinding(uri, portURI);
+        // not sure what uri was here ? String uri = reader.getAttributeValue(null, "uri");
+        String uri = null;
+        String endpointAttribute = reader.getAttributeValue(null, "endpoint");
+        //String portURI = reader.getAttributeValue(null, "port");
+        String wsdlLocation = reader.getAttributeValue(null, "location");
+        try {
+            return createBinding(uri, endpointAttribute, wsdlLocation, deploymentContext);
+        } catch (Exception e) {
+
+            throw new LoaderException(e);
+        }
     }
 
-    private WebServiceBinding createBinding(String port, String portURI) {
+    private WebServiceBinding createBinding(String port, String portURI, String wsdlLocation,
+                                            DeploymentContext deploymentContext)
+        throws WSDLException, IOException {
         List<Definition> definitions = null;
         // FIXME wsdlRegistry.getDefinitionsForNamespace(portNamespace,
         // resourceLoader);
@@ -98,10 +134,30 @@ public class WebServiceBindingLoader extends LoaderExtension<WebServiceBinding> 
                 portName = fragment;
             }
 
-            // FIXME definitions is always null
-            if (definitions == null) {
-                throw new IllegalArgumentException("Cannot find WSDL definition for " + portNamespace);
+            // FIXME need to find out how to get wsdl and what context to use --- terrible hack attack!
+            // URL wsdlurl = Thread.currentThread().getContextClassLoader().getResource(wsdlLocation);
+            if (null == wsdlLocation) {
+                throw new RuntimeException("Failed to determin wsdl location on binding. "
+                    + "Try specifying 'location' attribute on  binding.");
             }
+            URL wsdlurl = deploymentContext.getClassLoader().getResource(wsdlLocation);
+            if (wsdlurl == null) {
+                throw new RuntimeException("Failed to load wsdl from '" + wsdlLocation + "'");
+            }
+
+            WSDLFactory factory = WSDLFactory.newInstance();
+            WSDLReader reader = factory.newWSDLReader();
+            reader.setFeature("javax.wsdl.verbose", false);
+            InputSource input = new InputSource(wsdlurl.openStream());
+            Definition wsdlDef = reader.readWSDL(wsdlurl.toString(), input);
+            definitions = new LinkedList<Definition>();
+            definitions.add(wsdlDef);
+            // FIXME all the above needs to better addressed.
+
+            //FIXME: if a global wsdl cached is used, we need to do a registration here
+            String namespace = wsdlDef.getTargetNamespace();
+            wsdlRegistry.loadDefinition(namespace, wsdlurl);
+
             Definition definition = null;
             Port thePort = null;
             Service service = null;
@@ -128,7 +184,9 @@ public class WebServiceBindingLoader extends LoaderExtension<WebServiceBinding> 
                 throw new IllegalArgumentException("Cannot find WSDL port " + portURI);
 
             }
-            return new WebServiceBinding(definition, thePort, port, portURI, service);
+            WebServiceBinding wsBinding = new WebServiceBinding(definition, thePort, port, portURI, service);
+            wsBinding.setWSDLDefinitionRegistry(wsdlRegistry);
+            return wsBinding;
         }
         // FIXME - return a broken binding for now
         return new WebServiceBinding(null, null, null, portURI, null);
