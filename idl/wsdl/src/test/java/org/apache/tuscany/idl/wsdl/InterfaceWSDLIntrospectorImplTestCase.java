952b5ccc83a182e910a9762e63bba299c4a76f86
Zhaohui Feng <rfeng@apache.org>
2006-08-30 01:00:58 +0000

    Add InterfaceWSDLIntrospector to introspect WSDL portTypes to create WSDLServiceContracts
    git-svn-id: https://svn.apache.org/repos/asf/incubator/tuscany/java/sca@438318 13f79535-47bb-0310-9956-ffa450edef68

---
+++ idl/wsdl/src/test/java/org/apache/tuscany/idl/wsdl/InterfaceWSDLIntrospectorImplTestCase.java
@@ -0,0 +1,82 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.    
+ */
+
+package org.apache.tuscany.idl.wsdl;
+
+import java.net.URL;
+import java.util.List;
+import java.util.Map;
+
+import javax.wsdl.Definition;
+import javax.wsdl.PortType;
+import javax.wsdl.factory.WSDLFactory;
+import javax.wsdl.xml.WSDLReader;
+import javax.xml.namespace.QName;
+
+import org.apache.tuscany.spi.idl.InvalidServiceContractException;
+import org.apache.tuscany.spi.model.DataType;
+import org.apache.tuscany.spi.model.Operation;
+
+import junit.framework.Assert;
+import junit.framework.TestCase;
+
+/**
+ * 
+ */
+public class InterfaceWSDLIntrospectorImplTestCase extends TestCase {
+    private static final QName PORTTYPE_NAME = new QName("http://example.com/stockquote.wsdl", "StockQuotePortType");
+
+    private PortType portType;
+
+    /**
+     * @see junit.framework.TestCase#setUp()
+     */
+    protected void setUp() throws Exception {
+        super.setUp();
+        WSDLFactory factory = WSDLFactory.newInstance();
+        URL url = getClass().getResource("stockquote.wsdl");
+        WSDLReader reader = factory.newWSDLReader();
+        reader.setFeature("javax.wsdl.verbose", false);
+        Definition definition = reader.readWSDL(url.toExternalForm());
+        portType = definition.getPortType(PORTTYPE_NAME);
+    }
+
+    public final void testIntrospectPortType() throws InvalidServiceContractException {
+        InterfaceWSDLIntrospector introspector = new InterfaceWSDLIntrospectorImpl();
+        WSDLServiceContract contract = introspector.introspect(portType);
+        Assert.assertEquals(contract.getInterfaceName(), "StockQuotePortType");
+        Map<String, Operation<QName>> operations = contract.getOperations();
+        Assert.assertEquals(1, operations.size());
+        Operation<QName> operation = operations.get("getLastTradePrice");
+        Assert.assertNotNull(operation);
+        List<DataType<QName>> inputTypes = operation.getParameterTypes();
+        Assert.assertEquals(1, inputTypes.size());
+        DataType<QName> returnType = operation.getReturnType();
+        Assert.assertNotNull(returnType);
+        Assert.assertEquals(0, operation.getFaultTypes().size());
+    }
+
+    public final void testIntrospectPortTypePortType() throws InvalidServiceContractException {
+        InterfaceWSDLIntrospector introspector = new InterfaceWSDLIntrospectorImpl();
+        WSDLServiceContract contract = introspector.introspect(portType, portType);
+        Assert.assertEquals("StockQuotePortType", contract.getInterfaceName());
+        Assert.assertEquals("StockQuotePortType", contract.getCallbackName());
+    }
+
+}
