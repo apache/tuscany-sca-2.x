ba00d32d540f80d9c90949235fce16d2e4fb7858
Meeraj Kunnumpurath <meerajk@apache.org>
2006-09-17 11:41:08 +0000

    First revision : work in progress.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/tuscany/java/sca@447051 13f79535-47bb-0310-9956-ffa450edef68

---
+++ services/maven/src/test/java/org/apache/tuscany/services/maven/MavenArtifactRepositoryTest.java
@@ -0,0 +1,55 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.    
+ */
+package org.apache.tuscany.services.maven;
+
+import junit.framework.TestCase;
+
+/**
+ * @version
+ *
+ */
+public class MavenArtifactRepositoryTest extends TestCase {
+
+    public MavenArtifactRepositoryTest(String arg0) {
+        super(arg0);
+    }
+
+    protected void setUp() throws Exception {
+        super.setUp();
+    }
+
+    protected void tearDown() throws Exception {
+        super.tearDown();
+    }
+
+    /*
+     * Test method for 'org.apache.tuscany.services.maven.MavenArtifactRepository.resolve(Artifact)'
+     */
+    public void testResolveArtifact() {
+
+    }
+
+    /*
+     * Test method for 'org.apache.tuscany.services.maven.MavenArtifactRepository.resolve(Collection<? extends Artifact>)'
+     */
+    public void testResolveCollectionOfQextendsArtifact() {
+
+    }
+
+}
