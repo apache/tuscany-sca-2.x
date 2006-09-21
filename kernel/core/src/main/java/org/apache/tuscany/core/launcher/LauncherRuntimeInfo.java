1b99a3598f016a9af7df270f0af9b21981fcf6c2
Meeraj Kunnumpurath <meerajk@apache.org>
2006-09-21 20:59:13 +0000

    Added base URL.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/tuscany/java/sca@448682 13f79535-47bb-0310-9956-ffa450edef68

--- kernel/core/src/main/java/org/apache/tuscany/core/launcher/LauncherRuntimeInfo.java
+++ kernel/core/src/main/java/org/apache/tuscany/core/launcher/LauncherRuntimeInfo.java
@@ -19,6 +19,8 @@
 package org.apache.tuscany.core.launcher;
 
 import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 import org.apache.tuscany.spi.services.info.RuntimeInfo;
 
@@ -26,19 +28,54 @@ import org.apache.tuscany.spi.services.info.RuntimeInfo;
  * @version $Rev$ $Date$
  */
 public class LauncherRuntimeInfo implements RuntimeInfo {
+    
+    /** Install directory */
     private final File installDirectory;
+
+    /** Application root directory */
     private final File applicationRootDirectory;
 
+    /**
+     * Initializes the installation and application root directories.
+     * 
+     * @param installDirectory Installation directory.
+     * @param applicationRootDirectory Application root directory.
+     */
     public LauncherRuntimeInfo(File installDirectory, File applicationRootDirectory) {
         this.installDirectory = installDirectory;
         this.applicationRootDirectory = applicationRootDirectory;
     }
-
+    
+    /**
+     * Return the directory where the running runtime was installed.
+     *
+     * @return the directory where the runtime was installed
+     */
     public File getInstallDirectory() {
         return installDirectory;
     }
 
+    /**
+     * Return the root directory used to resolve application file paths.
+     *
+     * @return the directory used to resolve application file paths.
+     */
     public File getApplicationRootDirectory() {
         return applicationRootDirectory;
     }
+    
+    /**
+     * Gets the base URL for the runtime.
+     * 
+     * @return The base URL for the runtime.
+     */
+    public URL getBaseURL() {
+        try {
+            return getInstallDirectory().toURL();
+        } catch (MalformedURLException e) {
+            // TODO Decide on how to handle the exception
+            throw new RuntimeException(e);
+        }
+    }
+    
 }
