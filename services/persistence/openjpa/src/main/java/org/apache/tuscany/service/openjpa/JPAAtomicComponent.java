b2300047b9d528452b4b26a881034fea9ed82d33
James F. Marino <jmarino@apache.org>
2006-10-02 07:21:17 +0000

    skeleton for OpenJPA support
    git-svn-id: https://svn.apache.org/repos/asf/incubator/tuscany/java/sca@451902 13f79535-47bb-0310-9956-ffa450edef68

---
+++ services/persistence/openjpa/src/main/java/org/apache/tuscany/service/openjpa/JPAAtomicComponent.java
@@ -0,0 +1,68 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.tuscany.service.openjpa;
+
+import java.util.ArrayList;
+import java.util.Map;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import org.apache.tuscany.spi.ObjectCreationException;
+import org.apache.tuscany.spi.component.CompositeComponent;
+import org.apache.tuscany.spi.component.ScopeContainer;
+import org.apache.tuscany.spi.component.TargetException;
+import org.apache.tuscany.spi.extension.SystemAtomicComponentExtension;
+
+/**
+ * @version $Rev$ $Date$
+ */
+public class JPAAtomicComponent extends SystemAtomicComponentExtension {
+    private EntityManagerFactory factory;
+    private String persistenceUnit;
+    private Map<Object, Object> configProps;
+
+    public JPAAtomicComponent(String name,
+                              CompositeComponent parent,
+                              ScopeContainer scopeContainer,
+                              String persistenceUnit,
+                              Map<Object, Object> configProps,
+                              int initLevel) {
+        super(name, null, parent, scopeContainer, initLevel);
+        assert persistenceUnit != null;
+        this.persistenceUnit = persistenceUnit;
+        this.configProps = configProps;
+        interfazes = new ArrayList<Class<?>>();
+        interfazes.add(EntityManagerFactory.class);
+    }
+
+    public Object createInstance() throws ObjectCreationException {
+        if (configProps != null) {
+            factory = Persistence.createEntityManagerFactory(persistenceUnit, configProps);
+        } else {
+            factory = Persistence.createEntityManagerFactory(persistenceUnit);
+        }
+        return factory;
+    }
+
+    public Object getTargetInstance() throws TargetException {
+        return factory;
+    }
+
+
+}
