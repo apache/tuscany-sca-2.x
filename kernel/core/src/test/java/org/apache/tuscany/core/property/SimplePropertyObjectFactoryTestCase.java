bd544f331b2af10508feae84336919291ec31cc8
Zhaohui Feng <rfeng@apache.org>
2006-10-04 21:16:00 +0000

    Add some test cases for the property loading
    git-svn-id: https://svn.apache.org/repos/asf/incubator/tuscany/java/sca@453029 13f79535-47bb-0310-9956-ffa450edef68

---
+++ kernel/core/src/test/java/org/apache/tuscany/core/property/SimplePropertyObjectFactoryTestCase.java
@@ -0,0 +1,108 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.    
+ */
+package org.apache.tuscany.core.property;
+
+import junit.framework.TestCase;
+
+import org.apache.tuscany.core.property.PropertyObjectFactoryImpl;
+import org.apache.tuscany.spi.ObjectFactory;
+import org.apache.tuscany.spi.model.Property;
+import org.apache.tuscany.spi.model.PropertyValue;
+import org.easymock.EasyMock;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+
+/**
+ * @version $Rev$ $Date$
+ */
+@SuppressWarnings("unchecked")
+public class SimplePropertyObjectFactoryTestCase extends TestCase {
+
+    private <T> PropertyValue<T> mock(Class<T> cls, String value) {
+        Document document = EasyMock.createMock(Document.class);
+        Element element = EasyMock.createMock(Element.class);
+        EasyMock.expect(document.getDocumentElement()).andReturn(element);
+        EasyMock.expect(element.getTextContent()).andReturn(value);
+        EasyMock.replay(document, element);
+        return new PropertyValue<T>(null, document);
+    }
+
+    public void testInteger() throws Exception {
+
+        PropertyObjectFactoryImpl factory = new PropertyObjectFactoryImpl();
+        Property<Integer> property = new Property<Integer>();
+        property.setJavaType(Integer.class);
+        PropertyValue<Integer> propertyValue = mock(Integer.class, "1");
+        ObjectFactory<Integer> oFactory = factory.createObjectFactory(property, propertyValue);
+        assertEquals(1, oFactory.getInstance().intValue());
+    }
+
+    public void testPrimitiveInt() throws Exception {
+        PropertyObjectFactoryImpl factory = new PropertyObjectFactoryImpl();
+        Property<Integer> property = new Property<Integer>();
+        property.setJavaType(Integer.TYPE);
+        PropertyValue<Integer> propertyValue = mock(Integer.TYPE, "1");
+        ObjectFactory<Integer> oFactory = factory.createObjectFactory(property, propertyValue);
+        assertEquals(1, oFactory.getInstance().intValue());
+    }
+
+    public void testString() throws Exception {
+        PropertyObjectFactoryImpl factory = new PropertyObjectFactoryImpl();
+        Property<String> property = new Property<String>();
+        property.setJavaType(String.class);
+        PropertyValue<String> propertyValue = mock(String.class, "1");
+        ObjectFactory<String> oFactory = factory.createObjectFactory(property, propertyValue);
+        assertEquals("1", oFactory.getInstance());
+    }
+
+    public void testByteArray() throws Exception {
+        PropertyObjectFactoryImpl factory = new PropertyObjectFactoryImpl();
+        Property<byte[]> property = new Property<byte[]>();
+        property.setJavaType(byte[].class);
+        PropertyValue<byte[]> propertyValue = mock(byte[].class, "TWFu"); // BASE64 for "Man"
+        ObjectFactory<byte[]> oFactory = factory.createObjectFactory(property, propertyValue);
+        byte[] result = oFactory.getInstance();
+        byte[] expected = "Man".getBytes();
+        for (int i = 0; i < result.length; i++) {
+            byte b = result[i];
+            if (b != expected[i]) {
+                fail();
+            }
+        }
+    }
+
+    public void testBoolean() throws Exception {
+        PropertyObjectFactoryImpl factory = new PropertyObjectFactoryImpl();
+        Property<Boolean> property = new Property<Boolean>();
+        property.setJavaType(Boolean.class);
+        PropertyValue<Boolean> propertyValue = mock(Boolean.class, "true");
+        ObjectFactory<Boolean> oFactory = factory.createObjectFactory(property, propertyValue);
+        assertTrue(oFactory.getInstance());
+    }
+
+    public void testPrimitiveBoolean() throws Exception {
+        PropertyObjectFactoryImpl factory = new PropertyObjectFactoryImpl();
+        Property<Boolean> property = new Property<Boolean>();
+        property.setJavaType(Boolean.TYPE);
+        PropertyValue<Boolean> propertyValue = mock(Boolean.TYPE, "true");
+        ObjectFactory<Boolean> oFactory = factory.createObjectFactory(property, propertyValue);
+        assertTrue(oFactory.getInstance());
+    }
+
+}
