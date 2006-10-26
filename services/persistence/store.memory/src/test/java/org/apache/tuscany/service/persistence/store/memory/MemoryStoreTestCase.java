2992e084a1997e3d8d54369d125e4429307e4600
James F. Marino <jmarino@apache.org>
2006-10-26 23:58:11 +0000

    very rough cut of persistent store service
    git-svn-id: https://svn.apache.org/repos/asf/incubator/tuscany/java/sca@468198 13f79535-47bb-0310-9956-ffa450edef68

---
+++ services/persistence/store.memory/src/test/java/org/apache/tuscany/service/persistence/store/memory/MemoryStoreTestCase.java
@@ -0,0 +1,57 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.tuscany.service.persistence.store.memory;
+
+import org.apache.tuscany.service.persistence.store.StoreMonitor;
+
+import junit.framework.TestCase;
+import org.apache.tuscany.service.persistence.store.Store;
+import org.easymock.EasyMock;
+
+/**
+ * @version $Rev$ $Date$
+ */
+public class MemoryStoreTestCase extends TestCase {
+
+    public void testEviction() throws Exception {
+        MemoryStore store = new MemoryStore(EasyMock.createNiceMock(StoreMonitor.class));
+        store.setReaperInterval(10);
+        store.init();
+        Object id = new Object();
+        Object value = new Object();
+        store.writeRecord(id, value, 1);
+        Thread.sleep(100);
+        assertNull(store.readRecord(id));
+        store.destroy();
+    }
+
+    public void testNoEviction() throws Exception {
+        MemoryStore store = new MemoryStore(EasyMock.createNiceMock(StoreMonitor.class));
+        store.setReaperInterval(10);
+        store.init();
+        Object id = new Object();
+        Object value = new Object();
+        store.writeRecord(id, value, Store.NEVER);
+        Thread.sleep(100);
+        assertNotNull(store.readRecord(id));
+        store.destroy();
+    }
+
+
+}
