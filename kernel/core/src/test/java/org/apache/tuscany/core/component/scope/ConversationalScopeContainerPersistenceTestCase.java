db7701dcb74a9cc013bd2eb8246295d4dfdd4208
James F. Marino <jmarino@apache.org>
2006-11-24 06:54:19 +0000

    first cut at conversational scope container integrated with the store; more store tests
    git-svn-id: https://svn.apache.org/repos/asf/incubator/tuscany/java/sca@478785 13f79535-47bb-0310-9956-ffa450edef68

---
+++ kernel/core/src/test/java/org/apache/tuscany/core/component/scope/ConversationalScopeContainerPersistenceTestCase.java
@@ -0,0 +1,172 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.tuscany.core.component.scope;
+
+import java.util.UUID;
+
+import org.apache.tuscany.spi.component.AtomicComponent;
+import org.apache.tuscany.spi.component.PersistentScopeContainer;
+import org.apache.tuscany.spi.component.TargetNotFoundException;
+import org.apache.tuscany.spi.component.WorkContext;
+import org.apache.tuscany.spi.model.Scope;
+import org.apache.tuscany.spi.services.store.Store;
+import org.apache.tuscany.spi.services.store.StoreMonitor;
+
+import junit.framework.TestCase;
+import org.apache.tuscany.core.component.WorkContextImpl;
+import org.apache.tuscany.core.services.store.memory.MemoryStore;
+import org.easymock.EasyMock;
+
+/**
+ * @version $Rev$ $Date$
+ */
+public class ConversationalScopeContainerPersistenceTestCase extends TestCase {
+    private PersistentScopeContainer container;
+    private WorkContext context;
+
+    /**
+     * Verifies the scope container properly creates an instance
+     */
+    public void testNotYetPersistedInMemory() {
+        UUID id = UUID.randomUUID();
+        context.setIdentifier(Scope.CONVERSATIONAL, id);
+        AtomicComponent component = EasyMock.createMock(AtomicComponent.class);
+        component.addListener(EasyMock.eq(container));
+        EasyMock.expect(component.createInstance()).andReturn(new Foo());
+        EasyMock.replay(component);
+        container.register(component);
+        assertTrue(container.getInstance(component) instanceof Foo);
+        EasyMock.verify(component);
+    }
+
+    public void testPersistNewInMemory() throws Exception {
+        UUID id = UUID.randomUUID();
+        UUID id2 = UUID.randomUUID();
+        context.setIdentifier(Scope.CONVERSATIONAL, id);
+        AtomicComponent component = EasyMock.createMock(AtomicComponent.class);
+        component.addListener(EasyMock.eq(container));
+        EasyMock.replay(component);
+        container.register(component);
+        Foo foo = new Foo();
+        Foo foo2 = new Foo();
+        container.persistNew(component, id, foo, System.currentTimeMillis() + 100000);
+        assertEquals(foo, container.getInstance(component));
+        container.persistNew(component, id2, foo2, System.currentTimeMillis() + 100000);
+        context.setIdentifier(Scope.CONVERSATIONAL, id2);
+        assertEquals(foo2, container.getInstance(component));
+        EasyMock.verify(component);
+    }
+
+    public void testPersistInMemory() throws Exception {
+        UUID id = UUID.randomUUID();
+        context.setIdentifier(Scope.CONVERSATIONAL, id);
+        AtomicComponent component = EasyMock.createMock(AtomicComponent.class);
+        component.addListener(EasyMock.eq(container));
+        EasyMock.replay(component);
+        container.register(component);
+        Foo foo = new Foo();
+        container.persistNew(component, id, foo, System.currentTimeMillis() + 100000);
+        assertEquals(foo, container.getInstance(component));
+        container.persist(component, id, foo, System.currentTimeMillis() + 100000);
+        assertEquals(foo, container.getInstance(component));
+        EasyMock.verify(component);
+    }
+
+    public void testRemoveInMemory() throws Exception {
+        UUID id = UUID.randomUUID();
+        context.setIdentifier(Scope.CONVERSATIONAL, id);
+        AtomicComponent component = EasyMock.createMock(AtomicComponent.class);
+        component.addListener(EasyMock.eq(container));
+        EasyMock.expect(component.getName()).andReturn("foo").atLeastOnce();
+        EasyMock.replay(component);
+        container.register(component);
+        Foo foo = new Foo();
+        container.persistNew(component, id, foo, System.currentTimeMillis() + 100000);
+        assertEquals(foo, container.getInstance(component));
+        container.remove(component, id);
+        try {
+            container.getPersistedInstance(component);
+            fail();
+        } catch (TargetNotFoundException e) {
+            //expected
+        }
+        EasyMock.verify(component);
+    }
+
+    public void testRecreateAfterRemoveInMemory() throws Exception {
+        UUID id = UUID.randomUUID();
+        context.setIdentifier(Scope.CONVERSATIONAL, id);
+        AtomicComponent component = EasyMock.createMock(AtomicComponent.class);
+        component.addListener(EasyMock.eq(container));
+        EasyMock.expect(component.createInstance()).andReturn(new Foo());
+        EasyMock.replay(component);
+        container.register(component);
+        Foo foo = new Foo();
+        container.persistNew(component, id, foo, System.currentTimeMillis() + 100000);
+        assertEquals(foo, container.getInstance(component));
+        container.remove(component, id);
+        Foo foo2 = (Foo) container.getInstance(component);
+        assertNotNull(foo2);
+        assertNotSame(foo, foo2);
+        EasyMock.verify(component);
+    }
+
+    public void testGetPersistedInstance() throws Exception {
+        UUID id = UUID.randomUUID();
+        UUID id2 = UUID.randomUUID();
+        context.setIdentifier(Scope.CONVERSATIONAL, id);
+        AtomicComponent component = EasyMock.createMock(AtomicComponent.class);
+        EasyMock.expect(component.getName()).andReturn("foo").atLeastOnce();
+        component.addListener(EasyMock.eq(container));
+        EasyMock.replay(component);
+        container.register(component);
+        Foo foo = new Foo();
+        container.persistNew(component, id, foo, System.currentTimeMillis() + 100000);
+        assertEquals(foo, container.getPersistedInstance(component));
+        assertEquals(foo, container.getPersistedInstance(component));
+        context.setIdentifier(Scope.CONVERSATIONAL, id2);
+        try {
+            container.getPersistedInstance(component);
+            fail();
+        } catch (TargetNotFoundException e) {
+            //expected
+        }
+        EasyMock.verify(component);
+    }
+
+    protected void setUp() throws Exception {
+        super.setUp();
+        context = new WorkContextImpl();
+        StoreMonitor mock = EasyMock.createNiceMock(StoreMonitor.class);
+        EasyMock.replay(mock);
+        Store store = new MemoryStore(mock);
+        container = new ConversationalScopeContainerImpl(store, context);
+        container.start();
+    }
+
+    protected void tearDown() throws Exception {
+        super.tearDown();
+        context.clearIdentifier(Scope.CONVERSATIONAL);
+        container.stop();
+    }
+
+    private class Foo {
+
+    }
+}
