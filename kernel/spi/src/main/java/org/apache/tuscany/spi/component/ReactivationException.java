1417764f08490720c2b9facfad4ec66ee2a3960c
James F. Marino <jmarino@apache.org>
2006-11-27 07:43:04 +0000

    move ReactivationException to spi/component
    git-svn-id: https://svn.apache.org/repos/asf/incubator/tuscany/java/sca@479534 13f79535-47bb-0310-9956-ffa450edef68

--- kernel/spi/src/main/java/org/apache/tuscany/spi/component/ReactivationException.java
+++ kernel/spi/src/main/java/org/apache/tuscany/spi/component/ReactivationException.java
@@ -16,7 +16,7 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-package org.apache.tuscany.spi;
+package org.apache.tuscany.spi.component;
 
 import org.apache.tuscany.api.TuscanyRuntimeException;
 
