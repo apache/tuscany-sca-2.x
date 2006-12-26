8fe5bc026164fd4684683638c4e0f5c2ada64e67
Meeraj Kunnumpurath <meerajk@apache.org>
2006-12-26 16:44:40 +0000

    First working version.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/tuscany/java/sca@490325 13f79535-47bb-0310-9956-ffa450edef68

--- runtime/standalone/server.start/src/main/java/org/apache/tuscany/standalone/server/management/jmx/Agent.java
+++ runtime/standalone/server.start/src/main/java/org/apache/tuscany/standalone/server/management/jmx/Agent.java
@@ -18,9 +18,18 @@
  */
 package org.apache.tuscany.standalone.server.management.jmx;
 
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.rmi.server.UnicastRemoteObject;
+
 import javax.management.MBeanServer;
 import javax.management.MBeanServerFactory;
 import javax.management.ObjectName;
+import javax.management.remote.JMXConnectorServer;
+import javax.management.remote.JMXConnectorServerFactory;
+import javax.management.remote.JMXServiceURL;
 
 /**
  * Utility for starting the JMX server.
@@ -29,30 +38,57 @@ import javax.management.ObjectName;
  *
  */
 public class Agent {
-    
+
     /** Administration port system property. */
     private static final String ADMIN_PORT_PROPERTY = "tuscany.adminPort";
-    
+
     /** Default admin port. */
-    private static final int DEFAULT_ADMIN_PORT = 1066;
-    
+    private static final int DEFAULT_ADMIN_PORT = 1099;
+
     /** Instance */
     private static final Agent INSTANCE = new Agent();
-    
+
     /** Root domain */
     private static final String DOMAIN = "tuscany";
-    
+
     /** MBean server to use. */
     private MBeanServer mBeanServer;
     
+    /** RMI connector adaptor. */
+    private JMXConnectorServer connectorServer;
+    
+    /** RMI registry. */
+    private Registry registry;
+    
+    /** Listen port */
+    private int port = DEFAULT_ADMIN_PORT;
+
     /**
      * Initialies the server.
      * @throws ManagementException If unable to start the agent.
      */
     private Agent() throws ManagementException {
-        mBeanServer = MBeanServerFactory.createMBeanServer(DOMAIN);
+        
+        try {
+            
+            String portValue = System.getProperty(ADMIN_PORT_PROPERTY);
+            if(portValue != null) {
+                port = Integer.parseInt(portValue);
+            }
+            
+            mBeanServer = MBeanServerFactory.createMBeanServer(DOMAIN);
+            JMXServiceURL url = new JMXServiceURL("service:jmx:rmi:///jndi/rmi://localhost:" + port + "/server");
+            
+            connectorServer = JMXConnectorServerFactory.newJMXConnectorServer(url, null, mBeanServer);
+            
+        } catch (MalformedURLException ex) {
+            throw new ManagementException(ex);
+        } catch (IOException ex) {
+            throw new ManagementException(ex);
+        }
+        
     }
-    
+
     /**
      * Returns the singleton agent instance.
      * @return Agent instance.
@@ -61,7 +97,7 @@ public class Agent {
     public static Agent getInstance() throws ManagementException {
         return INSTANCE;
     }
-    
+
     /**
      * Registers a managed bean.
      * @param instance Instance to be registered.
@@ -75,21 +111,37 @@ public class Agent {
             throw new ManagementException(ex);
         }
     }
-    
+
     /**
      * Starts the JMX server.
      * @throws ManagementException If unable to start the server.
      *
      */
     public void start() throws ManagementException {
+
+        try {
+            registry = LocateRegistry.createRegistry(port);
+            connectorServer.start();
+        } catch (MalformedURLException ex) {
+            throw new ManagementException(ex);
+        } catch (IOException ex) {
+            throw new ManagementException(ex);
+        }
+
     }
-    
+
     /**
      * Shuts down the JMX server.
      * @throws ManagementException If unable to shutdown the server.
      *
      */
     public void shutdown() throws ManagementException {
+        try {
+            connectorServer.stop();
+            UnicastRemoteObject.unexportObject(registry, true);
+        } catch (IOException ex) {
+            throw new ManagementException(ex);
+        }
     }
 
 }
