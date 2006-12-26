ca9d4d3e183d996c13f8412c8eab3047fc27ce2c
Jeremy Boynes <jboynes@apache.org>
2006-12-26 19:06:27 +0000

    basic impl and testcase for AssemblyService
    git-svn-id: https://svn.apache.org/repos/asf/incubator/tuscany/java/sca@490357 13f79535-47bb-0310-9956-ffa450edef68

---
+++ kernel/core/src/main/java/org/apache/tuscany/core/services/deployment/AssemblyServiceImpl.java
@@ -0,0 +1,67 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.    
+ */
+package org.apache.tuscany.core.services.deployment;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.net.URLConnection;
+
+import org.apache.tuscany.host.deployment.AssemblyService;
+import org.apache.tuscany.host.deployment.DeploymentException;
+import org.apache.tuscany.host.deployment.UnsupportedContentTypeException;
+
+/**
+ * @version $Rev$ $Date$
+ */
+public class AssemblyServiceImpl implements AssemblyService {
+
+    public void applyChanges(URL changeSet) throws DeploymentException, IOException {
+        if (changeSet == null) {
+            throw new IllegalArgumentException("changeSet is null");
+        }
+
+        URLConnection connection = changeSet.openConnection();
+        String contentType = connection.getContentType();
+        //todo try and figure out content type from the URL
+        if (contentType == null) {
+            throw new UnsupportedContentTypeException(null, changeSet.toString());
+        }
+
+        InputStream is = connection.getInputStream();
+        try {
+            applyChanges(is, contentType);
+        } finally {
+            try {
+                is.close();
+            } catch (IOException e) {
+                // ignore
+            }
+        }
+    }
+
+    public void applyChanges(InputStream changeSet, String contentType) throws DeploymentException, IOException {
+        if (changeSet == null) {
+            throw new IllegalArgumentException("changeSet is null");
+        }
+        if (contentType == null) {
+            throw new IllegalArgumentException("contentType is null");
+        }
+    }
+}
