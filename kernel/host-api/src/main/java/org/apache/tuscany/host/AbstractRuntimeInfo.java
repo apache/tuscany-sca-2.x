8bd2595c00aa89f92875196f214480f767011d2d
Meeraj Kunnumpurath <meerajk@apache.org>
2006-12-31 17:16:59 +0000

    Abstract super class for all runtime info.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/tuscany/java/sca@491448 13f79535-47bb-0310-9956-ffa450edef68

---
+++ kernel/host-api/src/main/java/org/apache/tuscany/host/AbstractRuntimeInfo.java
@@ -0,0 +1,94 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.    
+ */
+package org.apache.tuscany.host;
+
+import java.io.File;
+import java.net.URL;
+
+/**
+ * Abstract runtime info implementation.
+ * 
+ * @version $Revision$ $Date$
+ */
+public abstract class AbstractRuntimeInfo implements RuntimeInfo {
+    
+    /**
+     * Application root directory.
+     */
+    private File applicationRootDirectory;
+    
+    /**
+     * Base URL.
+     */
+    private URL baseUrl;
+    
+    /**
+     * Install directory.
+     */
+    private File installDirectory;
+    
+    /**
+     * Online indicator.
+     */
+    private boolean online;
+
+    /**
+     * Initializes the runtime info instance.
+     * 
+     * @param applicationRootDirectory Application root directory.
+     * @param baseUrl Base Url.
+     * @param installDirectory Install directory.
+     * @param online Onlne indicator.
+     */
+    public AbstractRuntimeInfo(File applicationRootDirectory, URL baseUrl, File installDirectory, boolean online) {
+        this.applicationRootDirectory = applicationRootDirectory;
+        this.baseUrl = baseUrl;
+        this.installDirectory = installDirectory;
+        this.online = online;
+    }
+
+    /**
+     * @see org.apache.tuscany.host.RuntimeInfo#getApplicationRootDirectory()
+     */
+    public File getApplicationRootDirectory() {
+        return applicationRootDirectory;
+    }
+
+    /**
+     * @see org.apache.tuscany.host.RuntimeInfo#getBaseURL()
+     */
+    public URL getBaseURL() {
+        return baseUrl;
+    }
+
+    /**
+     * @see org.apache.tuscany.host.RuntimeInfo#getInstallDirectory()
+     */
+    public File getInstallDirectory() {
+        return installDirectory;
+    }
+
+    /**
+     * @see org.apache.tuscany.host.RuntimeInfo#isOnline()
+     */
+    public boolean isOnline() {
+        return online;
+    }
+
+}
