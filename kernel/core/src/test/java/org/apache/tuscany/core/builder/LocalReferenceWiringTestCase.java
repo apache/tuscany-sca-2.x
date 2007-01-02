9e5841c36f62095b98313742adf61961743f7461
James F. Marino <jmarino@apache.org>
2007-01-02 14:02:48 +0000

    remove dead code; consolidate test case
    git-svn-id: https://svn.apache.org/repos/asf/incubator/tuscany/java/sca@491796 13f79535-47bb-0310-9956-ffa450edef68

--- kernel/core/src/test/java/org/apache/tuscany/core/builder/LocalReferenceWiringTestCase.java
+++ kernel/core/src/test/java/org/apache/tuscany/core/builder/LocalReferenceWiringTestCase.java
@@ -2,17 +2,25 @@ package org.apache.tuscany.core.builder;
 
 import javax.xml.namespace.QName;
 
+import org.apache.tuscany.spi.QualifiedName;
+import org.apache.tuscany.spi.component.AtomicComponent;
 import org.apache.tuscany.spi.component.CompositeComponent;
 import org.apache.tuscany.spi.component.Reference;
+import org.apache.tuscany.spi.component.ReferenceBinding;
 import org.apache.tuscany.spi.component.Service;
 import org.apache.tuscany.spi.component.ServiceBinding;
-import org.apache.tuscany.spi.component.AtomicComponent;
+import org.apache.tuscany.spi.component.TargetInvokerCreationException;
 import org.apache.tuscany.spi.wire.InboundInvocationChain;
+import org.apache.tuscany.spi.wire.InboundWire;
 import org.apache.tuscany.spi.wire.Interceptor;
 import org.apache.tuscany.spi.wire.Message;
 import org.apache.tuscany.spi.wire.MessageImpl;
 import org.apache.tuscany.spi.wire.OutboundInvocationChain;
+import org.apache.tuscany.spi.wire.OutboundWire;
 
+import org.apache.tuscany.core.binding.local.LocalReferenceBinding;
+import org.apache.tuscany.core.binding.local.LocalServiceBinding;
+import org.apache.tuscany.core.implementation.composite.ReferenceImpl;
 import org.apache.tuscany.core.implementation.composite.ServiceImpl;
 import org.apache.tuscany.core.mock.binding.MockServiceBinding;
 import org.apache.tuscany.core.wire.InboundInvocationChainImpl;
@@ -29,9 +37,10 @@ import org.easymock.IAnswer;
  *
  * @version $Rev$ $Date$
  */
-public class LocalReferenceWiringTestCase extends AbstractLocalWiringTestCase {
+public class LocalReferenceWiringTestCase extends AbstractConnectorImplTestCase {
     private Service service;
     private Reference reference;
+    protected ReferenceBinding referenceBinding;
     private AtomicComponent atomicComponent;
 
     /**
@@ -121,7 +130,7 @@ public class LocalReferenceWiringTestCase extends AbstractLocalWiringTestCase {
         EasyMock.replay(parent);
 
         service = createLocalService(topComposite);
-        reference = createLocalReference(parent, AbstractLocalWiringTestCase.TARGET_NAME);
+        reference = createLocalReference(parent, TARGET_NAME);
     }
 
     private void createLocalReferenceToInvalidServiceConfiguration() throws Exception {
@@ -139,7 +148,7 @@ public class LocalReferenceWiringTestCase extends AbstractLocalWiringTestCase {
         EasyMock.replay(parent);
 
         service = createService();
-        reference = createLocalReference(parent, AbstractLocalWiringTestCase.TARGET_NAME);
+        reference = createLocalReference(parent, TARGET_NAME);
     }
 
     private void createLocalReferenceToSiblingCompositeServiceConfiguration() throws Exception {
@@ -240,6 +249,70 @@ public class LocalReferenceWiringTestCase extends AbstractLocalWiringTestCase {
         Service service = new ServiceImpl(TARGET, null, contract);
         service.addServiceBinding(serviceBinding);
         return service;
+    }
+
+    /**
+     * Creates a service configured with the local binding and places an invoker interceptor on the end of each outbound
+     * chain for invocation testing without needing to wire the service to a target
+     *
+     * @throws org.apache.tuscany.core.builder.WireConnectException
+     *
+     */
+    private Service createLocalService(CompositeComponent parent) throws WireConnectException {
+        LocalServiceBinding serviceBinding = new LocalServiceBinding(TARGET, parent);
+        InboundInvocationChain targetInboundChain = new InboundInvocationChainImpl(operation);
+        targetInboundChain.addInterceptor(new SynchronousBridgingInterceptor());
+        InboundWire targetInboundWire = new InboundWireImpl();
+        targetInboundWire.setServiceContract(contract);
+        targetInboundWire.addInvocationChain(operation, targetInboundChain);
+        targetInboundWire.setContainer(serviceBinding);
+
+        OutboundInvocationChain targetOutboundChain = new OutboundInvocationChainImpl(operation);
+        // place an invoker interceptor on the end
+        targetOutboundChain.addInterceptor(new InvokerInterceptor());
+        OutboundWire targetOutboundWire = new OutboundWireImpl();
+        targetOutboundWire.setServiceContract(contract);
+        targetOutboundWire.addInvocationChain(operation, targetOutboundChain);
+        targetOutboundWire.setContainer(serviceBinding);
+
+        serviceBinding.setInboundWire(targetInboundWire);
+        serviceBinding.setOutboundWire(targetOutboundWire);
+        // manually connect the service chains
+        connector.connect(targetInboundChain, targetOutboundChain);
+        Service service = new ServiceImpl(TARGET, null, contract);
+        service.addServiceBinding(serviceBinding);
+        return service;
+    }
+
+    private Reference createLocalReference(CompositeComponent parent, QualifiedName target) throws Exception {
+        ReferenceBinding referenceBinding = createLocalReferenceBinding(target);
+        Reference reference = new ReferenceImpl("foo", parent, contract);
+        reference.addReferenceBinding(referenceBinding);
+        return reference;
+    }
+
+    private ReferenceBinding createLocalReferenceBinding(QualifiedName target)
+        throws TargetInvokerCreationException {
+        referenceBinding = new LocalReferenceBinding("local", null);
+        InboundInvocationChain inboundChain = new InboundInvocationChainImpl(operation);
+        InboundWire inboundWire = new InboundWireImpl();
+        inboundWire.setServiceContract(contract);
+        inboundWire.setContainer(referenceBinding);
+        inboundWire.addInvocationChain(operation, inboundChain);
+
+        OutboundInvocationChain outboundChain = new OutboundInvocationChainImpl(operation);
+        // Outbound chains always contains at least one interceptor
+        outboundChain.addInterceptor(new SynchronousBridgingInterceptor());
+        OutboundWire outboundWire = new OutboundWireImpl();
+        outboundWire.setServiceContract(contract);
+        outboundWire.setTargetName(target);
+        outboundWire.addInvocationChain(operation, outboundChain);
+        outboundWire.setContainer(referenceBinding);
+
+        referenceBinding.setInboundWire(inboundWire);
+        referenceBinding.setOutboundWire(outboundWire);
+
+        return referenceBinding;
     }
 
 
