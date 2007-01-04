c251822c5aec39793ad097ae8c6f9814587200b2
James F. Marino <jmarino@apache.org>
2007-01-04 14:23:03 +0000

    add builder registry testcase
    git-svn-id: https://svn.apache.org/repos/asf/incubator/tuscany/java/sca@492578 13f79535-47bb-0310-9956-ffa450edef68

--- kernel/core/src/test/java/org/apache/tuscany/core/builder/BuilderRegistryTestCase.java
+++ kernel/core/src/test/java/org/apache/tuscany/core/builder/BuilderRegistryTestCase.java
@@ -22,22 +22,27 @@ import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.tuscany.spi.QualifiedName;
 import org.apache.tuscany.spi.builder.BindingBuilder;
 import org.apache.tuscany.spi.builder.BuilderConfigException;
 import org.apache.tuscany.spi.builder.ComponentBuilder;
+import org.apache.tuscany.spi.component.AtomicComponent;
 import org.apache.tuscany.spi.component.Component;
 import org.apache.tuscany.spi.component.CompositeComponent;
+import org.apache.tuscany.spi.component.Reference;
+import org.apache.tuscany.spi.component.ReferenceBinding;
 import org.apache.tuscany.spi.component.Service;
 import org.apache.tuscany.spi.component.ServiceBinding;
-import org.apache.tuscany.spi.component.AtomicComponent;
 import org.apache.tuscany.spi.deployer.DeploymentContext;
 import org.apache.tuscany.spi.model.BindingDefinition;
+import org.apache.tuscany.spi.model.BoundReferenceDefinition;
 import org.apache.tuscany.spi.model.BoundServiceDefinition;
 import org.apache.tuscany.spi.model.ComponentDefinition;
 import org.apache.tuscany.spi.model.ComponentType;
 import org.apache.tuscany.spi.model.CompositeComponentType;
 import org.apache.tuscany.spi.model.CompositeImplementation;
 import org.apache.tuscany.spi.model.Implementation;
+import static org.apache.tuscany.spi.model.Multiplicity.ONE_ONE;
 import org.apache.tuscany.spi.model.ServiceContract;
 import org.apache.tuscany.spi.wire.WireService;
 
@@ -64,7 +69,7 @@ public class BuilderRegistryTestCase extends TestCase {
     }
 
     @SuppressWarnings({"unchecked"})
-    public void testBindingBuilderDispatch() throws Exception {
+    public void testServiceBindingBuilderDispatch() throws Exception {
         WireService wireService = EasyMock.createMock(WireService.class);
         wireService.createWires(EasyMock.isA(ServiceBinding.class),
             (ServiceContract) EasyMock.isNull(), EasyMock.isA(String.class)
@@ -87,6 +92,33 @@ public class BuilderRegistryTestCase extends TestCase {
         BoundServiceDefinition definition = new BoundServiceDefinition("foo", null, bindingDefs, false, new URI("foo"));
         Service service = registry.build(parent, definition, deploymentContext);
         assertEquals(2, service.getServiceBindings().size());
+        EasyMock.verify(wireService);
+    }
+
+    @SuppressWarnings({"unchecked"})
+    public void testReferenceBindingBuilderDispatch() throws Exception {
+        WireService wireService = EasyMock.createMock(WireService.class);
+        wireService.createWires(EasyMock.isA(ReferenceBinding.class),
+            (ServiceContract) EasyMock.isNull(), (QualifiedName) EasyMock.isNull()
+        );
+        EasyMock.expectLastCall().times(2);
+        EasyMock.replay(wireService);
+        registry.setWireService(wireService);
+        ReferenceBinding binding = EasyMock.createNiceMock(ReferenceBinding.class);
+        EasyMock.replay(binding);
+        BindingBuilder<MockBindingDefinition> builder = EasyMock.createMock(BindingBuilder.class);
+        EasyMock.expect(builder.build(EasyMock.isA(CompositeComponent.class),
+            EasyMock.isA(BoundReferenceDefinition.class),
+            EasyMock.isA(MockBindingDefinition.class),
+            EasyMock.isA(DeploymentContext.class))).andReturn(binding).times(2);
+        EasyMock.replay(builder);
+        registry.register(MockBindingDefinition.class, builder);
+        List<BindingDefinition> bindingDefs = new ArrayList<BindingDefinition>();
+        bindingDefs.add(new MockBindingDefinition());
+        bindingDefs.add(new MockBindingDefinition());
+        BoundReferenceDefinition definition = new BoundReferenceDefinition("foo", null, bindingDefs, ONE_ONE);
+        Reference reference = registry.build(parent, definition, deploymentContext);
+        assertEquals(2, reference.getReferenceBindings().size());
         EasyMock.verify(wireService);
     }
 
