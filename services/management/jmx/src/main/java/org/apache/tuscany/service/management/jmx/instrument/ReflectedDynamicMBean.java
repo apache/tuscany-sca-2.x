d595076189d4f954e2871d5251bc117e38a5a748
Meeraj Kunnumpurath <meerajk@apache.org>
2007-01-09 00:04:01 +0000

    removed jmx host
    git-svn-id: https://svn.apache.org/repos/asf/incubator/tuscany/java/sca@494268 13f79535-47bb-0310-9956-ffa450edef68

--- services/management/jmx/src/main/java/org/apache/tuscany/service/management/jmx/instrument/ReflectedDynamicMBean.java
+++ services/management/jmx/src/main/java/org/apache/tuscany/service/management/jmx/instrument/ReflectedDynamicMBean.java
@@ -16,7 +16,7 @@
  * specific language governing permissions and limitations
  * under the License.    
  */
-package org.apache.tuscany.runtime.standalone.jmx.management.instrument;
+package org.apache.tuscany.service.management.jmx.instrument;
 
 import java.beans.BeanInfo;
 import java.beans.IntrospectionException;
