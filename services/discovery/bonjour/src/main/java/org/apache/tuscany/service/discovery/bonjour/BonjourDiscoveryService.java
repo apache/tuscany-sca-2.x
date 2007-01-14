f27220ac31e43655d026cdd0c10e7997ba2f5781
Meeraj Kunnumpurath <meerajk@apache.org>
2007-01-14 00:43:28 +0000

    Added stub implementations for the jxta and bonjour discovery services
    git-svn-id: https://svn.apache.org/repos/asf/incubator/tuscany/java/sca@496011 13f79535-47bb-0310-9956-ffa450edef68

---
+++ services/discovery/bonjour/src/main/java/org/apache/tuscany/service/discovery/bonjour/BonjourDiscoveryService.java
@@ -0,0 +1,43 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.    
+ */
+package org.apache.tuscany.service.discovery.bonjour;
+
+import java.net.URI;
+
+import org.apache.tuscany.spi.services.discovery.AbstractDiscoveryService;
+
+/**
+ * Discovery service implemented using Apple bonjour.
+ * 
+ * @version $Revision$ $Date$
+ *
+ */
+public class BonjourDiscoveryService extends AbstractDiscoveryService {
+    
+    /**
+     * Publish the event to indicate that the specified runtime is started.
+     * 
+     * @param domain Domain in which the runtime is participating.
+     * @param profile Name of the runtime profile.
+     */
+    public void runtimeStarted(URI domain, String profile) {
+        throw new UnsupportedOperationException();
+    }
+
+}
