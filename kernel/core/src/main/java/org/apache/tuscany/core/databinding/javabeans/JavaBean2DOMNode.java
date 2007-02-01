45fad683b350e6cdcfef557f449b72939ed6b210
S Venkatakrishnan <svkrish@apache.org>
2007-02-01 19:48:34 +0000

    changes to fix broken build, added javabeans databinding and transformers to core, added them to databinding.scdl
    git-svn-id: https://svn.apache.org/repos/asf/incubator/tuscany/java/sca@502333 13f79535-47bb-0310-9956-ffa450edef68

---
+++ kernel/core/src/main/java/org/apache/tuscany/core/databinding/javabeans/JavaBean2DOMNode.java
@@ -0,0 +1,52 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.    
+ */
+package org.apache.tuscany.core.databinding.javabeans;
+
+import org.apache.tuscany.spi.databinding.PullTransformer;
+import org.apache.tuscany.spi.databinding.TransformationContext;
+import org.apache.tuscany.spi.databinding.Transformer;
+import org.apache.tuscany.spi.databinding.extension.TransformerExtension;
+import org.osoa.sca.annotations.Service;
+import org.w3c.dom.Node;
+
+/**
+ * Transformer to convert data from a JavaBean object to DOM Node
+ */
+@Service(Transformer.class)
+public class JavaBean2DOMNode extends TransformerExtension<Object, Node> implements
+        PullTransformer<Object, Node> {
+
+    protected XMLTypeMapperExtension<Node> mapper;
+
+    public JavaBean2DOMNode() {
+        this.mapper = new XMLTypeMapperExtension<Node>();
+    }
+
+    public Node transform(Object source, TransformationContext context) {
+        return mapper.toDOMNode(source, context);
+    }
+
+    public Class getTargetType() {
+        return Node.class;
+    }
+
+    public Class getSourceType() {
+        return Object.class;
+    }
+}
