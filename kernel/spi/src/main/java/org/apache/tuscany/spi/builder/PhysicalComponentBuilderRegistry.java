3cf465dc210effa5dbeffd4728da751ecfcfe8e5
Meeraj Kunnumpurath <meerajk@apache.org>
2007-02-04 14:57:56 +0000

    First cut of the new builder framework
    git-svn-id: https://svn.apache.org/repos/asf/incubator/tuscany/java/sca@503420 13f79535-47bb-0310-9956-ffa450edef68

---
+++ kernel/spi/src/main/java/org/apache/tuscany/spi/builder/PhysicalComponentBuilderRegistry.java
@@ -0,0 +1,49 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.    
+ */
+package org.apache.tuscany.spi.builder;
+
+import org.apache.tuscany.spi.model.physical.PhysicalComponentDefinition;
+
+/**
+ * A registry of physical component builders.
+ * 
+ * @version $Rev$ $Date$
+ */
+public interface PhysicalComponentBuilderRegistry {
+
+    /**
+     * Registers a physical component builder.
+     * 
+     * @param <PCD> Type of the physical component definition.
+     * @param definitionClass Class of the physical component definition.
+     * @param builder Builder for the physical component definition.
+     */
+    <PCD extends PhysicalComponentDefinition> void register(Class<PCD> definitionClass,
+                                                            PhysicalComponentBuilder<PCD> builder);
+
+    /**
+     * Gets a builder for the specified physical component builder.
+     * 
+     * @param <PCD> Type of the physical component definition.
+     * @param definitionClass Class of the physical component definition.
+     * @return Builder for the physical component definition.
+     */
+    PhysicalComponentBuilder getBuilder(Class definitionClass);
+
+}
---
+++ kernel/spi/src/main/java/org/apache/tuscany/spi/marshaller/ModelMarshallerRegistry.java
@@ -0,0 +1,63 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.    
+ */
+package org.apache.tuscany.spi.marshaller;
+
+import javax.xml.namespace.QName;
+
+import org.apache.tuscany.spi.model.physical.PhysicalComponentDefinition;
+
+/**
+ * A registry for physical component definition marshallers.
+ * 
+ * @version $Rev$ $Date$
+ */
+public interface ModelMarshallerRegistry {
+
+    /**
+     * Registers a physical component definition marshaller.
+     * 
+     * @param <PCD> Physical component definition.
+     * @param modelClass Physical component definition class.
+     * @param qualifiedName Qualified name of the marshalled XML stream.
+     * @param marshaller Marshaller responsible for marshalling.
+     */
+    <PCD extends PhysicalComponentDefinition> void registerMarshaller(Class<PCD> modelClass,
+                                                            QName qualifiedName,
+                                                            ModelMarshaller<PCD> marshaller);
+    
+    /**
+     * Gets a marshaller for marshalling the registered type.
+     * 
+     * @param <PCD> Physical component definition.
+     * @param modelClass Type of the physical model definition that needs to be marshalled.
+     * @return Marshaller capable for marshalling the specified type.
+     */
+    <PCD extends PhysicalComponentDefinition> ModelMarshaller<PCD> getMarshaller(Class<PCD> modelClass);
+
+
+    /**
+     * Gets a marshaller for unmarshalling XML with the specified qualified name.
+     * 
+     * @param <PCD> Physical component definition.
+     * @param qualifiedName Qualified name of the document element of the marshaleld data.
+     * @return Marshaller capable for unmarshalling the document type.
+     */
+    <PCD extends PhysicalComponentDefinition> ModelMarshaller<PCD> getMarshaller(QName qualifiedName);
+    
+}
