617cc99b359bd780686e368680205472a211b19d
Jeremy Boynes <jboynes@apache.org>
2007-02-11 19:11:08 +0000

    attempt to deploy test harness scdl currently fails because the AutowireResolver is not being injected into the system Deployer
    git-svn-id: https://svn.apache.org/repos/asf/incubator/tuscany/java/sca@506098 13f79535-47bb-0310-9956-ffa450edef68

--- runtime/itest/plugin/src/main/java/org/apache/tuscany/sca/plugin/itest/TuscanyITestMojo.java
+++ runtime/itest/plugin/src/main/java/org/apache/tuscany/sca/plugin/itest/TuscanyITestMojo.java
@@ -22,16 +22,28 @@ import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
+import java.util.Iterator;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.net.MalformedURLException;
 
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugin.logging.Log;
 import org.apache.maven.surefire.junit.JUnitDirectoryTestSuite;
 import org.apache.maven.surefire.report.BriefFileReporter;
 import org.apache.maven.surefire.report.ReporterException;
 import org.apache.maven.surefire.report.ReporterManager;
 import org.apache.maven.surefire.suite.SurefireTestSuite;
 import org.apache.maven.surefire.testset.TestSetFailedException;
+import org.apache.maven.artifact.resolver.ArtifactResolver;
+import org.apache.maven.artifact.metadata.ArtifactMetadataSource;
+import org.apache.maven.artifact.repository.ArtifactRepository;
+import org.apache.maven.artifact.factory.ArtifactFactory;
+
+import org.apache.tuscany.host.runtime.InitializationException;
+import org.apache.tuscany.api.TuscanyRuntimeException;
 
 /**
  * @version $Rev$ $Date$
@@ -42,7 +54,7 @@ public class TuscanyITestMojo extends AbstractMojo {
     /**
      * @parameter expression="${project.build.directory}/surefire-reports"
      */
-    private File reportsDirectory;
+    public File reportsDirectory;
 
     /**
      * Whether to trim the stack trace in the reports to just the lines within
@@ -50,7 +62,7 @@ public class TuscanyITestMojo extends AbstractMojo {
      * 
      * @parameter expression="${trimStackTrace}" default-value="true"
      */
-    private boolean trimStackTrace;
+    public boolean trimStackTrace;
 
     /**
      * The directory containing generated test classes of the project being
@@ -59,25 +71,114 @@ public class TuscanyITestMojo extends AbstractMojo {
      * @parameter expression="${project.build.testOutputDirectory}"
      * @required
      */
-    private File testClassesDirectory;
+    public File testClassesDirectory;
+
+    /**
+     * @parameter expression="${project.testClasspathElements}"
+     * @required
+     * @readonly
+     */
+    public List testClassPath;
 
     /**
      * @parameter
      */
-    private List includes = new ArrayList();
+    public List includes = new ArrayList();
 
     /**
      * @parameter
      */
-    private List excludes = new ArrayList();
+    public List excludes = new ArrayList();
+
+    /**
+     * @parameter expression="${project.build.testOutputDirectory}/itest.scdl"
+     */
+    public File testScdl;
+
+    /**
+     * @parameter
+     */
+    public URL systemScdl;
+
+    /**
+     * Used to look up Artifacts in the remote repository.
+     *
+     * @parameter expression="${component.org.apache.maven.artifact.resolver.ArtifactResolver}"
+     * @required
+     * @readonly
+     */
+    public ArtifactResolver resolver;
+
+    /**
+     * Used to look up Artifacts in the remote repository.
+     *
+     * @parameter expression="${component.org.apache.maven.artifact.metadata.ArtifactMetadataSource}"
+     * @required
+     * @readonly
+     */
+    public ArtifactMetadataSource metadataSource;
+
+    /**
+     * Location of the local repository.
+     *
+     * @parameter expression="${localRepository}"
+     * @readonly
+     * @required
+     */
+    public ArtifactRepository localRepository;
+
+    /**
+     * List of Remote Repositories used by the resolver
+     *
+     * @parameter expression="${project.remoteArtifactRepositories}"
+     * @readonly
+     * @required
+     */
+    public List remoteRepositories;
+
+    /**
+     * Used to look up Artifacts in the remote repository.
+     *
+     * @parameter expression="${component.org.apache.maven.artifact.factory.ArtifactFactory}"
+     * @required
+     * @readonly
+     */
+    public ArtifactFactory artifactFactory;
 
     public void execute() throws MojoExecutionException, MojoFailureException {
-        getLog().info("Executing tests...");
+        Log log = getLog();
+        log.info("Starting Tuscany...");
+        MavenEmbeddedRuntime runtime = createRuntime();
+        try {
+            runtime.initialize();
+        } catch (InitializationException e) {
+            throw new MojoExecutionException("Error initializing Tuscany runtime", e);
+        }
+        try {
+            log.debug("Deploying test SCDL from " + testScdl);
+            try {
+                // fixme this should probably be an isolated classloader
+                ClassLoader testClassLoader = createTestClassLoader(getClass().getClassLoader());
+                runtime.deployTestScdl(testScdl, testClassLoader);
+            } catch (Exception e) {
+                throw new MojoExecutionException("Error deploying test component " + testScdl, e);
+            }
+/*
+            log.info("Executing tests...");
 
-        boolean success = runSurefire();
-        if (!success) {
-            String msg = "There were test failures";
-            throw new MojoFailureException(msg);
+            boolean success = runSurefire();
+            if (!success) {
+                String msg = "There were test failures";
+                throw new MojoFailureException(msg);
+            }
+*/
+        } finally {
+            log.info("Stopping Tuscany...");
+            try {
+                runtime.destroy();
+            } catch (TuscanyRuntimeException e) {
+                log.error("Error stopping Tuscany runtime", e);
+            }
         }
     }
 
@@ -128,4 +229,41 @@ public class TuscanyITestMojo extends AbstractMojo {
         return reporterManager.getNumErrors() == 0 && reporterManager.getNumFailures() == 0;
     }
 
+    protected MavenEmbeddedRuntime createRuntime() throws MojoExecutionException {
+        ClassLoader hostClassLoader = getClass().getClassLoader();
+        if (systemScdl == null) {
+            systemScdl = hostClassLoader.getResource("META-INF/tuscany/embeddedMaven.scdl");
+        }
+
+        MavenRuntimeInfo runtimeInfo = new MavenRuntimeInfo();
+        MavenEmbeddedArtifactRepository artifactRepository = new MavenEmbeddedArtifactRepository(artifactFactory,
+                                                                                                 resolver,
+                                                                                                 metadataSource,
+                                                                                                 localRepository,
+                                                                                                 remoteRepositories);
+        MavenEmbeddedRuntime runtime = new MavenEmbeddedRuntime();
+        runtime.setRuntimeInfo(runtimeInfo);
+        runtime.setSystemScdl(systemScdl);
+        runtime.setHostClassLoader(hostClassLoader);
+        runtime.setArtifactRepository(artifactRepository);
+        return runtime;
+    }
+
+    public ClassLoader createTestClassLoader(ClassLoader parent) {
+        URL[] urls = new URL[testClassPath.size()];
+        int idx = 0;
+        for (Iterator i = testClassPath.iterator(); i.hasNext();) {
+            File pathElement = new File((String) i.next());
+            try {
+                URL url = pathElement.toURI().toURL();
+                getLog().debug("Adding application URL: " + url);
+                urls[idx++] = url;
+            } catch (MalformedURLException e) {
+                // toURI should have encoded the URL
+                throw new AssertionError();
+            }
+
+        }
+        return new URLClassLoader(urls, parent);
+    }
 }
