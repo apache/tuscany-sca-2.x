28d4334658cd931b2a92e3d0b05408f71393a115
Jeremy Boynes <jboynes@apache.org>
2007-02-12 02:56:59 +0000

    add related Java class to PojoComponentType clean up javadoc
    git-svn-id: https://svn.apache.org/repos/asf/incubator/tuscany/java/sca@506237 13f79535-47bb-0310-9956-ffa450edef68

--- kernel/spi/src/main/java/org/apache/tuscany/spi/implementation/java/PojoComponentType.java
+++ kernel/spi/src/main/java/org/apache/tuscany/spi/implementation/java/PojoComponentType.java
@@ -35,6 +35,7 @@ import org.apache.tuscany.spi.model.ServiceDefinition;
  */
 public class PojoComponentType<S extends ServiceDefinition, R extends ReferenceDefinition, P extends Property<?>>
     extends ComponentType<S, R, P> {
+    private Class<?> implClass;
     private boolean allowsPassByReference;
     private ConstructorDefinition<?> constructorDefinition;
     private Method initMethod;
@@ -43,7 +44,39 @@ public class PojoComponentType<S extends ServiceDefinition, R extends ReferenceD
     private Member conversationIDMember;
 
     /**
-     * Returns the constructor used to instantiate implementation instances
+     * Deprecated no-arg constructor, replaced with one that takes the POJO class.
+     */
+    @Deprecated
+    public PojoComponentType() {
+    }
+
+    /**
+     * Constructor specifying the java class for the POJO this is describing.
+     * @param implClass the java class for the POJO this is describing
+     */
+    public PojoComponentType(Class<?> implClass) {
+        this.implClass = implClass;
+    }
+
+    /**
+     * Returns the java class for the POJO this is describing.
+     * @return the java class for the POJO this is describing
+     */
+    public Class<?> getImplClass() {
+        return implClass;
+    }
+
+    /**
+     * Sets the java class for the POJO this is describing.
+     * @param implClass the java class for the POJO this is describing
+     */
+    public void setImplClass(Class<?> implClass) {
+        this.implClass = implClass;
+    }
+
+    /**
+     * Returns the constructor used to instantiate implementation instances.
+     * @return the constructor used to instantiate implementation instances
      */
     public ConstructorDefinition<?> getConstructorDefinition() {
         return constructorDefinition;
@@ -51,34 +84,39 @@ public class PojoComponentType<S extends ServiceDefinition, R extends ReferenceD
 
     /**
      * Sets the constructor used to instantiate implementation instances
+     * @param definition the constructor used to instantiate implementation instances
      */
     public void setConstructorDefinition(ConstructorDefinition<?> definition) {
         this.constructorDefinition = definition;
     }
 
     /**
-     * Returns the component initializer method
+     * Returns the component initializer method.
+     * @return the component initializer method
      */
     public Method getInitMethod() {
         return initMethod;
     }
 
     /**
-     * Sets the component initializer method
+     * Sets the component initializer method.
+     * @param initMethod the component initializer method
      */
     public void setInitMethod(Method initMethod) {
         this.initMethod = initMethod;
     }
 
     /**
-     * Returns the component destructor method
+     * Returns the component destructor method.
+     * @return the component destructor method
      */
     public Method getDestroyMethod() {
         return destroyMethod;
     }
 
     /**
-     * Sets the component destructor method
+     * Sets the component destructor method.
+     * @param destroyMethod the component destructor method
      */
     public void setDestroyMethod(Method destroyMethod) {
         this.destroyMethod = destroyMethod;
