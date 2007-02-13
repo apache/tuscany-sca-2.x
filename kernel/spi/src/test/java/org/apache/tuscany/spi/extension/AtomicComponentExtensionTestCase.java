80d8469692bc7e96a234e20d6641f7402e481aed
Jeremy Boynes <jboynes@apache.org>
2007-02-13 19:15:33 +0000

    refactor ComponentContextImpl out of extension to allow individual implementations to define their functionality
    git-svn-id: https://svn.apache.org/repos/asf/incubator/tuscany/java/sca@507165 13f79535-47bb-0310-9956-ffa450edef68

--- kernel/spi/src/test/java/org/apache/tuscany/spi/extension/AtomicComponentExtensionTestCase.java
+++ kernel/spi/src/test/java/org/apache/tuscany/spi/extension/AtomicComponentExtensionTestCase.java
@@ -18,104 +18,65 @@
  */
 package org.apache.tuscany.spi.extension;
 
-import java.lang.reflect.InvocationTargetException;
 import java.net.URI;
+
+import junit.framework.AssertionFailedError;
+import junit.framework.TestCase;
+import org.easymock.EasyMock;
 
 import org.apache.tuscany.spi.ObjectCreationException;
 import org.apache.tuscany.spi.component.ScopeContainer;
+import org.apache.tuscany.spi.component.TargetInvokerCreationException;
 import org.apache.tuscany.spi.component.TargetResolutionException;
 import org.apache.tuscany.spi.model.Operation;
 import org.apache.tuscany.spi.model.Scope;
 import org.apache.tuscany.spi.wire.InboundWire;
-import org.apache.tuscany.spi.wire.InvocationRuntimeException;
-import org.apache.tuscany.spi.wire.Message;
 import org.apache.tuscany.spi.wire.TargetInvoker;
-
-import junit.framework.TestCase;
-import org.easymock.EasyMock;
 
 /**
  * @version $Rev$ $Date$
  */
 public class AtomicComponentExtensionTestCase extends TestCase {
+    private AtomicComponentExtension ext;
+    private URI uri;
 
-    public void testIsEagerInit() throws Exception {
-        TestExtension ext = new TestExtension();
-        ext.isEagerInit();
+    public void testURI() {
+        assertSame(uri, ext.getUri());
     }
-
-    public void testInit() throws Exception {
-        TestExtension ext = new TestExtension();
-        ext.init(null);
-    }
-
-    public void testDestroy() throws Exception {
-        TestExtension ext = new TestExtension();
-        ext.destroy(null);
-    }
-
-    public void testInboundWire() throws Exception {
-        TestExtension ext = new TestExtension();
-        ext.getInboundWire(null);
-    }
-
+    
     public void testRemoveInstance() throws Exception {
-        ScopeContainer container = EasyMock.createMock(ScopeContainer.class);
-        EasyMock.expect(container.getScope()).andReturn(Scope.COMPOSITE);
-        container.remove(EasyMock.isA(AtomicComponentExtension.class));
-        EasyMock.replay(container);
-        TestExtension ext = new TestExtension(container);
+        ScopeContainer scopeContainer = EasyMock.createMock(ScopeContainer.class);
+        EasyMock.expect(scopeContainer.getScope()).andReturn(Scope.COMPOSITE);
+        scopeContainer.remove(ext);
+        EasyMock.replay(scopeContainer);
+        ext.setScopeContainer(scopeContainer);
         ext.removeInstance();
-        EasyMock.verify(container);
+        EasyMock.verify(scopeContainer);
     }
 
-    private class TestExtension extends AtomicComponentExtension {
-        public TestExtension() {
-            super(URI.create("_foo"), null, null, null, null, 0);
+    protected void setUp() throws Exception {
+        super.setUp();
+        uri = URI.create("http://example.com/foo");
+        ext = new TestExtension(uri);
+    }
+
+    private static class TestExtension extends AtomicComponentExtension {
+
+        public TestExtension(URI uri) {
+            super(uri, null, null, null, null, 0);
         }
 
-        public TestExtension(ScopeContainer scopeContainer) {
-            super(URI.create("_foo"), null, null, null, null, 0);
-            setScopeContainer(scopeContainer);
+        public TargetInvoker createTargetInvoker(String targetName, Operation operation, InboundWire callbackWire)
+            throws TargetInvokerCreationException {
+            throw new AssertionFailedError();
         }
 
         public Object createInstance() throws ObjectCreationException {
-            return null;
+            throw new AssertionFailedError();
         }
 
         public Object getTargetInstance() throws TargetResolutionException {
-            return null;
+            throw new AssertionFailedError();
         }
-
-        public TargetInvoker createTargetInvoker(String targetName, Operation operation, InboundWire callbackWire) {
-            return new TargetInvoker() {
-
-                public Object invokeTarget(final Object payload, final short sequence)
-                    throws InvocationTargetException {
-                    return null;
-                }
-
-                public Message invoke(Message msg) throws InvocationRuntimeException {
-                    return null;
-                }
-
-                public boolean isCacheable() {
-                    return false;
-                }
-
-                public void setCacheable(boolean cacheable) {
-
-                }
-
-                public boolean isOptimizable() {
-                    return false;
-                }
-
-                public Object clone() throws CloneNotSupportedException {
-                    return super.clone();
-                }
-            };
-        }
-
     }
 }
