ecc8c9b9390a1c636889d8021a0015961e9f13f1
Meeraj Kunnumpurath <meerajk@apache.org>
2007-02-16 00:25:30 +0000

    Added code to deploy the app scdl
    git-svn-id: https://svn.apache.org/repos/asf/incubator/tuscany/java/sca@508251 13f79535-47bb-0310-9956-ffa450edef68

--- runtime/standalone/standalone-host/src/main/java/org/apache/tuscany/runtime/standalone/host/StandaloneRuntimeImpl.java
+++ runtime/standalone/standalone-host/src/main/java/org/apache/tuscany/runtime/standalone/host/StandaloneRuntimeImpl.java
@@ -18,16 +18,26 @@
  */
 package org.apache.tuscany.runtime.standalone.host;
 
+import java.net.URI;
+import java.net.URL;
+
+import org.apache.tuscany.spi.component.AtomicComponent;
 import org.apache.tuscany.spi.component.RegistrationException;
+import org.apache.tuscany.spi.component.TargetResolutionException;
+import org.apache.tuscany.spi.deployer.Deployer;
+import org.apache.tuscany.spi.model.ComponentDefinition;
+import org.apache.tuscany.spi.model.CompositeImplementation;
 
 import org.apache.tuscany.core.runtime.AbstractRuntime;
 import org.apache.tuscany.host.runtime.InitializationException;
+import org.apache.tuscany.runtime.standalone.StandaloneRuntime;
 import org.apache.tuscany.runtime.standalone.StandaloneRuntimeInfo;
+import org.osoa.sca.ComponentContext;
 
 /**
  * @version $Rev$ $Date$
  */
-public class StandaloneRuntimeImpl extends AbstractRuntime {
+public class StandaloneRuntimeImpl extends AbstractRuntime implements StandaloneRuntime {
 
     protected void registerSystemComponents() throws InitializationException {
         super.registerSystemComponents();
@@ -37,6 +47,37 @@ public class StandaloneRuntimeImpl extends AbstractRuntime {
                 (StandaloneRuntimeInfo) getRuntimeInfo());
         } catch (RegistrationException e) {
             throw new InitializationException(e);
+        }
+    }
+    
+    /**
+     * Deploys the specified application SCDL.
+     * 
+     * @param compositeUri URI by which the composite is deployed.
+     * @param applicationScdl Application SCDL that implements the composite.
+     * @param applicationClassLoader Classloader used to deploy the composite.
+     * @return The component context for the deployed composite.
+     * @deprecated This is a hack for deployment and should be removed.
+     */
+    public ComponentContext deploy(URI compositeUri, URL applicationScdl, ClassLoader applicationClassLoader) throws Exception {
+        
+        CompositeImplementation impl = new CompositeImplementation();
+        impl.setScdlLocation(applicationScdl);
+        impl.setClassLoader(applicationClassLoader);
+
+        ComponentDefinition<CompositeImplementation> definition =
+            new ComponentDefinition<CompositeImplementation>(compositeUri, impl);
+        
+        return getDeployer().deploy(null, definition).getComponentContext();
+    }
+
+    protected Deployer getDeployer() {
+        try {
+            URI uri = URI.create("sca://root.system/main/deployer");
+            AtomicComponent component = (AtomicComponent) getComponentManager().getComponent(uri);
+            return (Deployer) component.getTargetInstance();
+        } catch (TargetResolutionException e) {
+            throw new AssertionError(e);
         }
     }
 }
