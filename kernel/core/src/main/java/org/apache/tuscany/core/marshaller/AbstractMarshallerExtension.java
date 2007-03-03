aa1e94fe2f49497de7d50e6231deabae2ed0c467
Meeraj Kunnumpurath <meerajk@apache.org>
2007-03-03 12:08:20 +0000

    Added support to abstract extensions
    git-svn-id: https://svn.apache.org/repos/asf/incubator/tuscany/java/sca@514136 13f79535-47bb-0310-9956-ffa450edef68

--- kernel/core/src/main/java/org/apache/tuscany/core/marshaller/AbstractMarshallerExtension.java
+++ kernel/core/src/main/java/org/apache/tuscany/core/marshaller/AbstractMarshallerExtension.java
@@ -20,12 +20,18 @@ package org.apache.tuscany.core.marshaller;
 
 import javax.xml.namespace.QName;
 
-import org.osoa.sca.annotations.Reference;
-
 import org.apache.tuscany.spi.marshaller.ModelMarshaller;
 import org.apache.tuscany.spi.marshaller.ModelMarshallerRegistry;
 import org.apache.tuscany.spi.model.ModelObject;
+import org.osoa.sca.annotations.Reference;
 
+/**
+ * Abstract marshaller that supports marshaller registry.
+ * 
+ * @version $Revision$ $Date$
+ *
+ * @param <MD>
+ */
 public abstract class AbstractMarshallerExtension<MD extends ModelObject> implements ModelMarshaller<MD> {
 
     // Private Model marshaller registry
@@ -33,7 +39,6 @@ public abstract class AbstractMarshallerExtension<MD extends ModelObject> implem
 
     /**
      * Injects the model marshaller registry.
-     *
      * @param registry Model marshaller registry.
      */
     @Reference
@@ -50,14 +55,12 @@ public abstract class AbstractMarshallerExtension<MD extends ModelObject> implem
 
     /**
      * Gets the qualified name of the XML fragment for the marshalled model object.
-     *
      * @return Qualified name of the XML fragment.
      */
     protected abstract QName getModelObjectQName();
 
     /**
      * Retursn the type of the model object.
-     *
      * @return Model object type.
      */
     protected abstract Class<MD> getModelObjectType();
---
+++ kernel/core/src/main/java/org/apache/tuscany/core/marshaller/AbstractPhysicalServiceDefinitionMarshaller.java
@@ -0,0 +1,83 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.    
+ */
+package org.apache.tuscany.core.marshaller;
+
+import static javax.xml.stream.XMLStreamConstants.END_ELEMENT;
+import static javax.xml.stream.XMLStreamConstants.START_ELEMENT;
+
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.XMLStreamReader;
+import javax.xml.stream.XMLStreamWriter;
+
+import org.apache.tuscany.spi.marshaller.MarshallException;
+import org.apache.tuscany.spi.model.ModelObject;
+import org.apache.tuscany.spi.model.physical.PhysicalOperationDefinition;
+import org.apache.tuscany.spi.model.physical.PhysicalServiceDefinition;
+
+/**
+ * Marshaller for java physical service definition.
+ * 
+ * @version $Revision$ $Date$
+ */
+public abstract class AbstractPhysicalServiceDefinitionMarshaller<PSD extends PhysicalServiceDefinition> extends AbstractExtensibleMarshallerExtension<PSD> {
+
+//  Local part for operation
+    private static final String OPERATION = "operation";
+    
+    // Source name attribute
+    private static final String NAME = "name";
+
+    /**
+     * Marshalls a physical java reference definition to the xml writer.
+     */
+    public void marshall(PSD modelObject, XMLStreamWriter writer) throws MarshallException {
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     * Unmarshalls a java physical reference definition from the xml reader.
+     */
+    public PSD unmarshall(XMLStreamReader reader) throws MarshallException {
+        
+        try {
+            PSD serviceDefinition = getConcreteModelObject();
+            serviceDefinition.setName(reader.getAttributeValue(null, NAME));
+            while (true) {
+                switch (reader.next()) {
+                    case START_ELEMENT:
+                        ModelObject modelObject = registry.unmarshall(reader);
+                        String name = reader.getName().getLocalPart();
+                        if(OPERATION.equals(name)) {
+                            serviceDefinition.addOperation((PhysicalOperationDefinition)modelObject);
+                        } else {
+                            handleExtensions(serviceDefinition, reader);
+                        }
+                        break;
+                    case END_ELEMENT:
+                        return serviceDefinition;
+
+                }
+            }
+        } catch (XMLStreamException ex) {
+            throw new MarshallException(ex);
+        }
+        
+    }
+
+}
