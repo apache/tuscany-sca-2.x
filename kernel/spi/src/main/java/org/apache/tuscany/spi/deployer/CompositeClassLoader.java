3c37baa8bc47309170a86d19c230cc3bdd8bb6ba
Jeremy Boynes <jboynes@apache.org>
2007-03-07 05:46:38 +0000

    merge multi-parent function into composite classloader
    git-svn-id: https://svn.apache.org/repos/asf/incubator/tuscany/java/sca@515464 13f79535-47bb-0310-9956-ffa450edef68

--- kernel/spi/src/main/java/org/apache/tuscany/spi/deployer/CompositeClassLoader.java
+++ kernel/spi/src/main/java/org/apache/tuscany/spi/deployer/CompositeClassLoader.java
@@ -18,9 +18,16 @@
  */
 package org.apache.tuscany.spi.deployer;
 
-import java.net.URLClassLoader;
+import java.io.IOException;
 import java.net.URL;
+import java.net.URLClassLoader;
 import java.net.URLStreamHandlerFactory;
+import java.util.Collections;
+import java.util.Enumeration;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 /**
  * ClassLoader associated with a composite.
@@ -29,24 +36,107 @@ import java.net.URLStreamHandlerFactory;
  */
 public class CompositeClassLoader extends URLClassLoader  {
     private static final URL[] NOURLS = {};
-    
-    public CompositeClassLoader(ClassLoader classLoader) {
-        super(NOURLS, classLoader);
+    private final List<ClassLoader> parents = new CopyOnWriteArrayList<ClassLoader>();
+
+    public CompositeClassLoader(ClassLoader parent) {
+        super(NOURLS);
+        parents.add(parent);
     }
 
-    public CompositeClassLoader(URL[] urls, ClassLoader classLoader) {
-        super(urls, classLoader);
+    public CompositeClassLoader(URL[] urls, ClassLoader parent) {
+        super(urls);
+        parents.add(parent);
     }
 
     public CompositeClassLoader(URL[] urls) {
         super(urls);
     }
 
-    public CompositeClassLoader(URL[] urls, ClassLoader classLoader, URLStreamHandlerFactory urlStreamHandlerFactory) {
-        super(urls, classLoader, urlStreamHandlerFactory);
+    public CompositeClassLoader(URL[] urls, ClassLoader parent, URLStreamHandlerFactory urlStreamHandlerFactory) {
+        super(urls, null, urlStreamHandlerFactory);
+        parents.add(parent);
     }
 
     public void addURL(URL url) {
+        // Require RuntimePermission("createClassLoader")
+        SecurityManager sm = System.getSecurityManager();
+        if (sm != null) {
+            sm.checkCreateClassLoader();
+        }
         super.addURL(url);
+    }
+
+    public void addParent(ClassLoader parent) {
+        // Require RuntimePermission("createClassLoader")
+        SecurityManager sm = System.getSecurityManager();
+        if (sm != null) {
+            sm.checkCreateClassLoader();
+        }
+        parents.add(parent);
+    }
+
+
+    protected synchronized Class<?> loadClass(String name, boolean resolve) throws ClassNotFoundException {
+        // look for already loaded classes
+        Class clazz = findLoadedClass(name);
+        if (clazz != null) {
+            if (resolve) {
+                resolveClass(clazz);
+            }
+            return clazz;
+        }
+
+        // look in our parents
+        for (ClassLoader parent : parents) {
+            try {
+                clazz = parent.loadClass(name);
+                if (resolve) {
+                    resolveClass(clazz);
+                }
+                return clazz;
+            } catch (ClassNotFoundException e) {
+                continue;
+            }
+        }
+
+        // look in our classpath
+        clazz = findClass(name);
+        if (resolve) {
+            resolveClass(clazz);
+        }
+        return clazz;
+    }
+
+
+    protected Class<?> findClass(String string) throws ClassNotFoundException {
+        return super.findClass(string);
+    }
+
+    public URL findResource(String name) {
+        // look in our parents
+        for (ClassLoader parent : parents) {
+            URL resource = parent.getResource(name);
+            if (resource != null) {
+                return resource;
+            }
+        }
+        // look in our classpath
+        return super.findResource(name);
+    }
+
+    public Enumeration<URL> findResources(String name) throws IOException {
+        // LinkedHashSet because we want all resources in the order found but no duplicates
+        Set<URL> resources = new LinkedHashSet<URL>();
+        for (ClassLoader parent : parents) {
+            Enumeration<URL> parentResources = parent.getResources(name);
+            while (parentResources.hasMoreElements()) {
+                resources.add(parentResources.nextElement());
+            }
+        }
+        Enumeration<URL> myResources = super.findResources(name);
+        while (myResources.hasMoreElements()) {
+            resources.add(myResources.nextElement());
+        }
+        return Collections.enumeration(resources);
     }
 }
