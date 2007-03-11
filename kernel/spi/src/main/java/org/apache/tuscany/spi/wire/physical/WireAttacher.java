b4d201889c27ef919e0135556a0f31bc874910b1
Meeraj Kunnumpurath <meerajk@apache.org>
2007-03-11 23:17:12 +0000

    Initial cut of the wire attacher.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/tuscany/java/sca@517043 13f79535-47bb-0310-9956-ffa450edef68

---
+++ kernel/spi/src/main/java/org/apache/tuscany/spi/wire/physical/WireAttacher.java
@@ -0,0 +1,51 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.    
+ */
+
+package org.apache.tuscany.spi.wire.physical;
+
+import org.apache.tuscany.spi.component.Component;
+import org.apache.tuscany.spi.model.physical.PhysicalWireSourceDefinition;
+import org.apache.tuscany.spi.model.physical.PhysicalWireTargetDefinition;
+
+/**
+ * Abstraction for the wire attacher.
+ * 
+ * @version $Date$ $Revision$
+ */
+public interface WireAttacher<C extends Component, 
+                              PWSD extends PhysicalWireSourceDefinition, 
+                              PWTD extends PhysicalWireTargetDefinition> {
+    
+    /**
+     * Attaches the source to the component.
+     * 
+     * @param component Component.
+     * @param source Source.
+     */
+    void attach(C component, PWSD source);
+    
+    /**
+     * Attaches the target to the component.
+     * 
+     * @param component Component.
+     * @param target Target.
+     */
+    void attach(C component, PWTD target);
+
+}
