3c3ee3475f6945b85700ef7d61a9299f3444bc7b
Meeraj Kunnumpurath <meerajk@apache.org>
2007-03-13 22:43:13 +0000

    Restructured injection site mapping.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/tuscany/java/sca@517921 13f79535-47bb-0310-9956-ffa450edef68

---
+++ kernel/core/src/main/java/org/apache/tuscany/core/model/physical/instancefactory/Site.java
@@ -0,0 +1,9 @@
+package org.apache.tuscany.core.model.physical.instancefactory;
+
+/**
+ * Marker for an injection site.
+ *
+ */
+public interface Site {
+
+}
---
+++ kernel/core/src/main/java/org/apache/tuscany/core/model/physical/instancefactory/ValueSource.java
@@ -0,0 +1,56 @@
+package org.apache.tuscany.core.model.physical.instancefactory;
+
+/**
+ * Represents a value source.
+ * 
+ * @version $Revision$ $Date$
+ *
+ */
+public class ValueSource {
+
+    // Type
+    private ValueSourceType valueType;
+    
+    // Name
+    private String name;
+    
+    // Type enumeration
+    public static enum ValueSourceType {
+        CALLBACK,
+        REFERENCE,
+        PROPERTY
+    }
+
+    /**
+     * Sets the type (callback, reference, property
+     * @return Type.
+     */
+    public ValueSourceType getValueType() {
+        return valueType;
+    }
+
+    /**
+     * Gets the type (callback, reference, property.
+     * @param valueType Type.
+     */
+    public void setValueType(ValueSourceType valueType) {
+        this.valueType = valueType;
+    }
+
+    /**
+     * Gets the name.
+     * @return Name.
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Sets the name.
+     * @param name Name.
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
