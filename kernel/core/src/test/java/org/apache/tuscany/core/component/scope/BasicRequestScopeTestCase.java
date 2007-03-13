1cc63195b1cd81ad98c999386c691df366c13d46
Jeremy Boynes <jboynes@apache.org>
2007-03-13 01:34:59 +0000

    clean up scope unit tests
    git-svn-id: https://svn.apache.org/repos/asf/incubator/tuscany/java/sca@517487 13f79535-47bb-0310-9956-ffa450edef68

--- kernel/core/src/test/java/org/apache/tuscany/core/component/scope/BasicRequestScopeTestCase.java
+++ kernel/core/src/test/java/org/apache/tuscany/core/component/scope/BasicRequestScopeTestCase.java
@@ -19,22 +19,15 @@
 package org.apache.tuscany.core.component.scope;
 
 import java.net.URI;
-import java.net.URISyntaxException;
-
-import org.apache.tuscany.spi.component.AtomicComponent;
-import org.apache.tuscany.spi.component.ScopeContainer;
-import org.apache.tuscany.spi.component.TargetNotFoundException;
-import org.apache.tuscany.spi.component.InstanceWrapper;
-import org.apache.tuscany.spi.component.WorkContext;
 
 import junit.framework.TestCase;
+import org.easymock.EasyMock;
+
 import org.apache.tuscany.core.component.event.RequestEnd;
-import org.apache.tuscany.core.implementation.PojoConfiguration;
-import org.apache.tuscany.core.implementation.system.component.SystemAtomicComponentImpl;
-import org.apache.tuscany.core.injection.EventInvoker;
-import org.apache.tuscany.core.injection.MethodEventInvoker;
-import org.apache.tuscany.core.injection.PojoObjectFactory;
-import org.apache.tuscany.core.mock.component.RequestScopeInitDestroyComponent;
+import org.apache.tuscany.spi.component.AtomicComponent;
+import org.apache.tuscany.spi.component.InstanceWrapper;
+import org.apache.tuscany.spi.component.ScopeContainer;
+import org.apache.tuscany.spi.component.TargetNotFoundException;
 
 /**
  * @version $$Rev$$ $$Date$$
@@ -43,94 +36,57 @@ public class BasicRequestScopeTestCase extends TestCase {
     private AtomicComponent component;
     private InstanceWrapper wrapper;
     private ScopeContainer scopeContainer;
-    private WorkContext workContext;
-
-    private EventInvoker<Object> initInvoker;
-    private EventInvoker<Object> destroyInvoker;
-    private PojoObjectFactory<?> factory;
 
     public void testLifecycleManagement() throws Exception {
-        AtomicComponent component = createComponent(scopeContainer);
-        // start the request
-        RequestScopeInitDestroyComponent o1 =
-            (RequestScopeInitDestroyComponent) scopeContainer.getInstance(component);
-        assertTrue(o1.isInitialized());
-        assertFalse(o1.isDestroyed());
-        RequestScopeInitDestroyComponent o2 =
-            (RequestScopeInitDestroyComponent) scopeContainer.getInstance(component);
-        assertSame(o1, o2);
-        scopeContainer.onEvent(new RequestEnd(this));
-        assertTrue(o1.isDestroyed());
-        scopeContainer.stop();
+        EasyMock.expect(component.createInstanceWrapper()).andReturn(wrapper);
+        EasyMock.replay(component, wrapper);
+        assertSame(wrapper, scopeContainer.getWrapper(component));
+        assertSame(wrapper, scopeContainer.getWrapper(component));
+        EasyMock.verify(component, wrapper);
     }
 
     public void testGetAssociatedInstance() throws Exception {
-        AtomicComponent component = createComponent(scopeContainer);
-        // start the request
-        scopeContainer.getInstance(component);
-        scopeContainer.getAssociatedInstance(component);
-        scopeContainer.stop();
+        EasyMock.expect(component.createInstanceWrapper()).andReturn(wrapper);
+        EasyMock.replay(component, wrapper);
+        assertSame(wrapper, scopeContainer.getWrapper(component));
+        assertSame(wrapper, scopeContainer.getAssociatedWrapper(component));
+        EasyMock.verify(component, wrapper);
     }
 
     public void testGetAssociatedInstanceNonExistent() throws Exception {
-        AtomicComponent component = createComponent(scopeContainer);
-        // start the request
+        URI id = URI.create("oops");
+        EasyMock.expect(component.getUri()).andReturn(id);
+        EasyMock.replay(component);
+
         try {
-            scopeContainer.getAssociatedInstance(component);
+            scopeContainer.getAssociatedWrapper(component);
             fail();
         } catch (TargetNotFoundException e) {
-            // expected
+            assertEquals(id.toString(), e.getMessage());
+            EasyMock.verify(component);
         }
-        scopeContainer.stop();
     }
 
     public void testRequestIsolation() throws Exception {
-        AtomicComponent component = createComponent(scopeContainer);
-
-        RequestScopeInitDestroyComponent o1 =
-            (RequestScopeInitDestroyComponent) scopeContainer.getInstance(component);
-        assertTrue(o1.isInitialized());
+        InstanceWrapper wrapper2 = EasyMock.createNiceMock(InstanceWrapper.class);
+        EasyMock.expect(component.createInstanceWrapper()).andReturn(wrapper).andReturn(wrapper2);
+        EasyMock.replay(component, wrapper);
+        assertSame(wrapper, scopeContainer.getWrapper(component));
         scopeContainer.onEvent(new RequestEnd(this));
-        assertTrue(o1.isDestroyed());
-
-        RequestScopeInitDestroyComponent o2 =
-            (RequestScopeInitDestroyComponent) scopeContainer.getInstance(component);
-        assertNotSame(o1, o2);
-        scopeContainer.onEvent(new RequestEnd(this));
-        assertTrue(o2.isDestroyed());
-        scopeContainer.stop();
+        assertSame(wrapper2, scopeContainer.getWrapper(component));
+        EasyMock.verify(component, wrapper);
     }
 
     protected void setUp() throws Exception {
         super.setUp();
+        component = EasyMock.createNiceMock(AtomicComponent.class);
+        wrapper = EasyMock.createNiceMock(InstanceWrapper.class);
         scopeContainer = new RequestScopeContainer(null, null);
         scopeContainer.start();
 
-        factory = new PojoObjectFactory<RequestScopeInitDestroyComponent>(
-            RequestScopeInitDestroyComponent.class.getConstructor((Class[]) null));
-        initInvoker = new MethodEventInvoker<Object>(
-            RequestScopeInitDestroyComponent.class.getMethod("init", (Class[]) null));
-        destroyInvoker = new MethodEventInvoker<Object>(
-            RequestScopeInitDestroyComponent.class.getMethod("destroy", (Class[]) null));
-    }
-
-    protected void tearDown() throws Exception {
-        super.tearDown();
-    }
-
-    private AtomicComponent createComponent(ScopeContainer scopeContainer) {
-        PojoConfiguration configuration = new PojoConfiguration();
-        configuration.setInstanceFactory(factory);
-        configuration.setInitInvoker(initInvoker);
-        configuration.setDestroyInvoker(destroyInvoker);
-        try {
-            configuration.setName(new URI("foo"));
-        } catch (URISyntaxException e) {
-            // will not happen
-        }
-        SystemAtomicComponentImpl component = new SystemAtomicComponentImpl(configuration);
-        component.setScopeContainer(scopeContainer);
-        component.start();
-        return component;
+        component.addListener(scopeContainer);
+        EasyMock.replay(component);
+        scopeContainer.register(component);
+        EasyMock.reset(component);
     }
 }
