f9f981b576ebd2a5f3635c7c11906ecd4fc47436
Jeremy Boynes <jboynes@apache.org>
2007-03-14 23:10:54 +0000

    cosmetic
    git-svn-id: https://svn.apache.org/repos/asf/incubator/tuscany/java/sca@518377 13f79535-47bb-0310-9956-ffa450edef68

--- kernel/core/src/main/java/org/apache/tuscany/core/model/physical/instancefactory/InjectionSource.java
+++ kernel/core/src/main/java/org/apache/tuscany/core/model/physical/instancefactory/InjectionSource.java
@@ -2,18 +2,17 @@ package org.apache.tuscany.core.model.physical.instancefactory;
 
 /**
  * Represents a value source.
- * 
- * @version $Revision$ $Date$
  *
+ * @version $Revision$ $Date$
  */
 public class InjectionSource {
 
     // Type
     private ValueSourceType valueType;
-    
+
     // Name
     private String name;
-    
+
     // Type enumeration
     public static enum ValueSourceType {
         CALLBACK,
@@ -25,12 +24,13 @@ public class InjectionSource {
     }
 
     public InjectionSource(ValueSourceType valueType, String name) {
-    this.valueType = valueType;
-    this.name = name;
+        this.valueType = valueType;
+        this.name = name;
     }
 
     /**
      * Sets the type (callback, reference, property
+     *
      * @return Type.
      */
     public ValueSourceType getValueType() {
@@ -39,6 +39,7 @@ public class InjectionSource {
 
     /**
      * Gets the type (callback, reference, property.
+     *
      * @param valueType Type.
      */
     public void setValueType(ValueSourceType valueType) {
@@ -47,6 +48,7 @@ public class InjectionSource {
 
     /**
      * Gets the name.
+     *
      * @return Name.
      */
     public String getName() {
@@ -55,6 +57,7 @@ public class InjectionSource {
 
     /**
      * Sets the name.
+     *
      * @param name Name.
      */
     public void setName(String name) {
