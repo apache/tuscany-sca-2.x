389767feb2a329cbcfeb3b9c69e6aec7d501f68c
James F. Marino <jmarino@apache.org>
2007-03-15 18:32:50 +0000

    start on wire attachement for the Hessian binding
    git-svn-id: https://svn.apache.org/repos/asf/incubator/tuscany/java/sca@518719 13f79535-47bb-0310-9956-ffa450edef68

---
+++ extensions/hessian/binding/src/main/java/org/apache/tuscany/hessian/model/HessianBindingComponentDefinition.java
@@ -0,0 +1,10 @@
+package org.apache.tuscany.hessian.model;
+
+import org.apache.tuscany.spi.model.physical.PhysicalComponentDefinition;
+
+/**
+ * @version $Rev$ $Date$
+ */
+public class HessianBindingComponentDefinition extends PhysicalComponentDefinition {
+    
+}
