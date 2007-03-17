9f45c4f34fb7d1b1430faf8cf8172a611516e369
Meeraj Kunnumpurath <meerajk@apache.org>
2007-03-17 19:00:25 +0000

    First cut of the console.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/tuscany/java/sca@519411 13f79535-47bb-0310-9956-ffa450edef68

---
+++ console/src/main/java/org/apache/tuscany/sca/console/handler/scdl/SdclContributionForm.java
@@ -0,0 +1,45 @@
+package org.apache.tuscany.sca.console.handler.scdl;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.tuscany.spi.host.ServletHost;
+import org.osoa.sca.annotations.Property;
+import org.osoa.sca.annotations.Reference;
+
+@SuppressWarnings("serial")
+public class SdclContributionForm extends TuscanyServlet {
+
+    /**
+     * Injects the servlet host and path mapping.
+     * 
+     * @param servletHost Servlet host to use.
+     * @param path Path mapping for the servlet.
+     */
+    public SdclContributionForm(@Reference(name = "servletHost") ServletHost servletHost, 
+                                @Property(name = "path") String path) {
+        super(servletHost, path);
+    }
+
+    /**
+     * Processes the request.
+     * 
+     * @param req Servlet request.
+     * @param res Servlet response.
+     * @throws ServletException Servlet exception.
+     * @throws IOException IO Exception.
+     */
+    protected void process(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+
+        PrintWriter writer = res.getWriter();
+        writer.println("Hi there");
+        writer.flush();
+        writer.close();
+
+    }
+
+}
