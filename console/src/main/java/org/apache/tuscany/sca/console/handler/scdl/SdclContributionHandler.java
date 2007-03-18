448d1b243cc9fbb2139d23cf13c905f86e34b7e0
Meeraj Kunnumpurath <meerajk@apache.org>
2007-03-18 23:54:41 +0000

    Hooked in assembly service.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/tuscany/java/sca@519744 13f79535-47bb-0310-9956-ffa450edef68

--- console/src/main/java/org/apache/tuscany/sca/console/handler/scdl/SdclContributionHandler.java
+++ console/src/main/java/org/apache/tuscany/sca/console/handler/scdl/SdclContributionHandler.java
@@ -1,12 +1,16 @@
 package org.apache.tuscany.sca.console.handler.scdl;
 
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.tuscany.host.deployment.AssemblyService;
+import org.apache.tuscany.host.deployment.DeploymentException;
 import org.apache.tuscany.spi.host.ServletHost;
 import org.osoa.sca.annotations.Property;
 import org.osoa.sca.annotations.Reference;
@@ -17,6 +21,9 @@ public class SdclContributionHandler extends TuscanyServlet {
     // SCDL query parameter
     private static final String SCDL_PARAM = "scdl";
     
+    // Assembly service to use
+    private AssemblyService assemblyService;
+    
     /**
      * Injects the servlet host and path mapping.
      * 
@@ -24,8 +31,10 @@ public class SdclContributionHandler extends TuscanyServlet {
      * @param path Path mapping for the servlet.
      */
     public SdclContributionHandler(@Reference(name = "servletHost") ServletHost servletHost, 
-                                   @Property(name = "path") String path) {
+                                   @Property(name = "path") String path,
+                                   @Reference(name = "assemblyService") AssemblyService assemblyService) {
         super(servletHost, path);
+        this.assemblyService = assemblyService;
     }
     
     /**
@@ -38,10 +47,17 @@ public class SdclContributionHandler extends TuscanyServlet {
      */
     protected void process(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
 
-        PrintWriter writer = res.getWriter();
-        writer.println("Got SCDL" + req.getParameter(SCDL_PARAM));
-        writer.flush();
-        writer.close();
+        try {
+            String scdl = req.getParameter(SCDL_PARAM);
+            InputStream in = new ByteArrayInputStream(scdl.getBytes());
+            assemblyService.include(in);
+            PrintWriter writer = res.getWriter();
+            writer.println("Processed SCDL ");
+            writer.flush();
+            writer.close();
+        } catch(DeploymentException ex) {
+            throw new ServletException(ex);
+        }
         
     }
 
