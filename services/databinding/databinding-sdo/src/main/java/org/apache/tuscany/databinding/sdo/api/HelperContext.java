12c78afcedec314ddac9b2dd562b37b51ccff7c0
Zhaohui Feng <rfeng@apache.org>
2007-03-19 18:26:52 +0000

    Add a test case for HelperContextProcessor
    git-svn-id: https://svn.apache.org/repos/asf/incubator/tuscany/java/sca@520037 13f79535-47bb-0310-9956-ffa450edef68

--- services/databinding/databinding-sdo/src/main/java/org/apache/tuscany/databinding/sdo/api/HelperContext.java
+++ services/databinding/databinding-sdo/src/main/java/org/apache/tuscany/databinding/sdo/api/HelperContext.java
@@ -30,7 +30,7 @@ import java.lang.annotation.Target;
  * 
  * @version $Rev$ $Date$
  */
-@Target( {METHOD, FIELD})
+@Target({METHOD, FIELD})
 @Retention(RUNTIME)
 public @interface HelperContext {
 }
