31a48c4973a92de8a4c60ff476c663f7691b1ee7
Luciano Resende <lresende@apache.org>
2007-03-23 17:28:34 +0000

    Contribution services module contents
    git-svn-id: https://svn.apache.org/repos/asf/incubator/tuscany/java/sca@521820 13f79535-47bb-0310-9956-ffa450edef68

---
+++ runtime/services/contribution/src/main/java/org/apache/tuscany/services/contribution/resolver/ComponentDefinitionArtifactResolver.java
@@ -0,0 +1,69 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.    
+ */
+
+package org.apache.tuscany.services.contribution.resolver;
+
+import java.net.URI;
+import java.net.URL;
+import java.util.Map;
+
+import org.apache.tuscany.spi.deployer.ArtifactResolver;
+import org.apache.tuscany.spi.deployer.ArtifactResolverRegistry;
+import org.apache.tuscany.spi.deployer.DeploymentContext;
+import org.apache.tuscany.spi.extension.ArtifactResolverExtension;
+import org.apache.tuscany.spi.model.ComponentDefinition;
+import org.apache.tuscany.spi.model.CompositeComponentType;
+import org.apache.tuscany.spi.model.Contribution;
+import org.apache.tuscany.spi.model.DeployedArtifact;
+import org.osoa.sca.annotations.Reference;
+
+public class ComponentDefinitionArtifactResolver extends ArtifactResolverExtension implements ArtifactResolver {
+
+    public ComponentDefinitionArtifactResolver(@Reference
+    ArtifactResolverRegistry registry) {
+        super(registry);
+    }
+
+    @Override
+    public Class<?> getType() {
+        return ComponentDefinition.class;
+    }
+
+    public <ComponentDefinition> ComponentDefinition resolve(Contribution contribution,
+                                                             Class<ComponentDefinition> modelClass,
+                                                             String namespace,
+                                                             String name,
+                                                             Map attributes,
+                                                             DeploymentContext context) {
+
+        // generate artifact uri based on it's name
+        URI artifactURI = contribution.getUri().resolve(name);
+        DeployedArtifact artifact = contribution.getArtifact(artifactURI);
+
+        ComponentDefinition componentDefinition =
+            (ComponentDefinition)artifact.getModelObject(CompositeComponentType.class, null);
+        return componentDefinition;
+    }
+
+    public URL resolve(Contribution contribution, String targetNamespace, String location, String baseURI) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
