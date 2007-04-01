7719b34f6d8bb241a446025814898efbb08dc856
Jean Sebastien Delfino <jsdelfino@apache.org>
2007-04-01 02:22:55 +0000

    Basic performance tests for the StAX and SAX loaders.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/tuscany/java/sca@524513 13f79535-47bb-0310-9956-ffa450edef68

---
+++ scdl4j/stax/src/test/java/org/apache/tuscany/scdl/stax/impl/StAXPerfTest.java
@@ -0,0 +1,93 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.    
+ */
+
+package org.apache.tuscany.scdl.stax.impl;
+
+import java.io.InputStream;
+
+import javax.xml.stream.XMLInputFactory;
+import javax.xml.stream.XMLStreamReader;
+
+import org.apache.tuscany.assembly.model.AssemblyFactory;
+import org.apache.tuscany.assembly.model.Composite;
+import org.apache.tuscany.assembly.model.impl.DefaultAssemblyFactory;
+import org.apache.tuscany.policy.model.PolicyFactory;
+import org.apache.tuscany.policy.model.impl.DefaultPolicyFactory;
+import org.apache.tuscany.scdl.stax.LoaderRegistry;
+
+/**
+ * Test the usability of the assembly model API when loading SCDL
+ * 
+ * @version $Rev$ $Date$
+ */
+public class StAXPerfTest {
+	
+    private XMLInputFactory inputFactory;
+    private AssemblyFactory assemblyFactory;
+    private PolicyFactory policyFactory;
+    private LoaderRegistry loaderRegistry;
+
+	public static void main(String[] args) throws Exception {
+		
+		StAXPerfTest perfTest = new StAXPerfTest();
+		perfTest.setUp();
+		
+		// Warm up
+		for (long i = 0; i<500; i++) {
+			perfTest.testReadComposite();
+		}
+		
+		long begin = System.currentTimeMillis();
+		long iter = 10000;
+		for (long i = 0; i<iter; i++) {
+			perfTest.testReadComposite();
+		}
+		long end = System.currentTimeMillis();
+		System.out.println("Iterations: "+ iter);
+		System.out.println("Time: "+ (end - begin));
+		System.out.println("Memory: "+Runtime.getRuntime().totalMemory()/1024);
+	}
+
+    public void setUp() throws Exception {
+        inputFactory = XMLInputFactory.newInstance();
+        assemblyFactory = new DefaultAssemblyFactory();
+        policyFactory = new DefaultPolicyFactory();
+        loaderRegistry = new LoaderRegistryImpl();
+    }
+
+    public void tearDown() throws Exception {
+        assemblyFactory = null;
+        policyFactory = null;
+        inputFactory = null;
+        loaderRegistry = null;
+    }
+
+    public void testReadComposite() throws Exception {
+        InputStream is = getClass().getClassLoader().getResourceAsStream("TestAllCalculator.composite");
+        CompositeLoader loader = new CompositeLoader(assemblyFactory, policyFactory, loaderRegistry);
+        XMLStreamReader reader = inputFactory.createXMLStreamReader(is);
+
+        Composite composite = loader.load(reader);
+        is.close();
+
+        if (composite == null)
+        	throw new IllegalStateException("Null composite");
+    }
+
+}
