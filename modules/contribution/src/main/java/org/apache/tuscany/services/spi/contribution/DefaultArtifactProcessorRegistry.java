7cc06899612332b8aa19567017c8dfcf1d5cc2f3
Jean Sebastien Delfino <jsdelfino@apache.org>
2007-04-05 00:27:02 +0000

    More generics fixes in the registry, and renamed the load method to read.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/tuscany/java/sca@525653 13f79535-47bb-0310-9956-ffa450edef68

--- modules/contribution/src/main/java/org/apache/tuscany/services/spi/contribution/DefaultArtifactProcessorRegistry.java
+++ modules/contribution/src/main/java/org/apache/tuscany/services/spi/contribution/DefaultArtifactProcessorRegistry.java
@@ -27,8 +27,8 @@ import java.util.Map;
  * @version $Rev$ $Date$
  */
 abstract class DefaultArtifactProcessorRegistry<S, M, K> implements ArtifactProcessorRegistry<S, M, K> {
-    private final Map<K, ArtifactProcessor<S, M, K>> processorsByArtifactType = new HashMap<K, ArtifactProcessor<S, M, K>>();
-    private final Map<Class<?>, ArtifactProcessor<S, M, K>> processorsByModelType = new HashMap<Class<?>, ArtifactProcessor<S, M, K>>();
+    private final Map<K, ArtifactProcessor<S, ?, K>> processorsByArtifactType = new HashMap<K, ArtifactProcessor<S, ?, K>>();
+    private final Map<Class<?>, ArtifactProcessor<S, ?, K>> processorsByModelType = new HashMap<Class<?>, ArtifactProcessor<S, ?, K>>();
 
     /**
      * Constructs a new loader registry.
@@ -41,7 +41,7 @@ abstract class DefaultArtifactProcessorRegistry<S, M, K> implements ArtifactProc
      * @param artifactType an artifact type
      * @return the processor associated with the given artifact type
      */
-    protected ArtifactProcessor<S, M, K> getProcessor(K artifactType) {
+    protected ArtifactProcessor<S, ?, K> getProcessor(K artifactType) {
         return processorsByArtifactType.get(artifactType);
     }
 
@@ -50,18 +50,19 @@ abstract class DefaultArtifactProcessorRegistry<S, M, K> implements ArtifactProc
      * @param modelType a model type
      * @return the processor associated with the given model type
      */
-    protected ArtifactProcessor<S, M, K> getProcessor(Class<M> modelType) {
+    protected ArtifactProcessor<S, ?, K> getProcessor(Class<M> modelType) {
         Class<?>[] classes = modelType.getClasses();
         for (Class<?> c: classes) {
-            ArtifactProcessor<S, M, K> processor = processorsByModelType.get(c);
+            ArtifactProcessor<S, ?, K> processor = processorsByModelType.get(c);
             if (processor != null)
                 return processor;
         }
         return null;
     }
 
-    public void addArtifactProcessor(ArtifactProcessor<S, M, K> artifactProcessor) {
+    public void addArtifactProcessor(ArtifactProcessor<S, ?, K> artifactProcessor) {
         processorsByArtifactType.put(artifactProcessor.getArtifactType(), artifactProcessor);
+        processorsByModelType.put(artifactProcessor.getModelType(), artifactProcessor);
     }
     
     public K getArtifactType() {
