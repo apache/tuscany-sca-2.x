219696c5d49a4407c28a7de4ec672df183788f55
Zhaohui Feng <rfeng@apache.org>
2007-04-05 05:36:41 +0000

    Copy kernel/host-api to modules/host-spi
    git-svn-id: https://svn.apache.org/repos/asf/incubator/tuscany/java/sca@525707 13f79535-47bb-0310-9956-ffa450edef68

---
+++ modules/host-spi/src/main/java/org/apache/tuscany/host/runtime/TuscanyRuntime.java
@@ -0,0 +1,125 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.    
+ */
+package org.apache.tuscany.host.runtime;
+
+import java.net.URL;
+import java.net.URI;
+
+import org.osoa.sca.ComponentContext;
+
+import org.apache.tuscany.host.MonitorFactory;
+import org.apache.tuscany.host.RuntimeInfo;
+import org.apache.tuscany.host.management.ManagementService;
+
+/**
+ * @version $Rev$ $Date$
+ */
+public interface TuscanyRuntime<I extends RuntimeInfo> {
+    /**
+     * Returns the location of the SCDL used to boot this runtime.
+     *
+     * @return the location of the SCDL used to boot this runtime
+     */
+    URL getSystemScdl();
+
+    /**
+     * Sets the location of the SCDL used to boot this runtime.
+     *
+     * @param systemScdl the location of the SCDL used to boot this runtime
+     */
+    void setSystemScdl(URL systemScdl);
+
+    /**
+     * Returns the host ClassLoader that is parent to all Tuscany classloaders.
+     *
+     * @return the host's ClassLoader
+     */
+    ClassLoader getHostClassLoader();
+
+    /**
+     * Sets the host ClassLoader; this will be a parent for all Tuscany classloaders.
+     *
+     * @param classLoader the host's ClassLoader
+     */
+    void setHostClassLoader(ClassLoader classLoader);
+
+    /**
+     * Returns the info this runtime will make available to service components.
+     *
+     * @return the info this runtime will make available to service components
+     */
+    I getRuntimeInfo();
+
+    /**
+     * Sets the info this runtime should make available to service components.
+     *
+     * @param runtimeInfo the information this runtime should make available to service components
+     */
+    void setRuntimeInfo(I runtimeInfo);
+
+    /**
+     * Returns the MonitorFactory that this runtime is using.
+     *
+     * @return the MonitorFactory that this runtime is using
+     */
+    MonitorFactory getMonitorFactory();
+
+    /**
+     * Sets the MonitorFactory that this runtime should use.
+     *
+     * @param monitorFactory the MonitorFactory that this runtime should use
+     */
+    void setMonitorFactory(MonitorFactory monitorFactory);
+
+    /**
+     * Sets the ManagementService that this runtime should use.
+     *
+     * @param managementService the ManagementService that this runtime should use
+     */
+    void setManagementService(ManagementService<?> managementService);
+
+    /**
+     * Returns the ManagementService that this runtime is using.
+     *
+     * @return the ManagementService that this runtime is using
+     */
+    ManagementService<?> getManagementService();
+
+    /**
+     * Initialize a runtime.
+     *
+     * @throws InitializationException if there is an error initializing the runtime
+     */
+    void initialize() throws InitializationException;
+
+    /**
+     * Destroy the runtime. Any further invocations should result in an error.
+     *
+     * @throws ShutdownException if there is an error destroying the runtime
+     */
+    void destroy() throws ShutdownException;
+
+    /**
+     * Returns the ComponentContext for the designated component.
+     *
+     * @param componentId the id of the component whose context should be returned
+     * @return the ComponentContext for the designated component
+     */
+    ComponentContext getComponentContext(URI componentId);
+}
