fa611aa1452427f546b50c499b8a411d9f7615a9
Jean Sebastien Delfino <jsdelfino@apache.org>
2007-04-12 07:10:14 +0000

    Prepared pom for integration in the build. Removed old run.bat file.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/tuscany/java/sca@527796 13f79535-47bb-0310-9956-ffa450edef68

--- samples/supplychain/run.bat
+++
@@ -1,7 +0,0 @@
-echo off
-rem set java_debug_set=-Xdebug -Xnoagent -Xrunjdwp:transport=dt_socket,address=3720,server=y,suspend=y
-mkdir target\standalone
-pushd target\standalone
-jar -xf "%USERPROFILE%\.m2\repository\org\apache\tuscany\standalone\1.0-SNAPSHOT\standalone-1.0-SNAPSHOT-bin.zip" 
-popd
-java %java_debug_set% -jar target\standalone\bin\launcher.jar  --classpath "%USERPROFILE%\.m2\repository\org\apache\tuscany\samples\sca\sample-supplychain\1.0-SNAPSHOT\sample-supplychain-1.0-SNAPSHOT.jar" %*
