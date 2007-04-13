be59d736dd3cc608af1d9089ac6ab3975e2454b9
Jean Sebastien Delfino <jsdelfino@apache.org>
2007-04-13 20:05:04 +0000

    Show how to implement a service binding in the echo-binding sample.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/tuscany/java/sca@528638 13f79535-47bb-0310-9956-ffa450edef68

--- samples/echo-binding/src/main/java/echo/EchoService.java
+++ samples/echo-binding/src/main/java/echo/EchoService.java
@@ -18,14 +18,21 @@
  */
 package echo;
 
+import java.lang.reflect.InvocationTargetException;
 import java.net.URI;
 
 import javax.xml.namespace.QName;
 
+import org.apache.tuscany.core.util.PojoWorkContextTunnel;
 import org.apache.tuscany.interfacedef.Operation;
 import org.apache.tuscany.spi.CoreRuntimeException;
 import org.apache.tuscany.spi.component.TargetInvokerCreationException;
+import org.apache.tuscany.spi.component.WorkContext;
 import org.apache.tuscany.spi.extension.ServiceBindingExtension;
+import org.apache.tuscany.spi.wire.Interceptor;
+import org.apache.tuscany.spi.wire.InvocationChain;
+import org.apache.tuscany.spi.wire.Message;
+import org.apache.tuscany.spi.wire.MessageImpl;
 import org.apache.tuscany.spi.wire.TargetInvoker;
 
 /**
@@ -45,12 +52,36 @@ public class EchoService extends ServiceBindingExtension {
     }
 
     public TargetInvoker createTargetInvoker(String targetName, Operation operation, boolean isCallback) throws TargetInvokerCreationException {
-        // TODO Auto-generated method stub
-        return null;
+        //TODO Show support for callbacks in this sample
+        throw new UnsupportedOperationException();
     }
 
-    String sendReceive(String input) {
-        return input;
+    String sendReceive(String input) throws InvocationTargetException {
+        
+        // Get the invocation chain for the first operation in the service interface
+        InvocationChain chain = wire.getInvocationChains().get(0);
+        Interceptor headInterceptor = chain.getHeadInterceptor();
+        WorkContext workContext = PojoWorkContextTunnel.getThreadWorkContext();
+        if (headInterceptor == null) {
+            // short-circuit the dispatch and invoke the target directly
+            TargetInvoker targetInvoker = chain.getTargetInvoker();
+            return (String)targetInvoker.invokeTarget(new Object[]{input}, TargetInvoker.NONE, workContext);
+        } else {
+
+            Message msg = new MessageImpl();
+            msg.setTargetInvoker(chain.getTargetInvoker());
+            msg.setBody(new Object[]{input});
+            msg.setWorkContext(workContext);
+            Message resp;
+
+            // dispatch and get the response
+            resp = headInterceptor.invoke(msg);
+            Object body = resp.getBody();
+            if (resp.isFault()) {
+                throw new InvocationTargetException((Throwable) body);
+            }
+            return (String)body;
+        }
     }
     
 }
