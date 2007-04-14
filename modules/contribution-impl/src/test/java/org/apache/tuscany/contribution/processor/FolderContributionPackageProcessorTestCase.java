36eb0b58d72481ad2241874da2eab37c6da8cfda
Luciano Resende <lresende@apache.org>
2007-04-14 18:09:55 +0000

    Contribution Service refactoring - Part VI
    git-svn-id: https://svn.apache.org/repos/asf/incubator/tuscany/java/sca@528847 13f79535-47bb-0310-9956-ffa450edef68

--- modules/contribution-impl/src/test/java/org/apache/tuscany/contribution/processor/FolderContributionPackageProcessorTestCase.java
+++ modules/contribution-impl/src/test/java/org/apache/tuscany/contribution/processor/FolderContributionPackageProcessorTestCase.java
@@ -16,42 +16,34 @@
  * specific language governing permissions and limitations
  * under the License.    
  */
-package org.apache.tuscany.contribution.services.processor;
+package org.apache.tuscany.contribution.processor;
 
-import java.io.InputStream;
+import java.io.File;
 import java.net.URI;
-import java.net.URL;
 import java.util.List;
 
 import junit.framework.TestCase;
 
 import org.apache.tuscany.contribution.processor.ContributionPackageProcessorRegistry;
 import org.apache.tuscany.contribution.processor.impl.ContributionPackageProcessorRegistryImpl;
-import org.apache.tuscany.contribution.processor.impl.JarContributionProcessor;
+import org.apache.tuscany.contribution.processor.impl.FolderContributionProcessor;
 import org.apache.tuscany.contribution.service.impl.PackageTypeDescriberImpl;
-import org.apache.tuscany.contribution.service.util.IOHelper;
 
-public class JarContributionPackageProcessorTestCase extends TestCase {
-    private static final String JAR_CONTRIBUTION = "/repository/sample-calculator.jar";
+public class FolderContributionPackageProcessorTestCase extends TestCase {
+    private static final String FOLDER_CONTRIBUTION = ".";
     
+    private File contributionRoot;
+
     protected void setUp() throws Exception {
         super.setUp();
+        this.contributionRoot = new File(FOLDER_CONTRIBUTION);
     }
     
     public final void testProcessPackageArtifacts() throws Exception {
         ContributionPackageProcessorRegistry packageProcessorRegistry = new ContributionPackageProcessorRegistryImpl(new PackageTypeDescriberImpl()); 
-        JarContributionProcessor jarProcessor = new JarContributionProcessor(packageProcessorRegistry);
+        FolderContributionProcessor folderProcessor = new FolderContributionProcessor(packageProcessorRegistry);
 
-        URL jarURL = getClass().getResource(JAR_CONTRIBUTION);
-        InputStream jarStream = jarURL.openStream();
-        List<URI> artifacts = null;
-        try{
-            artifacts = jarProcessor.getArtifacts(jarURL, jarStream);
-        }finally{
-            IOHelper.closeQuietly(jarStream);
-        }
-        
+        List<URI> artifacts = folderProcessor.getArtifacts(contributionRoot.toURL(), null);
         assertNotNull(artifacts);
     }
-    
-    }
+}
