6aeaaf572be6d76d3c49dfd5c7c1aedb2c7d6be4
Jean Sebastien Delfino <jsdelfino@apache.org>
2007-04-14 17:45:02 +0000

    Copied assembly DSL builder module from the scdl4j work in sandbox.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/tuscany/java/sca@528844 13f79535-47bb-0310-9956-ffa450edef68

---
+++ modules/assembly-java-dsl/src/main/java/org/apache/tuscany/assembly/builder/impl/AssemblyBuilderImpl.java
@@ -0,0 +1,70 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.    
+ */
+package org.apache.tuscany.assembly.builder.impl;
+
+import javax.xml.namespace.QName;
+
+import org.apache.tuscany.assembly.builder.AssemblyBuilder;
+import org.apache.tuscany.assembly.builder.ComponentBuilder;
+import org.apache.tuscany.assembly.builder.ComponentPropertyBuilder;
+import org.apache.tuscany.assembly.builder.ComponentReferenceBuilder;
+import org.apache.tuscany.assembly.builder.ComponentServiceBuilder;
+import org.apache.tuscany.assembly.builder.CompositeBuilder;
+import org.apache.tuscany.assembly.impl.DefaultAssemblyFactory;
+
+public class AssemblyBuilderImpl extends DefaultAssemblyFactory implements AssemblyBuilder {
+
+	public ComponentBuilder component(String name) {
+		ComponentBuilderImpl component = new ComponentBuilderImpl();
+		component.setName(name);
+		return component;
+	}
+
+	public CompositeBuilder composite(String name) {
+		CompositeBuilderImpl composite = new CompositeBuilderImpl();
+		//TODO handle namespace
+		composite.setName(new QName("", name));
+		return composite;
+	}
+
+	public CompositeBuilder domain(String uri) {
+		CompositeBuilderImpl composite = new CompositeBuilderImpl();
+		composite.setName(new QName(uri, ""));
+		return composite;
+	}
+
+	public ComponentPropertyBuilder property(String name) {
+		ComponentPropertyBuilderImpl property = new ComponentPropertyBuilderImpl();
+		property.setName(name);
+		return property;
+	}
+
+	public ComponentReferenceBuilder reference(String name) {
+		ComponentReferenceBuilderImpl reference = new ComponentReferenceBuilderImpl();
+		reference.setName(name);
+		return reference;
+	}
+
+	public ComponentServiceBuilder service(String name) {
+		ComponentServiceBuilderImpl service = new ComponentServiceBuilderImpl();
+		service.setName(name);
+		return service;
+	}
+
+}
