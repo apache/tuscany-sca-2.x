f432c77a71cd01a8954c91e36fae314a5c0aa7b7
Luciano Resende <lresende@apache.org>
2007-04-16 01:00:49 +0000

    [work in progress] Bringing up sample das service
    git-svn-id: https://svn.apache.org/repos/asf/incubator/tuscany/java/sca@529111 13f79535-47bb-0310-9956-ffa450edef68

--- samples/das-service/service/src/main/java/das/DASService.java
+++ samples/das-service/service/src/main/java/das/DASService.java
@@ -16,7 +16,7 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-package org.apache.tuscany.samples.das.service;
+package das;
 
 import java.io.InputStream;
 import java.util.Vector;
