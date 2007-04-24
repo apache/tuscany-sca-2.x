e579fb186dba7fa802f98b4a10bec9093f59a786
Jean Sebastien Delfino <jsdelfino@apache.org>
2007-04-24 02:37:50 +0000

    Copied WS binding helloworld sample.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/tuscany/java/sca@531696 13f79535-47bb-0310-9956-ffa450edef68

---
+++ samples/helloworld-ws/src/main/java/helloworld/HelloWorldServer.java
@@ -0,0 +1,28 @@
+package helloworld;
+
+
+import java.io.IOException;
+
+import org.apache.tuscany.api.SCARuntime;
+
+public class HelloWorldServer {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		SCARuntime.start("helloworldws.composite");
+		
+		try {
+			System.out.println("HelloWorld server started");
+			System.in.read();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		SCARuntime.stop();
+		System.out.println("HelloWorld server stopped");
+	}
+
+}
---
+++ samples/helloworld-wsclient/src/main/java/helloworld/HelloWorldClient.java
@@ -0,0 +1,41 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.    
+ */
+package helloworld;
+
+import org.apache.tuscany.api.SCARuntime;
+import org.osoa.sca.CompositeContext;
+import org.osoa.sca.CurrentCompositeContext;
+
+/**
+ * This client program shows how to create an SCA runtime, start it,
+ * locate the HelloWorld service and invoke it.
+ */
+public class HelloWorldClient {
+
+    public  final static void main(String[] args) throws Exception {
+    	SCARuntime.start("helloworldwsclient.composite");
+    	
+        CompositeContext compositeContext = CurrentCompositeContext.getContext();
+        HelloWorldService helloWorldService= compositeContext.locateService(HelloWorldService.class, "HelloWorldServiceComponent");
+        String value = helloWorldService.getGreetings("World");
+        System.out.println(value);
+        
+        SCARuntime.stop();
+    }
+}
