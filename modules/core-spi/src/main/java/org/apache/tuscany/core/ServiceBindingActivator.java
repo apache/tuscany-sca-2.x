9abd4450e1dd7702623f2df83e3a34faf8c13bd1
Zhaohui Feng <rfeng@apache.org>
2007-04-27 21:00:59 +0000

    Add activator interfaces for Component Implementation, Reference Binding and Service Binding
    git-svn-id: https://svn.apache.org/repos/asf/incubator/tuscany/java/sca@533222 13f79535-47bb-0310-9956-ffa450edef68

---
+++ modules/core-spi/src/main/java/org/apache/tuscany/core/ServiceBindingActivator.java
@@ -0,0 +1,49 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.    
+ */
+
+package org.apache.tuscany.core;
+
+import org.apache.tuscany.assembly.Component;
+import org.apache.tuscany.assembly.ComponentService;
+
+/**
+ * A binding can optionally implement this interface to control how the service
+ * binding is started or stopped.
+ * 
+ * @version $Rev$ $Date$
+ */
+public interface ServiceBindingActivator {
+    /**
+     * This method will be invoked when a service binding is activated for the
+     * given component service.
+     * 
+     * @param component The SCA component
+     * @param service The service that owns the binding
+     */
+    void start(Component component, ComponentService service);
+
+    /**
+     * This method will be invoked when a service binding is deactivated or the
+     * given component service.
+     * 
+     * @param component The SCA component
+     * @param service The service that owns the binding
+     */
+    void stop(Component component, ComponentService service);
+}
