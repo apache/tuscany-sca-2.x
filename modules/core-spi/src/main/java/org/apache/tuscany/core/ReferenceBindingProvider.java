e1b0452d2c6ac3863f4b9c2f1e79ecf2387e434f
Zhaohui Feng <rfeng@apache.org>
2007-04-27 21:59:51 +0000

    Add interfaces for Implementation/Binding extension providers
    git-svn-id: https://svn.apache.org/repos/asf/incubator/tuscany/java/sca@533238 13f79535-47bb-0310-9956-ffa450edef68

---
+++ modules/core-spi/src/main/java/org/apache/tuscany/core/ReferenceBindingProvider.java
@@ -0,0 +1,54 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.    
+ */
+
+package org.apache.tuscany.core;
+
+import org.apache.tuscany.assembly.Component;
+import org.apache.tuscany.assembly.ComponentReference;
+import org.apache.tuscany.interfacedef.InterfaceContract;
+import org.apache.tuscany.interfacedef.Operation;
+import org.apache.tuscany.spi.wire.Interceptor;
+
+/**
+ * @version $Rev$ $Date$
+ */
+public interface ReferenceBindingProvider {
+    /**
+     * Create an intercetor for the reference binding in the invocation chain.
+     * The interceptor is responsible for making the outbound invocation over
+     * the binding protocol.
+     * 
+     * @param model The service that defines the binding
+     * @param operation The operation that the interceptor will handle
+     * @param isCallback A flag to tell if the operation is for the callback
+     * @return An interceptor that handles the invocation logic, null should be
+     *         returned if no interceptor is required
+     */
+    Interceptor createInterceptor(Component component, ComponentReference reference, Operation operation, boolean isCallback);
+
+    /**
+     * Get the effective interface contract imposed by the binding. For example,
+     * it will be interface contract introspected from the WSDL portType used by
+     * the endpoint for a WebService binding.
+     * 
+     * @param model The model object
+     * @return The effective interface contract
+     */
+    InterfaceContract getBindingInterfaceContract(ComponentReference reference);
+}
