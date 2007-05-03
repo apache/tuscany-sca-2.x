10f315a9883552508462a81f0025e2b8fdb513d3
Anthony Elder <antelder@apache.org>
2007-05-03 09:35:03 +0000

    Clean up some eclipse warnings in the commonj-api module
    git-svn-id: https://svn.apache.org/repos/asf/incubator/tuscany/java/sca@534762 13f79535-47bb-0310-9956-ffa450edef68

--- modules/commonj-api/src/main/java/commonj/work/WorkException.java
+++ modules/commonj-api/src/main/java/commonj/work/WorkException.java
@@ -32,6 +32,7 @@ package commonj.work;
  * @version $Rev$ $Date$
  */
 public class WorkException extends Exception {
+    private static final long serialVersionUID = 1L;
 
     public WorkException() {
         super();
