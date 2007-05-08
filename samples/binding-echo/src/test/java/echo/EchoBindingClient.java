336f48aa38735f24d92ff939f6f947b7ad0389fb
Simon Laws <slaws@apache.org>
2007-05-08 12:17:58 +0000

    Add readme and ant build files
    git-svn-id: https://svn.apache.org/repos/asf/incubator/tuscany/java/sca@536171 13f79535-47bb-0310-9956-ffa450edef68

---
+++ samples/binding-echo/src/test/java/echo/EchoBindingClient.java
@@ -0,0 +1,49 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.    
+ */
+package echo;
+
+import org.apache.tuscany.host.embedded.SCADomain;
+import echo.server.EchoServer;
+
+/**
+ * This client program shows how to create an SCA runtime, start it,
+ * and locate and invoke a SCA component
+ * @version $Rev$ $Date$
+ */
+public class EchoBindingClient {
+    public static void main(String[] args) throws Exception {
+
+        SCADomain domain  = SCADomain.newInstance("EchoBinding.composite");
+        
+        
+        // Call the echo service component which will, in turn, call its 
+        // reference which will echo the string back
+        Echo service = domain.getService(Echo.class, "EchoComponent");
+        String echoString = service.echo("foo");
+        System.out.println("Echo reference = " + echoString );
+
+        // Call the echo servic which will echo the string back straight
+        // away
+        echoString = EchoServer.getServer().sendReceive("EchoComponent/EchoService", "baa");
+        System.out.println("Echo service = " + echoString );
+        domain.close();
+
+    }
+
+}
