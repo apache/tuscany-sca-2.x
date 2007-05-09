3f84045dbae402e626622700641b289aa5dac1db
Simon Laws <slaws@apache.org>
2007-05-09 20:00:56 +0000

    Added ant build and test and readme material
    git-svn-id: https://svn.apache.org/repos/asf/incubator/tuscany/java/sca@536637 13f79535-47bb-0310-9956-ffa450edef68

---
+++ samples/simple-bigbank/src/main/java/bigbank/client/BigBankClient.java
@@ -0,0 +1,43 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.    
+ */
+
+package bigbank.client;
+
+import org.apache.tuscany.host.embedded.SCADomain;
+
+import bigbank.account.AccountService;
+
+/**
+ * This client program shows how to create an SCA runtime, start it,
+ * and locate and invoke a SCA component
+ */
+public class BigBankClient {
+    public static void main(String[] args) throws Exception {
+
+        SCADomain domain = SCADomain.newInstance("BigBank.composite");
+        
+        AccountService accountService = domain.getService(AccountService.class,
+                                                          "AccountServiceComponent");
+
+        System.out.println("Account summary: " + accountService.getAccountReport("Foo") );
+
+        domain.close();
+    }  
+
+}
