7b42ad6172b5087a0df4953edab2ce9421be5c7c
Anthony Elder <antelder@apache.org>
2007-05-09 18:16:14 +0000

    Fix webapp host to work with 2.4 servlet apis. Remove the use of all the init-params as that will be changed to use sca-contribution.xml
    git-svn-id: https://svn.apache.org/repos/asf/incubator/tuscany/java/sca@536611 13f79535-47bb-0310-9956-ffa450edef68

--- modules/host-webapp/src/main/java/org/apache/tuscany/sca/webapp/SCADomainHelper.java
+++ modules/host-webapp/src/main/java/org/apache/tuscany/sca/webapp/SCADomainHelper.java
@@ -33,13 +33,7 @@ import org.apache.tuscany.host.embedded.SCADomain;
 public class SCADomainHelper {
 
     public static final String SCA_DOMAIN_ATTRIBUTE = "org.apache.tuscany.sca.SCADomain";
-
-    public static final String DEPLOYABLE_COMPOSITES_PARAM = "deployableComposites";
-    public static final String DEPLOYABLE_COMPOSITES_DIR_PARAM = "deployableCompositesDirectory";
     public static final String DEFAULT_COMPOSITES_DIR = "/WEB-INF/classes";
-
-    public static final String DOMAIN_URI_PARAM = "domainURI";
-    public static final String CONTRABUTION_LOCATION_PARAM = "contrabutionLocation";
 
     /**
      * Initializes the SCADomian associated with a webapp context. If a SCADomain
@@ -48,52 +42,22 @@ public class SCADomainHelper {
     public static SCADomain initSCADomain(ServletContext servletContext) {
         SCADomain scaDomain = (SCADomain)servletContext.getAttribute(SCA_DOMAIN_ATTRIBUTE);
         if (scaDomain == null) {
+
             String[] compositeNames = getDeployableComposites(servletContext);
-            String domainURI = getDomainURI(servletContext);
-            String contrabutionLocation = getContrabutionLocation(servletContext);
-            scaDomain = SCADomain.newInstance(domainURI, contrabutionLocation, compositeNames);
+            String domainURI = "http://localhost/" + servletContext.getServletContextName().replace(' ', '.');
+            scaDomain = SCADomain.newInstance(domainURI, ".", compositeNames);
+
             servletContext.setAttribute(SCA_DOMAIN_ATTRIBUTE, scaDomain);
         }
         return scaDomain;
     }
 
-    protected static String getDomainURI(ServletContext servletContext) {
-        String domainURI = servletContext.getInitParameter(DOMAIN_URI_PARAM);
-        if (domainURI == null || domainURI.length() < 1) {
-            domainURI = "http:/" + servletContext.getContextPath();
-        }
-        return domainURI;
-    }
-
-    protected static String getContrabutionLocation(ServletContext servletContext) {
-        String location = servletContext.getInitParameter(CONTRABUTION_LOCATION_PARAM);
-        if (location == null || location.length() < 1) {
-            location = ".";
-        }
-        return location;
-    }
-
     /**
-     * Gets all the deployable composites. These may be specified by naming them
-     * individualy using the DEPLOYABLE_COMPOSITES_PARAM init-param, or else by
-     * naming a directory that contains all the composites to be deployable using
-     * the DEPLOYABLE_COMPOSITES_DIR_PARAM init-param. If neither of those are
-     * specified then it defaults to all the composites in the DEFAULT_COMPOSITES_DIR
-     * directory.
+     * Gets all the composites in the DEFAULT_COMPOSITES_DIR directory.
      */
     protected static String[] getDeployableComposites(ServletContext servletContext) {
-        String composites = servletContext.getInitParameter(DEPLOYABLE_COMPOSITES_PARAM);
-        if (composites != null && composites.length() > 0) {
-            return composites.split(",");
-        }
-
-        String compositeDir = servletContext.getInitParameter(DEPLOYABLE_COMPOSITES_DIR_PARAM);
-        if (compositeDir == null || compositeDir.length() < 1) {
-            compositeDir = DEFAULT_COMPOSITES_DIR;
-        }
-
         List<String> compositeNames = new ArrayList<String>();
-        Set resources = servletContext.getResourcePaths(compositeDir);
+        Set resources = servletContext.getResourcePaths(DEFAULT_COMPOSITES_DIR);
         for (Object o : resources) {
             String resource = o.toString();
             if (resource.endsWith(".composite")) {
--- modules/host-webapp/src/test/java/org/apache/tuscany/sca/webapp/SCADomainHelperTestCase.java
+++ modules/host-webapp/src/test/java/org/apache/tuscany/sca/webapp/SCADomainHelperTestCase.java
@@ -31,83 +31,8 @@ import org.easymock.EasyMock;
 
 public class SCADomainHelperTestCase extends TestCase {
 
-    public void testGetDomainURI() {
-        ServletContext servletContext = EasyMock.createMock(ServletContext.class);
-        EasyMock.expect(servletContext.getInitParameter(SCADomainHelper.DOMAIN_URI_PARAM)).andReturn("http://foo");
-        EasyMock.replay(servletContext);
-
-        assertEquals("http://foo", SCADomainHelper.getDomainURI(servletContext));
-
-        EasyMock.verify(servletContext);
-    }
-
-    public void testGetDomainURIDefault() {
-        ServletContext servletContext = EasyMock.createMock(ServletContext.class);
-        EasyMock.expect(servletContext.getInitParameter(SCADomainHelper.DOMAIN_URI_PARAM)).andReturn(null);
-        EasyMock.expect(servletContext.getContextPath()).andReturn("/myWebapp");
-        EasyMock.replay(servletContext);
-
-        assertEquals("http://myWebapp", SCADomainHelper.getDomainURI(servletContext));
-
-        EasyMock.verify(servletContext);
-    }
-
-    public void testgetContrabutionLocation() {
-        ServletContext servletContext = EasyMock.createMock(ServletContext.class);
-        EasyMock.expect(servletContext.getInitParameter(SCADomainHelper.CONTRABUTION_LOCATION_PARAM)).andReturn("/meta-inf/sca");
-        EasyMock.replay(servletContext);
-
-        assertEquals("/meta-inf/sca", SCADomainHelper.getContrabutionLocation(servletContext));
-
-        EasyMock.verify(servletContext);
-    }
-
-    public void testGetContrabutionLocationDefault() {
-        ServletContext servletContext = EasyMock.createMock(ServletContext.class);
-        EasyMock.expect(servletContext.getInitParameter(SCADomainHelper.CONTRABUTION_LOCATION_PARAM)).andReturn(null);
-        EasyMock.replay(servletContext);
-
-        assertEquals(".", SCADomainHelper.getContrabutionLocation(servletContext));
-
-        EasyMock.verify(servletContext);
-    }
-
-    public void testGetDeployableComposites() {
-        ServletContext servletContext = EasyMock.createMock(ServletContext.class);
-        EasyMock.expect(servletContext.getInitParameter(SCADomainHelper.DEPLOYABLE_COMPOSITES_PARAM)).andReturn("petra,beate");
-        EasyMock.replay(servletContext);
-
-        String[] composites = SCADomainHelper.getDeployableComposites(servletContext);
-        assertTrue(composites.length == 2);
-        assertEquals("petra", composites[0]);
-        assertEquals("beate", composites[1]);
-
-        EasyMock.verify(servletContext);
-    }
-
-    public void testGetDeployableCompositesDir() {
-        ServletContext servletContext = EasyMock.createMock(ServletContext.class);
-        EasyMock.expect(servletContext.getInitParameter(SCADomainHelper.DEPLOYABLE_COMPOSITES_PARAM)).andReturn(null);
-        EasyMock.expect(servletContext.getInitParameter(SCADomainHelper.DEPLOYABLE_COMPOSITES_DIR_PARAM)).andReturn("/myDir");
-        Set<String> resources = new HashSet<String>();
-        resources.add("foo");
-        resources.add("/myDir/petra.composite");
-        resources.add("/myDir/beate.composite");
-        EasyMock.expect(servletContext.getResourcePaths("/myDir")).andReturn(resources);
-        EasyMock.replay(servletContext);
-
-        String[] composites = SCADomainHelper.getDeployableComposites(servletContext);
-        assertTrue(composites.length == 2);
-        assertTrue(Arrays.asList(composites).contains("petra.composite"));
-        assertTrue(Arrays.asList(composites).contains("beate.composite"));
-
-        EasyMock.verify(servletContext);
-    }
-
     public void testGetDeployableCompositesDefaultDir() {
         ServletContext servletContext = EasyMock.createMock(ServletContext.class);
-        EasyMock.expect(servletContext.getInitParameter(SCADomainHelper.DEPLOYABLE_COMPOSITES_PARAM)).andReturn(null);
-        EasyMock.expect(servletContext.getInitParameter(SCADomainHelper.DEPLOYABLE_COMPOSITES_DIR_PARAM)).andReturn(null);
         Set<String> resources = new HashSet<String>();
         resources.add("foo");
         resources.add("/WEB-INF/classes/petra.composite");
