5b0b3ceb33ce4f702e4cf9f0e3b4ce62919dc9be
Zhaohui Feng <rfeng@apache.org>
2007-05-10 06:42:23 +0000

    Convert the test case to JUnit to work around the OutOfMemoryError
    git-svn-id: https://svn.apache.org/repos/asf/incubator/tuscany/java/sca@536756 13f79535-47bb-0310-9956-ffa450edef68

--- itest/wsdl/src/test/java/org/apache/tuscany/sca/itest/WSDLTestCase.java
+++ itest/wsdl/src/test/java/org/apache/tuscany/sca/itest/WSDLTestCase.java
@@ -19,10 +19,13 @@
 
 package org.apache.tuscany.sca.itest;
 
+import static junit.framework.Assert.assertEquals;
 import helloworld.HelloWorldService;
-import junit.framework.TestCase;
 
 import org.apache.tuscany.host.embedded.SCADomain;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
 
 /**
  * Tests all the combinations of wiring services, components, and references
@@ -31,95 +34,113 @@ import org.apache.tuscany.host.embedded.SCADomain;
  * reference (4). Each of those uses either interface.java (a) or interface.wsdl
  * (b). This results in 16 different combinations 1a2a3a4a thru 1b2b3b4b.
  */
-public class WSDLTestCase extends TestCase {
+public class WSDLTestCase {
 
-    private SCADomain domain;
+    private static SCADomain domain;
 
+    @Test
     public void testClient1a2a3a4a() {
         HelloWorldService client = domain.getService(HelloWorldService.class, "Client1a2a3a4a");
         assertEquals("Hi petra", client.getGreetings("petra"));
     }
 
+    @Test
     public void testClient1a2a3a4b() {
         HelloWorldService client = domain.getService(HelloWorldService.class, "Client1a2a3a4b");
         assertEquals("Hi petra", client.getGreetings("petra"));
     }
 
+    @Test
     public void testClient1a2a3b4a() {
         HelloWorldService client = domain.getService(HelloWorldService.class, "Client1a2a3b4a");
         assertEquals("Hi petra", client.getGreetings("petra"));
     }
 
+    @Test
     public void testClient1a2a3b4b() {
         HelloWorldService client = domain.getService(HelloWorldService.class, "Client1a2a3b4b");
         assertEquals("Hi petra", client.getGreetings("petra"));
     }
 
+    @Test
     public void testClient1a2b3a4a() {
         HelloWorldService client = domain.getService(HelloWorldService.class, "Client1a2b3a4a");
         assertEquals("Hi petra", client.getGreetings("petra"));
     }
 
+    @Test
     public void testClient1a2b3a4b() {
         HelloWorldService client = domain.getService(HelloWorldService.class, "Client1a2b3a4b");
         assertEquals("Hi petra", client.getGreetings("petra"));
     }
 
+    @Test
     public void testClient1a2b3b4a() {
         HelloWorldService client = domain.getService(HelloWorldService.class, "Client1a2b3b4a");
         assertEquals("Hi petra", client.getGreetings("petra"));
     }
 
+    @Test
     public void testClient1a2b3b4b() {
         HelloWorldService client = domain.getService(HelloWorldService.class, "Client1a2b3b4b");
         assertEquals("Hi petra", client.getGreetings("petra"));
     }
 
+    @Test
     public void testClient1b2a3a4a() {
         HelloWorldService client = domain.getService(HelloWorldService.class, "Client1b2a3a4a");
         assertEquals("Hi petra", client.getGreetings("petra"));
     }
 
+    @Test
     public void testClient1b2a3a4b() {
         HelloWorldService client = domain.getService(HelloWorldService.class, "Client1b2a3a4b");
         assertEquals("Hi petra", client.getGreetings("petra"));
     }
 
+    @Test
     public void testClient1b2a3b4a() {
         HelloWorldService client = domain.getService(HelloWorldService.class, "Client1b2a3b4a");
         assertEquals("Hi petra", client.getGreetings("petra"));
     }
 
+    @Test
     public void testClient1b2a3b4b() {
         HelloWorldService client = domain.getService(HelloWorldService.class, "Client1b2a3b4b");
         assertEquals("Hi petra", client.getGreetings("petra"));
     }
 
+    @Test
     public void testClient1b2b3a4a() {
         HelloWorldService client = domain.getService(HelloWorldService.class, "Client1b2b3a4a");
         assertEquals("Hi petra", client.getGreetings("petra"));
     }
 
+    @Test
     public void testClient1b2b3a4b() {
         HelloWorldService client = domain.getService(HelloWorldService.class, "Client1b2b3a4b");
         assertEquals("Hi petra", client.getGreetings("petra"));
     }
 
+    @Test
     public void testClient1b2b3b4a() {
         HelloWorldService client = domain.getService(HelloWorldService.class, "Client1b2b3b4a");
         assertEquals("Hi petra", client.getGreetings("petra"));
     }
 
+    @Test
     public void testClient1b2b3b4b() {
         HelloWorldService client = domain.getService(HelloWorldService.class, "Client1b2b3b4b");
         assertEquals("Hi petra", client.getGreetings("petra"));
     }
 
-    protected void setUp() throws Exception {
+    @BeforeClass
+    public static void init() throws Throwable {
         domain = SCADomain.newInstance("WSDLTest.composite");
     }
 
-    protected void tearDown() throws Exception {
+    @AfterClass
+    public static void destroy() throws Exception {
         domain.close();
     }
 }
