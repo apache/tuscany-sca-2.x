be432fc894d8ddfe1eaaf1c42b8e5c23c489f84e
Zhaohui Feng <rfeng@apache.org>
2007-05-12 17:36:15 +0000

    Remove the package for test cases
    git-svn-id: https://svn.apache.org/repos/asf/incubator/tuscany/java/sca@537496 13f79535-47bb-0310-9956-ffa450edef68

--- modules/core-spi/src/test/java/org/apache/tuscany/sca/factory/AssertionTestCase.java
+++ modules/core-spi/src/test/java/org/apache/tuscany/sca/factory/AssertionTestCase.java
@@ -16,20 +16,23 @@
  * specific language governing permissions and limitations
  * under the License.    
  */
-package org.apache.tuscany.sca.spi;
-
-import org.apache.tuscany.sca.factory.SingletonObjectFactory;
+package org.apache.tuscany.sca.factory;
 
 import junit.framework.TestCase;
 
 /**
  * @version $Rev$ $Date$
  */
-public class SingletonObjectFactoryTestCase extends TestCase {
-
-    public void testSingleton() throws Exception {
-        Object o = new Object();
-        SingletonObjectFactory<Object> factory = new SingletonObjectFactory<Object>(o);
-        assertEquals(o, factory.getInstance());
+public class AssertionTestCase extends TestCase {
+    /**
+     * test case that confirms that JRE assertions are enabled
+     */
+    public void testAssertionsAreEnabled() {
+        try {
+            assert false;
+            fail("assertions are not enabled");
+        } catch (AssertionError e) {
+            // ok
+        }
     }
 }
