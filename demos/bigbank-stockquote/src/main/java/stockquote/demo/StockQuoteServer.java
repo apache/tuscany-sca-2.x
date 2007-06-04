ac947a299de8ba544c6f5bd8ff5a2dfb5f64b4c2
Jean Sebastien Delfino <jsdelfino@apache.org>
2007-06-04 05:44:50 +0000

    Simplified initialization of the SCA domains. Removed unused class.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/tuscany/java/sca@544059 13f79535-47bb-0310-9956-ffa450edef68

--- demos/bigbank-stockquote/src/main/java/stockquote/demo/StockQuoteServer.java
+++ demos/bigbank-stockquote/src/main/java/stockquote/demo/StockQuoteServer.java
@@ -30,7 +30,7 @@ public class StockQuoteServer {
 
         System.out.println("Starting the Sample SCA StockQuote Service...");
 
-        SCADomain domain = SCADomain.newInstance("http://localhost:8081", "/", "StockQuote.composite");
+        SCADomain domain = SCADomain.newInstance("StockQuote.composite");
 
         System.out.println("Press Enter to Exit...");
         System.in.read();
