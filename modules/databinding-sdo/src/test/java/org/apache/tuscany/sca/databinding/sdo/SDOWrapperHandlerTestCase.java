152d46b5ee6e97591dc5ac85eb18c6a902e807a1
Zhaohui Feng <rfeng@apache.org>
2007-06-05 18:25:01 +0000

    Replace deprecated SDOUtil and XMLStreamHelper
    git-svn-id: https://svn.apache.org/repos/asf/incubator/tuscany/java/sca@544585 13f79535-47bb-0310-9956-ffa450edef68

--- modules/databinding-sdo/src/test/java/org/apache/tuscany/sca/databinding/sdo/SDOWrapperHandlerTestCase.java
+++ modules/databinding-sdo/src/test/java/org/apache/tuscany/sca/databinding/sdo/SDOWrapperHandlerTestCase.java
@@ -22,7 +22,7 @@ import java.util.List;
 
 import junit.framework.TestCase;
 
-import org.apache.tuscany.sdo.util.SDOUtil;
+import org.apache.tuscany.sdo.api.SDOUtil;
 
 import commonj.sdo.helper.HelperContext;
 import commonj.sdo.helper.XMLDocument;
