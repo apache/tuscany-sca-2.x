ac8f4ff3d248f9c7aa53143aca9ed68e23a6a016
Zhaohui Feng <rfeng@apache.org>
2007-06-06 02:18:19 +0000

    Port databinding-xmlbeans to trunk
    git-svn-id: https://svn.apache.org/repos/asf/incubator/tuscany/java/sca@544709 13f79535-47bb-0310-9956-ffa450edef68

---
+++ modules/databinding-xmlbeans/src/main/java/org/apache/tuscany/databinding/xmlbeans/Node2XmlObject.java
@@ -0,0 +1,53 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.    
+ */
+package org.apache.tuscany.databinding.xmlbeans;
+
+import org.apache.tuscany.sca.databinding.PullTransformer;
+import org.apache.tuscany.sca.databinding.TransformationContext;
+import org.apache.tuscany.sca.databinding.TransformationException;
+import org.apache.tuscany.sca.databinding.impl.BaseTransformer;
+import org.apache.xmlbeans.XmlException;
+import org.apache.xmlbeans.XmlObject;
+import org.w3c.dom.Node;
+
+public class Node2XmlObject extends BaseTransformer<Node, XmlObject> implements
+    PullTransformer<Node, XmlObject> {
+    // private XmlOptions options;
+
+    public XmlObject transform(Node source, TransformationContext context) {
+        try {
+            return XmlObject.Factory.parse(source);
+        } catch (XmlException e) {
+            throw new TransformationException(e);
+        }
+    }
+
+    public Class getTargetType() {
+        return XmlObject.class;
+    }
+
+    public Class getSourceType() {
+        return Node.class;
+    }
+
+    public int getWeight() {
+        return 30;
+    }
+
+}
---
+++ modules/databinding-xmlbeans/src/main/java/org/apache/tuscany/databinding/xmlbeans/XMLStreamReader2XmlObject.java
@@ -0,0 +1,53 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.    
+ */
+package org.apache.tuscany.databinding.xmlbeans;
+
+import javax.xml.stream.XMLStreamReader;
+
+import org.apache.tuscany.sca.databinding.PullTransformer;
+import org.apache.tuscany.sca.databinding.TransformationContext;
+import org.apache.tuscany.sca.databinding.TransformationException;
+import org.apache.tuscany.sca.databinding.impl.BaseTransformer;
+import org.apache.xmlbeans.XmlException;
+import org.apache.xmlbeans.XmlObject;
+
+public class XMLStreamReader2XmlObject extends BaseTransformer<XMLStreamReader, XmlObject> implements PullTransformer<XMLStreamReader, XmlObject> {
+    // private XmlOptions options;
+
+    public XmlObject transform(XMLStreamReader source, TransformationContext context) {
+        try {
+            return XmlObject.Factory.parse(source);
+        } catch (XmlException e) {
+            throw new TransformationException(e);
+        }
+    }
+
+    public Class getTargetType() {
+        return XmlObject.class;
+    }
+
+    public Class getSourceType() {
+        return XMLStreamReader.class;
+    }
+
+    public int getWeight() {
+        return 10;
+    }
+
+}
