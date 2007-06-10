6d17b3f68761833aa552cb5c899c69217b425fc4
Anthony Elder <antelder@apache.org>
2007-06-10 09:43:24 +0000

    Binding ejb, get test working from mvn without requireing the eclipse config
    git-svn-id: https://svn.apache.org/repos/asf/incubator/tuscany/java/sca@545861 13f79535-47bb-0310-9956-ffa450edef68

--- modules/binding-ejb/src/test/java/org/apache/tuscany/sca/binding/ejb/tests/EJBReferenceTestCase.java
+++ modules/binding-ejb/src/test/java/org/apache/tuscany/sca/binding/ejb/tests/EJBReferenceTestCase.java
@@ -1,3 +1,21 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.    
+ */
 package org.apache.tuscany.sca.binding.ejb.tests;
 
 import junit.framework.TestCase;
@@ -7,13 +25,16 @@ import org.apache.tuscany.sca.host.embedded.SCADomain;
 import account.Customer;
 
 /**
- * This shows how to test the Calculator service component.
+ * Invokes the component which calls the reference using the EJB binding 
  */
 public class EJBReferenceTestCase extends TestCase {
 
     private SCADomain scaDomain;
 
     protected void setUp() throws Exception {
+        System.setProperty("managed", "false");
+        System.setProperty("java.naming.factory.initial", "org.openejb.client.RemoteInitialContextFactory");
+        System.setProperty("java.naming.provider.url", "localhost:4201");
         scaDomain = SCADomain.newInstance("account/account.composite");
     }
 
@@ -23,9 +44,10 @@ public class EJBReferenceTestCase extends TestCase {
 
     public void testCalculator() throws Exception {
         Customer customer = scaDomain.getService(Customer.class, "CustomerComponent");
-        String accountNo = "1234567890"; // This is one of the customer numbers in bank application running on
-                                            // Geronimo
+        // This is one of the customer numbers in bank application running on Geronimo
+        String accountNo = "1234567890"; 
         Double balance = customer.depositAmount(accountNo, new Double(100));
         System.out.println("Balance amount for account " + accountNo + " is $" + balance);
+        assertEquals(1105.35, balance);
     }
 }
