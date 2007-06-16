664964ec75ba4436dd1160b8300eb23e35d7b099
Anthony Elder <antelder@apache.org>
2007-06-16 07:46:18 +0000

    Move json binding to binding spi
    git-svn-id: https://svn.apache.org/repos/asf/incubator/tuscany/java/sca@547880 13f79535-47bb-0310-9956-ffa450edef68

--- modules/binding-jsonrpc/src/main/java/org/apache/tuscany/sca/binding/jsonrpc/ScaDomainScriptServlet.java
+++ modules/binding-jsonrpc/src/main/java/org/apache/tuscany/sca/binding/jsonrpc/ScaDomainScriptServlet.java
@@ -29,7 +29,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import static org.apache.tuscany.sca.binding.jsonrpc.JSONRPCServiceBindingProvider.SERVICE_PREFIX;
+import static org.apache.tuscany.sca.binding.jsonrpc.JSONRPCServiceListener.SERVICE_PREFIX;
 
 /**
  * Servlet to handle requests for the scaDomain.js script.
