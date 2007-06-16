5f8cb1a0daedf31113f4aa0f366a1d60efe27bff
Anthony Elder <antelder@apache.org>
2007-06-16 12:51:03 +0000

    Add a bit of javadoc
    git-svn-id: https://svn.apache.org/repos/asf/incubator/tuscany/java/sca@547916 13f79535-47bb-0310-9956-ffa450edef68

--- modules/extension-helper/src/main/java/org/apache/tuscany/sca/spi/ImplementationActivator.java
+++ modules/extension-helper/src/main/java/org/apache/tuscany/sca/spi/ImplementationActivator.java
@@ -22,6 +22,38 @@ package org.apache.tuscany.sca.spi;
 import org.apache.tuscany.sca.assembly.ComponentType;
 import org.apache.tuscany.sca.runtime.RuntimeComponent;
 
+/**
+ * An ImplementationActivator adds an SCA implementation type to the Tuscany runtime.
+ * 
+ * The SCDL XML used for the implementation is derived from the name of the
+ * class returned from the getImplementationClass method - the package name and
+ * any trailing "Implementation" string is removed, leading upper case characters
+ * are converted to lowercase, and the suffix "implementation." is added. For 
+ * example if getImplementationClass returns a class named "mypkg.FooImplementation"
+ * then the SCDL for the implementation will be <implementation.foo>.
+ * 
+ * Attributes of the <implementation.foo> SCDL are based on the getters/setters of
+ * the Implementation class. So if FooImplementation had getBar/setBar then there
+ * would be an attribute name 'bar', for example, <implementation.foo bar="xxx">. 
+ * 
+ * BindingActivator implementations may use constructor arguments to have 
+ * Tuscany ExtensionPointRegistry objects passed in on their constructor.
+ * For example:
+ * 
+ *    public class MyImplementationActivator implements ImplementationActivator {
+ *       ServletHost servletHost;
+ *       public MyImplementationActivator(ServletHost servletHost) {
+ *          this.servletHost = servletHost;
+ *       }
+ *       ...
+ *    }
+ *    
+ * ImplementationActivator implementations are discovered by the Tuscany runtime
+ * using the J2SE jar file extensions for service provider discovery. All
+ * that means is packaging the new binding type in a jar which contains a  
+ * file META-INF/services/org.apache.tuscany.sca.spi.ImplementationActivator and
+ * that file lists the ImplementationActivator implementation class name.
+ */
 public interface ImplementationActivator<T> {
 
     Class<T> getImplementationClass();
