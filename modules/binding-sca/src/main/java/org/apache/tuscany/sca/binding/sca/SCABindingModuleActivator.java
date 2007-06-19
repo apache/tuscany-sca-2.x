46bef7db74a8b4006af9682c410725f2b23248f9
Simon Laws <slaws@apache.org>
2007-06-19 15:58:39 +0000

    A new implementation of the sca binding to handle the case where components are distributed across nodes TUSCANY-1338
    git-svn-id: https://svn.apache.org/repos/asf/incubator/tuscany/java/sca@548776 13f79535-47bb-0310-9956-ffa450edef68

---
+++ modules/binding-sca/src/main/java/org/apache/tuscany/sca/binding/sca/SCABindingModuleActivator.java
@@ -0,0 +1,68 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.    
+ */
+
+package org.apache.tuscany.sca.binding.sca;
+
+
+
+import org.apache.tuscany.sca.assembly.SCABindingFactory;
+
+import org.apache.tuscany.sca.core.ExtensionPointRegistry;
+import org.apache.tuscany.sca.core.ModelFactoryExtensionPoint;
+import org.apache.tuscany.sca.core.ModuleActivator;
+import org.apache.tuscany.sca.distributed.assembly.DistributedSCABinding;
+import org.apache.tuscany.sca.distributed.host.SCADomainNode;
+import org.apache.tuscany.sca.provider.BindingProviderFactory;
+import org.apache.tuscany.sca.provider.ProviderFactoryExtensionPoint;
+
+/**
+ * A module activator for the JMS binding extension.
+ *
+ * @version $Rev$ $Date$
+ */
+public class SCABindingModuleActivator implements ModuleActivator {
+    
+    public Object[] getExtensionPoints() {
+        // No extensionPoints being contributed here
+        return null;
+    }
+
+    public void start(ExtensionPointRegistry registry) {
+        
+        // get the local domain from the extension registry
+        SCADomainNode domainNode = registry.getExtensionPoint(SCADomainNode.class);
+        
+        // Create the SCA binding model factory
+        SCABindingFactory bindingFactory = new SCABindingFactoryImpl(domainNode, registry);
+        
+        // add binding gactory to the factories list
+        ModelFactoryExtensionPoint factories = registry.getExtensionPoint(ModelFactoryExtensionPoint.class);
+        factories.addFactory(bindingFactory);
+
+        // Add the SCABindingProviderFactory extension
+        BindingProviderFactory<DistributedSCABinding> providerFactory = new SCABindingProviderFactoryImpl(domainNode, registry);
+        
+        ProviderFactoryExtensionPoint providerFactories = registry.getExtensionPoint(ProviderFactoryExtensionPoint.class);
+        providerFactories.addProviderFactory(providerFactory);
+    }
+
+    public void stop(ExtensionPointRegistry registry) {        
+    }
+
+}
---
+++ modules/binding-sca/src/main/java/org/apache/tuscany/sca/binding/sca/SCABindingServiceBindingProvider.java
@@ -0,0 +1,131 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.    
+ */
+
+package org.apache.tuscany.sca.binding.sca;
+
+
+
+import org.apache.tuscany.sca.binding.jms.JMSBinding;
+import org.apache.tuscany.sca.binding.jms.JMSBindingConstants;
+import org.apache.tuscany.sca.binding.jms.JMSBindingFactory;
+import org.apache.tuscany.sca.binding.jms.JMSBindingFactoryImpl;
+import org.apache.tuscany.sca.binding.jms.JMSBindingProviderFactory;
+import org.apache.tuscany.sca.binding.jms.JMSBindingServiceBindingProvider;
+import org.apache.tuscany.sca.core.ExtensionPointRegistry;
+import org.apache.tuscany.sca.distributed.assembly.DistributedSCABinding;
+import org.apache.tuscany.sca.distributed.host.SCADomainNode;
+import org.apache.tuscany.sca.distributed.node.ComponentRegistry;
+import org.apache.tuscany.sca.interfacedef.InterfaceContract;
+import org.apache.tuscany.sca.provider.ProviderFactoryExtensionPoint;
+import org.apache.tuscany.sca.provider.ServiceBindingProvider;
+import org.apache.tuscany.sca.runtime.RuntimeComponent;
+import org.apache.tuscany.sca.runtime.RuntimeComponentService;
+
+/**
+ * Implementation of the JMS service binding provider.
+ * 
+ * @version $Rev$ $Date$
+ */
+public class SCABindingServiceBindingProvider implements ServiceBindingProvider {
+
+    ExtensionPointRegistry registry;      
+    SCADomainNode domainNode;
+    private RuntimeComponent component;
+    private RuntimeComponentService service;
+    private DistributedSCABinding binding;
+    private ComponentRegistry componentRegistry;
+    private JMSBinding jmsBinding;
+    private JMSBindingServiceBindingProvider jmsServiceProvider = null;    
+  
+    public SCABindingServiceBindingProvider(ExtensionPointRegistry registry,
+                                            SCADomainNode domainNode,
+                                            RuntimeComponent component,
+                                            RuntimeComponentService service,
+                                            DistributedSCABinding binding) {
+        this.registry  = registry;
+        this.domainNode= domainNode;        
+        this.component = component;
+        this.service   = service;
+        this.binding   = binding;
+        
+        /*  All of this has been replaced by the code to create the 
+           JMS binding in SCABindingImpl. 
+
+        
+        // if the domain node is available find the component registry 
+        if (domainNode != null) {
+            // get the ComponentRegistry
+            componentRegistry = domainNode.getNodeService(ComponentRegistry.class, "ComponentRegistry");
+        }         
+        
+        // if this SCA binding crosses a node boundary fire up an alternative
+        // binding that supports this
+        if (this.binding.getIsDistributed()) {
+            // first invent a model on the fly
+            JMSBindingFactory jmsBindingFactory = new JMSBindingFactoryImpl();
+            jmsBinding = jmsBindingFactory.createJMSBinding();  
+          
+            // it's at this point with a point to point binding we 
+            // would go and get the base URLs etc. I'm just using the
+            // node name here as JMS doesn't really need an endpoint
+            
+            // get the service information
+            
+            String serviceName = service.getName();            
+            String serviceNode = componentRegistry.getComponentNode(component.getName());
+            
+          
+            // set the destination queue to the target service name
+            jmsBinding.setDestinationName(serviceNode + 
+                                          "." +
+                                          component.getName() + 
+                                          "." + 
+                                          serviceName);
+            jmsBinding.setDestinationCreate(JMSBindingConstants.CREATE_ALLWAYS);            
+            
+         
+            // create the reference provider based on this binding
+            ProviderFactoryExtensionPoint providerFactories = registry.getExtensionPoint(ProviderFactoryExtensionPoint.class);
+            JMSBindingProviderFactory providerFactory = 
+               (JMSBindingProviderFactory)providerFactories.getProviderFactory(JMSBinding.class);
+         
+            jmsServiceProvider = 
+               (JMSBindingServiceBindingProvider) providerFactory.createServiceBindingProvider(component, service, jmsBinding); 
+        }   
+        
+           */
+                  
+    }
+
+    public InterfaceContract getBindingInterfaceContract() {
+        return service.getInterfaceContract();
+    }
+
+    public void start() {
+        if (jmsServiceProvider != null) {
+            jmsServiceProvider.start();
+        }
+    }
+
+    public void stop() {
+        if (jmsServiceProvider != null) {
+            jmsServiceProvider.stop();
+        }
+    }
+}
