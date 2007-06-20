9a793ee517e2a21d7fb0051b14c0d3b1f53a2885
Anthony Elder <antelder@apache.org>
2007-06-20 15:28:00 +0000

    TUSCANY-1363, add osgi sample conributed by Rajini Sivaram
    git-svn-id: https://svn.apache.org/repos/asf/incubator/tuscany/java/sca@549133 13f79535-47bb-0310-9956-ffa450edef68

---
+++ samples/osgi-supplychain/src/main/java/supplychain/retailer/JavaRetailerComponentImpl.java
@@ -0,0 +1,54 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.    
+ */
+package supplychain.retailer;
+
+
+import org.osoa.sca.annotations.Reference;
+import org.osoa.sca.annotations.Scope;
+import org.osoa.sca.annotations.Service;
+
+import supplychain.warehouse.Warehouse;
+
+/**
+ * This class implements the Retailer service component (POJO implementation).
+ */
+@Service(Retailer.class)
+@Scope("STATELESS")
+public class JavaRetailerComponentImpl implements Retailer {
+    
+    private Warehouse warehouse;
+    
+    
+    @Reference
+    public void setWarehouse(Warehouse warehouse) {
+        this.warehouse = warehouse;
+    }
+    
+       
+    public void submitOrder(String order) {
+    	
+    	System.out.println("Retailer.submitOrder, warehouse is " + warehouse);
+        warehouse.fulfillOrder(order + ", submitted");
+        
+    }
+
+    
+    
+   
+}
