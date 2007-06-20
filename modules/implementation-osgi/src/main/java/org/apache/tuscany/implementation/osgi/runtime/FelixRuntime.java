602d82e9cffead2d145433c0c57729346fffcf49
Anthony Elder <antelder@apache.org>
2007-06-20 14:58:18 +0000

    TUSCANY-1363, implementation-osgi contributed by Rajini Sivaram
    git-svn-id: https://svn.apache.org/repos/asf/incubator/tuscany/java/sca@549122 13f79535-47bb-0310-9956-ffa450edef68

---
+++ modules/implementation-osgi/src/main/java/org/apache/tuscany/implementation/osgi/runtime/FelixRuntime.java
@@ -0,0 +1,146 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.    
+ */
+package org.apache.tuscany.implementation.osgi.runtime;
+
+import java.io.File;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+public class FelixRuntime extends OSGiRuntime implements BundleActivator {
+    
+    private static BundleContext bundleContext;
+    
+    private static FelixRuntime instance;
+    
+    private static Class felixClass;
+    private static Object felix;
+    
+    public static OSGiRuntime getInstance() {
+        if (instance == null)
+            instance = new FelixRuntime();
+        return instance;
+    }
+    
+    
+    private static void deleteDirectory(File dir) {
+        
+        File[] files = dir.listFiles();
+        for (int i = 0; i < files.length; i++) {
+            if (files[i].isDirectory())
+                deleteDirectory(files[i]);
+            else
+                files[i].delete();
+        }
+        dir.delete();
+        
+    }
+    
+    protected BundleContext startRuntime() throws Exception {
+        
+        if (bundleContext != null)
+            return bundleContext;
+        
+               
+        ClassLoader cl = FelixRuntime.class.getClassLoader();
+        
+        Class felixMainClass = cl.loadClass("org.apache.felix.main.Main");
+        felixClass = cl.loadClass("org.apache.felix.framework.Felix");
+        Class propertyResolverClass = cl.loadClass("org.apache.felix.framework.util.MutablePropertyResolver");
+        Class propertyResolverImplClass = cl.loadClass("org.apache.felix.framework.util.MutablePropertyResolverImpl");
+        
+        Method propsMethod = felixMainClass.getMethod("loadConfigProperties");
+        Properties props = (Properties)propsMethod.invoke(null);
+        
+        File profileDir = new File(".felix"); 
+        if (profileDir.isDirectory()) 
+            deleteDirectory(profileDir);
+        else
+            profileDir.delete();
+        profileDir.mkdir();
+        profileDir.deleteOnExit();
+        
+        props.put("felix.cache.profiledir", profileDir.getAbsolutePath());
+        props.put("felix.embedded.execution", "true");
+        props.put("org.osgi.framework.system.packages", 
+                "org.osgi.framework; version=1.3.0," +
+                "org.osgi.service.packageadmin; version=1.2.0, " +
+                "org.osgi.service.startlevel; version=1.0.0, " +
+                "org.osgi.service.url; version=1.0.0 ");
+        
+        
+        
+        Constructor implConstructor = propertyResolverImplClass.getConstructor(Map.class);
+        Object mutableProps = implConstructor.newInstance(props);
+        
+        felix = felixClass.newInstance();
+        Method startMethod = felixClass.getMethod("start", propertyResolverClass, List.class);
+        List<BundleActivator> activators = new ArrayList<BundleActivator>();
+        BundleActivator activator = new FelixRuntime();
+        activators.add(activator);
+        startMethod.invoke(felix, mutableProps, activators);
+        
+        synchronized (activator) {
+            int retries = 0;
+            while (bundleContext == null && retries++ < 10) {
+                activator.wait(1000);
+            }
+        }
+        
+        return bundleContext;
+        
+    }
+
+    public void start(BundleContext context) throws Exception {
+        
+        bundleContext = context;
+        synchronized (this) {
+            this.notify();
+        }
+    }
+
+    public void stop(BundleContext context) throws Exception {
+        bundleContext = null;
+    }
+    
+    
+    @Override
+    protected void shutdownRuntime() throws Exception {
+
+        if (bundleContext == null)
+            return;
+        bundleContext = null;
+        if (felix != null) {
+            Method shutdownMethod = felixClass.getMethod("shutdown");
+            try {
+                shutdownMethod.invoke(felix);
+            } catch (Exception e) {
+                // Ignore errors
+            }            
+            felix = null;
+        }
+    }
+
+}
