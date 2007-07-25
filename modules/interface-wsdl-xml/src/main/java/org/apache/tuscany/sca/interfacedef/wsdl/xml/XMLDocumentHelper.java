e9c8a44c2807ce59c828e8260398fc3e4bb32e75
Zhaohui Feng <rfeng@apache.org>
2007-07-25 21:54:21 +0000

    Add readTargetNamespace() in XMLDocumentHelper and a test case
    git-svn-id: https://svn.apache.org/repos/asf/incubator/tuscany/java/sca@559627 13f79535-47bb-0310-9956-ffa450edef68

--- modules/interface-wsdl-xml/src/main/java/org/apache/tuscany/sca/interfacedef/wsdl/xml/XMLDocumentHelper.java
+++ modules/interface-wsdl-xml/src/main/java/org/apache/tuscany/sca/interfacedef/wsdl/xml/XMLDocumentHelper.java
@@ -25,6 +25,12 @@ import java.io.InputStream;
 import java.io.UnsupportedEncodingException;
 import java.net.URL;
 
+import javax.xml.namespace.QName;
+import javax.xml.stream.XMLInputFactory;
+import javax.xml.stream.XMLStreamConstants;
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.XMLStreamReader;
+
 import org.apache.ws.commons.schema.resolver.URIResolver;
 import org.xml.sax.InputSource;
 
@@ -32,8 +38,11 @@ import org.xml.sax.InputSource;
  * @version $Rev$ $Date$
  */
 public class XMLDocumentHelper {
+    public static final QName WSDL11 = new QName("http://schemas.xmlsoap.org/wsdl/", "definitions");
+    public static final QName XSD = new QName("http://www.w3.org/2001/XMLSchema", "schema");
+    public static final QName WSDL20 = new QName("http://www.w3.org/ns/wsdl", "description");
 
-   protected static final int BUFFER_SIZE = 256;
+    protected static final int BUFFER_SIZE = 256;
 
     /**
      * Detect the XML encoding of the WSDL document
@@ -45,7 +54,7 @@ public class XMLDocumentHelper {
     public static String getEncoding(InputStream is) throws IOException {
         if (!is.markSupported())
             is = new BufferedInputStream(is);
-    
+
         byte[] buffer = readBuffer(is);
         return getXMLEncoding(buffer);
     }
@@ -55,7 +64,7 @@ public class XMLDocumentHelper {
      */
     protected static String getXMLEncoding(byte[] bytes) {
         String javaEncoding = null;
-    
+
         if (bytes.length >= 4) {
             if (((bytes[0] == -2) && (bytes[1] == -1)) || ((bytes[0] == 0) && (bytes[1] == 60)))
                 javaEncoding = "UnicodeBig";
@@ -64,9 +73,9 @@ public class XMLDocumentHelper {
             else if ((bytes[0] == -17) && (bytes[1] == -69) && (bytes[2] == -65))
                 javaEncoding = "UTF8";
         }
-    
+
         String header = null;
-    
+
         try {
             if (javaEncoding != null)
                 header = new String(bytes, 0, bytes.length, javaEncoding);
@@ -75,25 +84,25 @@ public class XMLDocumentHelper {
         } catch (UnsupportedEncodingException e) {
             return null;
         }
-    
+
         if (!header.startsWith("<?xml"))
             return "UTF-8";
-    
+
         int endOfXMLPI = header.indexOf("?>");
         int encodingIndex = header.indexOf("encoding", 6);
-    
+
         if ((encodingIndex == -1) || (encodingIndex > endOfXMLPI))
             return "UTF-8";
-    
+
         int firstQuoteIndex = header.indexOf("\"", encodingIndex);
         int lastQuoteIndex;
-    
+
         if ((firstQuoteIndex == -1) || (firstQuoteIndex > endOfXMLPI)) {
             firstQuoteIndex = header.indexOf("'", encodingIndex);
             lastQuoteIndex = header.indexOf("'", firstQuoteIndex + 1);
         } else
             lastQuoteIndex = header.indexOf("\"", firstQuoteIndex + 1);
-    
+
         return header.substring(firstQuoteIndex + 1, lastQuoteIndex);
     }
 
@@ -101,25 +110,25 @@ public class XMLDocumentHelper {
         if (is.available() == 0) {
             return new byte[0];
         }
-    
+
         byte[] buffer = new byte[BUFFER_SIZE];
         is.mark(BUFFER_SIZE);
         int bytesRead = is.read(buffer, 0, BUFFER_SIZE);
         int totalBytesRead = bytesRead;
-    
+
         while (bytesRead != -1 && (totalBytesRead < BUFFER_SIZE)) {
             bytesRead = is.read(buffer, totalBytesRead, BUFFER_SIZE - totalBytesRead);
-    
+
             if (bytesRead != -1)
                 totalBytesRead += bytesRead;
         }
-    
+
         if (totalBytesRead < BUFFER_SIZE) {
             byte[] smallerBuffer = new byte[totalBytesRead];
             System.arraycopy(buffer, 0, smallerBuffer, 0, totalBytesRead);
             smallerBuffer = buffer;
         }
-    
+
         is.reset();
         return buffer;
     }
@@ -138,12 +147,39 @@ public class XMLDocumentHelper {
         inputSource.setSystemId(url.toString());
         return inputSource;
     }
-    
+
+    private final static XMLInputFactory inputFactory = XMLInputFactory.newInstance();
+
+    public static String readTargetNamespace(URL doc, QName element, String attribute) throws IOException,
+        XMLStreamException {
+        if (attribute == null) {
+            attribute = "targetNamespace";
+        }
+        InputStream is = doc.openStream();
+        try {
+            XMLStreamReader reader = inputFactory.createXMLStreamReader(is);
+            int eventType = reader.getEventType();
+            while (true) {
+                if (eventType == XMLStreamConstants.START_ELEMENT && element.equals(reader.getName())) {
+                    return reader.getAttributeValue(null, attribute);
+                }
+                if (reader.hasNext()) {
+                    eventType = reader.next();
+                } else {
+                    break;
+                }
+            }
+            return null;
+        } finally {
+            is.close();
+        }
+    }
+
     /**
      * URI resolver implementation for xml schema
      */
     public static class URIResolverImpl implements URIResolver {
-    
+
         public org.xml.sax.InputSource resolveEntity(java.lang.String targetNamespace,
                                                      java.lang.String schemaLocation,
                                                      java.lang.String baseUri) {
@@ -159,5 +195,4 @@ public class XMLDocumentHelper {
         }
     }
 
- 
 }
