a27d0c2aa0a3d8749d49aef956c34adb9653bb4d
Luciano Resende <lresende@apache.org>
2007-07-27 23:29:14 +0000

    [work in progress] Contribution import/export java support based on new extensibility of contribution import/export
    git-svn-id: https://svn.apache.org/repos/asf/incubator/tuscany/java/sca@560420 13f79535-47bb-0310-9956-ffa450edef68

---
+++ modules/contribution-java/src/main/java/org/apache/tuscany/sca/contribution/service/impl/JavaExportMetadataProcessor.java
@@ -0,0 +1,103 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.    
+ */
+
+package org.apache.tuscany.sca.contribution.service.impl;
+
+import static javax.xml.stream.XMLStreamConstants.START_ELEMENT;
+
+import javax.xml.namespace.QName;
+import javax.xml.stream.XMLStreamConstants;
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.XMLStreamReader;
+import javax.xml.stream.XMLStreamWriter;
+
+import org.apache.tuscany.sca.contribution.java.JavaExport;
+import org.apache.tuscany.sca.contribution.java.impl.JavaExportImpl;
+import org.apache.tuscany.sca.contribution.processor.StAXArtifactProcessor;
+import org.apache.tuscany.sca.contribution.resolver.ModelResolver;
+import org.apache.tuscany.sca.contribution.service.ContributionReadException;
+import org.apache.tuscany.sca.contribution.service.ContributionResolveException;
+import org.apache.tuscany.sca.contribution.service.ContributionWriteException;
+
+public class JavaExportMetadataProcessor implements StAXArtifactProcessor<JavaExport> {
+    private static final String SCA10_NS = "http://www.osoa.org/xmlns/sca/1.0";
+    
+    private static final QName EXPORT_JAVA = new QName(SCA10_NS, "export.java");
+    
+    private static final String PACKAGE = "package";
+    
+    public JavaExportMetadataProcessor() {
+        super();
+    }
+
+    public QName getArtifactType() {
+        return EXPORT_JAVA;
+    }
+    
+    public Class<JavaExport> getModelType() {
+        return JavaExport.class;
+    }
+    
+    
+    public JavaExport read(XMLStreamReader reader) throws ContributionReadException, XMLStreamException {
+        JavaExport javaExport = new JavaExportImpl();
+        QName element = null;
+
+        
+        while (reader.hasNext()) {
+            int event = reader.getEventType();
+            switch (event) {
+                case START_ELEMENT:
+                    element = reader.getName();
+                    
+                    if (EXPORT_JAVA.equals(element)) {
+                        String packageName = reader.getAttributeValue(null, PACKAGE);
+                        if (packageName == null) {
+                            throw new ContributionReadException("Attribute 'package' is missing");
+                        }
+                        
+                        javaExport.setPackage(packageName);
+                    }
+                    break;
+                case XMLStreamConstants.END_ELEMENT:
+                    if (EXPORT_JAVA.equals(reader.getName())) {
+                        return javaExport;
+                    }
+                    break;        
+            }
+            
+            //Read the next element
+            if (reader.hasNext()) {
+                reader.next();
+            }
+        }
+        
+        return javaExport;
+    }
+
+    public void write(JavaExport model, XMLStreamWriter outputSource) throws ContributionWriteException, XMLStreamException {
+        // TODO Auto-generated method stub
+        
+    }
+
+    public void resolve(JavaExport model, ModelResolver resolver) throws ContributionResolveException {
+        // TODO Auto-generated method stub
+        
+    }
+}
---
+++ modules/contribution-java/src/test/java/org/apache/tuscany/sca/contribution/java/impl/JavaImportMetadataProcessorTestCase.java
@@ -0,0 +1,60 @@
+package org.apache.tuscany.sca.contribution.java.impl;
+
+
+import java.io.StringReader;
+
+import javax.xml.stream.XMLInputFactory;
+import javax.xml.stream.XMLStreamReader;
+
+import junit.framework.TestCase;
+
+import org.apache.tuscany.sca.contribution.java.JavaImport;
+import org.apache.tuscany.sca.contribution.service.ContributionReadException;
+import org.apache.tuscany.sca.contribution.service.impl.JavaImportMetadataProcessor;
+
+/**
+ * @version $Rev: 538445 $ $Date: 2007-05-15 23:20:37 -0700 (Tue, 15 May 2007) $
+ */
+public class JavaImportMetadataProcessorTestCase extends TestCase {
+
+    private static final String VALID_XML =
+        "<?xml version=\"1.0\" encoding=\"ASCII\"?>" 
+            + "<contribution xmlns=\"http://www.osoa.org/xmlns/sca/1.0\" xmlns:ns=\"http://ns\">"
+            + "<import.java package=\"org.apache.tuscany.sca.contribution.java\" location=\"sca://contributions/001\"/>"
+            + "</contribution>";
+
+    private static final String INVALID_XML =
+        "<?xml version=\"1.0\" encoding=\"ASCII\"?>" 
+            + "<contribution xmlns=\"http://www.osoa.org/xmlns/sca/1.0\" xmlns:ns=\"http://ns\">"
+            + "<import.java location=\"sca://contributions/001\"/>"
+            + "</contribution>";
+
+    private XMLInputFactory xmlFactory;
+
+    protected void setUp() throws Exception {
+        super.setUp();
+        xmlFactory = XMLInputFactory.newInstance();
+    }
+
+    public void testLoad() throws Exception {
+        XMLStreamReader reader = xmlFactory.createXMLStreamReader(new StringReader(VALID_XML));
+
+        JavaImportMetadataProcessor importProcessor = new JavaImportMetadataProcessor();
+        JavaImport javaImport = importProcessor.read(reader);
+        
+        assertEquals("org.apache.tuscany.sca.contribution.java", javaImport.getPackage());
+        assertEquals("sca://contributions/001", javaImport.getLocation());
+    }
+
+    public void testLoadInvalid() throws Exception {
+        XMLStreamReader reader = xmlFactory.createXMLStreamReader(new StringReader(INVALID_XML));
+
+        JavaImportMetadataProcessor importProcessor = new JavaImportMetadataProcessor();
+        try {
+            importProcessor.read(reader);
+            fail("readerException should have been thrown");
+        } catch (ContributionReadException e) {
+            assertTrue(true);
+        }
+    }    
+}
