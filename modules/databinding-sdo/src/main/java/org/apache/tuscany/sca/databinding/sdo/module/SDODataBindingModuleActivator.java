fc9c9eaa355fb607d679405e3939e2c938d340f9
Luciano Resende <lresende@apache.org>
2007-08-01 22:24:21 +0000

    Moving introspection of SDO Static Types to use Listener interface, instead of deprecated PostProcessor interfaces
    git-svn-id: https://svn.apache.org/repos/asf/incubator/tuscany/java/sca@561978 13f79535-47bb-0310-9956-ffa450edef68

--- modules/databinding-sdo/src/main/java/org/apache/tuscany/sca/databinding/sdo/module/SDODataBindingModuleActivator.java
+++ modules/databinding-sdo/src/main/java/org/apache/tuscany/sca/databinding/sdo/module/SDODataBindingModuleActivator.java
@@ -20,8 +20,8 @@
 package org.apache.tuscany.sca.databinding.sdo.module;
 
 import org.apache.tuscany.sca.assembly.AssemblyFactory;
-import org.apache.tuscany.sca.contribution.processor.ContributionPostProcessorExtensionPoint;
 import org.apache.tuscany.sca.contribution.processor.StAXArtifactProcessorExtensionPoint;
+import org.apache.tuscany.sca.contribution.service.ContributionListenerExtensionPoint;
 import org.apache.tuscany.sca.core.ExtensionPointRegistry;
 import org.apache.tuscany.sca.core.ModelFactoryExtensionPoint;
 import org.apache.tuscany.sca.core.ModuleActivator;
@@ -32,7 +32,7 @@ import org.apache.tuscany.sca.databinding.sdo.DataObject2XMLStreamReader;
 import org.apache.tuscany.sca.databinding.sdo.HelperContextProcessor;
 import org.apache.tuscany.sca.databinding.sdo.HelperContextRegistry;
 import org.apache.tuscany.sca.databinding.sdo.HelperContextRegistryImpl;
-import org.apache.tuscany.sca.databinding.sdo.ImportSDOPostProcessor;
+import org.apache.tuscany.sca.databinding.sdo.SDOTypeRegisterListener;
 import org.apache.tuscany.sca.databinding.sdo.ImportSDOProcessor;
 import org.apache.tuscany.sca.databinding.sdo.SDODataBinding;
 import org.apache.tuscany.sca.databinding.sdo.String2DataObject;
@@ -62,8 +62,8 @@ public class SDODataBindingModuleActivator implements ModuleActivator {
         HelperContextRegistry contextRegistry = new HelperContextRegistryImpl();
         processors.addArtifactProcessor(new ImportSDOProcessor(contextRegistry));
 
-        ContributionPostProcessorExtensionPoint postProcessors = registry.getExtensionPoint(ContributionPostProcessorExtensionPoint.class);
-        postProcessors.addPostProcessor(new ImportSDOPostProcessor(contextRegistry));
+        ContributionListenerExtensionPoint listeners = registry.getExtensionPoint(ContributionListenerExtensionPoint.class);
+        listeners.addContributionListener(new SDOTypeRegisterListener(contextRegistry));
         
         TransformerExtensionPoint transformers = registry.getExtensionPoint(TransformerExtensionPoint.class);
         transformers.addTransformer(new DataObject2String());
