64a35590a919e6e9358b742b1e5407c51f712f6d
Zhaohui Feng <rfeng@apache.org>
2007-08-01 15:45:35 +0000

    Add a reference itest suite to verify different variations of declaring SCA references
    git-svn-id: https://svn.apache.org/repos/asf/incubator/tuscany/java/sca@561845 13f79535-47bb-0310-9956-ffa450edef68

---
+++ itest/references/src/main/java/org/apache/tuscany/sca/itest/references/AComponentImpl.java
@@ -0,0 +1,76 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.    
+ */
+package org.apache.tuscany.sca.itest.references;
+
+import org.osoa.sca.annotations.Reference;
+
+public class AComponentImpl implements AComponent {
+
+    @Reference(name = "bReference")
+    protected BComponent b;
+
+    @Reference
+    protected CComponent cReference;
+
+    @Reference(required = false)
+    protected DComponent dReference;
+
+    private DComponent dReference1;
+
+    private DComponent dReference2;
+
+    @Reference(name = "dReference1")
+    public void setDReference(DComponent dReference) {
+        this.dReference1 = dReference;
+    }
+
+    @Reference
+    public void setDReference2(DComponent dReference2) {
+        this.dReference2 = dReference2;
+    }
+
+    public String fooB() {
+        return b.foo();
+    }
+
+    public String fooC() {
+        return cReference.foo();
+    }
+
+    public String fooBC() {
+        return b.fooC();
+    }
+
+    public String fooD() {
+        return dReference1.foo();
+    }
+
+    public String fooD1() {
+        return dReference1.foo();
+    }
+
+    public String fooD2() {
+        return dReference2.foo();
+    }
+
+    public DComponent getDReference() {
+        return dReference;
+    }
+
+}
---
+++ itest/references/src/test/java/org/apache/tuscany/sca/itest/references/InnerReferenceTestCase.java
@@ -0,0 +1,79 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.    
+ */
+package org.apache.tuscany.sca.itest.references;
+
+import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertNull;
+
+import org.apache.tuscany.sca.host.embedded.SCADomain;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class InnerReferenceTestCase {
+    private static SCADomain domain;
+    private static AComponent acomponent;
+
+    @BeforeClass
+    public static void init() throws Exception {
+        domain = SCADomain.newInstance("InnerReferencesTest.composite");
+        acomponent = domain.getService(AComponent.class, "AComponent");
+    }
+
+    @AfterClass
+    public static void destroy() throws Exception {
+        domain.close();
+    }
+
+    @Test
+    public void testBReference() {
+        assertEquals("BComponent", acomponent.fooB());
+    }
+
+    @Test
+    public void testCReference() {
+        assertEquals("CComponent", acomponent.fooC());
+    }
+
+    @Test
+    public void testBCReference() {
+        assertEquals("BCComponent", acomponent.fooBC());
+    }
+
+    @Test
+    public void testD1Reference() {
+        assertEquals("DComponent", acomponent.fooD1());
+    }
+
+    @Test
+    public void testD2Reference() {
+        assertEquals("DComponent", acomponent.fooD2());
+    }
+
+    @Test
+    public void testRequiredFalseReference() {
+        assertNull(acomponent.getDReference());
+    }
+
+    public static void main(String[] args) throws Exception {
+        InnerReferenceTestCase.init();
+        InnerReferenceTestCase tester = new InnerReferenceTestCase();
+        InnerReferenceTestCase.destroy();
+    }
+}
