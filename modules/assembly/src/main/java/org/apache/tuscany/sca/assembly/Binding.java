c0101a408e64db24039773bef63936edfde99a95
Zhaohui Feng <rfeng@apache.org>
2007-08-18 03:47:14 +0000

    Remove unused imports
    git-svn-id: https://svn.apache.org/repos/asf/incubator/tuscany/java/sca@567222 13f79535-47bb-0310-9956-ffa450edef68

--- modules/assembly/src/main/java/org/apache/tuscany/sca/assembly/Binding.java
+++ modules/assembly/src/main/java/org/apache/tuscany/sca/assembly/Binding.java
@@ -18,12 +18,6 @@
  */
 package org.apache.tuscany.sca.assembly;
 
-import java.util.List;
-
-import org.apache.tuscany.sca.policy.Intent;
-import org.apache.tuscany.sca.policy.IntentAttachPoint;
-import org.apache.tuscany.sca.policy.PolicySet;
-import org.apache.tuscany.sca.policy.PolicySetAttachPoint;
 
 /**
  * Represents a binding.
