5a56f49918842126975cad26097f0ac137336f24
Ignacio Silva Lepe <isilval@apache.org>
2007-09-12 17:38:42 +0000

    Add test case
    git-svn-id: https://svn.apache.org/repos/asf/incubator/tuscany/java/sca@575022 13f79535-47bb-0310-9956-ffa450edef68

---
+++ samples/binding-notification-consumer/src/test/java/notification/consumer/TrafficAdvisoryTestCase.java
@@ -0,0 +1,61 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.    
+ */
+package notification.consumer;
+
+import org.apache.tuscany.sca.host.embedded.SCADomain;
+
+import junit.framework.TestCase;
+
+public class TrafficAdvisoryTestCase extends TestCase {
+
+    private SCADomain domain;
+    
+    public void testTrafficAdvisoryNotification() throws Exception {
+        System.out.println("Only instantiating and closing domain ...");
+
+        /* Uncomment to test with producer
+        try {
+            System.out.println("TrafficAdvisoryServer, hit return to end");
+            System.in.read();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        */
+    }
+
+    @Override
+    protected void setUp() throws Exception {
+        try {
+            domain = SCADomain.newInstance("TrafficAdvisoryNotification.composite");
+        } catch(Throwable e) {
+            e.printStackTrace();
+            if (e instanceof Exception) {
+                throw (Exception)e;
+            }
+            else {
+                throw new Exception(e);
+            }
+        }
+    }
+    
+    @Override
+    protected void tearDown() throws Exception {
+        domain.close();
+    }
+}
