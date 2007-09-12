d91d676783d638a766db53731141bb7483a8e01d
Ignacio Silva Lepe <isilval@apache.org>
2007-09-12 17:37:37 +0000

    Update test case
    git-svn-id: https://svn.apache.org/repos/asf/incubator/tuscany/java/sca@575021 13f79535-47bb-0310-9956-ffa450edef68

--- samples/binding-notification-producer/src/test/java/notification/producer/TrafficAdvisoryTestCase.java
+++ samples/binding-notification-producer/src/test/java/notification/producer/TrafficAdvisoryTestCase.java
@@ -18,6 +18,9 @@
  */
 package notification.producer;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 import notification.producer.TestCaseProducer;
 
 import org.apache.tuscany.sca.host.embedded.SCADomain;
@@ -31,13 +34,32 @@ public class TrafficAdvisoryTestCase extends TestCase {
     
     public void testTrafficAdvisoryNotification() throws Exception {
         System.out.println("Only instantiating and closing domain ...");
+        TestCaseProducer testCaseProducer = domain.getService(TestCaseProducer.class, "TrafficAdvisoryProducer");
+
+        /* Uncomment to test with consumer
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String value = "foo";
+        do {
+            try {
+                System.out.println("Send a report value, ^C or <end> to end");
+                value = reader.readLine();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            if(value == null || value.equals("end")) {
+                break;
+            }
+            testCaseProducer.produceTrafficNotification("Report value [" + value + "]");
+            value = "end";
+        }
+        while(true);
+        */
     }
 
     @Override
     protected void setUp() throws Exception {
         try {
             domain = SCADomain.newInstance("TrafficAdvisoryNotification.composite");
-            domain.getService(TestCaseProducer.class, "TrafficAdvisoryProducer");
         } catch(Throwable e) {
             e.printStackTrace();
             if (e instanceof Exception) {
