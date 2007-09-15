3e13beafba1d7d2e660dc6d324428aca4e6d7c98
Luciano Resende <lresende@apache.org>
2007-09-15 00:35:28 +0000

    Fixing HelloWorld case issues, as it was breaking linux build
    git-svn-id: https://svn.apache.org/repos/asf/incubator/tuscany/java/sca@575834 13f79535-47bb-0310-9956-ffa450edef68

--- modules/implementation-bpel/src/test/java/org/apache/tuscany/sca/implementation/bpel/BPELDocumentProcessorTestCase.java
+++ modules/implementation-bpel/src/test/java/org/apache/tuscany/sca/implementation/bpel/BPELDocumentProcessorTestCase.java
@@ -6,15 +6,15 @@
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
  * with the License.  You may obtain a copy of the License at
- * 
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
  * KIND, either express or implied.  See the License for the
  * specific language governing permissions and limitations
- * under the License.    
+ * under the License.
  */
 
 package org.apache.tuscany.sca.implementation.bpel;
@@ -36,30 +36,30 @@ import org.apache.tuscany.sca.interfacedef.wsdl.DefaultWSDLFactory;
  * @version $Rev$ $Date$
  */
 public class BPELDocumentProcessorTestCase extends TestCase {
-    
-    protected static final String BPEL_PROCESS_FILE = "HelloWorld.bpel";
+
+    protected static final String BPEL_PROCESS_FILE = "helloworld.bpel";
 
     private ModelFactoryExtensionPoint modelFactories;
 
     @Override
     protected void setUp() throws Exception {
         super.setUp();
-       
+
         modelFactories = new DefaultModelFactoryExtensionPoint();
         modelFactories.addFactory(new DefaultAssemblyFactory());
         modelFactories.addFactory(new DefaultWSDLFactory());
-        
+
         BPELFactory bpelFactory = new DefaultBPELFactory(modelFactories);
         modelFactories.addFactory(bpelFactory);
     }
 
     public void testLoadBPELProcessDefinition() throws Exception {
         BPELDocumentProcessor bpelDocumentProcessor = new BPELDocumentProcessor(modelFactories);
-        
+
         URI processURI = getClass().getClassLoader().getResource(BPEL_PROCESS_FILE).toURI();
         URL processLocation = getClass().getClassLoader().getResource(BPEL_PROCESS_FILE);
         BPELProcessDefinition bpelProcessDefinition = bpelDocumentProcessor.read(null, processURI, processLocation);
-        
+
         assertNotNull(bpelProcessDefinition);
         assertEquals(new QName("http://tuscany.apache.org/implementation/bpel/example/helloworld", "HelloWorld"), bpelProcessDefinition.getName());
         assertEquals(processLocation, bpelProcessDefinition.getLocation());
