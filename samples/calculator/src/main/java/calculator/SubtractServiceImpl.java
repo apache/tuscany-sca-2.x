b98c12b4bc4cba076e92eb27c6acbc12656329d0
S Venkatakrishnan <svkrish@apache.org>
2007-10-12 06:29:08 +0000

    Adding log messages into component implementations to demonstrate logging intent
    git-svn-id: https://svn.apache.org/repos/asf/incubator/tuscany/java/sca@584064 13f79535-47bb-0310-9956-ffa450edef68

--- samples/calculator/src/main/java/calculator/SubtractServiceImpl.java
+++ samples/calculator/src/main/java/calculator/SubtractServiceImpl.java
@@ -18,12 +18,17 @@
  */
 package calculator;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  * An implementation of the subtract service.
  */
 public class SubtractServiceImpl implements SubtractService {
 
     public double subtract(double n1, double n2) {
+        Logger logger = Logger.getLogger("calculator");
+        logger.log(Level.FINEST, "Subtracting " + n1 + " from " + n2);
         return n1 - n2;
     }
 
