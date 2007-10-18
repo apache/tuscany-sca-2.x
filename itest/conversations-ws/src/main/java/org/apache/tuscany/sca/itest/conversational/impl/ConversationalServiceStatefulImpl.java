ac7bef78c3cc851a7dea439df1d5d36bb55bc931
Simon Laws <slaws@apache.org>
2007-10-18 14:53:09 +0000

    TUSCANY-1857 Start of a test to run conversations over the web services binding. This is a straight copy of the conversation itest as quite a few strange things fails a the moment. Ultimately this can be merged back into the conversations itest so that we don;t have lots of duplicate code.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/tuscany/java/sca@585993 13f79535-47bb-0310-9956-ffa450edef68

---
+++ itest/conversations-ws/src/main/java/org/apache/tuscany/sca/itest/conversational/impl/ConversationalServiceStatefulImpl.java
@@ -0,0 +1,117 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.    
+ */
+package org.apache.tuscany.sca.itest.conversational.impl;
+
+import org.apache.tuscany.sca.itest.conversational.ConversationalCallback;
+import org.apache.tuscany.sca.itest.conversational.ConversationalService;
+import org.osoa.sca.annotations.Callback;
+import org.osoa.sca.annotations.ConversationAttributes;
+import org.osoa.sca.annotations.ConversationID;
+import org.osoa.sca.annotations.Destroy;
+import org.osoa.sca.annotations.Init;
+import org.osoa.sca.annotations.Scope;
+import org.osoa.sca.annotations.Service;
+
+
+/**
+ * The service used when testing stateful conversations
+ *
+ * @version $Rev: 537240 $ $Date: 2007-05-11 18:35:03 +0100 (Fri, 11 May 2007) $
+ */
+@Service(ConversationalService.class)
+@Scope("CONVERSATION")
+@ConversationAttributes(maxAge="10 minutes",
+                        maxIdleTime="5 minutes",
+                        singlePrincipal=false)
+public class ConversationalServiceStatefulImpl implements ConversationalService {
+
+    @ConversationID
+    protected String conversationId;
+    
+    @Callback
+    protected ConversationalCallback conversationalCallback; 
+    
+    // local count - accumulates during the conversation
+    private int count = 0;
+          
+    // a static member variable that records the number of times this service is called
+    public static StringBuffer calls = new StringBuffer();
+   
+    @Init
+    public void init(){
+        calls.append("init,");
+    }
+    
+    @Destroy
+    public void destroy(){
+        calls.append("destroy,");
+    }
+    
+    public void initializeCount(int count){
+        calls.append("initializeCount,");       
+        this.count = count;
+    }
+    
+    public void incrementCount(){
+        calls.append("incrementCount,");        
+        count++;
+    }
+    
+    public int retrieveCount(){
+        calls.append("retrieveCount,"); 
+        return count;
+    }
+    
+    public void businessException() throws Exception {
+        throw new Exception("Business Exception");
+    }     
+    
+    public void initializeCountCallback(int count){
+        calls.append("initializeCountCallback,"); 
+        this.count = count;
+        conversationalCallback.initializeCount(count);
+    }
+    
+    public void incrementCountCallback(){
+        calls.append("incrementCountCallback,"); 
+        count++;
+        conversationalCallback.incrementCount();
+    }
+    
+    public int retrieveCountCallback(){
+        calls.append("retrieveCountCallback,"); 
+        return conversationalCallback.retrieveCount();
+    }
+    
+    public void businessExceptionCallback() throws Exception {
+        calls.append("businessExceptionCallback,");        
+        conversationalCallback.businessException();
+    }    
+    
+    public String endConversation(){
+        calls.append("endConversation,"); 
+        count = 0;
+        return conversationId;
+    }
+    
+    public String endConversationCallback(){
+        calls.append("endConversationCallback,"); 
+        return conversationalCallback.endConversation();
+    }
+}
