6e18719256ebd83288ca99ea5624cb9f18b0127b
Anthony Elder <antelder@apache.org>
2007-10-26 13:57:14 +0000

    TUSCANY-1871, apply patch from Rajini Sivaram for Application contribution classloaders itests
    git-svn-id: https://svn.apache.org/repos/asf/incubator/tuscany/java/sca@588646 13f79535-47bb-0310-9956-ffa450edef68

---
+++ itest/contribution-classloader/contribution-classes/src/main/java/supplychain/illegal/JavaCustomerComponentImpl.java
@@ -0,0 +1,74 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.    
+ */
+package supplychain.illegal;
+
+import org.osoa.sca.annotations.Reference;
+import org.osoa.sca.annotations.Scope;
+import org.osoa.sca.annotations.Service;
+
+import supplychain.customer.Customer;
+import supplychain.retailer.JavaRetailerComponentImpl;
+import supplychain.retailer.Retailer;
+import supplychain.warehouse.JavaWarehouseComponentImpl;
+import supplychain.warehouse.Warehouse;
+
+/**
+ * This class implements the Customer service component (POJO implementation).
+ */
+@Service(Customer.class)
+@Scope("COMPOSITE")
+public class JavaCustomerComponentImpl implements Customer {
+    
+    private static int outstandingOrderCount;
+    
+    private Retailer retailer;
+    
+    public JavaCustomerComponentImpl() {
+        System.out.println("Created " + this.getClass().getName() + 
+                " using: " + this.getClass().getClassLoader());
+    }
+    
+    @Reference
+    public void setRetailer(Retailer retailer) {
+        this.retailer = retailer;
+    }
+    
+    public void purchaseGoods() {
+        
+        Retailer retailerImpl = new JavaRetailerComponentImpl();
+        System.out.println("Created a retailer from Customer " + retailerImpl);
+        
+        Warehouse warehouseImpl = new JavaWarehouseComponentImpl();
+        System.out.println("Created a warehouse from Customer " + warehouseImpl);
+
+        outstandingOrderCount++;
+        retailer.submitOrder("Order");
+    }
+    
+    public void notifyShipment(String order) {
+        outstandingOrderCount--;
+        System.out.print("Work thread " + Thread.currentThread() + " - ");
+        System.out.println(order);
+    }
+    
+    public int outstandingOrderCount() {
+        return outstandingOrderCount;
+    }
+
+}
