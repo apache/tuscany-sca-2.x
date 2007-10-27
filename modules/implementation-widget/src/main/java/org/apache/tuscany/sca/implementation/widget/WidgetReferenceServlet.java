818766d2627f07644b0f5f250246205758cd0383
Luciano Resende <lresende@apache.org>
2007-10-27 01:15:39 +0000

    [work in progress] New widget implementation
    git-svn-id: https://svn.apache.org/repos/asf/incubator/tuscany/java/sca@588863 13f79535-47bb-0310-9956-ffa450edef68

---
+++ modules/implementation-widget/src/main/java/org/apache/tuscany/sca/implementation/widget/WidgetReferenceServlet.java
@@ -0,0 +1,109 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.    
+ */
+package org.apache.tuscany.sca.implementation.widget;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.tuscany.sca.assembly.Binding;
+import org.apache.tuscany.sca.assembly.ComponentReference;
+import org.apache.tuscany.sca.runtime.RuntimeComponent;
+
+/**
+ * Servlet to handle requests for ..
+ * 
+ * @version $Rev$ $Date$
+ */
+public class WidgetReferenceServlet extends HttpServlet {
+    
+    protected transient RuntimeComponent component;
+
+    public WidgetReferenceServlet(RuntimeComponent component) {
+        this.component = component;
+    }
+
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+        ServletOutputStream os = response.getOutputStream();
+
+        os.println();
+        os.println("/* Apache Tuscany SCA Widget header */");
+        os.println();
+
+        writeSCAWidgetCode(os, request.getServletPath());
+    }
+
+    /**
+     */
+    protected void writeSCAWidgetCode(ServletOutputStream out, String path) throws IOException {
+        out.println();
+        out.println("/* Apache Tuscany SCA Widget header */");
+        out.println();
+        
+        // remove the leading slash '/' character
+        path = path.substring(1);
+
+        for(ComponentReference reference : component.getReferences()) {
+            String referenceName = reference.getName();
+            out.println("Reference::" + referenceName);
+            for(Binding binding : reference.getBindings()) {
+                out.println("::Bind::" + binding.getName());
+                out.println("::Bind class::" + binding.getClass());
+            }
+        }
+        
+       
+        out.println();
+        out.println("/** End of Apache Tuscany SCA Widget */");
+        out.println();
+    }
+
+    /**
+     */
+    protected void writeJavaScriptBindingProxy(ServletOutputStream os) throws IOException {
+        URL url = getClass().getResource("xxx.js");
+        InputStream is = url.openStream();
+        int i;
+        while ((i = is.read()) != -1) {
+            os.write(i);
+        }
+    }
+
+    /*
+    public void addService(String serviceName) {
+        serviceNames.add(serviceName);
+    }
+
+    public void removeService(String serviceName) {
+        serviceNames.remove(serviceName);
+    }
+
+    public List<String> getServiceNames() {
+        return serviceNames;
+    }
+    */
+
+}
