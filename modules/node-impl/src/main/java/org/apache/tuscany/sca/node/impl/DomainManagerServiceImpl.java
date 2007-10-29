e6b6115387c9c4173943ac3d59bbfcc7649c5423
Jean Sebastien Delfino <jsdelfino@apache.org>
2007-10-29 01:37:31 +0000

    Use a more reliable way to get a free IP port. Changed the node launcher to pick a free IP port for the launched node.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/tuscany/java/sca@589464 13f79535-47bb-0310-9956-ffa450edef68

--- modules/node-impl/src/main/java/org/apache/tuscany/sca/node/impl/DomainManagerServiceImpl.java
+++ modules/node-impl/src/main/java/org/apache/tuscany/sca/node/impl/DomainManagerServiceImpl.java
@@ -58,7 +58,6 @@ public class DomainManagerServiceImpl implements DomainManagerNodeEventService{
                 returnValue =  domainManager.registerNode(nodeURI, nodeURL);
                 break;
             } catch(UndeclaredThrowableException ex) {
-                ex.printStackTrace();
                 logger.log(Level.INFO, "Trying to register node " + 
                                        nodeURI + 
                                        " at endpoint " +
