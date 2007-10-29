5e2c6a6773af769d2e2c8bd41a274e53e512c3c1
Jean Sebastien Delfino <jsdelfino@apache.org>
2007-10-29 08:18:04 +0000

    Let the launchers use default node URIs. Renamed UI components and configure their widget URIs.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/tuscany/java/sca@589540 13f79535-47bb-0310-9956-ffa450edef68

--- tutorial/store-eu/launch/LaunchEUStore.java
+++ tutorial/store-eu/launch/LaunchEUStore.java
@@ -31,7 +31,7 @@ public class LaunchEUStore {
     public static void main(String[] args) throws Exception {
         System.out.println("Starting ...");
         SCANodeFactory nodeFactory = SCANodeFactory.newInstance();
-        SCANode node = nodeFactory.createSCANode("http://localhost:8102/store", "http://localhost:9999");
+        SCANode node = nodeFactory.createSCANode(null, "http://localhost:9999");
         
         URL contribution = SCAContributionUtil.findContributionFromClass(LaunchEUStore.class);
         node.addContribution("http://store", contribution);
