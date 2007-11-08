ced8a44d30c1bb7710c9783b1d82b8f0553facbf
Luciano Resende <lresende@apache.org>
2007-11-08 01:17:42 +0000

    Removing Debug message to investigate continuun build issue
    git-svn-id: https://svn.apache.org/repos/asf/incubator/tuscany/java/sca@592991 13f79535-47bb-0310-9956-ffa450edef68

--- modules/implementation-bpel/src/test/java/org/apache/tuscany/sca/implementation/bpel/BPELHelloWorldTestCase.java
+++ modules/implementation-bpel/src/test/java/org/apache/tuscany/sca/implementation/bpel/BPELHelloWorldTestCase.java
@@ -54,8 +54,6 @@ public class BPELHelloWorldTestCase extends TestCase {
     
     public void testInvoke() {
         String response = bpelService.hello("Hello");
-        System.out.println("Response : " + response);
-        System.out.println("Expected Response : 'Hello World'");
         assertEquals("Hello World", response);
     }
 }
