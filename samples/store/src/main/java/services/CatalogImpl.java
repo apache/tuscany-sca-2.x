a8311757437dfd03b160b829d86ed5774158abb1
Luciano Resende <lresende@apache.org>
2007-12-21 12:39:22 +0000

    Simplifying store sample not to use a database and to be more aligned with the "getting started guide" For database, please see tutorial store scenarios
    git-svn-id: https://svn.apache.org/repos/asf/incubator/tuscany/java/sca@606177 13f79535-47bb-0310-9956-ffa450edef68

--- samples/store/src/main/java/services/CatalogImpl.java
+++ samples/store/src/main/java/services/CatalogImpl.java
@@ -6,115 +6,44 @@
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
  * with the License.  You may obtain a copy of the License at
- *
+ * 
  *   http://www.apache.org/licenses/LICENSE-2.0
- *
+ * 
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
  * KIND, either express or implied.  See the License for the
  * specific language governing permissions and limitations
- * under the License.
+ * under the License.    
  */
 
 package services;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
+import org.osoa.sca.annotations.Init;
 import org.osoa.sca.annotations.Property;
 import org.osoa.sca.annotations.Reference;
 
 public class CatalogImpl implements Catalog {
     @Property
     public String currencyCode = "USD";
-    
     @Reference
     public CurrencyConverter currencyConverter;
-    
-    private String currencySymbol;
-    
-    public String[] get() {
-        
-        String[] catalogArray = null;
-        
-        String itemName;
-        float itemPrice;
-        String itemCurrencyCode;
-        
-        Connection conn = null;
-        PreparedStatement pstmt = null;
-        ResultSet rs = null;
-        
-        currencySymbol = currencyConverter.getCurrencySymbol(currencyCode);
-        
-        try {
-            //initialize driver and register it with DriverManager
-            Class.forName("org.apache.derby.jdbc.EmbeddedDriver");
-            
-            conn = DriverManager.getConnection(
-                    "jdbc:derby:target/store_db",
-                    "",
-                    "");
-            
-            pstmt = conn.prepareStatement("select * from \"CATALOG\"",
-                    ResultSet.TYPE_SCROLL_INSENSITIVE,
-                    ResultSet.CONCUR_READ_ONLY);
-            
-            rs = pstmt.executeQuery();
-            rs.last();
-            
-            catalogArray = new String[rs.getRow()];
-            
-            do {
-                itemName = rs.getString(2);
-                itemPrice = rs.getFloat(4);
-                itemCurrencyCode = rs.getString(3);
-                
-                catalogArray[rs.getRow()-1] = new String(itemName+" - "+
-                        currencySymbol+" "+
-                        currencyConverter.getConversion(itemCurrencyCode, currencyCode, itemPrice));
-                
-            } while(rs.previous());
-            
-        } catch (SQLException ex) {         
-            ex.printStackTrace();
-        }catch (ClassNotFoundException ex) {
-            ex.printStackTrace();
-        } finally {
-            cleanup(conn,pstmt,rs);
-        }
-        
-        return catalogArray;
+    private List<String> catalog = new ArrayList<String>();
+
+    @Init
+    public void init() {
+        String currencySymbol = currencyConverter.getCurrencySymbol(currencyCode);
+        catalog.add("Apple - " + currencySymbol + currencyConverter.getConversion("USD", currencyCode, 2.99f));
+        catalog.add("Orange - " + currencySymbol + currencyConverter.getConversion("USD", currencyCode, 3.55f));
+        catalog.add("Pear - " + currencySymbol + currencyConverter.getConversion("USD", currencyCode, 1.55f));
     }
-    
-    private void cleanup(Connection conn, PreparedStatement pstmt, ResultSet rs) {
-        
-        if (rs!=null) {
-            try {
-                rs.close();
-            } catch (SQLException ex) {
-                ex.printStackTrace();
-            }
-        }
-        
-        if (pstmt!=null) {
-            try {
-                pstmt.close();
-            } catch (SQLException ex) {
-                ex.printStackTrace();
-            }
-        }
-        
-        if (conn!=null) {
-            try {
-                conn.close();
-            } catch (SQLException ex) {
-                ex.printStackTrace();
-            }
-        }
+
+    public String[] get() {
+        String[] catalogArray = new String[catalog.size()];
+        catalog.toArray(catalogArray);
+        return catalogArray;
     }
 }
