8feb46cd6701c2643bce0dd44fc4d5ccb891df39
Simon Laws <slaws@apache.org>
2007-12-21 12:58:07 +0000

    Make test wait a little longer for domain configuration to complete
    git-svn-id: https://svn.apache.org/repos/asf/incubator/tuscany/java/sca@606183 13f79535-47bb-0310-9956-ffa450edef68

--- modules/node-impl/src/test/java/org/apache/tuscany/sca/node/impl/NodeDrivenTestCase.java
+++ modules/node-impl/src/test/java/org/apache/tuscany/sca/node/impl/NodeDrivenTestCase.java
@@ -89,7 +89,7 @@ public class NodeDrivenTestCase {
             nodeC.start();
             
             // wait for domain to configure
-            Thread.sleep(1000);
+            Thread.sleep(2000);
             
         } catch(Exception ex){
             ex.printStackTrace();
