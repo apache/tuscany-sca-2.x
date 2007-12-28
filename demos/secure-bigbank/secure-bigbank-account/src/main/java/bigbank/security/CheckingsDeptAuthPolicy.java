1db0042fd9d5e005a46c5fa3fa26a427d9206c87
S Venkatakrishnan <svkrish@apache.org>
2007-12-28 11:33:02 +0000

    adding authorization scenario to checkingsaccount service
    git-svn-id: https://svn.apache.org/repos/asf/incubator/tuscany/java/sca@607212 13f79535-47bb-0310-9956-ffa450edef68

--- demos/secure-bigbank/secure-bigbank-account/src/main/java/bigbank/security/CheckingsDeptAuthPolicy.java
+++ demos/secure-bigbank/secure-bigbank-account/src/main/java/bigbank/security/CheckingsDeptAuthPolicy.java
@@ -1,5 +1,4 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<!--
+/*
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements.  See the NOTICE file
  * distributed with this work for additional information
@@ -7,27 +6,22 @@
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
  * with the License.  You may obtain a copy of the License at
- *
+ * 
  *   http://www.apache.org/licenses/LICENSE-2.0
- *
+ * 
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
  * KIND, either express or implied.  See the License for the
  * specific language governing permissions and limitations
- * under the License.
--->
-<composite xmlns="http://www.osoa.org/xmlns/sca/1.0"
-	targetNamespace="http://bigbank/checkings"
-	xmlns:bbck="http://bigbank/checkings"
-	name="CheckingsAccountDept">
-	
-    <service name="CheckingsAccountService" promote="CheckingAccountServiceComponent" requires="authentication">
-        <interface.java interface="bigbank.account.checking.CheckingAccountService"/>
-        <binding.ws uri="http://localhost:8085/services/CheckingAcWebService"/>
-    </service>
+ * under the License.    
+ */
 
-    <component name="CheckingAccountServiceComponent">
-        <implementation.java class="bigbank.account.checking.CheckingAccountServiceImpl" requires="bbck:chkDeptAuthorization"/>
-    </component>
-</composite>
+package bigbank.security;
+
+/**
+ * @version $Rev$ $Date$
+ */
+public class CheckingsDeptAuthPolicy {
+
+}
