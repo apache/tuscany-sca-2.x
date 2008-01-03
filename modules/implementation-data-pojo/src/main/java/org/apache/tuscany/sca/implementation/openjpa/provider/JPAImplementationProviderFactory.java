1e67648dde98ff6b132e87450923685cbfeabea3
Luciano Resende <lresende@apache.org>
2008-01-03 14:49:24 +0000

    [Work-in-progress] implementation-data-pojo based on implementation-openjpa
    git-svn-id: https://svn.apache.org/repos/asf/incubator/tuscany/java/sca@608509 13f79535-47bb-0310-9956-ffa450edef68

---
+++ modules/implementation-data-pojo/src/main/java/org/apache/tuscany/sca/implementation/openjpa/provider/JPAImplementationProviderFactory.java
@@ -0,0 +1,45 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.    
+ */
+
+package org.apache.tuscany.sca.implementation.openjpa.provider;
+
+import org.apache.tuscany.sca.core.ExtensionPointRegistry;
+import org.apache.tuscany.sca.implementation.openjpa.JPAImplementation;
+import org.apache.tuscany.sca.provider.ImplementationProvider;
+import org.apache.tuscany.sca.provider.ImplementationProviderFactory;
+import org.apache.tuscany.sca.runtime.RuntimeComponent;
+
+public class JPAImplementationProviderFactory implements ImplementationProviderFactory<JPAImplementation> {
+    private ExtensionPointRegistry eP;
+
+    public JPAImplementationProviderFactory(ExtensionPointRegistry extensionPoints) {
+        eP = extensionPoints;
+    }
+
+    public ImplementationProvider createImplementationProvider(RuntimeComponent component,
+                                                               JPAImplementation implementation) {
+        return new JPAImplementationProvider(component, implementation, eP);
+    }
+
+    public Class<JPAImplementation> getModelType() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
