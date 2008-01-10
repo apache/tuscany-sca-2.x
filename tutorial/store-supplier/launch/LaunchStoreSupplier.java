3cbed26b4b049e3654b6cbf62bfb9b3c0b04a6ec
Jean Sebastien Delfino <jsdelfino@apache.org>
2008-01-10 01:30:23 +0000

    Adding sca-contribution.xml, updating Launch class to the latest API, fixing URIs in composite.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/tuscany/java/sca@610633 13f79535-47bb-0310-9956-ffa450edef68

--- tutorial/store-supplier/launch/LaunchStoreSupplier.java
+++ tutorial/store-supplier/launch/LaunchStoreSupplier.java
@@ -19,31 +19,42 @@
 
 package launch;
 
+import java.io.File;
 import java.net.URL;
 
 import javax.xml.namespace.QName;
 
 import org.apache.tuscany.sca.node.SCANode;
 import org.apache.tuscany.sca.node.SCANodeFactory;
-import org.apache.tuscany.sca.node.util.SCAContributionUtil;
 
 public class LaunchStoreSupplier {
     public static void main(String[] args) throws Exception {
         System.out.println("Starting ...");
+        
+        URL storeSupplierContribution = new File("./target/classes").toURL();
+        URL assetsContribution = new File("../assets/target/classes").toURL();
+        URL derbyContribution = new File(System.getProperty("user.home") + "/.m2/repository/org/apache/derby/derby/10.1.2.1/derby-10.1.2.1.jar").toURL();
+        URL dataAPIContribution = new File(System.getProperty("user.home") + "/.m2/repository/org/apache/tuscany/sca/tuscany-implementation-data-api/1.2-incubating-SNAPSHOT/tuscany-implementation-data-api-1.2-incubating-SNAPSHOT.jar").toURL();
+        
         SCANodeFactory nodeFactory = SCANodeFactory.newInstance();
         SCANode node = nodeFactory.createSCANode(null, "http://localhost:9998");
         
-        URL contribution = SCAContributionUtil.findContributionFromClass(LaunchStoreSupplier.class);
-        node.addContribution("http://store", contribution);
+        node.addContribution("http://org/apache/derby", derbyContribution);
+        node.addContribution("http://org/apache/tuscany/sca/implementation-data-api", dataAPIContribution);
+        node.addContribution("http://assets", assetsContribution);
+        node.addContribution("http://store-supplier", storeSupplierContribution);
         
         node.addToDomainLevelComposite(new QName("http://store", "store-supplier"));
-        node.start();
+        //FIXME looks like we can't start/stop individual nodes anymore
+        node.getDomain().start();
 
         System.out.println("store-supplier.composite ready for big business !!!");
         System.in.read();
         
         System.out.println("Stopping ...");
-        node.stop();
+        //FIXME looks like we can't start/stop individual nodes anymore
+        node.getDomain().stop();
+        //node.stop();
         node.destroy();
         System.out.println();
     }
