734a970805b2f48612b3655e1ff2eb4605ed6a46
Zhaohui Feng <rfeng@apache.org>
2008-02-03 07:00:58 +0000

    Add a maven-web-junit plugin to trigger junit tests to be run on the web server
    git-svn-id: https://svn.apache.org/repos/asf/incubator/tuscany/java/sca@617959 13f79535-47bb-0310-9956-ffa450edef68

---
+++ tools/maven/maven-web-junit/src/main/java/org/apache/tuscany/tools/sca/web/junit/plugin/WebJUnitMojo.java
@@ -0,0 +1,169 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.    
+ */
+package org.apache.tuscany.tools.sca.web.junit.plugin;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import org.apache.commons.logging.LogFactory;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.project.MavenProject;
+
+/**
+ * @version $Rev$ $Date$
+ * @goal test
+ * @phase integration-test
+ * @requiresDependencyResolution test
+ * @description Run the unit test over HTTP
+ */
+public class WebJUnitMojo extends AbstractMojo {
+    /**
+     * The project to create a build for.
+     *
+     * @parameter expression="${project}"
+     * @required
+     * @readonly
+     */
+    private MavenProject project;
+
+    /**
+     * The test cases to run
+     * @parameter 
+     */
+    private String testCases[];
+
+    /**
+     * The URL for the web site
+     * @parameter
+     */
+    private String url;
+
+    /**
+     * Timeout for the http connection
+     * @parameter
+     */
+    private int timeout = 60000; // 1 minute
+
+    /**
+     * Maximum number of retries
+     * @parameter
+     */
+    private int maxRetries = 3;
+
+    public void execute() throws MojoExecutionException {
+        reset();
+
+        if (url == null) {
+            url = "http://localhost:8080/" + project.getBuild().getFinalName() + "/junit";
+        }
+
+        getLog().info("Connecting to " + url);
+
+        int runs = 0, errors = 0, failures = 0;
+        String xml = "";
+        
+        try {
+            HttpClient client = new DefaultHttpClient();
+            HttpGet httpget = new HttpGet(url);
+
+            // Execute HTTP request
+            HttpResponse response = client.execute(httpget);
+            String header = response.getFirstHeader("junit.errors").getValue();
+            errors = header == null ? 0 : Integer.parseInt(header);
+            header = response.getFirstHeader("junit.failures").getValue();
+            failures = header == null ? 0 : Integer.parseInt(header);
+            header = response.getFirstHeader("junit.runs").getValue();
+            runs = header == null ? 0 : Integer.parseInt(header);
+            getLog().info("Runs: " + runs + ", Failures: " + failures + ", Errors: " + errors);
+
+            // Get hold of the response entity
+            HttpEntity entity = response.getEntity();
+
+            
+            // If the response does not enclose an entity, there is no need
+            // to bother about connection release
+            if (entity != null) {
+                BufferedReader reader = new BufferedReader(new InputStreamReader(entity.getContent()));
+                try {
+                    StringBuffer sb = new StringBuffer();
+                    while (true) {
+                        String line = reader.readLine();
+                        if (line == null) {
+                            break;
+                        }
+                        sb.append(line);
+                    }
+                    xml = sb.toString();
+                    getLog().info(xml);
+
+                } catch (IOException ex) {
+
+                    // In case of an IOException the connection will be released
+                    // back to the connection manager automatically
+                    throw ex;
+
+                } catch (RuntimeException ex) {
+
+                    // In case of an unexpected exception you may want to abort
+                    // the HTTP request in order to shut down the underlying 
+                    // connection and release it back to the connection manager.
+                    httpget.abort();
+                    throw ex;
+
+                } finally {
+
+                    // Closing the input stream will trigger connection release
+                    reader.close();
+
+                }
+
+            }
+        } catch (Exception e) {
+            throw new MojoExecutionException(e.getMessage(), e);
+        }
+        if (errors != 0 || failures != 0) {
+            throw new MojoExecutionException(xml);
+        }
+
+    }
+
+    /**
+     * A workaround to avoid logging conflict with geronimo
+     */
+    private static void reset() {
+        LogFactory.releaseAll();
+
+        // Restore a reasonable default log impl
+        System.setProperty("org.apache.commons.logging.Log", "org.apache.commons.logging.impl.SimpleLog");
+
+        // Make SimpleLog look more like Maven logs
+        System.setProperty("org.apache.commons.logging.simplelog.showShortLogname", "false");
+
+        // Restore default Geornimo bootstrap behavior
+        System.getProperties().remove("geronimo.bootstrap.logging.enabled");
+    }
+
+}
