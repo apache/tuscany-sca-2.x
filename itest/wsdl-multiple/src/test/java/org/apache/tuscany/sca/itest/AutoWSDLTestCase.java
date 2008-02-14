cf64370f7b3e97cc93789ba732740929b0a82a7b
Simon Laws <slaws@apache.org>
2008-02-14 17:29:30 +0000

    TUSCANY-2043 An itest related to this JIRA where two WSDL share the same namespace and subsequent WSDL and XSD processing doesn't work properly. This is not part of the build yet. More fixes to follow once ML discussion is completed
    git-svn-id: https://svn.apache.org/repos/asf/incubator/tuscany/java/sca@627812 13f79535-47bb-0310-9956-ffa450edef68

---
+++ itest/wsdl-multiple/src/test/java/org/apache/tuscany/sca/itest/AutoWSDLTestCase.java
@@ -0,0 +1,59 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.    
+ */
+
+package org.apache.tuscany.sca.itest;
+
+import static junit.framework.Assert.assertEquals;
+import helloworld.HelloWorldService;
+
+import org.apache.tuscany.sca.host.embedded.SCADomain;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * Tests the behaviour of the system when multiple WSDLs appear in the contribution
+ * with the same namespace
+ */
+public class AutoWSDLTestCase {
+
+    private static SCADomain domain;
+    
+    @BeforeClass
+    public static void init() throws Throwable {
+        try {
+            domain = SCADomain.newInstance("auto-wsdl.composite");
+        } catch(Exception ex){
+            ex.printStackTrace();
+        }
+    }
+
+    @AfterClass
+    public static void destroy() throws Exception {
+        domain.close();
+    }    
+
+    @Test
+    public void testLoadWSDL() {
+        HelloWorldService client = domain.getService(HelloWorldService.class, "HelloWorldClientComponent/HelloWorldService");
+        client.getGreetings("petra");
+        //assertEquals("Hi petra", client.getGreetings("petra"));
+    }
+
+}
