66370eb1ddc990a92a78f70734c13497657c2c60
S Venkatakrishnan <svkrish@apache.org>
2008-02-21 17:48:37 +0000

    adding itest for policy
    git-svn-id: https://svn.apache.org/repos/asf/incubator/tuscany/java/sca@629890 13f79535-47bb-0310-9956-ffa450edef68

---
+++ itest/policy/src/main/java/calculator/CalculatorClient.java
@@ -0,0 +1,69 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.    
+ */
+
+package calculator;
+
+import javax.security.auth.login.Configuration;
+
+import org.apache.tuscany.sca.host.embedded.SCADomain;
+
+/**
+ * This client program shows how to create an SCA runtime, start it,
+ * and locate and invoke a SCA component
+ */
+public class CalculatorClient {
+    public static void main(String[] args) throws Exception {
+        try {
+            Configuration secConf = Configuration.getConfiguration();
+        } catch ( java.lang.SecurityException e ) {
+            if ( e.getMessage().equals("Unable to locate a login configuration") ) {
+                System.setProperty("java.security.auth.login.config", "target/classes/CalculatorJass.config");
+            } else {
+                throw e;
+            }
+        }
+
+        SCADomain scaDomain = SCADomain.newInstance("Calculator.composite");
+        
+        CalculatorService calculatorService = 
+            scaDomain.getService(CalculatorService.class, "CalculatorServiceComponent");
+
+        // Calculate
+        System.out.println("Calling CalculatorServiceComponent configured with 'logging' " +
+                "policy for subtract and divide operations...");
+        System.out.println("3 + 2=" + calculatorService.add(3, 2));
+        System.out.println("3 - 2=" + calculatorService.subtract(3, 2));
+        System.out.println("3 * 2=" + calculatorService.multiply(3, 2));
+        System.out.println("3 / 2=" + calculatorService.divide(3, 2));
+        
+        calculatorService = 
+            scaDomain.getService(CalculatorService.class, "AnotherCalculatorServiceComponent");
+
+        // Calculate
+        System.out.println("Calling CalculatorServiceComponent configured with 'logging' " +
+                "for all operations in the implementation...");
+        System.out.println("3 + 2=" + calculatorService.add(3, 2));
+        System.out.println("3 - 2=" + calculatorService.subtract(3, 2));
+        System.out.println("3 * 2=" + calculatorService.multiply(3, 2));
+        System.out.println("3 / 2=" + calculatorService.divide(3, 2));
+
+        scaDomain.close();
+    }
+
+}
---
+++ itest/policy/src/main/java/calculator/policy/TestSvcPolicyHandler.java
@@ -0,0 +1,62 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.    
+ */
+
+package calculator.policy;
+
+import org.apache.tuscany.sca.interfacedef.Operation;
+import org.apache.tuscany.sca.invocation.Message;
+import org.apache.tuscany.sca.policy.PolicySet;
+import org.apache.tuscany.sca.policy.util.PolicyHandler;
+import org.junit.Assert;
+
+/**
+ * @version $Rev$ $Date$
+ */
+public class TestSvcPolicyHandler implements PolicyHandler {
+    private PolicySet applicablePolicySet = null;
+
+    public void afterInvoke(Object... arg0) {
+    }
+
+    public void beforeInvoke(Object... context) {
+        for ( Object contextObj : context) {
+            if ( contextObj instanceof Operation ) {
+                Operation op = (Operation)contextObj;
+                System.out.println(" *TestSvcPolicyHandler* " + op.getName() + " ** " + applicablePolicySet);
+            } else if ( contextObj instanceof Message ) {
+                Message msg = (Message)contextObj;
+                System.out.println(" *TestSvcPolicyHandler* " + msg.getOperation().getName() + " ** " + applicablePolicySet);
+            }
+        }
+    }
+
+    public void cleanUp(Object... arg0) {
+    }
+
+    public PolicySet getApplicablePolicySet() {
+        return applicablePolicySet;
+    }
+
+    public void setApplicablePolicySet(PolicySet arg0) {
+        this.applicablePolicySet = arg0;
+    }
+
+    public void setUp(Object... arg0) {
+    }
+}
