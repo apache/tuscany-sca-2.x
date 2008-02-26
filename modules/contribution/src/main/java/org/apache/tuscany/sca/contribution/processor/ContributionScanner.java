a32ccc2d01bf97eae97bea70bcb936b37f1ac38c
Jean Sebastien Delfino <jsdelfino@apache.org>
2008-02-26 15:57:32 +0000

    Adding an interface for scanners used to scan contributions and find artifacts in them.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/tuscany/java/sca@631266 13f79535-47bb-0310-9956-ffa450edef68

---
+++ modules/contribution/src/main/java/org/apache/tuscany/sca/contribution/processor/ContributionScanner.java
@@ -0,0 +1,67 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.    
+ */
+package org.apache.tuscany.sca.contribution.processor;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.List;
+
+import org.apache.tuscany.sca.contribution.service.ContributionException;
+
+/**
+ * Interface for contribution package scanners
+ * 
+ * Contribution scanners understand the format of the contribution and how to get the
+ * artifacts in the contribution.
+ * 
+ * @version $Rev$ $Date$
+ */
+public interface ContributionScanner {
+    
+    /**
+     * Returns the type of package supported by this package scanner.
+     * 
+     * @return the package type
+     */
+    String getContributionType();
+
+    /**
+     * Returns a list of artifacts in the contribution.
+     * 
+     * @param contributionURL Contribution URL
+     * @return List of artifact URIs
+     * @throws ContributionException
+     * @throws IOException
+     */
+    List<String> getArtifacts(URL contributionURL) throws ContributionException, IOException;
+
+    /**
+     * Return the URL for an artifact in the contribution.
+     * 
+     * This is needed for archives such as jar files that have specific URL schemes
+     * for the artifacts they contain.
+     * 
+     * @param contributionURL Contribution URL
+     * @param artifact The relative URI for the artifact
+     * @return The artifact URL
+     */
+    URL getArtifactURL(URL packageSourceURL, String artifact) throws MalformedURLException;
+
+}
