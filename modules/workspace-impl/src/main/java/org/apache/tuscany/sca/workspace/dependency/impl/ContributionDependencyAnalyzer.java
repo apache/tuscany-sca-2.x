8dec87d797ef6aba956c7266a73697af3fd653de
Jean Sebastien Delfino <jsdelfino@apache.org>
2008-02-28 05:42:01 +0000

    Implemented a contribution info processor and contribution scanners. Implemented logic to calculate contribution dependencies.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/tuscany/java/sca@631857 13f79535-47bb-0310-9956-ffa450edef68

---
+++ modules/workspace-impl/src/main/java/org/apache/tuscany/sca/workspace/dependency/impl/ContributionDependencyAnalyzer.java
@@ -0,0 +1,88 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.    
+ */
+
+package org.apache.tuscany.sca.workspace.dependency.impl;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.apache.tuscany.sca.contribution.Contribution;
+import org.apache.tuscany.sca.contribution.Export;
+import org.apache.tuscany.sca.contribution.Import;
+import org.apache.tuscany.sca.workspace.Workspace;
+
+/**
+ * A contribution dependency analyzer.
+ *
+ * @version $Rev$ $Date$
+ */
+public class ContributionDependencyAnalyzer {
+    
+    /**
+     * Constructs a new WorkspaceDependencyAnalyzer.
+     */
+    public ContributionDependencyAnalyzer() {
+        // TODO Auto-generated constructor stub
+    }
+    
+    /**
+     * Calculate the set of contributions that a contribution depends on.
+     * @param workspace
+     * @param contribution
+     * @return
+     */
+    public Set<Contribution> calculateContributionDependencies(Workspace workspace, Contribution contribution) {
+        Set<Contribution> dependencies = new HashSet<Contribution>();
+
+        addContributionDependencies(workspace, contribution, dependencies);
+        
+        return dependencies;
+    }
+    
+    /**
+     * Analyze a contribution and add its dependencies to the given dependency set.
+     * @param workspace
+     * @param contribution
+     * @param dependencies
+     */
+    private void addContributionDependencies(Workspace workspace, Contribution contribution, Set<Contribution> dependencies) {
+        
+        // Go through the contribution imports
+        for (Import import_: contribution.getImports()) {
+            
+            // Go through all contribution candidates and their exports
+            for (Contribution dependency: workspace.getContributions()) {
+                for (Export export: dependency.getExports()) {
+                    
+                    // If an export from a contribution matches the import in hand
+                    // add that contribution to the dependency set
+                    if (import_.match(export)) {
+                        if (!dependencies.contains(dependency)) {
+                            dependencies.add(dependency);
+                            
+                            // Now add the dependencies of that contribution
+                            addContributionDependencies(workspace, dependency, dependencies);
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+}
