9da130b42f73680240a5b6addbf72d92ec6f2a55
Jean Sebastien Delfino <jsdelfino@apache.org>
2008-03-09 21:51:51 +0000

    Make sure that the latest up to date version of a contribution JAR is loaded.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/tuscany/java/sca@635373 13f79535-47bb-0310-9956-ffa450edef68

--- modules/assembly-xml/src/main/java/org/apache/tuscany/sca/assembly/xml/ConstrainingTypeDocumentProcessor.java
+++ modules/assembly-xml/src/main/java/org/apache/tuscany/sca/assembly/xml/ConstrainingTypeDocumentProcessor.java
@@ -23,6 +23,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.net.URI;
 import java.net.URL;
+import java.net.URLConnection;
 
 import javax.xml.stream.XMLInputFactory;
 import javax.xml.stream.XMLStreamException;
@@ -61,7 +62,9 @@ public class ConstrainingTypeDocumentProcessor extends BaseAssemblyProcessor imp
         try {
             
             // Create a stream reader
-            urlStream = url.openStream();
+            URLConnection connection = url.openConnection();
+            connection.setUseCaches(false);
+            urlStream = connection.getInputStream();
             XMLStreamReader reader = inputFactory.createXMLStreamReader(url.toString(), urlStream);
             reader.nextTag();
             
