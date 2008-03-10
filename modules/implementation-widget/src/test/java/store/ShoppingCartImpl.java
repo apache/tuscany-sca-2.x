340ff4f9a83f67ba80f3b5ed91b64d384c67d070
Luciano Resende <lresende@apache.org>
2008-03-10 23:10:10 +0000

    Reacting changes after binding-atom refactoring.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/tuscany/java/sca@635752 13f79535-47bb-0310-9956-ffa450edef68

--- modules/implementation-widget/src/test/java/store/ShoppingCartImpl.java
+++ modules/implementation-widget/src/test/java/store/ShoppingCartImpl.java
@@ -27,8 +27,8 @@ import java.util.UUID;
 import org.apache.abdera.Abdera;
 import org.apache.abdera.model.Entry;
 import org.apache.abdera.model.Feed;
-import org.apache.tuscany.sca.binding.feed.collection.Collection;
-import org.apache.tuscany.sca.binding.feed.collection.NotFoundException;
+import org.apache.tuscany.sca.binding.atom.collection.Collection;
+import org.apache.tuscany.sca.binding.atom.collection.NotFoundException;
 
 public class ShoppingCartImpl implements Collection {
 
