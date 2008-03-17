debd5c4a915e8bd2caf708a8fbcce08a3070132a
Jean Sebastien Delfino <jsdelfino@apache.org>
2008-03-17 10:02:46 +0000

    Changed sample to use the new launchers.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/tuscany/java/sca@637814 13f79535-47bb-0310-9956-ffa450edef68

--- samples/calculator-distributed/src/main/java/node/LaunchCalculatorNodeB.java
+++ samples/calculator-distributed/src/main/java/node/LaunchCalculatorNodeB.java
@@ -19,10 +19,10 @@
 
 package node;
 
-import org.apache.tuscany.sca.implementation.node.launcher.NodeImplementationLauncher;
+import org.apache.tuscany.sca.node.launcher.NodeLauncher;
 
 public class LaunchCalculatorNodeB {
     public static void main(String[] args) throws Exception {
-        NodeImplementationLauncher.main(new String[] {"http://localhost:9990/node-image/NodeB"});
+        NodeLauncher.main(new String[] {"http://localhost:9990/node-image/NodeB"});
     }
 }
