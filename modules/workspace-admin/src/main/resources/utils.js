1fdff172eff3fad266a23337867be7a108a95e46
Jean Sebastien Delfino <jsdelfino@apache.org>
2008-04-15 20:56:56 +0000

    Added a utility method to suggest available options on input fields in the admin app. Added corresponding CSS entries to admin.css. Next step to improve usability is to add calls to it in the various admin pages to allow people to select contribution uris, namespaces, composite names without having to type them.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/tuscany/java/sca@648408 13f79535-47bb-0310-9956-ffa450edef68

--- modules/workspace-admin/src/main/resources/utils.js
+++ modules/workspace-admin/src/main/resources/utils.js
@@ -17,6 +17,122 @@
  * under the License.    
  */
  
+/**
+ * Autocomplete / suggest support for input fields
+ *
+ * To use it declare a 'suggest' function as follows:
+ * function suggestItems() {
+ *   	return new Array('abc', 'def', 'ghi');
+ * }
+ *
+ * then hook it to an input field as follows:
+ * suggest(document.yourForm.yourInputField, suggestItems);
+ */ 
+function selectSuggestion(node, value) {
+	for (;;) {
+		node = node.parentNode;
+		if (node.tagName.toLowerCase() == 'div') {
+      		break;
+      	}
+	}
+	node.selectSuggestion(value);
+}
+
+function hilightSuggestion(node, over) {
+	if (over) {
+  		node.className = 'suggestHilighted';
+	} else {
+		node.className = 'suggestItem';
+	}
+}
+
+function suggest(input, suggestFunction) {
+  	
+  	input.suggest = suggestFunction;
+  	
+	input.selectSuggestion = function(value) {
+		this.hideSuggestDiv();
+		this.value = value;
+	}
+	
+	input.hideSuggestDiv = function() {
+		if (this.suggestDiv != null) {
+			this.suggestDiv.style.visibility = 'hidden';
+		}
+	}
+	
+  	input.showSuggestDiv = function() {
+  		if (this.suggestDiv == null) {
+	  		this.suggestDiv = document.createElement('div');
+	  		this.suggestDiv.input = this;
+	  		this.suggestDiv.className = 'suggest';
+			input.parentNode.insertBefore(this.suggestDiv, input);
+			this.suggestDiv.style.visibility = 'hidden';
+		  	this.suggestDiv.style.zIndex = '99';
+		  	
+		  	this.suggestDiv.selectSuggestion = function(value) {
+		  		this.input.selectSuggestion(value);
+		  	}
+		}
+	  	
+  		var values = this.suggest();
+    	var items = "";
+    	for (var i = 0; i < values.length; i++) {
+    		if (values[i].indexOf(this.value) == -1) {
+    			continue;
+    		}
+    		if (items.length == 0) {
+    			items += '<table class=suggestTable>';
+    		}
+    		items += '<tr><td class="suggestItem" ' +
+    		'onmouseover="hilightSuggestion(this, true)" onmouseout="hilightSuggestion(this, false)" ' +
+    		'onclick="selectSuggestion(this, \'' + values[i] + '\')">' + values[i] + '</td></tr>';
+    	}
+    	if (items.length != 0) {
+    		items += '</table>';
+    	}
+	  	this.suggestDiv.innerHTML = items;
+	  	
+		if (items.length != 0) {
+			var node = input;		  	
+	        var left = 0;
+	        var top = 0;
+	        for (;;) {
+	            left += node.offsetLeft;
+	            top += node.offsetTop;
+	            node = node.offsetParent;
+	            if (node.tagName.toLowerCase() == 'body') {
+	            	break;
+	            }
+	        }
+		  	this.suggestDiv.style.left = left;
+		  	this.suggestDiv.style.top = top + input.offsetHeight;
+		  	this.suggestDiv.style.visibility = 'visible';
+		} else {
+			this.suggestDiv.style.visibility = 'hidden';
+		}
+  	}
+  	
+	input.onkeydown = function(event) {
+    	this.showSuggestDiv();
+	};
+
+	input.onkeyup = function(event) {
+    	this.showSuggestDiv();
+	};
+
+	input.onmousedown = function(event) {
+    	this.showSuggestDiv();
+	};
+
+	input.onblur = function(event) {
+		setTimeout(function() { input.hideSuggestDiv(); }, 50);
+	};
+}
+
+/**
+ * A Toolbar class
+ */ 
 function Tool(name, href) {
 	this.name = name;
 	this.href = href;
@@ -31,8 +147,10 @@ Tool.prototype.print = function() {
 	}
 }
 
+/**
+ * Initialize the toolbar
+ */
 function toolbar() {
-
 	var toolbar = '<table width="100%" cellpadding="0" cellspacing="0" class=tbar><tr>' +
 	'<td class=ltbar><table border="0" cellspacing="0" cellpadding="0"><tr>';
    
@@ -48,6 +166,9 @@ function toolbar() {
 	document.getElementById('toolbar').innerHTML = toolbar;
 }
 
+/**
+ * Utility function returning an array from an array or an object. 
+ */ 
 function array(obj) {
     if (obj.length == undefined) {
 		var a = new Array();
@@ -59,6 +180,9 @@ function array(obj) {
   	}
 }
 
+/**
+ * Populate the default toolbar
+ */
 var tools = new Array();
 tools[0] = new Tool("Contributions", "/ui/workspace");
 tools[1] = new Tool("Composites", "/ui/composite");
