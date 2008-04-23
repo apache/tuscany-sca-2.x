f154fbee4eb22239d9e49fd8509c76cb4a734d34
Jean Sebastien Delfino <jsdelfino@apache.org>
2008-04-23 02:02:51 +0000

    Moved ExtensionPointRegistry from module core to module extensibility. Added extension points for module activators and utility services to make it easier to bootstrap a subset of Tuscany, get the list of available module activators, or get an instance of a utility like a monitor factory or interfacecontractapper for example.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/tuscany/java/sca@650725 13f79535-47bb-0310-9956-ffa450edef68

---
+++ modules/extensibility/src/main/java/org/apache/tuscany/sca/core/DefaultExtensionPointRegistry.java
@@ -0,0 +1,188 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.    
+ */
+
+package org.apache.tuscany.sca.core;
+
+import java.io.IOException;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Modifier;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+import org.apache.tuscany.sca.extensibility.ServiceDiscovery;
+
+/**
+ * Default implementation of a registry to hold all the Tuscany core extension
+ * points. As the point of contact for all extension artifacts this registry
+ * allows loaded extensions to find all other parts of the system and register
+ * themselves appropriately.
+ *
+ * @version $Rev$ $Date$
+ */
+public class DefaultExtensionPointRegistry implements ExtensionPointRegistry {
+    private Map<Class<?>, Object> extensionPoints = new HashMap<Class<?>, Object>();
+    
+    /**
+     * Constructs a new registry. 
+     */
+    public DefaultExtensionPointRegistry() {
+    }
+
+    /**
+     * Add an extension point to the registry. This default implementation
+     * stores extensions against the interfaces that they implement.
+     *
+     * @param extensionPoint The instance of the extension point
+     *
+     * @throws IllegalArgumentException if extensionPoint is null
+     */
+    public void addExtensionPoint(Object extensionPoint) {
+        if (extensionPoint == null) {
+            throw new IllegalArgumentException("Cannot register null as an ExtensionPoint");
+        }
+
+        Set<Class> interfaces = getAllInterfaces(extensionPoint.getClass());
+        for (Class i : interfaces) {
+            extensionPoints.put(i, extensionPoint);
+        }
+    }
+    
+    private Constructor<?> getConstructor(Constructor<?>[] constructors, Class<?>[] paramTypes) {
+        for (Constructor<?> c : constructors) {
+            Class<?> types[] = c.getParameterTypes();
+            if (c.getParameterTypes().length == paramTypes.length) {
+                boolean found = true;
+                for (int i = 0; i < types.length; i++) {
+                    if (types[i] != paramTypes[i]) {
+                        found = false;
+                        break;
+                    }
+                }
+                if (found) {
+                    return c;
+                }
+            }
+        }
+        return null;
+    }
+
+    /**
+     * Get the extension point by the interface that it implements
+     *
+     * @param extensionPointType The lookup key (extension point interface)
+     * @return The instance of the extension point
+     *
+     * @throws IllegalArgumentException if extensionPointType is null
+     */
+    public <T> T getExtensionPoint(Class<T> extensionPointType) {
+        if (extensionPointType == null) {
+            throw new IllegalArgumentException("Cannot lookup ExtensionPoint of type null");
+        }
+
+        Object extensionPoint = extensionPoints.get(extensionPointType);
+        if (extensionPoint == null) {
+            
+            // Dynamically load an extension point class declared under META-INF/services           
+            try {
+                Class<?> extensionPointClass = 
+                	ServiceDiscovery.getInstance().loadFirstServiceClass(extensionPointType);
+                if (extensionPointClass != null) {
+                    // Construct the extension point
+                    Constructor<?>[] constructors = extensionPointClass.getConstructors();
+                    Constructor<?> constructor = getConstructor(constructors, new Class<?>[] {ExtensionPointRegistry.class});
+                    if (constructor != null) {
+                        extensionPoint = constructor.newInstance(this);
+                    } else {
+                        constructor = getConstructor(constructors, new Class<?>[] {});
+                        if (constructor != null) {
+                            extensionPoint = constructor.newInstance();
+                        } else {
+                            throw new IllegalArgumentException(
+                                                               "No valid constructor is found for " + extensionPointClass);
+                        }
+                    }
+                   
+                    // Cache the loaded extension point
+                    addExtensionPoint(extensionPoint);
+                }
+            } catch (InvocationTargetException e) {
+                throw new IllegalArgumentException(e);
+            } catch (IOException e) {
+                throw new IllegalArgumentException(e);
+            } catch (ClassNotFoundException e) {
+                throw new IllegalArgumentException(e);
+            } catch (InstantiationException e) {
+                throw new IllegalArgumentException(e);
+            } catch (IllegalAccessException e) {
+                throw new IllegalArgumentException(e);
+            }
+        }
+        return extensionPointType.cast(extensionPoint);
+    }
+
+    /**
+     * Remove an extension point based on the interface that it implements
+     *
+     * @param extensionPoint The extension point to remove
+     *
+     * @throws IllegalArgumentException if extensionPoint is null
+     */
+    public void removeExtensionPoint(Object extensionPoint) {
+        if (extensionPoint == null) {
+            throw new IllegalArgumentException("Cannot remove null as an ExtensionPoint");
+        }
+
+        Set<Class> interfaces = getAllInterfaces(extensionPoint.getClass());
+        for (Class i : interfaces) {
+            extensionPoints.remove(i);
+        }
+    }
+
+    /**
+     * Returns the set of interfaces implemented by the given class and its
+     * ancestors or a blank set if none
+     */
+    private static Set<Class> getAllInterfaces(Class clazz) {
+        Set<Class> implemented = new HashSet<Class>();
+        getAllInterfaces(clazz, implemented);
+        return implemented;
+    }
+
+    private static void getAllInterfaces(Class clazz, Set<Class> implemented) {
+        Class[] interfaces = clazz.getInterfaces();
+        for (Class interfaze : interfaces) {
+//            String name = interfaze.getName();
+//            if (name.startsWith("java.") || name.startsWith("javax.")) {
+//                continue;
+//            }
+            if (Modifier.isPublic(interfaze.getModifiers())) {
+                implemented.add(interfaze);
+            }
+        }
+        Class<?> superClass = clazz.getSuperclass();
+        // Object has no superclass so check for null
+        if (superClass != null && !superClass.equals(Object.class)) {
+            getAllInterfaces(superClass, implemented);
+        }
+    }
+
+}
---
+++ modules/extensibility/src/main/java/org/apache/tuscany/sca/core/DefaultModuleActivatorExtensionPoint.java
@@ -0,0 +1,92 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.    
+ */
+
+package org.apache.tuscany.sca.core;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+import org.apache.tuscany.sca.extensibility.ServiceDeclaration;
+import org.apache.tuscany.sca.extensibility.ServiceDiscovery;
+
+/**
+ * Default implementation of an extension point to hold Tuscany module activators.
+ *
+ * @version $Rev$ $Date$
+ */
+public class DefaultModuleActivatorExtensionPoint implements ModuleActivatorExtensionPoint {
+    private List<ModuleActivator> activators = new ArrayList<ModuleActivator>();
+    private boolean loadedActivators;
+    
+    /**
+     * Constructs a new extension point. 
+     */
+    public DefaultModuleActivatorExtensionPoint() {
+    }
+
+    public void addModuleActivator(ModuleActivator activator) {
+        activators.add(activator);
+    }
+    
+    public List<ModuleActivator> getModuleActivators() {
+        loadModuleActivators();
+        return activators;
+    }
+    
+    public void removeModuleActivator(Object activator) {
+        activators.remove(activator);
+    }
+
+    /**
+     * Dynamically load module activators declared under META-INF/services
+     */
+    private void loadModuleActivators() {
+        if (loadedActivators)
+            return;
+
+        // Get the activator service declarations
+        Set<ServiceDeclaration> activatorDeclarations; 
+        try {
+            activatorDeclarations = ServiceDiscovery.getInstance().getServiceDeclarations(ModuleActivator.class);
+        } catch (IOException e) {
+            throw new IllegalStateException(e);
+        }
+        
+        // Load and instantiate module activators
+        for (ServiceDeclaration activatorDeclaration: activatorDeclarations) {
+            ModuleActivator activator;
+            try {
+                Class<ModuleActivator> activatorClass = (Class<ModuleActivator>)activatorDeclaration.loadClass();
+                activator = activatorClass.newInstance();
+            } catch (ClassNotFoundException e) {
+                throw new IllegalArgumentException(e);
+            } catch (InstantiationException e) {
+                throw new IllegalArgumentException(e);
+            } catch (IllegalAccessException e) {
+                throw new IllegalArgumentException(e);
+            }
+            addModuleActivator(activator);
+        }
+        
+        loadedActivators = true;
+    }
+
+}
