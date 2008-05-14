f01e3e2b75b46462f1093b5214ad809293a4c111
Mark Combellack <mcombellack@apache.org>
2008-05-14 09:08:05 +0000

    Added missing @version SVN revision tags
    git-svn-id: https://svn.apache.org/repos/asf/incubator/tuscany/java/sca@656166 13f79535-47bb-0310-9956-ffa450edef68

--- modules/databinding-fastinfoset/src/test/java/org/apache/tuscany/sca/databinding/fastinfoset/FastInfosetTransformerTestCase.java
+++ modules/databinding-fastinfoset/src/test/java/org/apache/tuscany/sca/databinding/fastinfoset/FastInfosetTransformerTestCase.java
@@ -34,6 +34,10 @@ import org.apache.tuscany.sca.databinding.xml.Node2OutputStream;
 import org.apache.tuscany.sca.databinding.xml.Node2String;
 import org.w3c.dom.Node;
 
+/**
+ *
+ * @version $Rev$ $Date$
+ */
 public class FastInfosetTransformerTestCase extends TestCase {
     private static final String IPO_XML =
         "<?xml version=\"1.0\"?>" + "<ipo:purchaseOrder"
