73c4c34d5d7e0a8024c0bd4f8ed18e1b66ae55b9
Mark Combellack <mcombellack@apache.org>
2008-05-21 12:42:03 +0000

    Removed unused imports
    git-svn-id: https://svn.apache.org/repos/asf/incubator/tuscany/java/sca@658659 13f79535-47bb-0310-9956-ffa450edef68

--- modules/implementation-bpel-ode/src/main/java/org/apache/tuscany/sca/implementation/bpel/provider/BPELInvoker.java
+++ modules/implementation-bpel-ode/src/main/java/org/apache/tuscany/sca/implementation/bpel/provider/BPELInvoker.java
@@ -25,7 +25,6 @@ import java.util.concurrent.Future;
 import javax.transaction.SystemException;
 import javax.transaction.TransactionManager;
 import javax.wsdl.Part;
-import javax.wsdl.Service;
 import javax.xml.namespace.QName;
 
 import org.apache.commons.logging.Log;
