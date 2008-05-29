ebd0ae733327749c27b4b65de906349ed7adc8a8
Zhaohui Feng <rfeng@apache.org>
2008-05-29 17:36:12 +0000

    Bring up the List support for doc-lit-wrapped Web Service
    git-svn-id: https://svn.apache.org/repos/asf/incubator/tuscany/java/sca@661416 13f79535-47bb-0310-9956-ffa450edef68

--- itest/databindings/jaxb-bottom-up/src/main/java/org/apache/tuscany/sca/itest/databindings/jaxb/impl/HelloServiceSimpleImpl.java
+++ itest/databindings/jaxb-bottom-up/src/main/java/org/apache/tuscany/sca/itest/databindings/jaxb/impl/HelloServiceSimpleImpl.java
@@ -24,6 +24,11 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import javax.jws.WebMethod;
+import javax.jws.WebParam;
+import javax.jws.WebResult;
+import javax.jws.WebService;
+
 import org.apache.tuscany.sca.itest.databindings.jaxb.HelloServiceSimple;
 import org.osoa.sca.annotations.Service;
 
@@ -31,43 +36,63 @@ import org.osoa.sca.annotations.Service;
  * An implementation of HelloServiceSimple.
  */
 @Service(HelloServiceSimple.class)
+@WebService(targetNamespace = "http://jaxb.databindings.itest.sca.tuscany.apache.org/")
 public class HelloServiceSimpleImpl implements HelloServiceSimple {
 
-    public String getGreetings(String name) {
-        return "Hello "+name;
+    @WebMethod
+    @WebResult(name = "return", targetNamespace = "")
+    public String getGreetings(@WebParam(name = "arg0", targetNamespace = "")
+    String name) {
+        return "Hello " + name;
     }
 
-    public String[] getGreetingsArray(String[] names) {
-        for(int i = 0; i < names.length; ++i) {
-            names[i] = "Hello "+names[i];
+    @WebMethod
+    @WebResult(name = "return", targetNamespace = "")
+    public String[] getGreetingsArray(@WebParam(name = "arg0", targetNamespace = "")
+    String[] names) {
+        for (int i = 0; i < names.length; ++i) {
+            names[i] = "Hello " + names[i];
         }
         return names;
     }
 
-    public List<String> getGreetingsList(List<String> names) {
-        for(int i = 0; i < names.size(); ++i) {
-            names.set(i, "Hello "+names.get(i));
+    @WebMethod
+    @WebResult(name = "return", targetNamespace = "")
+    public List<String> getGreetingsList(@WebParam(name = "arg0", targetNamespace = "")
+    List<String> names) {
+        for (int i = 0; i < names.size(); ++i) {
+            names.set(i, "Hello " + names.get(i));
         }
         return names;
     }
 
-    public ArrayList<String> getGreetingsArrayList(ArrayList<String> names) {
-        for(int i = 0; i < names.size(); ++i) {
-            names.set(i, "Hello "+names.get(i));
+    @WebMethod
+    @WebResult(name = "return", targetNamespace = "")
+    public ArrayList<String> getGreetingsArrayList(@WebParam(name = "arg0", targetNamespace = "")
+    ArrayList<String> names) {
+        for (int i = 0; i < names.size(); ++i) {
+            names.set(i, "Hello " + names.get(i));
         }
         return names;
     }
 
-    public Map<String, String> getGreetingsMap(Map<String, String> namesMap) {
-        for(Map.Entry<String, String> entry: namesMap.entrySet()) {
-            entry.setValue("Hello "+entry.getKey());
+    //    @WebMethod
+    //    @WebResult(name = "return", targetNamespace = "")
+    public Map<String, String> getGreetingsMap(
+    // @WebParam(name = "arg0", targetNamespace = "")
+    Map<String, String> namesMap) {
+        for (Map.Entry<String, String> entry : namesMap.entrySet()) {
+            entry.setValue("Hello " + entry.getKey());
         }
         return namesMap;
     }
 
-    public HashMap<String, String> getGreetingsHashMap(HashMap<String, String> namesMap) {
-        for(Map.Entry<String, String> entry: namesMap.entrySet()) {
-            entry.setValue("Hello "+entry.getKey());
+    @WebMethod
+    @WebResult(name = "return", targetNamespace = "")
+    public HashMap<String, String> getGreetingsHashMap(@WebParam(name = "arg0", targetNamespace = "")
+    HashMap<String, String> namesMap) {
+        for (Map.Entry<String, String> entry : namesMap.entrySet()) {
+            entry.setValue("Hello " + entry.getKey());
         }
         return namesMap;
     }
