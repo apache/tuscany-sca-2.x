b3e7b45734ad3b10684c4962b2efb6da92d74c21
Anthony Elder <antelder@apache.org>
2008-06-02 11:17:33 +0000

    TUSCANY-2357: Apply patch from Wojtek Janiszewski for CORBA objects invocation mechanism
    git-svn-id: https://svn.apache.org/repos/asf/incubator/tuscany/java/sca@662395 13f79535-47bb-0310-9956-ffa450edef68

---
+++ modules/binding-corba/src/test/java/org/apache/tuscany/sca/binding/corba/testing/hierarchy/SimpleStruct.java
@@ -0,0 +1,8 @@
+package org.apache.tuscany.sca.binding.corba.testing.hierarchy;
+
+public class SimpleStruct {
+	
+	public String field1;
+	public int field2;
+	
+}
---
+++ modules/binding-corba/src/test/java/org/apache/tuscany/sca/binding/corba/testing/servants/PrimitivesSetterServant.java
@@ -0,0 +1,70 @@
+package org.apache.tuscany.sca.binding.corba.testing.servants;
+
+import org.apache.tuscany.sca.binding.corba.testing.generated.RemoteObject;
+import org.apache.tuscany.sca.binding.corba.testing.generated._PrimitivesSetterImplBase;
+
+public class PrimitivesSetterServant extends _PrimitivesSetterImplBase {
+	
+	private static final long serialVersionUID = 1L;
+
+	public boolean setBoolean(boolean arg) {
+		return arg;
+	}
+
+	public char setChar(char arg) {
+		return arg;
+	}
+
+	public double setDouble(double arg) {
+		return arg;
+	}
+
+	public float setFloat(float arg) {
+		return arg;
+	}
+
+	public int setLong(int arg) {
+		return arg;
+	}
+
+	public long setLongLong(long arg) {
+		return arg;
+	}
+
+	public byte setOctet(byte arg) {
+		return arg;
+	}
+
+	public RemoteObject setRemoteObject(RemoteObject obj) {
+		return obj;
+	}
+
+	public short setShort(short arg) {
+		return arg;
+	}
+
+	public String setString(String arg) {
+		return arg;
+	}
+
+	public int setUnsignedLong(int arg) {
+		return arg;
+	}
+
+	public long setUnsignedLongLong(long arg) {
+		return arg;
+	}
+
+	public short setUnsignedShort(short arg) {
+		return arg;
+	}
+
+	public char setWchar(char arg) {
+		return arg;
+	}
+
+	public String setWstring(String arg) {
+		return arg;
+	}
+
+}
