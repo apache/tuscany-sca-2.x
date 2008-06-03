8caf42fdff7901eeea0ef3d99321d1e74086f464
Kevin Joe Williams <kwilliams@apache.org>
2008-06-03 21:03:41 +0000

    Add structure for Assembly specification vtest
    git-svn-id: https://svn.apache.org/repos/asf/incubator/tuscany/java/sca@662887 13f79535-47bb-0310-9956-ffa450edef68

---
+++ vtest/assembly/ctypefile/src/test/java/org/apache/tuscany/sca/vtest/assembly/ctypefile/CompomnentTypeFileTestCase.java
@@ -0,0 +1,63 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.    
+ */
+
+package org.apache.tuscany.sca.vtest.assembly.ctypefile;
+
+import junit.framework.Assert;
+
+import org.apache.tuscany.sca.vtest.utilities.ServiceFinder;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * This test class tests the Service annotation described in section 1.2.1 and
+ * 1.8.17
+ */
+public class CompomnentTypeFileTestCase {
+
+    protected static String compositeName = "typefile.composite";
+    protected static AService aService = null;
+
+    @BeforeClass
+    public static void init() throws Exception {
+        try {
+            System.out.println("Setting up");
+            ServiceFinder.init(compositeName);
+            aService = ServiceFinder.getService(AService.class, "AComponent");
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    @AfterClass
+    public static void destroy() throws Exception {
+
+        System.out.println("Cleaning up");
+        ServiceFinder.cleanup();
+
+    }
+
+    @Test
+    public void typeFile1() throws Exception {
+        String thisState = "This State";
+        Assert.assertSame("SomeStateFromB", aService.getState());
+    }
+
+}
