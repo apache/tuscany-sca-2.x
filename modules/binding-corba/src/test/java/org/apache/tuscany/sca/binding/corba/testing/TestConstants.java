09998435cc04965a8493e8e00f93410f178138df
Anthony Elder <antelder@apache.org>
2008-06-04 11:34:15 +0000

    TUSCANY-2357: Apply patch from Wojtek Janiszewski for CORBA objects invocation mechanism
    git-svn-id: https://svn.apache.org/repos/asf/incubator/tuscany/java/sca@663066 13f79535-47bb-0310-9956-ffa450edef68

--- modules/binding-corba/src/test/java/org/apache/tuscany/sca/binding/corba/testing/TestConstants.java
+++ modules/binding-corba/src/test/java/org/apache/tuscany/sca/binding/corba/testing/TestConstants.java
@@ -1,5 +1,27 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.    
+ */
+
 package org.apache.tuscany.sca.binding.corba.testing;
 
+/**
+ * @version $Rev$ $Date$
+ */
 public class TestConstants {
 
 	public static final String STR_1 = "Hello world!";
@@ -13,6 +35,6 @@ public class TestConstants {
 	public static final int[][][] INT_ARRAY_3_DIM = { { { 1, 2 }, { 3, 4 } },
 			{ { 5, 6 }, { 7, 8 } } };
 
-	public static final long TNAMESERV_SPAWN_WAIT = 100;
+	public static final long TNAMESERV_SPAWN_WAIT = 500;
 
 }
--- modules/binding-corba/src/test/java/org/apache/tuscany/sca/binding/corba/testing/servants/TestObjectServant.java
+++ modules/binding-corba/src/test/java/org/apache/tuscany/sca/binding/corba/testing/servants/TestObjectServant.java
@@ -1,3 +1,22 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.    
+ */
+
 package org.apache.tuscany.sca.binding.corba.testing.servants;
 
 import org.apache.tuscany.sca.binding.corba.testing.generated.SimpleStruct;
@@ -9,6 +28,9 @@ import org.apache.tuscany.sca.binding.corba.testing.generated.long_seq1Holder;
 import org.apache.tuscany.sca.binding.corba.testing.generated.long_seq2Holder;
 import org.apache.tuscany.sca.binding.corba.testing.generated.long_seq3Holder;
 
+/**
+ * @version $Rev$ $Date$
+ */
 public class TestObjectServant extends _TestObjectImplBase {
 
 	private static final long serialVersionUID = 1L;
