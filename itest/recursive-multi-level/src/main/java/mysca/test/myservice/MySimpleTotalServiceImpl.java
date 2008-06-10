a02c3deeab6b290e690f5b08806ed3679b763486
Simon Laws <slaws@apache.org>
2008-06-10 07:01:58 +0000

    TUSCANY-2360 - Correct operation of autowire within recursive composites. Stop autowire wiring to self. Make top level composite reference override promoted reference.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/tuscany/java/sca@665992 13f79535-47bb-0310-9956-ffa450edef68

---
+++ itest/recursive-multi-level/src/main/java/mysca/test/myservice/MySimpleTotalServiceImpl.java
@@ -0,0 +1,46 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.    
+ */
+
+package mysca.test.myservice;
+
+import org.osoa.sca.annotations.Reference;
+import org.osoa.sca.annotations.Service;
+
+import mysca.test.myservice.MyService;
+import mysca.test.myservice.MySimpleTotalService;
+
+@Service(MySimpleTotalService.class)
+public class MySimpleTotalServiceImpl implements MySimpleTotalService
+{
+    // default required==true so it is 1:1
+    @Reference
+    public MyService myService;
+
+
+    public String getLocation()
+    {
+        return myService.getLocation();
+    }
+
+    public String getYear()
+    {
+        return myService.getYear();
+    }
+
+}
