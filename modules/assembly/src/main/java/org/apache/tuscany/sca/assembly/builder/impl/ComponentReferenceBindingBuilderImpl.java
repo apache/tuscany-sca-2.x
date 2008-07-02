ec051310fe6f2f0a3eec09e9942858af77bdf5aa
Simon Nash <nash@apache.org>
2008-07-02 07:58:35 +0000

    Fix TUSCANY-2449
    git-svn-id: https://svn.apache.org/repos/asf/tuscany/java/sca@673310 13f79535-47bb-0310-9956-ffa450edef68

--- modules/assembly/src/main/java/org/apache/tuscany/sca/assembly/builder/impl/ComponentReferenceBindingBuilderImpl.java
+++ modules/assembly/src/main/java/org/apache/tuscany/sca/assembly/builder/impl/ComponentReferenceBindingBuilderImpl.java
@@ -21,10 +21,9 @@ package org.apache.tuscany.sca.assembly.builder.impl;
 
 import org.apache.tuscany.sca.assembly.Binding;
 import org.apache.tuscany.sca.assembly.Component;
+import org.apache.tuscany.sca.assembly.ComponentReference;
 import org.apache.tuscany.sca.assembly.Composite;
-import org.apache.tuscany.sca.assembly.CompositeService;
 import org.apache.tuscany.sca.assembly.Implementation;
-import org.apache.tuscany.sca.assembly.Service;
 import org.apache.tuscany.sca.assembly.builder.BindingBuilderExtension;
 import org.apache.tuscany.sca.assembly.builder.CompositeBuilder;
 import org.apache.tuscany.sca.assembly.builder.CompositeBuilderException;
@@ -32,37 +31,38 @@ import org.apache.tuscany.sca.monitor.Monitor;
 
 /**
  * A composite builder that performs any additional building steps that
- * composite service bindings may need.  Used for WSDL generation.
+ * component reference bindings may need.  Used for WSDL generation.
  *
  * @version $Rev$ $Date$
  */
-public class CompositeServiceBindingBuilderImpl implements CompositeBuilder {
+public class ComponentReferenceBindingBuilderImpl implements CompositeBuilder {
     private Monitor monitor;
 
-    public CompositeServiceBindingBuilderImpl(Monitor monitor) {
+    public ComponentReferenceBindingBuilderImpl(Monitor monitor) {
         this.monitor = monitor;
     }
 
     public void build(Composite composite) throws CompositeBuilderException {
-        buildServiceBindings(composite);
+        buildReferenceBindings(composite);
     }
     
-    private void buildServiceBindings(Composite composite) {
+    private void buildReferenceBindings(Composite composite) {
         
         // build bindings recursively
         for (Component component : composite.getComponents()) {
             Implementation implementation = component.getImplementation();
             if (implementation instanceof Composite) {
-                buildServiceBindings((Composite)implementation);
+                buildReferenceBindings((Composite)implementation);
             }
         }
-
-        // find all the composite service bindings     
-        for (Service service : composite.getServices()) {
-            for (Binding binding : service.getBindings()) {
-                if (binding instanceof BindingBuilderExtension) {
-                    Component component = BaseConfigurationBuilderImpl.getPromotedComponent((CompositeService)service);
-                    ((BindingBuilderExtension)binding).getBuilder().build(component, service, binding, monitor);
+    
+        // find all the component reference bindings     
+        for (Component component : composite.getComponents()) {
+            for (ComponentReference componentReference : component.getReferences()) {
+                for (Binding binding : componentReference.getBindings()) {
+                    if (binding instanceof BindingBuilderExtension) {
+                        ((BindingBuilderExtension)binding).getBuilder().build(component, componentReference, binding, monitor);
+                    }
                 }
             }
         }
