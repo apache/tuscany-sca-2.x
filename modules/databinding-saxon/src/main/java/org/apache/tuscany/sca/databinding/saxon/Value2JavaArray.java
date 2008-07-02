f860a20cb2ab4f273b6b0fc708dc3c551f3b4823
Zhaohui Feng <rfeng@apache.org>
2008-07-02 06:14:13 +0000

    Fix the compilation errors
    git-svn-id: https://svn.apache.org/repos/asf/tuscany/java/sca@673290 13f79535-47bb-0310-9956-ffa450edef68

--- modules/databinding-saxon/src/main/java/org/apache/tuscany/sca/databinding/saxon/Value2JavaArray.java
+++ modules/databinding-saxon/src/main/java/org/apache/tuscany/sca/databinding/saxon/Value2JavaArray.java
@@ -20,8 +20,6 @@ package org.apache.tuscany.sca.databinding.saxon;
 
 import java.lang.reflect.Array;
 
-import net.sf.saxon.om.Item;
-import net.sf.saxon.om.NodeInfo;
 import net.sf.saxon.trans.XPathException;
 import net.sf.saxon.value.Value;
 
@@ -29,50 +27,48 @@ import org.apache.tuscany.sca.databinding.PullTransformer;
 import org.apache.tuscany.sca.databinding.TransformationContext;
 import org.apache.tuscany.sca.databinding.TransformationException;
 import org.apache.tuscany.sca.databinding.impl.BaseTransformer;
-import org.apache.tuscany.sca.databinding.saxon.collection.ItemList;
 
 /**
  * Transforms a Value object to a Java Object and return it in an array.
  * 
  * @version $Rev: 659284 $ $Date: 2008-05-22 14:26:18 -0800 (Thu, 22 May 2008) $
  */
-public class Value2JavaArray extends
-		BaseTransformer<Value, Object[]> implements
-		PullTransformer<Value, Object[]> {
+public class Value2JavaArray extends BaseTransformer<Value, Object[]> implements PullTransformer<Value, Object[]> {
 
-	public Value2JavaArray() {}
+    public Value2JavaArray() {
+    }
 
-	@Override
-	protected Class<Value> getSourceType() {
-		return Value.class;
-	}
+    @Override
+    protected Class<Value> getSourceType() {
+        return Value.class;
+    }
 
-	@Override
-	protected Class<Object[]> getTargetType() {
-		return Object[].class;
-	}
+    @Override
+    protected Class<Object[]> getTargetType() {
+        return Object[].class;
+    }
 
-	@Override
-	public int getWeight() {
-		return super.getWeight() + 100;
-	}
+    @Override
+    public int getWeight() {
+        return super.getWeight() + 100;
+    }
 
-	public Object[] transform(Value source, TransformationContext context) {
-		
-		try {
-			Object object = Value.convertToJava(Value.asItem(source));
-			
-			Class<?> componentClass = context.getTargetDataType().getPhysical().getComponentType();
-			Object[] result = (Object[]) Array.newInstance(componentClass, 1);
-			
-			result[0] = object;
-			
-			return result;
-			
-		} catch (XPathException e) {
-			throw new TransformationException(e.getMessage());
-		}
+    public Object[] transform(Value source, TransformationContext context) {
 
-	}
+        try {
+            Object object = Value.convert(Value.asItem(source));
+
+            Class<?> componentClass = context.getTargetDataType().getPhysical().getComponentType();
+            Object[] result = (Object[])Array.newInstance(componentClass, 1);
+
+            result[0] = object;
+
+            return result;
+
+        } catch (XPathException e) {
+            throw new TransformationException(e.getMessage());
+        }
+
+    }
 
 }
