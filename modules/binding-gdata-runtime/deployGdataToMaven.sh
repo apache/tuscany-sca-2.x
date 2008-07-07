bd7a44f27314b3e4b2f3ec229ad99c861cd49c66
Mark Combellack <mcombellack@apache.org>
2008-07-07 09:02:37 +0000

    Fixed spelling mistake in google (was googoe) that was prevening the gdata-core artefact being uploaded
    git-svn-id: https://svn.apache.org/repos/asf/tuscany/java/sca@674425 13f79535-47bb-0310-9956-ffa450edef68

--- modules/binding-gdata-runtime/deployGdataToMaven.sh
+++ modules/binding-gdata-runtime/deployGdataToMaven.sh
@@ -1,3 +1,3 @@
-mvn gpg:sign-and-deploy-file -DgroupId=com.google.gdata -DartifactId=gdata-core -Dversion=1.0 -Dpackaging=jar -Dfile=gdata-core-1.0.jar -DrepositoryId=apache.people -Durl=scp://people.apache.org/home/lresende/public_html/googoe-gdata/maven -Dkeyname=lresende 
+mvn gpg:sign-and-deploy-file -DgroupId=com.google.gdata -DartifactId=gdata-core -Dversion=1.0 -Dpackaging=jar -Dfile=gdata-core-1.0.jar -DrepositoryId=apache.people -Durl=scp://people.apache.org/home/lresende/public_html/google-gdata/maven -Dkeyname=lresende 
 
 mvn gpg:sign-and-deploy-file -DgroupId=com.google.gdata -DartifactId=gdata-client -Dversion=1.0 -Dpackaging=jar -Dfile=gdata-client-1.0.jar -DrepositoryId=apache.people -Durl=scp://people.apache.org/home/lresende/public_html/google-gdata/maven -Dkeyname=lresende
