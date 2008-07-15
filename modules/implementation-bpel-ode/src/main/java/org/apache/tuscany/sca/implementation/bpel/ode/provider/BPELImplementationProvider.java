8fa04cc6d8fdc25cc80b205510f448bab83d6881
Zhaohui Feng <rfeng@apache.org>
2008-07-15 22:06:58 +0000

    Change the package names and bundle symbolic name to avoid conflicts in OSGi
    git-svn-id: https://svn.apache.org/repos/asf/tuscany/java/sca@677065 13f79535-47bb-0310-9956-ffa450edef68

--- modules/implementation-bpel-ode/src/main/java/org/apache/tuscany/sca/implementation/bpel/ode/provider/BPELImplementationProvider.java
+++ modules/implementation-bpel-ode/src/main/java/org/apache/tuscany/sca/implementation/bpel/ode/provider/BPELImplementationProvider.java
@@ -16,7 +16,7 @@
  * specific language governing permissions and limitations
  * under the License.    
  */
-package org.apache.tuscany.sca.implementation.bpel.provider;
+package org.apache.tuscany.sca.implementation.bpel.ode.provider;
 
 import java.io.File;
 import java.net.URL;
--- modules/implementation-bpel-ode/src/main/java/org/apache/tuscany/sca/implementation/bpel/ode/provider/BPELImplementationProviderFactory.java
+++ modules/implementation-bpel-ode/src/main/java/org/apache/tuscany/sca/implementation/bpel/ode/provider/BPELImplementationProviderFactory.java
@@ -16,7 +16,7 @@
  * specific language governing permissions and limitations
  * under the License.    
  */
-package org.apache.tuscany.sca.implementation.bpel.provider;
+package org.apache.tuscany.sca.implementation.bpel.ode.provider;
 
 import javax.transaction.TransactionManager;
 
