e35c3f5ba031772dfd660d1080176edcfb0eb15e
Luciano Resende <lresende@apache.org>
2008-08-01 21:42:45 +0000

    TUSCANY-2494 - Applying Douglas patch
    git-svn-id: https://svn.apache.org/repos/asf/tuscany/java/sca@681850 13f79535-47bb-0310-9956-ffa450edef68

--- modules/binding-gdata/src/main/java/org/apache/tuscany/sca/binding/gdata/GDataImplementationProcessor.java
+++ modules/binding-gdata/src/main/java/org/apache/tuscany/sca/binding/gdata/GDataImplementationProcessor.java
@@ -1,18 +1,20 @@
 /*
- *  Copyright 2008 douglas.
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
  * 
- *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
+ *   http://www.apache.org/licenses/LICENSE-2.0
  * 
- *       http://www.apache.org/licenses/LICENSE-2.0
- * 
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
- *  under the License.
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.    
  */
 package org.apache.tuscany.sca.binding.gdata;
 
@@ -34,8 +36,8 @@ public class GDataImplementationProcessor implements StAXArtifactProcessor<GData
     protected static final QName BINDING_GDATA = new QName(Constants.SCA10_TUSCANY_NS, "binding.gdata");
 
     public GDataImplementationProcessor(ModelFactoryExtensionPoint modelFactories, Monitor monitor) {
-        
-        this.gdataFactory = modelFactories.getFactory(GDataBindingFactory.class);
+
+        this.gdataFactory = (GDataBindingFactory) modelFactories.getFactory(GDataBindingFactory.class);
     }
 
     public GDataBinding read(XMLStreamReader reader) {
@@ -51,11 +53,13 @@ public class GDataImplementationProcessor implements StAXArtifactProcessor<GData
          */
 
         String uri = reader.getAttributeValue(null, "uri");
-        String login = reader.getAttributeValue(null, "login");
+        String serviceType = reader.getAttributeValue(null, "serviceType");
+        String login = reader.getAttributeValue(null, "username");
         String password = reader.getAttributeValue(null, "password");
 
         gdataBinding.setURI(uri);
-        gdataBinding.setLogin(login);
+        gdataBinding.setServiceType(serviceType);
+        gdataBinding.setUsername(login);
         gdataBinding.setPassword(password);
 
         return gdataBinding;
@@ -68,13 +72,17 @@ public class GDataImplementationProcessor implements StAXArtifactProcessor<GData
             if (gdataBinding.getURI() != null) {
                 writer.writeAttribute("uri", gdataBinding.getURI());
             }
-            if (gdataBinding.getLogin() != null) {
-                writer.writeAttribute("login", gdataBinding.getLogin());
+
+            if (gdataBinding.getServiceType() != null) {
+                writer.writeAttribute("serviceType", gdataBinding.getServiceType());
+            }
+            if (gdataBinding.getUsername() != null) {
+                writer.writeAttribute("username", gdataBinding.getUsername());
             }
             if (gdataBinding.getPassword() != null) {
                 writer.writeAttribute("password", gdataBinding.getPassword());
             }
-
+            
             writer.writeEndElement();
         }
     }
