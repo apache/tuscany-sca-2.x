2fc9e336c3b6a9f8490d7679f73e7bf578470ea1
Zhaohui Feng <rfeng@apache.org>
2008-09-06 01:37:56 +0000

    Add a sample to use Equinox OSGi launcher
    git-svn-id: https://svn.apache.org/repos/asf/tuscany/java/sca@692602 13f79535-47bb-0310-9956-ffa450edef68

---
+++ samples/calculator-osgi/src/test/java/calculator/CalculatorTestCase.java
@@ -0,0 +1,72 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.    
+ */
+package calculator;
+
+import junit.framework.TestCase;
+
+import org.apache.tuscany.sca.node.SCANode;
+import org.apache.tuscany.sca.node.equinox.launcher.NodeLauncher;
+import org.osoa.sca.annotations.EagerInit;
+import org.osoa.sca.annotations.Init;
+import org.osoa.sca.annotations.Reference;
+import org.osoa.sca.annotations.Scope;
+
+/**
+ * This shows how to test the Calculator composition.
+ */
+@Scope("COMPOSITE")
+@EagerInit
+public class CalculatorTestCase extends TestCase {
+
+    private static CalculatorService calculatorService;
+    private NodeLauncher launcher;
+    private SCANode node;
+
+    @Reference
+    public void setCalculatorService(CalculatorService calculatorService) {
+        CalculatorTestCase.calculatorService = calculatorService;
+    }
+
+    @Override
+    protected void setUp() throws Exception {
+        launcher = NodeLauncher.newInstance();
+        node = launcher.createNodeFromClassLoader("CalculatorTest.composite", getClass().getClassLoader());
+        node.start();
+    }
+
+    @Override
+    protected void tearDown() throws Exception {
+        if (launcher != null) {
+            node.stop();
+            launcher.destroy();
+        }
+    }
+
+    public void testDummy() {
+    }
+
+    @Init
+    public void init() throws Exception {
+        // Calculate
+        assertEquals(calculatorService.add(3, 2), 5.0);
+        assertEquals(calculatorService.subtract(3, 2), 1.0);
+        assertEquals(calculatorService.multiply(3, 2), 6.0);
+        assertEquals(calculatorService.divide(3, 2), 1.5);
+    }
+}
