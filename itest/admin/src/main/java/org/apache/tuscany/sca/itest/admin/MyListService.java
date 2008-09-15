6b274c9f31b0a02d7eedc2bb07ac876f1cf7167a
Jean Sebastien Delfino <jsdelfino@apache.org>
2008-09-15 09:31:45 +0000

    Minor cleanup, removed a few unused modules. Set missing svn:ignore properties.
    git-svn-id: https://svn.apache.org/repos/asf/tuscany/branches/sca-equinox@695392 13f79535-47bb-0310-9956-ffa450edef68

--- itest/admin/src/main/java/org/apache/tuscany/sca/itest/admin/MyListService.java
+++
@@ -1,9 +0,0 @@
-package org.apache.tuscany.sca.itest.admin;
-
-import org.osoa.sca.annotations.Remotable;
-
-@Remotable
-public interface MyListService {
-	String[] getHolidays();
-    String getYear();
-}
--- itest/admin/src/main/java/org/apache/tuscany/sca/itest/admin/MyListServiceByYear.java
+++
@@ -1,8 +0,0 @@
-package org.apache.tuscany.sca.itest.admin;
-
-import org.osoa.sca.annotations.Remotable;
-
-@Remotable
-public interface MyListServiceByYear {
-	String[] getHolidays(int year);
-}
