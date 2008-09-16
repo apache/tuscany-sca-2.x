6773cd767644d6e602276f2f54d4c0ea52900864
Simon Laws <slaws@apache.org>
2008-09-16 14:51:24 +0000

    Add in policies to support JMS message header setting. Also add in reliability intents. No policies or implementation changes to support these reliability intents here.
    git-svn-id: https://svn.apache.org/repos/asf/tuscany/java/sca@695928 13f79535-47bb-0310-9956-ffa450edef68

--- modules/binding-jms-policy/src/main/java/org/apache/tuscany/sca/binding/jms/policy/header/JMSHeaderPolicy.java
+++ modules/binding-jms-policy/src/main/java/org/apache/tuscany/sca/binding/jms/policy/header/JMSHeaderPolicy.java
@@ -18,6 +18,9 @@
  */
 package org.apache.tuscany.sca.binding.jms.policy.header;
 
+import java.util.Hashtable;
+import java.util.Map;
+
 import javax.xml.namespace.QName;
 
 import org.apache.tuscany.sca.assembly.xml.Constants;
@@ -30,21 +33,68 @@ import org.apache.tuscany.sca.policy.Policy;
  * @version $Rev$ $Date$
  */
 public class JMSHeaderPolicy implements Policy {
-    public static final QName AXIS2_HEADER_POLICY_QNAME = new QName(Constants.SCA10_TUSCANY_NS, "axis2Header");
-    public static final String AXIS2_HEADER_NAME = "headerName";
+    public static final QName JMS_HEADER_POLICY_QNAME = new QName(Constants.SCA10_TUSCANY_NS, "jmsHeader");
+    public static final String JMS_HEADER_JMS_TYPE = "JMSType";
+    public static final String JMS_HEADER_JMS_CORRELATION_ID = "JMSDeliveryMode";
+    public static final String JMS_HEADER_JMS_DELIVERY_MODE = "JMSDeliveryMode";
+    public static final String JMS_HEADER_JMS_TIME_TO_LIVE = "JMSTimeToLive";
+    public static final String JMS_HEADER_JMS_PRIORITY = "JMSPriority";
+    public static final String JMS_HEADER_JMS_PROPERTY = "property";
+    public static final String JMS_HEADER_JMS_PROPERTY_NAME = "name";
 
-    private QName headerName;
-
-    public QName getHeaderName() {
-        return headerName;
+    private String jmsType = null;
+    private String jmsCorrelationId = null;
+    private Boolean deliveryModePersistent = null;
+    private Long timeToLive = null;
+    private Integer jmsPriority = null;
+    private Map<String, String> properties = new Hashtable<String, String>();
+    
+    public String getJmsType() {
+        return jmsType;
     }
     
-    public void setHeaderName(QName headerName) {
-        this.headerName = headerName;
+    public void setJmsType(String jmsType) {
+        this.jmsType = jmsType;
     }
     
+    public String getJmsCorrelationId() {
+        return jmsCorrelationId;
+    }
+    
+    public void setJmsCorrelationId(String jmsCorrelationId) {
+        this.jmsCorrelationId = jmsCorrelationId;
+    }
+    
+    public Boolean getDeliveryModePersistent() {
+        return deliveryModePersistent;
+    }
+    
+    public void setDeliveryModePersistent(Boolean deliveryModePersistent) {
+        this.deliveryModePersistent = deliveryModePersistent;
+    }
+    
+    public Long getTimeToLive() {
+        return timeToLive;
+    }
+    
+    public void setTimeToLive(Long timeToLive) {
+        this.timeToLive = timeToLive;
+    }
+    
+    public Integer getJmsPriority() {
+        return jmsPriority;
+    }
+    
+    public void setJmsPriority(Integer jmsPriority) {
+        this.jmsPriority = jmsPriority;
+    }
+    
+    public Map<String, String> getProperties() {
+        return properties;
+    }
+  
     public QName getSchemaName() {
-        return AXIS2_HEADER_POLICY_QNAME;
+        return JMS_HEADER_POLICY_QNAME;
     }
 
     public boolean isUnresolved() {
@@ -52,5 +102,30 @@ public class JMSHeaderPolicy implements Policy {
     }
 
     public void setUnresolved(boolean unresolved) {
+    }
+    
+    @Override
+    public String toString() {
+        String result = "jmsHeader";
+        
+        result += " JMSType ";
+        result += getJmsType();
+        result += " JMSDeliveryMode ";
+        result += getJmsCorrelationId();
+        result += " JMSDeliveryMode ";
+        result += getDeliveryModePersistent();
+        result += " JMSTimeToLive ";
+        result += getTimeToLive();
+        result += " JMSPriority ";
+        result += getJmsPriority();
+        
+        for (String propertyName : properties.keySet()){
+            result += " property ";
+            result += propertyName;
+            result += " ";
+            result += properties.get(propertyName);
+        }
+        
+        return result;
     }
 }
