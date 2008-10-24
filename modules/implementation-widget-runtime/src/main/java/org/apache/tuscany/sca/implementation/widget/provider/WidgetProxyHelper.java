613f21713328cef22a4ebeb530fb9ebd466a123d
Luciano Resende <lresende@apache.org>
2008-10-24 22:41:51 +0000

    Fixing issue where JSON-RPC binding was not being concatenated to generated java script proxy and was causing catalog.get is not a function when running store sample.
    git-svn-id: https://svn.apache.org/repos/asf/tuscany/branches/sca-equinox@707770 13f79535-47bb-0310-9956-ffa450edef68

--- modules/implementation-widget-runtime/src/main/java/org/apache/tuscany/sca/implementation/widget/provider/WidgetProxyHelper.java
+++ modules/implementation-widget-runtime/src/main/java/org/apache/tuscany/sca/implementation/widget/provider/WidgetProxyHelper.java
@@ -6,15 +6,15 @@
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
  * with the License.  You may obtain a copy of the License at
- * 
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
  * KIND, either express or implied.  See the License for the
  * specific language governing permissions and limitations
- * under the License.    
+ * under the License.
  */
 
 package org.apache.tuscany.sca.implementation.widget.provider;
@@ -33,18 +33,18 @@ class WidgetProxyHelper {
     static {
         proxyFileRegistry.put("org.apache.tuscany.sca.binding.atom.impl.AtomBindingImpl", "binding-atom.js");
         proxyClient.put("org.apache.tuscany.sca.binding.atom.impl.AtomBindingImpl", "AtomClient");
-        
-        proxyFileRegistry.put("org.apache.tuscany.sca.binding.jsonrpc.JSONRPCBinding", "binding-jsonrpc.js");
-        proxyClient.put("org.apache.tuscany.sca.binding.jsonrpc.JSONRPCBinding", "JSONRpcClient");
+
+        proxyFileRegistry.put("org.apache.tuscany.sca.binding.jsonrpc.impl.JSONRPCBindingImpl", "binding-jsonrpc.js");
+        proxyClient.put("org.apache.tuscany.sca.binding.jsonrpc.impl.JSONRPCBindingImpl", "JSONRpcClient");
 
         proxyFileRegistry.put("org.apache.tuscany.sca.binding.http.impl.HTTPBindingImpl", "binding-http.js");
         proxyClient.put("org.apache.tuscany.sca.binding.http.impl.HTTPBindingImpl", "HTTPClient");
     }
-    
+
     static String getJavaScriptProxyFile(String bindingClass) {
         return proxyFileRegistry.get(bindingClass);
     }
-    
+
     static String getJavaScriptProxyClient(String bindingClass) {
         return proxyClient.get(bindingClass);
     }
