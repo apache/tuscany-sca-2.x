a1c78ee2c122e50d8109982faeec6e2a880fbac2
Mike Edwards <edwardsmj@apache.org>
2008-12-05 17:34:16 +0000

    Changes to make policy-security work in 2.0.
    SecurityUtil.java moved to org.apache.tuscany.policy.security to avoid a clash with a package name used in the policy module
    
    git-svn-id: https://svn.apache.org/repos/asf/tuscany/java/sca@723800 13f79535-47bb-0310-9956-ffa450edef68

--- modules/policy-security/src/main/java/org/apache/tuscany/sca/policy/authentication/basic/BasicAuthenticationServicePolicyInterceptor.java
+++ modules/policy-security/src/main/java/org/apache/tuscany/sca/policy/authentication/basic/BasicAuthenticationServicePolicyInterceptor.java
@@ -27,7 +27,7 @@ import org.apache.tuscany.sca.invocation.Interceptor;
 import org.apache.tuscany.sca.invocation.Invoker;
 import org.apache.tuscany.sca.invocation.Message;
 import org.apache.tuscany.sca.policy.PolicySet;
-import org.apache.tuscany.sca.policy.SecurityUtil;
+import org.apache.tuscany.sca.policy.security.SecurityUtil;
 
 
 /**
--- modules/policy-security/src/main/java/org/apache/tuscany/sca/policy/security/SecurityUtil.java
+++ modules/policy-security/src/main/java/org/apache/tuscany/sca/policy/security/SecurityUtil.java
@@ -16,7 +16,7 @@
  * specific language governing permissions and limitations
  * under the License.    
  */
-package org.apache.tuscany.sca.policy;
+package org.apache.tuscany.sca.policy.security;
 
 import java.security.Principal;
 
