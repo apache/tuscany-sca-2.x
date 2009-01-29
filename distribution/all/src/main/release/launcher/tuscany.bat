fa774b68ded1834ff8e035833bc6b33617d9611d
Anthony Elder <antelder@apache.org>
2009-01-29 07:35:36 +0000

    More launcher bringup, start making the .bat file a bit more useable
    git-svn-id: https://svn.apache.org/repos/asf/tuscany/java/sca@738784 13f79535-47bb-0310-9956-ffa450edef68

--- distribution/all/src/main/release/launcher/tuscany.bat
+++ distribution/all/src/main/release/launcher/tuscany.bat
@@ -1,6 +1,35 @@
 @echo off
 
-REM bla
+if not "%TUSCANY_HOME%"=="" goto gotHome
+SET TUSCANY_HOME=%~dp0\..
+if not "%TUSCANY_HOME%"=="" goto gotHome
+echo.
+echo cannot find TUSCANY_HOME please set TUSCANY_HOME variable to the Tuscany installation dir
+echo.
+goto error
+:gotHome
 
-java -Xdebug -Xrunjdwp:transport=dt_socket,address=8000,server=y,suspend=y -jar launcher.jar %1 %2 %3 %4 %5 %6 %7
-REM java -jar launcher.jar %1 %2 %3 %4 %5 %6 %7
+
+set _CMD_LINE_ARGS=
+:argsLoop
+if %1a==a goto doneInit
+set _CMD_LINE_ARGS=%_CMD_LINE_ARGS% %1
+shift
+goto argsLoop
+
+
+:doneInit
+
+java -jar %TUSCANY_HOME%/bin/launcher.jar %_CMD_LINE_ARGS%
+
+goto end
+
+
+:error
+set ERROR_CODE=1
+
+
+:end
+@endlocal
+exit /B %ERROR_CODE%
+
