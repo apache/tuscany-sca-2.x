dda233315c313ad6da7713e6c7f34665e909e1c6
Luciano Resende <lresende@apache.org>
2009-02-08 08:36:52 +0000

    Updates to README, fixing build.xml to proper create sample jar
    git-svn-id: https://svn.apache.org/repos/asf/tuscany/java/sca@742022 13f79535-47bb-0310-9956-ffa450edef68

--- samples/implementation-java-calculator/src/main/java/calculator/CalculatorClient.java
+++ samples/implementation-java-calculator/src/main/java/calculator/CalculatorClient.java
@@ -19,7 +19,6 @@
 
 package calculator;
 
-import org.oasisopen.sca.ServiceRuntimeException;
 import org.oasisopen.sca.annotation.EagerInit;
 import org.oasisopen.sca.annotation.Init;
 import org.oasisopen.sca.annotation.Reference;
