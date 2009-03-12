51972d53f35e6a3380c90160058af88dddd66a7a
Zhaohui Feng <rfeng@apache.org>
2009-03-12 17:02:22 +0000

    clean up the provider
    git-svn-id: https://svn.apache.org/repos/asf/tuscany/java/sca@752935 13f79535-47bb-0310-9956-ffa450edef68

--- modules/implementation-osgi/src/main/java/org/apache/tuscany/sca/implementation/osgi/runtime/OSGiImplementationProviderFactory.java
+++ modules/implementation-osgi/src/main/java/org/apache/tuscany/sca/implementation/osgi/runtime/OSGiImplementationProviderFactory.java
@@ -19,14 +19,8 @@
 package org.apache.tuscany.sca.implementation.osgi.runtime;
 
 import org.apache.tuscany.sca.core.ExtensionPointRegistry;
-import org.apache.tuscany.sca.core.FactoryExtensionPoint;
-import org.apache.tuscany.sca.core.UtilityExtensionPoint;
 import org.apache.tuscany.sca.core.invocation.ProxyFactoryExtensionPoint;
-import org.apache.tuscany.sca.core.scope.ScopeRegistry;
-import org.apache.tuscany.sca.databinding.DataBindingExtensionPoint;
 import org.apache.tuscany.sca.implementation.osgi.OSGiImplementation;
-import org.apache.tuscany.sca.interfacedef.InterfaceContractMapper;
-import org.apache.tuscany.sca.invocation.MessageFactory;
 import org.apache.tuscany.sca.provider.ImplementationProvider;
 import org.apache.tuscany.sca.provider.ImplementationProviderFactory;
 import org.apache.tuscany.sca.runtime.RuntimeComponent;
@@ -40,27 +34,9 @@ import org.osgi.framework.BundleException;
 public class OSGiImplementationProviderFactory implements ImplementationProviderFactory<OSGiImplementation> {
 
     private ProxyFactoryExtensionPoint proxyFactoryExtensionPoint;
-    private DataBindingExtensionPoint dataBindings;
-    private ScopeRegistry scopeRegistry;
-    private MessageFactory messageFactory;
-    private InterfaceContractMapper mapper;
 
     public OSGiImplementationProviderFactory(ExtensionPointRegistry extensionPoints) {
-
-        dataBindings = extensionPoints.getExtensionPoint(DataBindingExtensionPoint.class);
-
-        // FIXME: Scope registry is not an extension point, and this usage is specific
-        // to implementation.osgi since it needs to change scope after the component is
-        // created. Do we need to find a better way?
-        scopeRegistry = extensionPoints.getExtensionPoint(ScopeRegistry.class);
-        
         proxyFactoryExtensionPoint = extensionPoints.getExtensionPoint(ProxyFactoryExtensionPoint.class);
-
-        FactoryExtensionPoint modelFactories = extensionPoints.getExtensionPoint(FactoryExtensionPoint.class);
-        messageFactory = modelFactories.getFactory(MessageFactory.class);
-
-        UtilityExtensionPoint utilities = extensionPoints.getExtensionPoint(UtilityExtensionPoint.class);
-        mapper = utilities.getUtility(InterfaceContractMapper.class);
     }
 
     public ImplementationProvider createImplementationProvider(RuntimeComponent component,
@@ -68,8 +44,7 @@ public class OSGiImplementationProviderFactory implements ImplementationProvider
 
         try {
 
-            return new OSGiImplementationProvider(component, implementation, dataBindings, scopeRegistry,
-                                                  messageFactory, proxyFactoryExtensionPoint, mapper);
+            return new OSGiImplementationProvider(component, implementation, proxyFactoryExtensionPoint);
 
         } catch (BundleException e) {
             throw new RuntimeException(e);
