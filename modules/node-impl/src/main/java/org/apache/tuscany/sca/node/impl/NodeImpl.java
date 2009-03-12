336ab6cbf2eef9239d61585b00b0a416eed387b2
Zhaohui Feng <rfeng@apache.org>
2009-03-12 23:59:34 +0000

    Rename the constant
    git-svn-id: https://svn.apache.org/repos/asf/tuscany/java/sca@753068 13f79535-47bb-0310-9956-ffa450edef68

--- modules/node-impl/src/main/java/org/apache/tuscany/sca/node/impl/NodeImpl.java
+++ modules/node-impl/src/main/java/org/apache/tuscany/sca/node/impl/NodeImpl.java
@@ -103,7 +103,7 @@ import org.oasisopen.sca.ServiceRuntimeException;
  */
 public class NodeImpl implements Node, Client {
 
-    private static final String SCA10_TUSCANY_NS = "http://tuscany.apache.org/xmlns/sca/1.1";
+    private static final String SCA11_TUSCANY_NS = "http://tuscany.apache.org/xmlns/sca/1.1";
 
     private static final Logger logger = Logger.getLogger(NodeImpl.class.getName());
 
@@ -552,8 +552,8 @@ public class NodeImpl implements Node, Client {
         // Create a top level composite to host our composite
         // This is temporary to make the activator happy
         Composite tempComposite = assemblyFactory.createComposite();
-        tempComposite.setName(new QName(SCA10_TUSCANY_NS, "_tempComposite"));
-        tempComposite.setURI(SCA10_TUSCANY_NS);
+        tempComposite.setName(new QName(SCA11_TUSCANY_NS, "_tempComposite"));
+        tempComposite.setURI(SCA11_TUSCANY_NS);
 
         // Include the node composite in the top-level composite 
         tempComposite.getIncludes().add(composite);
