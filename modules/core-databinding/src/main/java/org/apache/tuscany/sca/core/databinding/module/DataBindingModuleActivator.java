mcombell/tuscany-sca

commit 4ca73afd421be83b2c1a95524f6544b3f7178879

Fix the J2W generation issue which misses the target namespace
git-svn-id: https://svn.apache.org/repos/asf/tuscany/branches/sca-java-1.x@755474 13f79535-47bb-0310-9956-ffa450edef68

modules/core-databinding/src/main/java/org/apache/tuscany/sca/core/databinding/module/DataBindingModuleActivator.java
@@ -42,6 +42,7 @@ import org.apache.tuscany.sca.interfacedef.FaultExceptionMapper;
 import org.apache.tuscany.sca.interfacedef.java.JavaInterfaceFactory;
 import org.apache.tuscany.sca.interfacedef.java.jaxws.JAXWSFaultExceptionMapper;
 import org.apache.tuscany.sca.interfacedef.java.jaxws.JAXWSJavaInterfaceProcessor;
+import org.apache.tuscany.sca.interfacedef.java.jaxws.WebServiceInterfaceProcessor;
 import org.apache.tuscany.sca.runtime.RuntimeWireProcessorExtensionPoint;
 import org.osoa.sca.CallableReference;
 import org.apache.axiom.om.OMElement;
@@ -86,12 +87,12 @@ public class DataBindingModuleActivator implements ModuleActivator {
 
         ModelFactoryExtensionPoint modelFactories = registry.getExtensionPoint(ModelFactoryExtensionPoint.class);
         JavaInterfaceFactory javaFactory = modelFactories.getFactory(JavaInterfaceFactory.class);
-
-        // [rfeng] The JAX-WS processor should come before the Databinding processor to make sure @WebService
-        // is honored as Remoteable
+        // Add the WebServiceInterfaceProcessor to mark the interface remotable 
+        javaFactory.addInterfaceVisitor(new WebServiceInterfaceProcessor());
+        // Introspect the data types
+        javaFactory.addInterfaceVisitor(new DataBindingJavaInterfaceProcessor(dataBindings));
         javaFactory.addInterfaceVisitor(new JAXWSJavaInterfaceProcessor(dataBindings, faultExceptionMapper, xmlAdapterExtensionPoint));
 
-        javaFactory.addInterfaceVisitor(new DataBindingJavaInterfaceProcessor(dataBindings));
 
         RuntimeWireProcessorExtensionPoint wireProcessorExtensionPoint = registry.getExtensionPoint(RuntimeWireProcessorExtensionPoint.class);
         if (wireProcessorExtensionPoint != null) {

modules/databinding-jaxb/src/main/java/org/apache/tuscany/sca/databinding/jaxb/JAXBContextHelper.java
@@ -396,7 +396,8 @@ public class JAXBContextHelper {
             typeQName = new QName(typeNamespace, typeName);
         } else {
             XmlEnum xmlEnum = javaType.getAnnotation(XmlEnum.class);
-            if (xmlEnum != null) {
+            // POJO can have the @XmlSchema on the package-info too
+            if (xmlEnum != null || namespace != null) {
                 name = jaxbDecapitalize(javaType.getSimpleName());
                 typeQName = new QName(namespace, name);
             }

modules/interface-java-jaxws/src/main/java/org/apache/tuscany/sca/interfacedef/java/jaxws/JAXWSJavaInterfaceProcessor.java
@@ -53,6 +53,7 @@ import org.apache.tuscany.sca.interfacedef.java.JavaOperation;
 import org.apache.tuscany.sca.interfacedef.java.introspect.JavaInterfaceVisitor;
 import org.apache.tuscany.sca.interfacedef.util.ElementInfo;
 import org.apache.tuscany.sca.interfacedef.util.JavaXMLMapper;
+import org.apache.tuscany.sca.interfacedef.util.TypeInfo;
 import org.apache.tuscany.sca.interfacedef.util.WrapperInfo;
 import org.apache.tuscany.sca.interfacedef.util.XMLType;
 
@@ -271,10 +272,12 @@ public class JAXWSJavaInterfaceProcessor implements JavaInterfaceVisitor {
                     name = getValue(name, "arg" + i);
                     QName element = new QName(ns, name);
                     Object logical = operation.getInputType().getLogical().get(i).getLogical();
+                    QName type = null;
                     if (logical instanceof XMLType) {
                         ((XMLType)logical).setElementName(element);
+                        type = ((XMLType)logical).getTypeName();
                     }
-                    inputElements.add(new ElementInfo(element, null));
+                    inputElements.add(new ElementInfo(element, new TypeInfo(type, false, null)));
                 }
 
                 List<ElementInfo> outputElements = new ArrayList<ElementInfo>();
@@ -288,10 +291,12 @@ public class JAXWSJavaInterfaceProcessor implements JavaInterfaceVisitor {
 
                 if (operation.getOutputType() != null) {
                     Object logical = operation.getOutputType().getLogical();
+                    QName type = null;
                     if (logical instanceof XMLType) {
                         ((XMLType)logical).setElementName(element);
+                        type = ((XMLType)logical).getTypeName();
                     }
-                    outputElements.add(new ElementInfo(element, null));
+                    outputElements.add(new ElementInfo(element, new TypeInfo(type, false, null)));
                 }
 
                 String db = inputWrapperDT != null ? inputWrapperDT.getDataBinding() : JAXB_DATABINDING;

modules/interface-java-jaxws/src/main/java/org/apache/tuscany/sca/interfacedef/java/jaxws/WebServiceInterfaceProcessor.java
@@ -1,0 +1,49 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.    
+ */
+
+package org.apache.tuscany.sca.interfacedef.java.jaxws;
+
+import javax.jws.WebService;
+
+import org.apache.tuscany.sca.interfacedef.InvalidInterfaceException;
+import org.apache.tuscany.sca.interfacedef.java.JavaInterface;
+import org.apache.tuscany.sca.interfacedef.java.introspect.JavaInterfaceVisitor;
+
+/**
+ * Introspect the java class/interface to see if it has @WebService annotation
+ * 
+ * @version $Rev$ $Date$
+ */
+public class WebServiceInterfaceProcessor implements JavaInterfaceVisitor {
+
+    public WebServiceInterfaceProcessor() {
+        super();
+    }
+
+    public void visitInterface(JavaInterface contract) throws InvalidInterfaceException {
+
+        final Class<?> clazz = contract.getJavaClass();
+        WebService webService = clazz.getAnnotation(WebService.class);
+        if (webService != null) {
+            // Mark SEI as Remotable
+            contract.setRemotable(true);
+        }
+    }
+
+}

apache/tuscany-sca-2.x

commit c7adb73f63e79ff193cd3f7853f21fbfb43b0fd9

Fix a regression caused by r755474
git-svn-id: https://svn.apache.org/repos/asf/tuscany/java/sca@755756 13f79535-47bb-0310-9956-ffa450edef68

modules/core-databinding/src/main/java/org/apache/tuscany/sca/core/databinding/module/DataBindingModuleActivator.java
@@ -23,6 +23,7 @@ import org.apache.tuscany.sca.core.ExtensionPointRegistry;
 import org.apache.tuscany.sca.core.FactoryExtensionPoint;
 import org.apache.tuscany.sca.core.ModuleActivator;
 import org.apache.tuscany.sca.core.databinding.processor.DataBindingJavaInterfaceProcessor;
+import org.apache.tuscany.sca.core.databinding.processor.WrapperJavaInterfaceProcessor;
 import org.apache.tuscany.sca.core.databinding.transformers.Array2ArrayTransformer;
 import org.apache.tuscany.sca.core.databinding.transformers.CallableReferenceXMLAdapter;
 import org.apache.tuscany.sca.core.databinding.transformers.Exception2ExceptionTransformer;
@@ -86,7 +87,7 @@ public class DataBindingModuleActivator implements ModuleActivator {
         // Introspect the data types
         javaFactory.addInterfaceVisitor(new DataBindingJavaInterfaceProcessor(dataBindings));
         javaFactory.addInterfaceVisitor(new JAXWSJavaInterfaceProcessor(dataBindings, faultExceptionMapper, xmlAdapterExtensionPoint));
-
+        javaFactory.addInterfaceVisitor(new WrapperJavaInterfaceProcessor(dataBindings));
 
         RuntimeWireProcessorExtensionPoint wireProcessorExtensionPoint = registry.getExtensionPoint(RuntimeWireProcessorExtensionPoint.class);
         if (wireProcessorExtensionPoint != null) {

modules/core-databinding/src/main/java/org/apache/tuscany/sca/core/databinding/processor/DataBindingJavaInterfaceProcessor.java
@@ -22,24 +22,17 @@ package org.apache.tuscany.sca.core.databinding.processor;
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Method;
 import java.util.HashMap;
-import java.util.HashSet;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 
 import org.apache.tuscany.sca.databinding.DataBindingExtensionPoint;
-import org.apache.tuscany.sca.databinding.WrapperHandler;
 import org.apache.tuscany.sca.databinding.annotation.DataBinding;
-import org.apache.tuscany.sca.databinding.javabeans.JavaBeansDataBinding;
-import org.apache.tuscany.sca.databinding.javabeans.SimpleJavaDataBinding;
 import org.apache.tuscany.sca.interfacedef.DataType;
 import org.apache.tuscany.sca.interfacedef.InvalidInterfaceException;
 import org.apache.tuscany.sca.interfacedef.Operation;
 import org.apache.tuscany.sca.interfacedef.java.JavaInterface;
 import org.apache.tuscany.sca.interfacedef.java.JavaOperation;
 import org.apache.tuscany.sca.interfacedef.java.introspect.JavaInterfaceVisitor;
-import org.apache.tuscany.sca.interfacedef.util.WrapperInfo;
 
 /**
  * The databinding annotation processor for java interfaces
@@ -47,7 +40,6 @@ import org.apache.tuscany.sca.interfacedef.util.WrapperInfo;
  * @version $Rev$ $Date$
  */
 public class DataBindingJavaInterfaceProcessor implements JavaInterfaceVisitor {
-    private static final String JAXB_DATABINDING = "javax.xml.bind.JAXBElement";
     private DataBindingExtensionPoint dataBindingRegistry;
 
     public DataBindingJavaInterfaceProcessor(DataBindingExtensionPoint dataBindingRegistry) {
@@ -140,71 +132,6 @@ public class DataBindingJavaInterfaceProcessor implements JavaInterfaceVisitor {
                 // TODO: Handle exceptions
                 dataBindingRegistry.introspectType(d, operation);
             }
-
-            // JIRA: TUSCANY-842
-            String db = operation.getDataBinding();
-            if (db == null || JAXB_DATABINDING.equals(db)) {
-                assignOperationDataBinding(operation);
-                db = operation.getDataBinding();
-            }
-
-            // Introspect the wrapper data type
-            if (operation.getWrapper() != null) {
-                org.apache.tuscany.sca.databinding.DataBinding dbObj =
-                    dataBindingRegistry.getDataBinding(db);
-                WrapperHandler handler = dbObj == null ? null : dbObj.getWrapperHandler();
-                if (handler != null) {
-                    WrapperInfo wrapper = operation.getWrapper();
-                    wrapper.setInputWrapperType(handler.getWrapperType(operation, true));
-                    wrapper.setOutputWrapperType(handler.getWrapperType(operation, false));
-                }
-                if (dbObj != null && handler == null) {
-                    // To avoid JAXB wrapper bean generation
-                    WrapperInfo wrapper = operation.getWrapper();
-                    wrapper.setInputWrapperType(null);
-                    wrapper.setOutputWrapperType(null);
-                }
-            }
-        }
-    }
-
-    /*
-     *  Assigns an operation DB if one of the input types, output type, fault types has a non-default DB.
-     *  However, if two of the input types, output type, fault types have two different non-default DBs 
-     *  ( e.g. SDO and JAXB), then we do nothing to the operation DB.
-     *  
-     *  The method logic assumes the JavaBeans DataBinding is the default 
-     */
-    private void assignOperationDataBinding(Operation operation) {
-
-        Set<String> dbs = new HashSet<String>();
-
-        // Can't use DataType<?> since operation.getInputType() returns: DataType<List<DataType>> 
-        List<DataType> opDataTypes = new LinkedList<DataType>();
-
-        opDataTypes.addAll(operation.getInputType().getLogical());
-        opDataTypes.add(operation.getOutputType());
-        for (DataType<DataType> ft : operation.getFaultTypes()) {
-            opDataTypes.add(ft.getLogical());
-        }
-
-        for (DataType<?> d : opDataTypes) {
-            if (d != null) {
-                String dataBinding = d.getDataBinding();
-                if ("java:array".equals(dataBinding)) {
-                    dataBinding = ((DataType)d.getLogical()).getDataBinding();
-                }
-                if (dataBinding != null) {
-                    dbs.add(dataBinding);
-                }
-            }
-        }
-
-        dbs.remove(JavaBeansDataBinding.NAME);
-        dbs.remove(SimpleJavaDataBinding.NAME);
-
-        if (dbs.size() == 1) {
-            operation.setDataBinding(dbs.iterator().next());
         }
     }
 }

modules/core-databinding/src/main/java/org/apache/tuscany/sca/core/databinding/processor/WrapperJavaInterfaceProcessor.java
@@ -1,0 +1,125 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.    
+ */
+
+package org.apache.tuscany.sca.core.databinding.processor;
+
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Set;
+
+import org.apache.tuscany.sca.databinding.DataBindingExtensionPoint;
+import org.apache.tuscany.sca.databinding.WrapperHandler;
+import org.apache.tuscany.sca.databinding.javabeans.JavaBeansDataBinding;
+import org.apache.tuscany.sca.databinding.javabeans.SimpleJavaDataBinding;
+import org.apache.tuscany.sca.interfacedef.DataType;
+import org.apache.tuscany.sca.interfacedef.InvalidInterfaceException;
+import org.apache.tuscany.sca.interfacedef.Operation;
+import org.apache.tuscany.sca.interfacedef.java.JavaInterface;
+import org.apache.tuscany.sca.interfacedef.java.introspect.JavaInterfaceVisitor;
+import org.apache.tuscany.sca.interfacedef.util.WrapperInfo;
+
+/**
+ * The databinding annotation processor for java interfaces
+ * 
+ * @version $Rev$ $Date$
+ */
+public class WrapperJavaInterfaceProcessor implements JavaInterfaceVisitor {
+    private static final String JAXB_DATABINDING = "javax.xml.bind.JAXBElement";
+    private DataBindingExtensionPoint dataBindingRegistry;
+
+    public WrapperJavaInterfaceProcessor(DataBindingExtensionPoint dataBindingRegistry) {
+        super();
+        this.dataBindingRegistry = dataBindingRegistry;
+    }
+
+    public void visitInterface(JavaInterface javaInterface) throws InvalidInterfaceException {
+        if (!javaInterface.isRemotable()) {
+            return;
+        }
+        for (Operation operation : javaInterface.getOperations()) {
+            WrapperInfo wrapper = operation.getWrapper();
+            if (wrapper == null) {
+                continue;
+            }
+            // JIRA: TUSCANY-842
+            String db = wrapper.getDataBinding();
+            if (db == null || JAXB_DATABINDING.equals(db)) {
+                db = assignOperationDataBinding(operation);
+            }
+
+            // Introspect the wrapper data type
+            org.apache.tuscany.sca.databinding.DataBinding dbObj = dataBindingRegistry.getDataBinding(db);
+            WrapperHandler handler = dbObj == null ? null : dbObj.getWrapperHandler();
+            if (handler != null) {
+                wrapper.setInputWrapperType(handler.getWrapperType(operation, true));
+                wrapper.setOutputWrapperType(handler.getWrapperType(operation, false));
+            }
+            if (dbObj != null && handler == null) {
+                // To avoid JAXB wrapper bean generation
+                wrapper.setInputWrapperType(null);
+                wrapper.setOutputWrapperType(null);
+            }
+        }
+    }
+
+    /*
+     *  Assigns an operation DB if one of the input types, output type, fault types has a non-default DB.
+     *  However, if two of the input types, output type, fault types have two different non-default DBs 
+     *  ( e.g. SDO and JAXB), then we do nothing to the operation DB.
+     *  
+     *  The method logic assumes the JavaBeans DataBinding is the default 
+     */
+    private String assignOperationDataBinding(Operation operation) {
+
+        Set<String> dbs = new HashSet<String>();
+
+        // Can't use DataType<?> since operation.getInputType() returns: DataType<List<DataType>> 
+        List<DataType> opDataTypes = new LinkedList<DataType>();
+
+        opDataTypes.addAll(operation.getInputType().getLogical());
+        opDataTypes.add(operation.getOutputType());
+        for (DataType<DataType> ft : operation.getFaultTypes()) {
+            opDataTypes.add(ft.getLogical());
+        }
+
+        for (DataType<?> d : opDataTypes) {
+            if (d != null) {
+                String dataBinding = d.getDataBinding();
+                if ("java:array".equals(dataBinding)) {
+                    dataBinding = ((DataType)d.getLogical()).getDataBinding();
+                }
+                if (dataBinding != null) {
+                    dbs.add(dataBinding);
+                }
+            }
+        }
+
+        dbs.remove(JavaBeansDataBinding.NAME);
+        dbs.remove(SimpleJavaDataBinding.NAME);
+
+        if (dbs.size() == 1) {
+            String db = dbs.iterator().next();
+            operation.getWrapper().setDataBinding(db);
+            return db;
+        } else {
+            return operation.getWrapper().getDataBinding();
+        }
+    }
+}