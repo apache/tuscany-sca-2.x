9f8fd19cdb360f61db511931d2186c26155bee44
Zhaohui Feng <rfeng@apache.org>
2009-03-30 21:28:56 +0000

    Add a map of attributes to the Interface/Operation model
    git-svn-id: https://svn.apache.org/repos/asf/tuscany/java/sca@760185 13f79535-47bb-0310-9956-ffa450edef68

--- modules/interface/src/main/java/org/apache/tuscany/sca/interfacedef/Interface.java
+++ modules/interface/src/main/java/org/apache/tuscany/sca/interfacedef/Interface.java
@@ -6,19 +6,20 @@
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
  * with the License.  You may obtain a copy of the License at
- * 
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
  * KIND, either express or implied.  See the License for the
  * specific language governing permissions and limitations
- * under the License.    
+ * under the License.
  */
 package org.apache.tuscany.sca.interfacedef;
 
 import java.util.List;
+import java.util.Map;
 
 import org.apache.tuscany.sca.policy.PolicySubject;
 
@@ -33,35 +34,35 @@ public interface Interface extends Cloneable, PolicySubject {
 
     /**
      * Returns true if the interface is a remotable interface..
-     * 
+     *
      * @return true if the interface is a remotable interface
      */
     boolean isRemotable();
 
     /**
      * Sets whether the interface is a remotable or local interface.
-     * 
+     *
      * @param remotable indicates whether the interface is remotable or local
      */
     void setRemotable(boolean remotable);
-    
-    
+
+
     // FIXME: [rfeng] We need to re-consider the conversational as an intent
     /**
      * Test if the interface is conversational
      * @return
      */
     boolean isConversational();
-    
+
     /**
-     * Set whether the interface is conversational 
+     * Set whether the interface is conversational
      * @param conversational
      */
     void setConversational(boolean conversational);
 
     /**
      * Returns the operations defined on this interface.
-     * 
+     *
      * @return the operations defined on this interface
      */
     List<Operation> getOperations();
@@ -73,7 +74,7 @@ public interface Interface extends Cloneable, PolicySubject {
      */
     @Deprecated
     void setDefaultDataBinding(String dataBinding);
-    
+
     /**
      * Reset the databinding for the interface
      * @param dataBinding
@@ -82,10 +83,16 @@ public interface Interface extends Cloneable, PolicySubject {
 
     /**
      * Returns true if the Interface is dynamic.
-     * 
+     *
      * @return true if the Interface is dynamic.
      */
     boolean isDynamic();
+
+    /**
+     * Get a map of attributes assoicated with the interface
+     * @return A map of attributes
+     */
+    Map<Object, Object> getAttributes();
 
     /**
      * Implementations must support cloning.
