61027899f33ca852697c19aeb74456f4498a1d5e
Anthony Elder <antelder@apache.org>
2009-04-02 10:08:42 +0000

    Copy itest for zip and folder contributions from 1.x to 2.x
    git-svn-id: https://svn.apache.org/repos/asf/tuscany/java/sca@761229 13f79535-47bb-0310-9956-ffa450edef68

---
+++ itest/contribution-zip/src/test/java/ContributionZIPTestCaseFIXME.java
@@ -0,0 +1,38 @@
+
+
+import junit.framework.TestCase;
+
+import org.apache.tuscany.sca.node.Contribution;
+import org.apache.tuscany.sca.node.Node;
+import org.apache.tuscany.sca.node.NodeFactory;
+
+import calculator.CalculatorService;
+
+
+
+/**
+ * Test SCADomain.newInstance and invocation of a service.
+ * 
+ * @version $Rev: 608205 $ $Date: 2008-01-02 20:29:05 +0000 (Wed, 02 Jan 2008) $
+ */
+public class ContributionZIPTestCaseFIXME extends TestCase {
+
+    private Node node;
+    
+    @Override
+    protected void setUp() throws Exception {
+		node = NodeFactory.newInstance().createNode(null, new Contribution("foo", "src/test/resources/repository3/contribution.zip"));
+        node.start();
+    }
+
+    public void testInvoke() throws Exception {
+        CalculatorService service = node.getService(CalculatorService.class, "CalculatorServiceComponent");
+        assertEquals(3.0, service.add(1, 2));
+    }
+
+    @Override
+    protected void tearDown() throws Exception {
+        node.stop();
+    }
+
+}
