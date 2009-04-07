a6a33efe6f35beea5885b08217d7b4aa61215763
Zhaohui Feng <rfeng@apache.org>
2009-04-07 20:31:59 +0000

    Fix the dependencies to get distributed OSGi samples working inside Eclipse
    git-svn-id: https://svn.apache.org/repos/asf/tuscany/java/sca@762942 13f79535-47bb-0310-9956-ffa450edef68

--- samples/dosgi-calculator/src/main/java/calculator/dosgi/impl/CalculatorServiceImpl.java
+++ samples/dosgi-calculator/src/main/java/calculator/dosgi/impl/CalculatorServiceImpl.java
@@ -6,15 +6,15 @@
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
  * with the License.  You may obtain a copy of the License at
- * 
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
  * KIND, either express or implied.  See the License for the
  * specific language governing permissions and limitations
- * under the License.    
+ * under the License.
  */
 package calculator.dosgi.impl;
 
@@ -69,12 +69,14 @@ public class CalculatorServiceImpl implements CalculatorService {
                 }
             }
         }
-        for (Object s : localServices.getServices()) {
-            if (cls.isInstance(s)) {
-                System.out.println("Local service: " + s);
-                return cls.cast(s);
+        Object[] localObjects = localServices.getServices();
+        if (localObjects != null)
+            for (Object s : localObjects) {
+                if (cls.isInstance(s)) {
+                    System.out.println("Local service: " + s);
+                    return cls.cast(s);
+                }
             }
-        }
         throw new IllegalStateException(cls.getSimpleName() + " is not available");
     }
 
