c0003e50e59ceb8e015516df9ae0df7b4cf505f4
Anthony Elder <antelder@apache.org>
2009-04-07 13:18:02 +0000

    Add an itest for the SCAClient API
    git-svn-id: https://svn.apache.org/repos/asf/tuscany/java/sca@762761 13f79535-47bb-0310-9956-ffa450edef68

---
+++ itest/scaclient-api/src/test/java/SCAClientTestCase.java
@@ -0,0 +1,34 @@
+
+
+import itest.HelloworldService;
+import junit.framework.TestCase;
+
+import org.oasisopen.sca.client.SCAClient;
+import org.oasisopen.sca.client.SCAClientFactory;
+
+
+
+/**
+ * Test SCADomain.newInstance and invocation of a service.
+ * 
+ * @version $Rev$ $Date$
+ */
+public class SCAClientTestCase extends TestCase {
+
+    private SCAClient scaClient;
+    
+    @Override
+    protected void setUp() throws Exception {
+        scaClient = SCAClientFactory.newInstance();
+    }
+
+    public void testInvoke() throws Exception {
+        HelloworldService service = scaClient.getService(HelloworldService.class, "HelloworldComponent", null);
+        assertEquals("Hello petra", service.sayHello("petra"));
+    }
+
+    @Override
+    protected void tearDown() throws Exception {
+    }
+
+}
