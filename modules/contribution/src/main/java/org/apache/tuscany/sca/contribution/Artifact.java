4912420b07c8852e01642f04dbdb3e07fe314e07
Zhaohui Feng <rfeng@apache.org>
2009-04-10 00:11:48 +0000

    Remove deprecated classes and use generic type for getModel
    git-svn-id: https://svn.apache.org/repos/asf/tuscany/java/sca@763837 13f79535-47bb-0310-9956-ffa450edef68

--- modules/contribution/src/main/java/org/apache/tuscany/sca/contribution/Artifact.java
+++ modules/contribution/src/main/java/org/apache/tuscany/sca/contribution/Artifact.java
@@ -6,15 +6,15 @@
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
  * with the License.  You may obtain a copy of the License at
- * 
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
  * KIND, either express or implied.  See the License for the
  * specific language governing permissions and limitations
- * under the License.    
+ * under the License.
  */
 
 package org.apache.tuscany.sca.contribution;
@@ -24,50 +24,50 @@ import org.apache.tuscany.sca.assembly.Base;
 
 /**
  * Represents an artifact in an SCA contribution.
- * 
+ *
  * @version $Rev$ $Date$
  */
 public interface Artifact extends Base {
-    
+
     /**
      * Returns the URI that unique identifies the artifact inside the contribution.
-     * 
+     *
      * @return The artifact URI
      */
     String getURI();
-    
+
     /**
      * Sets the URI that uniquely identifies the artifact inside the contribution.
-     * 
+     *
      * @param uri The artifact URI
      */
     void setURI(String uri);
 
     /**
      * Returns the location of the artifact.
-     * 
+     *
      * @return The artifact location
      */
     String getLocation();
-    
+
     /**
      * Set the location of the artifact.
-     * 
+     *
      * @param location The artifact location
      */
     void setLocation(String location);
 
-    
+
     /**
      * Returns the in-memory model representing the artifact.
-     * 
+     *
      * @return The model object
      */
-    Object getModel();
-    
+    <T> T getModel();
+
     /**
      * Sets the in-memory model representing the artifact.
-     * 
+     *
      * @param model The model object
      */
     void setModel(Object model);
@@ -77,7 +77,7 @@ public interface Artifact extends Base {
      * @return the contents of the artifact
      */
     byte[] getContents();
-    
+
     /**
      * Sets the contents of the artifact.
      * @param contents the contents of the artifact
--- modules/contribution/src/main/java/org/apache/tuscany/sca/contribution/ContributionFactory.java
+++ modules/contribution/src/main/java/org/apache/tuscany/sca/contribution/ContributionFactory.java
@@ -6,15 +6,15 @@
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
  * with the License.  You may obtain a copy of the License at
- * 
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
  * KIND, either express or implied.  See the License for the
  * specific language governing permissions and limitations
- * under the License.    
+ * under the License.
  */
 
 package org.apache.tuscany.sca.contribution;
@@ -22,52 +22,44 @@ package org.apache.tuscany.sca.contribution;
 
 /**
  * A factory for the contribution model.
- * 
+ *
  * @version $Rev$ $Date$
  */
 public interface ContributionFactory {
-    
+
     /**
      * Create a contribution model object
-     * 
+     *
      * @return The new contribution model object
      */
     Contribution createContribution();
-        
+
     /**
      * Create a contribution metadata model object
-     * 
+     *
      * @return The new contribution metadata model object
      */
     ContributionMetadata createContributionMetadata();
-        
-    /**
-     * Create a deployedArtifact model object
-     * 
-     * @return The new deployedArtifact model object
-     */
-    @Deprecated
-    DeployedArtifact createDeployedArtifact();   
 
     /**
      * Create an artifact model object
-     * 
+     *
      * @return The new artifact model object
      */
     Artifact createArtifact();
 
     /**
      * Create a default import model object.
-     * 
+     *
      * @return the new default import model object
      */
     DefaultImport createDefaultImport();
 
     /**
      * Create a default export model object.
-     * 
+     *
      * @return the new default export model object
      */
     DefaultExport createDefaultExport();
-    
+
 }
