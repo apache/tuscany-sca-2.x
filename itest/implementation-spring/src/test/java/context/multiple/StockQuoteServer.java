7f09cc1dcaac359440147b4c5af73c75f38d2832
Ramkumar Ramalingam <ramkumar@apache.org>
2009-04-20 13:16:35 +0000

    Porting Spring iTests into 2.x
    git-svn-id: https://svn.apache.org/repos/asf/tuscany/java/sca@766686 13f79535-47bb-0310-9956-ffa450edef68

---
+++ itest/implementation-spring/src/test/java/context/multiple/StockQuoteServer.java
@@ -0,0 +1,53 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package context.multiple;
+
+import org.apache.tuscany.sca.node.Contribution;
+import org.apache.tuscany.sca.node.ContributionLocationHelper;
+import org.apache.tuscany.sca.node.Node;
+import org.apache.tuscany.sca.node.NodeFactory;
+import org.springframework.context.ApplicationContext;
+import context.access.SCAApplicationContextProvider;
+
+/**
+ * This server program shows how to bootstrap SCA from a simple J2SE program
+ * and start it which activates the StockQuote Web service endpoint.
+ */
+public class StockQuoteServer {
+
+    public static void main(String[] args) throws Exception {
+
+        System.out.println("Starting the Sample SCA StockQuote Service...");
+
+        String location = ContributionLocationHelper.getContributionLocation("context/multiple/MultipleContext.composite");
+        Node node = NodeFactory.newInstance().createNode("MultipleContext.composite", new Contribution("c1", location));
+        node.start();
+        
+        // Method 1: To access the Spring Application Context instance
+        ApplicationContext ctx = SCAApplicationContextProvider.getApplicationContext();
+        if (ctx.containsBean("StockQuoteServiceBean"))
+            System.out.println("StockQuoteServiceBean is now available for use...");        
+        
+        System.out.println("Press Enter to Exit...");
+        Thread.sleep(1000);
+
+        node.stop();
+        System.out.println("Bye");
+    }
+}
