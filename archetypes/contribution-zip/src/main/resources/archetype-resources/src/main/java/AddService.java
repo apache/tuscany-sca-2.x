92807afacc41941348e8dcdebb206f3778faedf0
Anthony Elder <antelder@apache.org>
2009-04-28 15:40:48 +0000

    Update zip archetype to use an Add service
    git-svn-id: https://svn.apache.org/repos/asf/tuscany/java/sca@769438 13f79535-47bb-0310-9956-ffa450edef68

--- archetypes/contribution-zip/src/main/resources/archetype-resources/src/main/java/AddService.java
+++ archetypes/contribution-zip/src/main/resources/archetype-resources/src/main/java/AddService.java
@@ -1,5 +1,4 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<!--
+/*
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements.  See the NOTICE file
  * distributed with this work for additional information
@@ -16,14 +15,11 @@
  * KIND, either express or implied.  See the License for the
  * specific language governing permissions and limitations
  * under the License.    
--->
-<composite xmlns="http://docs.oasis-open.org/ns/opencsa/sca/200903"
-           xmlns:tuscany="http://tuscany.apache.org/xmlns/sca/1.0"
-           targetNamespace="http://${package}"
-           name="${artifactId}">
+ */
+package ${package};
 
-    <component name="HelloworldComponent">
-        <implementation.java class="${package}.HelloworldImpl"/>
-    </component>
+public interface AddService {
 
-</composite>
+    int add(int x, int y);
+
+}
