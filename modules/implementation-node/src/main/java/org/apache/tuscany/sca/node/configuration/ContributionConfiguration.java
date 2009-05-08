7efed0aee292ad5c72c90d3f73642f8b14c3bd7e
Zhaohui Feng <rfeng@apache.org>
2009-05-08 23:54:51 +0000

    Start to experiment a new way to provide the node configuration
    git-svn-id: https://svn.apache.org/repos/asf/tuscany/java/sca@773135 13f79535-47bb-0310-9956-ffa450edef68

---
+++ modules/implementation-node/src/main/java/org/apache/tuscany/sca/node/configuration/ContributionConfiguration.java
@@ -0,0 +1,49 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.apache.tuscany.sca.node.configuration;
+
+/**
+ * Configuration for an SCA contribution used by the SCA node
+ */
+public interface ContributionConfiguration {
+    /**
+     * Get the URI of the contribution
+     * @return The URI of the contribution
+     */
+    String getURI();
+
+    /**
+     * Set the URI of the contribution
+     * @param uri The URI of the contribution
+     */
+    void setURI(String uri);
+
+    /**
+     * Get the location of the contribution
+     * @return The location of the contribution
+     */
+    String getLocation();
+
+    /**
+     * Set the location of the contribution
+     * @param location The location of the contribution
+     */
+    void setLocation(String location);
+}
