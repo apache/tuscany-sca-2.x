5b87cad7b69cb678170626eeceae5ce57f963bf1
Zhaohui Feng <rfeng@apache.org>
2009-05-14 19:38:45 +0000

    Add @Service annotations
    git-svn-id: https://svn.apache.org/repos/asf/tuscany/java/sca@774895 13f79535-47bb-0310-9956-ffa450edef68

--- samples/calculator/src/main/java/calculator/SubtractServiceImpl.java
+++ samples/calculator/src/main/java/calculator/SubtractServiceImpl.java
@@ -6,24 +6,27 @@
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
  * with the License.  You may obtain a copy of the License at
- * 
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
  * KIND, either express or implied.  See the License for the
  * specific language governing permissions and limitations
- * under the License.    
+ * under the License.
  */
 package calculator;
 
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import org.oasisopen.sca.annotation.Service;
+
 /**
  * An implementation of the subtract service.
  */
+@Service(SubtractService.class)
 public class SubtractServiceImpl implements SubtractService {
 
     public double subtract(double n1, double n2) {
