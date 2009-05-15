27b94bccc60a47487fa05681b949514477a1ed20
Zhaohui Feng <rfeng@apache.org>
2009-05-15 22:39:01 +0000

    Fix the test case
    git-svn-id: https://svn.apache.org/repos/asf/tuscany/java/sca@775371 13f79535-47bb-0310-9956-ffa450edef68

--- itest/callback-complex-type/src/test/java/org/apache/tuscany/sca/test/CallBackCTypeTestCase.java
+++ itest/callback-complex-type/src/test/java/org/apache/tuscany/sca/test/CallBackCTypeTestCase.java
@@ -6,20 +6,21 @@
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
  * with the License.  You may obtain a copy of the License at
- * 
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
  * KIND, either express or implied.  See the License for the
  * specific language governing permissions and limitations
- * under the License.    
+ * under the License.
  */
 package org.apache.tuscany.sca.test;
 
 import junit.framework.TestCase;
 
+import org.apache.tuscany.sca.node.Contribution;
 import org.apache.tuscany.sca.node.ContributionLocationHelper;
 import org.apache.tuscany.sca.node.Node;
 import org.apache.tuscany.sca.node.NodeFactory;
@@ -27,7 +28,7 @@ import org.junit.After;
 import org.junit.Before;
 
 //FIXME Fix this test case
-public class CallBackCTypeTestCaseFIXME extends TestCase {
+public class CallBackCTypeTestCase extends TestCase {
 
     private Node node;
     private CallBackCTypeClient aCallBackClient;
@@ -38,8 +39,10 @@ public class CallBackCTypeTestCaseFIXME extends TestCase {
 
     @Before
     public void setUp() throws Exception {
-        String location = ContributionLocationHelper.getContributionLocation(".composite");
-        node = NodeFactory.newInstance().createNode("CallBackCTypeClient.composite");
+        String location = ContributionLocationHelper.getContributionLocation("CallBackCTypeClient.composite");
+        node =
+            NodeFactory.newInstance().createNode("CallBackCTypeClient.composite", new Contribution("c1", location))
+                .start();
 
         aCallBackClient = node.getService(CallBackCTypeClient.class, "CallBackCTypeClient");
     }
