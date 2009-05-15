9403baca8cf3e06ccb60254cc93e0ed8041aa83c
Zhaohui Feng <rfeng@apache.org>
2009-05-15 18:36:42 +0000

    Push down some of the createNode() methods to NodeFactory and only require the createNode(NodeConfiguration) to be implemented by subclasses
    git-svn-id: https://svn.apache.org/repos/asf/tuscany/java/sca@775283 13f79535-47bb-0310-9956-ffa450edef68

--- itest/contribution-zip/src/test/java/org/apache/tuscany/sca/contribution/zip/ContributionZIPTestCase.java
+++ itest/contribution-zip/src/test/java/org/apache/tuscany/sca/contribution/zip/ContributionZIPTestCase.java
@@ -6,15 +6,15 @@
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
  * with the License.  You may obtain a copy of the License at
- * 
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
  * KIND, either express or implied.  See the License for the
  * specific language governing permissions and limitations
- * under the License.    
+ * under the License.
  */
 package org.apache.tuscany.sca.contribution.zip;
 
@@ -26,19 +26,20 @@ import org.apache.tuscany.sca.node.NodeFactory;
 
 import calculator.CalculatorService;
 
-
 /**
  * Test SCADomain.newInstance and invocation of a service.
- * 
- * @version $Rev: 608205 $ $Date: 2008-01-02 20:29:05 +0000 (Wed, 02 Jan 2008) $
+ *
+ * @version $Rev$ $Date$
  */
 public class ContributionZIPTestCase extends TestCase {
 
     private Node node;
-    
+
     @Override
     protected void setUp() throws Exception {
-		node = NodeFactory.newInstance().createNode(null, new Contribution("foo", "src/test/resources/repository/contribution.zip"));
+        node =
+            NodeFactory.newInstance().createNode(new Contribution("foo",
+                                                                  "src/test/resources/repository/contribution.zip"));
         node.start();
     }
 
--- samples/calculator/src/test/java/calculator/CalculatorTestCase.java
+++ samples/calculator/src/test/java/calculator/CalculatorTestCase.java
@@ -6,15 +6,15 @@
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
  * with the License.  You may obtain a copy of the License at
- * 
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
  * KIND, either express or implied.  See the License for the
  * specific language governing permissions and limitations
- * under the License.    
+ * under the License.
  */
 package calculator;
 
@@ -28,25 +28,22 @@ import org.apache.tuscany.sca.node.NodeFactory;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
-import org.oasisopen.sca.annotation.EagerInit;
-import org.oasisopen.sca.annotation.Scope;
 
 /**
  * This shows how to test the Calculator service component.
  */
-@Scope("COMPOSITE") @EagerInit
 public class CalculatorTestCase{
-	private static Node node;
+    private static Node node;
     private static CalculatorService calculatorService;
-    
+
 
     @BeforeClass
     public static void setUpBeforeClass() throws Exception {
     	NodeFactory factory = NodeFactory.newInstance();
         String contribution = ContributionLocationHelper.getContributionLocation(CalculatorClient.class);
-        node = factory.createNode("Calculator.composite", new Contribution("calculator", contribution));
-        node.start();
-        
+        node = factory.createNode("Calculator.composite", new Contribution("calculator", contribution)).start();
+        // node = factory.createNode().start();
+
         calculatorService = ((Client)node).getService(CalculatorService.class, "CalculatorServiceComponent");
     }
 
