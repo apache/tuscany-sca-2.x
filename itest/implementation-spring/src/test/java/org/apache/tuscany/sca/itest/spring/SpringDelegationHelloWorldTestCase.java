0213f1c3bf01c3f00a50b2868da7bd62287127c7
Ramkumar Ramalingam <ramkumar@apache.org>
2009-05-26 11:39:11 +0000

    Fixes for TUSCANY-3027
    git-svn-id: https://svn.apache.org/repos/asf/tuscany/java/sca@778659 13f79535-47bb-0310-9956-ffa450edef68

--- itest/implementation-spring/src/test/java/org/apache/tuscany/sca/itest/spring/SpringDelegationHelloWorldTestCase.java
+++ itest/implementation-spring/src/test/java/org/apache/tuscany/sca/itest/spring/SpringDelegationHelloWorldTestCase.java
@@ -19,8 +19,6 @@
 
 package org.apache.tuscany.sca.itest.spring;
 
-import org.junit.Ignore;
-
 /**
  * A basic test case of:
  * 1) A composite containing a component with a Spring implementation
@@ -28,7 +26,6 @@ import org.junit.Ignore;
  * Spring implementation to satisfy a reference
  *
  */
-@Ignore("TUSCANY-3027")
 public class SpringDelegationHelloWorldTestCase extends AbstractHelloWorldTestCase {
     // super class does it all getting composite based on this class name
 }
