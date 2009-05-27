561d115888cf5a574c9fe9e689129d05b9bde2b9
Zhaohui Feng <rfeng@apache.org>
2009-05-27 19:00:30 +0000

    Clean up the code
    git-svn-id: https://svn.apache.org/repos/asf/tuscany/java/sca@779276 13f79535-47bb-0310-9956-ffa450edef68

--- samples/webapps/helloworld-rest/src/main/java/helloworldrest/ClientJavaTestService.java
+++ samples/webapps/helloworld-rest/src/main/java/helloworldrest/ClientJavaTestService.java
@@ -1,29 +1,29 @@
 package helloworldrest;
 
-import org.apache.tuscany.sca.host.embedded.SCADomain;
-
-import helloworldrest.HelloWorldService;
+import org.apache.tuscany.sca.node.Node;
+import org.apache.tuscany.sca.node.NodeFactory;
 
 /*
  * To test, simply run the program
- * Access the service by invoking the getName() method of HelloWorldService 
+ * Access the service by invoking the getName() method of HelloWorldService
  */
 
 public class ClientJavaTestService {
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		SCADomain scaDomain = SCADomain.newInstance("rest.composite");
-        HelloWorldService helloService = 
-            scaDomain.getService(HelloWorldService.class, "HelloWorldRESTServiceComponent");
-        
-		//HelloWorldService helloService = new HelloWorldServiceImpl();
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        NodeFactory factory = NodeFactory.newInstance();
+        Node node = factory.createNode("rest.composite", ClientJavaTestService.class.getClassLoader()).start();
+        HelloWorldService helloService = node.getService(HelloWorldService.class, "HelloWorldRESTServiceComponent");
+
+        //HelloWorldService helloService = new HelloWorldServiceImpl();
         System.out.println("### Message from REST service " + helloService.getName());
-        
-        scaDomain.close();
-	}
+
+        node.stop();
+        node.destroy();
+        factory.destroy();
+    }
 
 }
--- samples/webapps/helloworld-rest/src/main/java/helloworldrest/HelloWorldService.java
+++ samples/webapps/helloworld-rest/src/main/java/helloworldrest/HelloWorldService.java
@@ -1,11 +1,13 @@
 package helloworldrest;
 
-import org.osoa.sca.annotations.Remotable;
+import org.oasisopen.sca.annotation.Remotable;
 
 @Remotable
 public interface HelloWorldService {
 
     public void setName(String name);
+
     public String getName();
+
     public void postOperationTest(String name);
 }
