c10134348144cfefc8941d62215b2293b93c1ce4
Zhaohui Feng <rfeng@apache.org>
2009-06-10 16:02:08 +0000

    Change the file separator
    git-svn-id: https://svn.apache.org/repos/asf/tuscany/java/sca@783408 13f79535-47bb-0310-9956-ffa450edef68

--- itest/nodes/one-node-test/src/test/java/itest/OneNodeTestCase.java
+++ itest/nodes/one-node-test/src/test/java/itest/OneNodeTestCase.java
@@ -6,15 +6,15 @@
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
  * with the License.  You may obtain a copy of the License at
- * 
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
  * KIND, either express or implied.  See the License for the
  * specific language governing permissions and limitations
- * under the License.    
+ * under the License.
  */
 
 package itest;
@@ -42,20 +42,20 @@ public class OneNodeTestCase{
         System.setProperty("org.apache.tuscany.sca.contribution.processor.ValidationSchemaExtensionPoint.enabled", "false");
         NodeFactory factory = NodeFactory.newInstance();
         node = factory.createNode(
-               new Contribution("service", "..\\helloworld-service\\target\\itest-nodes-helloworld-service-2.0-SNAPSHOT.jar"),
-               new Contribution("client", "..\\helloworld-client\\target\\itest-nodes-helloworld-client-2.0-SNAPSHOT.jar"));
+               new Contribution("service", "../helloworld-service/target/itest-nodes-helloworld-service-2.0-SNAPSHOT.jar"),
+               new Contribution("client", "../helloworld-client/target/itest-nodes-helloworld-client-2.0-SNAPSHOT.jar"));
         node.start();
     }
 
     @Test
     public void testCalculator() throws Exception {
-        
+
         // Ideally this would use the SCAClient API but leaving that tillwe have the basics working
-        
+
         Helloworld service = node.getService(Helloworld.class, "HelloworldService");
         assertNotNull(service);
         assertEquals("Hello Petra", service.sayHello("Petra"));
-        
+
         Helloworld client = node.getService(Helloworld.class, "HelloworldClient");
         assertNotNull(client);
         assertEquals("Hi Hello Petra", client.sayHello("Petra"));
