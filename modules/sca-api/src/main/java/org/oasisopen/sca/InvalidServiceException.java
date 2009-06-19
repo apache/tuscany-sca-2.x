c0d6322f24771a2fea89663edf9804ecb603ec3e
Anthony Elder <antelder@apache.org>
2009-06-19 10:13:43 +0000

    Add new InvalidServiceException thats defined in the public review version of the Java CAA spec
    git-svn-id: https://svn.apache.org/repos/asf/tuscany/java/sca@786437 13f79535-47bb-0310-9956-ffa450edef68

---
+++ modules/sca-api/src/main/java/org/oasisopen/sca/InvalidServiceException.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright(C) OASIS(R) 2005,2009. All Rights Reserved.
+ * OASIS trademark, IPR and other policies apply.
+ */
+package org.oasisopen.sca;
+
+/**
+ * This exception signals that the ServiceReference is no longer valid.
+ * This can happen when the target of the reference is undeployed.
+ *
+ * This exception is not transient and therefore is unlikely to be
+ * resolved by retrying the operation and will most likely require
+ * human intervention.
+ */
+public class InvalidServiceException extends ServiceRuntimeException {
+    /**
+     * Constructs a InvalidServiceException with no detail message. 
+     */
+    public InvalidServiceException() {
+        super();
+    }
+
+    /**
+     * Constructs a InvalidServiceException with the specified detail
+     * message. 
+     *
+     * @param     message the detail message
+     */
+    public InvalidServiceException(String message) {
+        super(message);
+    }
+
+    /**
+     * Constructs a InvalidServiceException with the specified detail
+     * message and cause.
+     *
+     * The detail message associated with <code>cause</code> is not
+     * automatically incorporated in this exception's detail message.
+     *
+     * @param     message the detail message
+     * @param     cause the cause, or null if the cause is nonexistent
+     *            or unknown
+     */
+    public InvalidServiceException(String message, Throwable cause) {
+        super(message, cause);
+    }
+ 
+    /**
+     * Constructs a InvalidServiceException with the specified cause and
+     * a detail message of <tt>(cause==null ? null : cause.toString())</tt>.
+     *
+     * @param     cause the cause, or null if the cause is nonexistent
+     *            or unknown
+     */
+    public InvalidServiceException(Throwable cause) {
+        super(cause);
+    }
+
+    private static final long serialVersionUID = 7520492728695222145L;
+}
