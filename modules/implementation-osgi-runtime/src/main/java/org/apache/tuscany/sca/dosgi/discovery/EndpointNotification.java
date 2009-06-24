b8e4d5547d065d1f7c8527800def80e98eac3d7a
Zhaohui Feng <rfeng@apache.org>
2009-06-24 23:34:57 +0000

    Start to add discovery service for distributed OSGi over SCA
    git-svn-id: https://svn.apache.org/repos/asf/tuscany/java/sca@788210 13f79535-47bb-0310-9956-ffa450edef68

---
+++ modules/implementation-osgi-runtime/src/main/java/org/apache/tuscany/sca/dosgi/discovery/EndpointNotification.java
@@ -0,0 +1,67 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.apache.tuscany.sca.dosgi.discovery;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashSet;
+
+import org.osgi.service.discovery.DiscoveredServiceNotification;
+import org.osgi.service.discovery.ServiceEndpointDescription;
+
+class EndpointNotification implements DiscoveredServiceNotification {
+
+    private ServiceEndpointDescription discription;
+    private Collection<String> interfaces;
+    private Collection<String> filters;
+    private int type;
+
+    EndpointNotification(ServiceEndpointDescription sd, boolean isFilter, String match, int type) {
+        this.discription = sd;
+        if (isFilter) {
+            filters = new ArrayList<String>();
+            filters.add(match);
+            interfaces = Collections.emptySet();
+        } else {
+            interfaces = new HashSet<String>();
+            interfaces.add(match);
+            filters = Collections.emptyList();
+        }
+
+        this.type = type;
+    }
+
+    public ServiceEndpointDescription getServiceEndpointDescription() {
+        return discription;
+    }
+
+    public int getType() {
+        return type;
+    }
+
+    public Collection<String> getInterfaces() {
+        return interfaces;
+    }
+
+    public Collection<String> getFilters() {
+        return filters;
+    }
+}
---
+++ modules/implementation-osgi-runtime/src/main/java/org/apache/tuscany/sca/dosgi/discovery/LocalDiscoveryService.java
@@ -0,0 +1,108 @@
+/**
+  * Licensed to the Apache Software Foundation (ASF) under one
+  * or more contributor license agreements. See the NOTICE file
+  * distributed with this work for additional information
+  * regarding copyright ownership. The ASF licenses this file
+  * to you under the Apache License, Version 2.0 (the
+  * "License"); you may not use this file except in compliance
+  * with the License. You may obtain a copy of the License at
+  *
+  * http://www.apache.org/licenses/LICENSE-2.0
+  *
+  * Unless required by applicable law or agreed to in writing,
+  * software distributed under the License is distributed on an
+  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+  * KIND, either express or implied. See the License for the
+  * specific language governing permissions and limitations
+  * under the License.
+  */
+package org.apache.tuscany.sca.dosgi.discovery;
+
+import static org.osgi.framework.Bundle.ACTIVE;
+import static org.osgi.framework.BundleEvent.STARTED;
+import static org.osgi.framework.BundleEvent.STOPPING;
+import static org.osgi.service.discovery.DiscoveredServiceNotification.AVAILABLE;
+import static org.osgi.service.discovery.DiscoveredServiceNotification.UNAVAILABLE;
+
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import org.apache.tuscany.sca.core.ExtensionPointRegistry;
+import org.osgi.framework.Bundle;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.BundleEvent;
+import org.osgi.framework.BundleListener;
+import org.osgi.service.discovery.ServiceEndpointDescription;
+
+public class LocalDiscoveryService extends AbstractDiscoveryService implements BundleListener {
+    private ExtensionPointRegistry registry;
+
+    public LocalDiscoveryService(BundleContext context) {
+        super(context);
+        context.addBundleListener(this);
+        this.registry = getExtensionPointRegistry();
+        processExistingBundles();
+    }
+
+    public void bundleChanged(BundleEvent event) {
+        switch (event.getType()) {
+            case STARTED:
+                discover(event.getBundle());
+                break;
+            case STOPPING:
+                removeServicesDeclaredInBundle(event.getBundle());
+                break;
+        }
+    }
+
+    private void processExistingBundles() {
+        Bundle[] bundles = context.getBundles();
+        if (bundles == null) {
+            return;
+        }
+
+        for (Bundle b : bundles) {
+            if (b.getState() == ACTIVE) {
+                discover(b);
+            }
+        }
+    }
+
+    private void discover(Bundle b) {
+        // TODO: Use SCA contribution to load the service discription files
+        List<ServiceEndpointDescription> refs = Collections.emptyList();
+        for (ServiceEndpointDescription sed : refs) {
+            servicesInfo.put(sed, b);
+            serviceDescriptionAdded(sed);
+        }
+        throw new RuntimeException("To be implemented");
+    }
+
+    private void removeServicesDeclaredInBundle(Bundle bundle) {
+        for (Iterator<Map.Entry<ServiceEndpointDescription, Bundle>> i = servicesInfo.entrySet().iterator(); i
+            .hasNext();) {
+            Entry<ServiceEndpointDescription, Bundle> entry = i.next();
+            if (entry.getValue().equals(bundle)) {
+                serviceDescriptionRemoved(entry.getKey());
+                i.remove();
+            }
+        }
+    }
+
+    private void serviceDescriptionAdded(ServiceEndpointDescription sd) {
+        discoveredServiceChanged(sd, AVAILABLE);
+    }
+
+    private void serviceDescriptionRemoved(ServiceEndpointDescription sd) {
+        discoveredServiceChanged(sd, UNAVAILABLE);
+    }
+
+    public void stop() {
+        context.removeBundleListener(this);
+        super.stop();
+    }
+
+}
