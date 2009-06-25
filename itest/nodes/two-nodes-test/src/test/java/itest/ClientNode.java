93ae0422dcc1a72bc900e010ff3bdc99426c8800
Zhaohui Feng <rfeng@apache.org>
2009-06-25 16:36:49 +0000

    Add a testcase that runs two nodes in the same JVM but different classloaders
    git-svn-id: https://svn.apache.org/repos/asf/tuscany/java/sca@788418 13f79535-47bb-0310-9956-ffa450edef68

---
+++ itest/nodes/two-nodes-test/src/test/java/itest/ClientNode.java
@@ -0,0 +1,87 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package itest;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import itest.nodes.Helloworld;
+
+import java.io.File;
+
+import org.apache.tuscany.sca.node.Contribution;
+import org.apache.tuscany.sca.node.Node;
+import org.apache.tuscany.sca.node.NodeFactory;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.oasisopen.sca.client.SCAClient;
+import org.oasisopen.sca.client.SCAClientFactory;
+
+/**
+ * This shows how to test the Calculator service component.
+ */
+public class ClientNode{
+
+    private static Node clientNode;
+    private static TestCaseRunner runner;
+
+    @BeforeClass
+    public static void setUpBeforeClass() throws Exception {
+        runner = new TestCaseRunner(ServiceNode.class);
+        runner.beforeClass();
+        System.setProperty("org.apache.tuscany.sca.contribution.processor.ValidationSchemaExtensionPoint.enabled", "false");
+        NodeFactory factory = NodeFactory.newInstance();
+        clientNode = factory.createNode(new Contribution("client", new File("../helloworld-client/target/itest-nodes-helloworld-client-2.0-SNAPSHOT.jar").toURI().toString()));
+        clientNode.start();
+        Thread.sleep(2000);
+    }
+
+    @Test
+    public void testCalculator() throws Exception {
+
+        Helloworld client = clientNode.getService(Helloworld.class, "HelloworldClient");
+        assertNotNull(client);
+        assertEquals("Hi Hello Petra", client.sayHello("Petra"));
+    }
+
+    @Test
+    @Ignore("SCAClient needs to leverage the EndpointRegistry to invoke services that are not hosted on the local node")
+    public void testCalculatorClientAPI() throws Exception {
+        SCAClient scaClient = SCAClientFactory.newInstance();
+        Helloworld service = scaClient.getService(Helloworld.class, "HelloworldService", null);
+        assertNotNull(service);
+        assertEquals("Hello Petra", service.sayHello("Petra"));
+
+        Helloworld client = scaClient.getService(Helloworld.class, "HelloworldClient", null);
+        assertNotNull(client);
+        assertEquals("Hi Hello Petra", client.sayHello("Petra"));
+    }
+
+    @AfterClass
+    public static void tearDownAfterClass() throws Exception {
+        if (clientNode != null) {
+            clientNode.stop();
+        }
+        if(runner!=null) {
+            runner.afterClass();
+        }
+    }
+}
