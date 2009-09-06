9edc1389ed281cbff30f26ade1cae5d18a08122c
Luciano Resende <lresende@apache.org>
2009-09-06 05:53:25 +0000

    TUSCANY-2995 - Increasing invocation timeout to 2 seconds
    git-svn-id: https://svn.apache.org/repos/asf/tuscany/java/sca@811764 13f79535-47bb-0310-9956-ffa450edef68

--- modules/implementation-bpel-runtime/src/main/java/org/apache/tuscany/sca/implementation/bpel/ode/provider/BPELInvoker.java
+++ modules/implementation-bpel-runtime/src/main/java/org/apache/tuscany/sca/implementation/bpel/ode/provider/BPELInvoker.java
@@ -58,7 +58,7 @@ import org.w3c.dom.Element;
  * @version $Rev$ $Date$
  */
 public class BPELInvoker implements Invoker {
-	private final static long TIME_OUT = 500L;
+	private final static long TIME_OUT = 2000L;
 	
     protected final Log __log = LogFactory.getLog(getClass());
 
