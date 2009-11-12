ca38d92e2c6168d8dc6ba6e9b123fe25f917bc47
Luciano Resende <lresende@apache.org>
2009-11-12 04:26:15 +0000

    Start porting Implementation Widget to 2.x
    git-svn-id: https://svn.apache.org/repos/asf/tuscany/java/sca@835222 13f79535-47bb-0310-9956-ffa450edef68

---
+++ modules/binding-jsonrpc-js-dojo/src/main/java/org/apache/tuscany/sca/binding/jsonrpc/js/dojo/JSONRPCBindingJavascriptProxyFactoryImpl.java
@@ -0,0 +1,60 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.    
+ */
+
+package org.apache.tuscany.sca.binding.jsonrpc.js.dojo;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URI;
+
+import javax.xml.namespace.QName;
+
+import org.apache.tuscany.sca.assembly.Binding;
+import org.apache.tuscany.sca.assembly.ComponentReference;
+import org.apache.tuscany.sca.binding.jsonrpc.JSONRPCBinding;
+import org.apache.tuscany.sca.web.javascript.JavascriptProxyFactory;
+
+public class JSONRPCBindingJavascriptProxyFactoryImpl implements JavascriptProxyFactory {
+    private static final QName NAME = new QName("http://tuscany.apache.org/xmlns/sca/1.0", "binding.jsonrpc");
+    
+    public Class<?> getModelType() {
+        return JSONRPCBinding.class;
+    }
+
+    public QName getQName() {
+        return NAME;
+    }
+
+    public String getJavascriptProxyFile() {
+        return null;
+    }
+
+    public InputStream getJavascriptProxyFileAsStream() throws IOException {
+        return null;
+    }
+    
+    public String createJavascriptReference(ComponentReference componentReference) throws IOException {
+        Binding binding = componentReference.getBindings().get(0);
+        URI targetURI = URI.create(binding.getURI());
+        String targetPath = targetURI.getPath();
+        
+        return "dojo.rpc.JsonService(\"" + targetPath + "?smd\")";        
+    }
+
+}
