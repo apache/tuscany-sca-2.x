e95d4fbd43580f69a12dfd854f9fc240f6ecfeb1
Anthony Elder <antelder@apache.org>
2010-01-20 11:02:48 +0000

    Update the client api classes to be the very latest from the oasis svn
    git-svn-id: https://svn.apache.org/repos/asf/tuscany/sca-java-2.x/trunk@901126 13f79535-47bb-0310-9956-ffa450edef68

--- modules/sca-api/src/main/java/org/oasisopen/sca/client/SCAClientFactory.java
+++ modules/sca-api/src/main/java/org/oasisopen/sca/client/SCAClientFactory.java
@@ -1,6 +1,6 @@
-/* 
- * Copyright(C) OASIS(R) 2005,2009. All Rights Reserved. 
- * OASIS trademark, IPR and other policies apply. 
+/*
+ * Copyright(C) OASIS(R) 2005,2009. All Rights Reserved.
+ * OASIS trademark, IPR and other policies apply.
  */
 package org.oasisopen.sca.client;
 
@@ -9,28 +9,49 @@ import java.util.Properties;
 
 import org.oasisopen.sca.NoSuchDomainException;
 import org.oasisopen.sca.NoSuchServiceException;
-import org.oasisopen.sca.client.impl.SCAClientFactoryFinder;
+import org.oasisopen.sca.client.SCAClientFactoryFinder;
+import org.oasisopen.sca.client.impl.SCAClientFactoryFinderImpl;
 
 /**
- * The SCAClientFactory can be used by non-SCA managed code to lookup services
- * that exist in a SCADomain.
+ * The SCAClientFactory can be used by non-SCA managed code to 
+ * lookup services that exist in a SCADomain.
  * 
- * @see SCAClientFactoryFinder
- * @see SCAClient
+ * @see SCAClientFactoryFinderImpl
+ * 
  * @author OASIS Open
  */
+
 public abstract class SCAClientFactory {
-    
+
+    /**
+     * The SCAClientFactoryFinder. 
+     * Provides a means by which a provider of an SCAClientFactory
+     * implementation can inject a factory finder implementation into
+     * the abstract SCAClientFactory class - once this is done, future
+     * invocations of the SCAClientFactory use the injected factory
+     * finder to locate and return an instance of a subclass of
+     * SCAClientFactory.
+     */
+    protected static SCAClientFactoryFinder factoryFinder;
+    /**
+     * The Domain URI of the SCA Domain which is accessed by this 
+     * SCAClientFactory
+     */
     private URI domainURI;
 
+    /**
+     * Prevent concrete subclasses from using the no-arg constructor
+     */
     private SCAClientFactory() {
     }
 
     /**
      * Constructor used by concrete subclasses
-     * @param domainURI - The Domain URI of the Domain accessed via this SCAClientFactory
+     * @param domainURI - The Domain URI of the Domain accessed via this
+     * SCAClientFactory
      */
-    protected SCAClientFactory(URI domainURI) {
+    protected SCAClientFactory(URI domainURI)
+        throws NoSuchDomainException {
         this.domainURI = domainURI;
     }
 
@@ -41,69 +62,73 @@ public abstract class SCAClientFactory {
     protected URI getDomainURI() {
         return domainURI;
     }
-    
+   
+      
     /**
-     * The default implementation of the SCAClientFactory. A Vendor may use
-     * reflection to inject a default SCAClientFactory instance that will be
-     * used in the newInstance() methods rather than using the
-     * SCAClientFactoryFinder.
-     */
-    protected static SCAClientFactory defaultFactory;
-
-    /**
-     * Creates a new instance of the SCAClient that can be used to lookup SCA
-     * Services.
+     * Creates a new instance of the SCAClientFactory that can be 
+     * used to lookup SCA Services.
      * 
-     * @return A new SCAClient
+     * @param domainURI         URI of the target domain for the SCAClientFactory
+     * @return A new SCAClientFactory 
      */
-    public static SCAClientFactory newInstance(URI domainURI) throws NoSuchDomainException {
+    public static SCAClientFactory newInstance( URI domainURI ) 
+        throws NoSuchDomainException {
         return newInstance(null, null, domainURI);
     }
-
+    
     /**
-     * Creates a new instance of the SCAClient that can be used to lookup SCA
-     * Services.
-     * 
-     * @param properties Properties that may be used when creating a new
-     *                instance of the SCAClient
-     * @return A new SCAClient instance
+     * Creates a new instance of the SCAClientFactory that can be 
+     * used to lookup SCA Services.
+     *
+     * @param properties   Properties that may be used when 
+     * creating a new instance of the SCAClientFactory
+     * @param domainURI         URI of the target domain for the SCAClientFactory
+     * @return A new SCAClientFactory instance
      */
-    public static SCAClientFactory newInstance(Properties properties, URI domainURI) {
+    public static SCAClientFactory newInstance(Properties properties,
+                                                                        URI domainURI) 
+        throws NoSuchDomainException {
         return newInstance(properties, null, domainURI);
     }
 
     /**
-     * Creates a new instance of the SCAClient that can be used to lookup SCA
-     * Services.
-     * 
-     * @param classLoader ClassLoader that may be used when creating a new
-     *                instance of the SCAClient
-     * @return A new SCAClient instance
+     * Creates a new instance of the SCAClientFactory that can be 
+     * used to lookup SCA Services.
+     *
+     * @param classLoader   ClassLoader that may be used when 
+     * creating a new instance of the SCAClientFactory
+     * @param domainURI         URI of the target domain for the SCAClientFactory
+     * @return A new SCAClientFactory instance
      */
-    public static SCAClientFactory newInstance(ClassLoader classLoader, URI domainURI) {
+    public static SCAClientFactory newInstance(ClassLoader classLoader, 
+                                                                        URI domainURI) 
+        throws NoSuchDomainException {
         return newInstance(null, classLoader, domainURI);
     }
 
     /**
-     * Creates a new instance of the SCAClient that can be used to lookup SCA
-     * Services.
-     * 
-     * @param properties Properties that may be used when creating a new
-     *                instance of the SCAClient
-     * @param classLoader ClassLoader that may be used when creating a new
-     *                instance of the SCAClient
-     * @return A new SCAClient instance
+     * Creates a new instance of the SCAClientFactory that can be 
+     * used to lookup SCA Services.
+     *
+     * @param properties    Properties that may be used when 
+     * creating a new instance of the SCAClientFactory
+     * @param classLoader   ClassLoader that may be used when 
+     * creating a new instance of the SCAClientFactory
+     * @param domainURI         URI of the target domain for the SCAClientFactory
+     * @return A new SCAClientFactory instance
      */
-    public static SCAClientFactory newInstance(Properties properties, ClassLoader classLoader, URI domainURI) {
-        final SCAClientFactory factory;
-        if (defaultFactory == null) {
-            factory = SCAClientFactoryFinder.find(properties, classLoader, domainURI);
-        } else {
-            factory = defaultFactory;
-        }
+    public static SCAClientFactory newInstance(Properties properties, 
+                                            ClassLoader classLoader,
+                                            URI domainURI) 
+        throws NoSuchDomainException {
+        final SCAClientFactoryFinder finder =
+            factoryFinder != null ? factoryFinder :
+                new SCAClientFactoryFinderImpl();
+        final SCAClientFactory factory
+            = finder.find(properties, classLoader, domainURI);
         return factory;
     }
-
+  
     /**
      * Returns a reference proxy that implements the business interface <T>
      * of a service in the SCA Domain handled by this SCAClientFactory
@@ -112,15 +137,16 @@ public abstract class SCAClientFactory {
      * form componentName/serviceName.
      * Can also take the extended form componentName/serviceName/bindingName
      * to use a specific binding of the target service
-     *
-     * @param interfaze The business interface class of the service in the
+     * 
+     * @param interfaze The business interface class of the service in the 
      * domain
      * @param <T> The business interface class of the service in the domain
      *
-     * @return a proxy to the target service, in the specified SCA Domain
+     * @return a proxy to the target service, in the specified SCA Domain 
      * that implements the business interface <B>.
      * @throws NoSuchServiceException Service requested was not found
      * @throws NoSuchDomainException Domain requested was not found
      */
-     public abstract <T> T getService(Class<T> interfaze, String serviceURI) throws NoSuchServiceException, NoSuchDomainException;
+    public abstract <T> T getService(Class<T> interfaze, String serviceURI) 
+        throws NoSuchServiceException, NoSuchDomainException;    
 }
---
+++ modules/sca-api/src/main/java/org/oasisopen/sca/client/SCAClientFactoryFinder.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright(C) OASIS(R) 2005,2009. All Rights Reserved.
+ * OASIS trademark, IPR and other policies apply.
+ */
+
+package org.oasisopen.sca.client;
+
+import java.net.URI;
+import java.util.Properties;
+
+import org.oasisopen.sca.NoSuchDomainException;
+
+/* A Service Provider Interface representing a SCAClientFactory finder.  
+ * SCA provides a default reference implementation of this interface.
+ * SCA runtime vendors can create alternative implementations of this 
+ * interface that use different class loading or lookup mechanisms.
+ */
+public interface SCAClientFactoryFinder {
+
+        /**
+         * Method for finding the SCAClientFactory for a given Domain URI using
+         * a specified set of properties and a a specified ClassLoader
+         * @param properties - properties to use - may be null
+         * @param classLoader - ClassLoader to use - may be null
+         * @param domainURI - the Domain URI - must be a valid SCA Domain URI
+         * @return - the SCAClientFactory or null if the factory could not be
+         * @throws - NoSuchDomainException if the domainURI does not reference 
+         * a valid SCA Domain 
+         * found
+         */
+    SCAClientFactory find(Properties properties,
+                          ClassLoader classLoader,
+                          URI domainURI ) 
+        throws NoSuchDomainException ;
+}
