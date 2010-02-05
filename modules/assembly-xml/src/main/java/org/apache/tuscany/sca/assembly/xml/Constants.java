c5c8c43ae621bbaf1edb5e83f41cb3ca7cee4b40
Simon Laws <slaws@apache.org>
2010-02-05 11:11:29 +0000

    Add constants for some of the intents defined by SCA policy spec.
    git-svn-id: https://svn.apache.org/repos/asf/tuscany/sca-java-2.x/trunk@906900 13f79535-47bb-0310-9956-ffa450edef68

--- modules/assembly-xml/src/main/java/org/apache/tuscany/sca/assembly/xml/Constants.java
+++ modules/assembly-xml/src/main/java/org/apache/tuscany/sca/assembly/xml/Constants.java
@@ -101,4 +101,52 @@ public interface Constants {
     String ZERO_N = "0..n";
     String ONE_ONE = "1..1";
     String ONE_N = "1..n";
+    
+    String SERVER_AUTHENTICATION = "serverAuthentication";
+    QName SERVER_AUTHENTICATION_INTENT = new QName(SCA11_NS, SERVER_AUTHENTICATION);
+    String SERVER_AUTHENTICATION_TRANSPORT = "serverAuthentication.transport";
+    QName SERVER_AUTHENTICATION_TRANSPORT_INTENT = new QName(SCA11_NS, SERVER_AUTHENTICATION_TRANSPORT);
+    String SERVER_AUTHENTICATION_MESSAGE = "serverAuthentication.message";
+    QName SERVER_AUTHENTICATION_MESSAGE_INTENT = new QName(SCA11_NS, SERVER_AUTHENTICATION_MESSAGE);
+
+    String CLIENT_AUTHENTICATION = "clientAuthentication";
+    QName CLIENT_AUTHENTICATION_INTENT = new QName(SCA11_NS, CLIENT_AUTHENTICATION);
+    String CLIENT_AUTHENTICATION_TRANSPORT = "clientAuthentication.transport";
+    QName CLIENT_AUTHENTICATION_TRANSPORT_INTENT = new QName(SCA11_NS, CLIENT_AUTHENTICATION_TRANSPORT);
+    String CLIENT_AUTHENTICATION_MESSAGE = "clientAuthentication.message";
+    QName CLIENT_AUTHENTICATION_MESSAGE_INTENT = new QName(SCA11_NS, CLIENT_AUTHENTICATION_MESSAGE);
+    
+    String AUTHENTICATION = "authentication";
+    QName AUTHENTICATION_INTENT = new QName(SCA11_NS, AUTHENTICATION);
+    
+    String MUTUAL_AUTHENTICATION = "mutualAuthentication";
+    QName MUTUAL_AUTHENTICATION_INTENT = new QName(SCA11_NS, MUTUAL_AUTHENTICATION);    
+    
+    String CONFIDENTIALITY = "confidentiality";
+    QName CONFIDENTIALITY_INTENT = new QName(SCA11_NS, CONFIDENTIALITY);
+    String CONFIDENTIALITY_TRANSPORT = "confidentiality.transport";
+    QName CONFIDENTIALITY_TRANSPORT_INTENT = new QName(SCA11_NS, CONFIDENTIALITY_TRANSPORT);
+    String CONFIDENTIALITY_MESSAGE = "confidentiality.message";
+    QName CONFIDENTIALITY_MESSAGE_INTENT = new QName(SCA11_NS, CONFIDENTIALITY_MESSAGE);    
+    
+    String INTEGRITY = "integrity";
+    QName INTEGRITY_INTENT = new QName(SCA11_NS, "INTEGRITY");
+    String INTEGRITY_TRANSPORT = "integrity.transport";
+    QName INTEGRITY_TRANSPORT_INTENT = new QName(SCA11_NS, INTEGRITY_TRANSPORT);
+    String INTEGRITY_MESSAGE = "integrity.message";
+    QName INTEGRITY_MESSAGE_INTENT = new QName(SCA11_NS, INTEGRITY_MESSAGE); 
+    
+    String AUTHORIZATION = "authorization";
+    QName AUTHORIZATION_INTENT = new QName(SCA11_NS, "INTEGRITY");
+    String AUTHORIZATION_FINE_GRAIN = "authorization.fineGrain";
+    QName AUTHORIZATION_FINE_GRAIN_INTENT = new QName(SCA11_NS, AUTHORIZATION_FINE_GRAIN);  
+    
+    // TODO - add transaction intent constants
+    
+    String SOAP = "SOAP";
+    QName SOAP_INTENT = new QName(SCA11_NS, SOAP);
+    String SOAP11 = "SOAP.v1_1";
+    QName SOAP11_INTENT = new QName(SCA11_NS, SOAP11);
+    String SOAP12 = "SOAP.v1_2";
+    QName SOAP12_INTENT = new QName(SCA11_NS, SOAP12);
 }
