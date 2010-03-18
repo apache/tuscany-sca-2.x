944f8140e677e44f86034a49890e384276efdaf1
Anthony Elder <antelder@apache.org>
2010-03-18 15:29:59 +0000

    Start of testcases for hazelcast binding
    git-svn-id: https://svn.apache.org/repos/asf/tuscany/sca-java-2.x/trunk@924864 13f79535-47bb-0310-9956-ffa450edef68

---
+++ modules/binding-hazelcast-runtime/src/test/java/org/apache/tuscany/sca/binding/hazelcast/HazelcastBindingTestCase.java
@@ -0,0 +1,64 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.apache.tuscany.sca.binding.hazelcast;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import java.net.URI;
+
+import org.apache.tuscany.sca.node.Node;
+import org.apache.tuscany.sca.node.NodeFactory;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class HazelcastBindingTestCase {
+
+    private static Node serviceNode;
+    private static Node clientNode;
+
+    @BeforeClass
+    public static void setUpBeforeClass() throws Exception {
+        // Note use of NodeFactory.newInstance() so as to start separate runtimes
+        serviceNode = NodeFactory.newInstance().createNode(URI.create("tuscany:HazelcastBindingTestCase"), "service.composite", new String[]{"target/test-classes"});
+        serviceNode.start();
+        clientNode = NodeFactory.newInstance().createNode(URI.create("tuscany:HazelcastBindingTestCase"),  "client.composite", new String[]{"target/test-classes"});
+        clientNode.start();
+    }
+
+    @Test
+    public void testNode() throws Exception {
+        TestService service = clientNode.getService(TestService.class, "TestServiceClient");
+        assertNotNull(service);
+        assertEquals("Petra", service.echoString("Petra"));
+    }
+
+    @AfterClass
+    public static void tearDownAfterClass() throws Exception {
+        if (clientNode != null) {
+            clientNode.stop();
+        }
+        if (serviceNode != null) {
+            serviceNode.stop();
+        }
+    }
+}
+
