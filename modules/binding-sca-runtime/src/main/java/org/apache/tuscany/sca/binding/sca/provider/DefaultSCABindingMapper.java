574a39d4beca34add160fecaae188a22405c34bf
Anthony Elder <antelder@apache.org>
2010-03-18 20:43:47 +0000

    Update sca binding mapper to use hazelcast binding if its there
    git-svn-id: https://svn.apache.org/repos/asf/tuscany/sca-java-2.x/trunk@924985 13f79535-47bb-0310-9956-ffa450edef68

--- modules/binding-sca-runtime/src/main/java/org/apache/tuscany/sca/binding/sca/provider/DefaultSCABindingMapper.java
+++ modules/binding-sca-runtime/src/main/java/org/apache/tuscany/sca/binding/sca/provider/DefaultSCABindingMapper.java
@@ -78,9 +78,12 @@ public class DefaultSCABindingMapper implements SCABindingMapper {
                 mappedBinding = ServiceDeclarationParser.getQName(qname);
             } else {
                 // By default, mapping to binding.ws or if thats not available then binding.rmi
-                mappedBinding = new QName(Base.SCA11_NS, "binding.ws");
+                mappedBinding = new QName(Base.SCA11_TUSCANY_NS, "binding.hazelcast");
                 if (registry.getExtensionPoint(StAXArtifactProcessorExtensionPoint.class).getProcessor(mappedBinding) == null) {
-                    mappedBinding = new QName(Base.SCA11_TUSCANY_NS, "binding.rmi");
+                    mappedBinding = new QName(Base.SCA11_NS, "binding.ws");
+                    if (registry.getExtensionPoint(StAXArtifactProcessorExtensionPoint.class).getProcessor(mappedBinding) == null) {
+                        mappedBinding = new QName(Base.SCA11_TUSCANY_NS, "binding.rmi");
+                    }
                 }
             }
         }
