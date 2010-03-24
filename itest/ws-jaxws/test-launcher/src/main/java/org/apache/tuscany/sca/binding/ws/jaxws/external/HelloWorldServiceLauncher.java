c5cebfdaf79e2f18b38162e1b1a7752488b62d73
Simon Laws <slaws@apache.org>
2010-03-24 14:03:50 +0000

    Test for JAXWS based ws binding that separates the SCA contribution from external JAXWS client and service with which the SCA component interacts.
    git-svn-id: https://svn.apache.org/repos/asf/tuscany/sca-java-2.x/trunk@927061 13f79535-47bb-0310-9956-ffa450edef68

---
+++ itest/ws-jaxws/test-launcher/src/main/java/org/apache/tuscany/sca/binding/ws/jaxws/external/HelloWorldServiceLauncher.java
@@ -0,0 +1,33 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.apache.tuscany.sca.binding.ws.jaxws.external;
+
+import javax.xml.ws.Endpoint;
+
+public class HelloWorldServiceLauncher {
+    
+    public static void main(String[] args) throws Exception {
+        
+        // default JVM JAXWS support. 
+        Endpoint.publish("http://localhost:8086/External/HelloWorld",
+                         new HelloWorldService());
+
+    }
+}
