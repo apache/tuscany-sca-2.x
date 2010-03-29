16123f97b503b858d6e0b28680b5a8541a92e358
Zhaohui Feng <rfeng@apache.org>
2010-03-29 17:04:35 +0000

    Port databinding-sdo-axiom into 2.x based on the patch from Ramanjaneyulu Malisetti (https://issues.apache.org/jira/browse/TUSCANY-3470)
    git-svn-id: https://svn.apache.org/repos/asf/tuscany/sca-java-2.x/trunk@928835 13f79535-47bb-0310-9956-ffa450edef68

---
+++ modules/databinding-sdo-axiom/src/main/java/org/apache/tuscany/sca/databinding/sdo2om/SDODataSource.java
@@ -0,0 +1,89 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.    
+ */
+
+package org.apache.tuscany.sca.databinding.sdo2om;
+
+import static org.apache.tuscany.sca.databinding.sdo.SDODataBinding.ROOT_ELEMENT;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.Writer;
+
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.XMLStreamReader;
+import javax.xml.stream.XMLStreamWriter;
+
+import org.apache.axiom.om.OMDataSource;
+import org.apache.axiom.om.OMOutputFormat;
+import org.apache.axiom.om.impl.serialize.StreamingOMSerializer;
+import org.apache.tuscany.sdo.api.SDOUtil;
+import org.apache.tuscany.sdo.api.XMLStreamHelper;
+
+import commonj.sdo.DataObject;
+import commonj.sdo.helper.HelperContext;
+import commonj.sdo.helper.XMLDocument;
+
+/**
+ *
+ * @version $Rev$ $Date$
+ */
+public class SDODataSource implements OMDataSource {
+    private HelperContext helperContext;
+    private XMLDocument sourceDocument;
+
+    public SDODataSource(XMLDocument source, HelperContext helperContext) {
+        this.sourceDocument = source;
+        this.helperContext = helperContext;
+    }
+
+    public SDODataSource(DataObject obj, HelperContext helperContext) {
+        this.helperContext = helperContext;
+        this.sourceDocument =
+            helperContext.getXMLHelper().createDocument(obj,
+                                                        ROOT_ELEMENT.getNamespaceURI(),
+                                                        ROOT_ELEMENT.getLocalPart());
+    }
+
+    public XMLStreamReader getReader() throws XMLStreamException {
+        XMLStreamHelper streamHelper = SDOUtil.createXMLStreamHelper(helperContext);
+        return streamHelper.createXMLStreamReader(sourceDocument);
+    }
+
+    public void serialize(XMLStreamWriter xmlWriter) throws XMLStreamException {
+        StreamingOMSerializer serializer = new StreamingOMSerializer();
+        serializer.serialize(getReader(), xmlWriter);
+    }
+
+    public void serialize(OutputStream output, OMOutputFormat format) throws XMLStreamException {
+        try {
+            helperContext.getXMLHelper().save(sourceDocument, output, null);
+        } catch (Exception e) {
+            throw new XMLStreamException(e);
+        }
+    }
+
+    public void serialize(Writer writer, OMOutputFormat format) throws XMLStreamException {
+        try {
+            helperContext.getXMLHelper().save(sourceDocument, writer, null);
+        } catch (IOException e) {
+            throw new XMLStreamException(e);
+        }
+    }
+
+}
---
+++ modules/databinding-sdo-axiom/src/test/java/org/apache/tuscany/sca/databinding/sdo2om/DataObject2OMElementTestCase.java
@@ -0,0 +1,78 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.    
+ */
+
+package org.apache.tuscany.sca.databinding.sdo2om;
+
+import java.io.StringWriter;
+
+import javax.xml.stream.XMLStreamException;
+
+import junit.framework.Assert;
+
+import org.apache.axiom.om.OMAbstractFactory;
+import org.apache.axiom.om.OMElement;
+import org.apache.axiom.om.OMNamespace;
+import org.apache.tuscany.sca.interfacedef.DataType;
+import org.apache.tuscany.sca.interfacedef.impl.DataTypeImpl;
+import org.apache.tuscany.sca.interfacedef.util.XMLType;
+
+import commonj.sdo.DataObject;
+
+/**
+ *
+ * @version $Rev$ $Date$
+ */
+public class DataObject2OMElementTestCase extends SDOTransformerTestCaseBase {
+
+    @Override
+    protected DataType<?> getSourceDataType() {
+        return new DataTypeImpl<XMLType>(DataObject.class.getName(), DataObject.class, new XMLType(ORDER_QNAME, null));
+    }
+
+    @Override
+    protected DataType<?> getTargetDataType() {
+        return new DataTypeImpl<XMLType>(OMElement.class.getName(), OMElement.class, new XMLType(ORDER_QNAME, null));
+    }
+
+    public final void testTransform() throws XMLStreamException {
+        OMElement element = new DataObject2OMElement().transform(dataObject, context);
+        Assert.assertEquals(ORDER_QNAME.getNamespaceURI(), element.getNamespace().getNamespaceURI());
+        Assert.assertEquals(ORDER_QNAME.getLocalPart(), element.getLocalName());
+        // TODO: See https://issues.apache.org/jira/browse/WSCOMMONS-226
+        // element.getBuilder().setCache(false);
+        StringWriter writer = new StringWriter();
+        element.serialize(writer);
+    }
+
+    public final void testTransformWrapper() throws XMLStreamException {
+        OMElement element = new DataObject2OMElement().transform(dataObject, context);
+        Assert.assertEquals(ORDER_QNAME.getNamespaceURI(), element.getNamespace().getNamespaceURI());
+        Assert.assertEquals(ORDER_QNAME.getLocalPart(), element.getLocalName());
+
+        OMNamespace ns = OMAbstractFactory.getOMFactory().createOMNamespace("http://ns1", "ns1");
+        element.setNamespace(ns);
+        element.setLocalName("dummy");
+        // TODO: See https://issues.apache.org/jira/browse/WSCOMMONS-226
+        // element.getBuilder().setCache(true);
+        StringWriter writer = new StringWriter();
+        element.serializeAndConsume(writer);
+        // System.out.println(writer);
+    }
+
+}
