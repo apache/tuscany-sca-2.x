c5b3d26606a96723a85feab80565f1f357a47ec1
Anthony Elder <antelder@apache.org>
2010-04-08 11:02:01 +0000

    Update sca-api with latest OASIS code, these ones have only formatting and javadoc changes
    git-svn-id: https://svn.apache.org/repos/asf/tuscany/sca-java-2.x/trunk@931875 13f79535-47bb-0310-9956-ffa450edef68

--- modules/sca-api/src/main/java/org/oasisopen/sca/ServiceReference.java
+++ modules/sca-api/src/main/java/org/oasisopen/sca/ServiceReference.java
@@ -1,19 +1,36 @@
 /*
- * Copyright(C) OASIS(R) 2005,2009. All Rights Reserved.
+ * Copyright(C) OASIS(R) 2005,2010. All Rights Reserved.
  * OASIS trademark, IPR and other policies apply.
  */
 package org.oasisopen.sca;
 
-import java.io.Serializable;
-
-
 /**
- * A ServiceReference represents a client's perspective of a reference to another service.
+ * The ServiceReference interface represents a component reference.
+ * It can be injected using the @Reference annotation
+ * on a field, a setter method, or constructor parameter taking the
+ * type ServiceReference.
  *
- * @version $Rev$ $Date$
- * @param <B> the Java interface associated with this reference
+ * @param     <B> the type of the service reference
  */
-public interface ServiceReference<B> extends Serializable {
+public interface ServiceReference<B> extends java.io.Serializable {
+
+    /**
+     * Returns a type-safe reference to the target of this reference.
+     * The instance returned is guaranteed to implement the business
+     * interface for this reference.  The value returned is a proxy
+     * to the target that implements the business interface associated
+     * with this reference.
+     * 
+     * @return a type-safe reference to the target of this reference.
+     */
     B getService();
-    Class<B> getBusinessInterface();    
+
+    /**
+     * Returns the Java class for the business interface associated
+     * with this reference.
+     * 
+     * @return the Java class for the business interface associated
+     *         with this reference.
+     */
+    Class<B> getBusinessInterface();
 }
