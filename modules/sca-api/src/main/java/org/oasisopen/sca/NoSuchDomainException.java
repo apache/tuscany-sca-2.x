1ee76359acbe5576f57e4bf6c904ac68cd906ae8
Anthony Elder <antelder@apache.org>
2010-04-08 11:00:43 +0000

    Update sca-api with latest OASIS code, these ones have only formatting and javadoc changes
    git-svn-id: https://svn.apache.org/repos/asf/tuscany/sca-java-2.x/trunk@931873 13f79535-47bb-0310-9956-ffa450edef68

--- modules/sca-api/src/main/java/org/oasisopen/sca/NoSuchDomainException.java
+++ modules/sca-api/src/main/java/org/oasisopen/sca/NoSuchDomainException.java
@@ -1,40 +1,56 @@
 /*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- * 
- *   http://www.apache.org/licenses/LICENSE-2.0
- * 
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.    
+ * Copyright(C) OASIS(R) 2005,2010. All Rights Reserved.
+ * OASIS trademark, IPR and other policies apply.
  */
-
 package org.oasisopen.sca;
 
-public class NoSuchDomainException extends Exception {
-    private static final long serialVersionUID = -3990087696861167748L;
+/**
+ * This exception signals that the given SCA Domain does not exist. 
+ */
 
+public class NoSuchDomainException extends Exception {
+    /**
+     * Constructs a NoSuchDomainException with no detail message. 
+     */
     public NoSuchDomainException() {
-        super((Throwable)null);
+        super();
     }
 
+    /**
+     * Constructs a NoSuchDomainException with the specified detail
+     * message. 
+     *
+     * @param     message the detail message
+     */
     public NoSuchDomainException(String message) {
         super(message);
     }
 
+    /**
+     * Constructs a NoSuchDomainException with the specified detail
+     * message and cause.
+     *
+     * The detail message associated with <code>cause</code> is not
+     * automatically incorporated in this exception's detail message.
+     *
+     * @param     message the detail message
+     * @param     cause the cause, or null if the cause is nonexistent
+     *            or unknown
+     */
+    public NoSuchDomainException(String message, Throwable cause) {
+        super(message, cause);
+    }
+ 
+    /**
+     * Constructs a NoSuchDomainException with the specified cause and a
+     * detail message of <tt>(cause==null ? null : cause.toString())</tt>.
+     *
+     * @param     cause the cause, or null if the cause is nonexistent
+     *            or unknown
+     */
     public NoSuchDomainException(Throwable cause) {
         super(cause);
     }
 
-    public NoSuchDomainException(String message, Throwable cause) {
-        super(message, cause);
-    }
+    private static final long serialVersionUID = 6761623124602414622L;
 }
