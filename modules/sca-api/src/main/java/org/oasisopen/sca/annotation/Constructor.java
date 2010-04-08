67d957a8f48679de8cadf0457ef907790a1ce69d
Anthony Elder <antelder@apache.org>
2010-04-08 11:43:57 +0000

    Revert the change to the contsructor annotation to the latest oasis code as it removes named parameters so there;s some asscociated tuscany changes i need to do first
    git-svn-id: https://svn.apache.org/repos/asf/tuscany/sca-java-2.x/trunk@931893 13f79535-47bb-0310-9956-ffa450edef68

--- modules/sca-api/src/main/java/org/oasisopen/sca/annotation/Constructor.java
+++ modules/sca-api/src/main/java/org/oasisopen/sca/annotation/Constructor.java
@@ -1,23 +1,22 @@
 /*
- * Copyright(C) OASIS(R) 2005,2010. All Rights Reserved.
+ * Copyright(C) OASIS(R) 2005,2009. All Rights Reserved.
  * OASIS trademark, IPR and other policies apply.
  */
 package org.oasisopen.sca.annotation;
 
 import static java.lang.annotation.ElementType.CONSTRUCTOR;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
 /**
- * The @Constructor annotation is used to mark a particular
- * constructor to use when instantiating a Java component
- * implementation. If this constructor has parameters, each
- * of these parameters MUST have either a @Property annotation
- * or a @Reference annotation.
+ * Used to indicate the constructor the runtime is to use when instantiating a component implementation instance
+ *
+ * @version $Rev$ $Date$
  */
 @Target(CONSTRUCTOR)
 @Retention(RUNTIME)
 public @interface Constructor {
-
+    String[] value() default "";
 }
