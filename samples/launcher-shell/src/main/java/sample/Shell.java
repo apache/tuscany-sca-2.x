3b9cd12b54206fa623a5a55f291f9d4ae55bda94
Jean Sebastien Delfino <jsdelfino@apache.org>
2010-07-11 00:39:09 +0000

    Minor command cleanup, factored out System.in/out to work as well in a Webapp, renamed test scripts.
    git-svn-id: https://svn.apache.org/repos/asf/tuscany/sca-java-2.x/trunk@962959 13f79535-47bb-0310-9956-ffa450edef68

--- samples/launcher-shell/src/main/java/sample/Shell.java
+++ samples/launcher-shell/src/main/java/sample/Shell.java
@@ -21,6 +21,8 @@ package sample;
 
 import static java.lang.System.in;
 import static java.lang.System.out;
+import static java.util.Collections.emptyList;
+import static java.util.Collections.singletonList;
 
 import java.util.Arrays;
 import java.util.List;
@@ -29,7 +31,9 @@ import java.util.Map;
 import java.util.HashMap;
 import java.util.concurrent.Callable;
 import java.io.BufferedReader;
+import java.io.PrintWriter;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.io.IOException;
 import org.apache.tuscany.sca.node.Contribution;
 import org.apache.tuscany.sca.node.Node;
@@ -40,7 +44,7 @@ import org.apache.tuscany.sca.node.NodeFactory;
  * A little SCA command shell.
  */
 public class Shell {
-    final NodeFactory nodeFactory = NodeFactory.newInstance();
+    final NodeFactory nodeFactory;
 
     public static class NodeInfo {
         final String name;
@@ -59,83 +63,112 @@ public class Shell {
             return name + " " + curi + " " + cloc;
         }
     }
+
     final Map<String, NodeInfo> nodes = new HashMap<String, NodeInfo>();
-
     final List<String> history = new ArrayList<String>();
-    
-    public static void main(final String[] args) throws Exception {
-        new Shell().run();
-    }
 
-    boolean start(final String name, final String curi, final String cloc) {
+    public Shell(NodeFactory nf) {
+        this.nodeFactory = nf;
+    }
+    
+    List<?> start(final String name, final String curi, final String cloc) {
+        if (nodes.containsKey(name))
+            return emptyList();
         final Node node = nodeFactory.createNode(new Contribution(curi, cloc));
         nodes.put(name, new NodeInfo(name, curi, cloc, node));
         node.start();
-        return true;
+        return emptyList();
     }
 
-    boolean stop(final String name) {
-        nodes.get(name).node.stop();
+    List<?> stop(final String name) {
+        final NodeInfo ninfo = nodes.get(name);
+        if (ninfo == null)
+            return emptyList();
+        ninfo.node.stop();
         nodes.remove(name);
-        return true;
+        return emptyList();
     }
 
-    boolean status() {
-        out.println(nodes.values());
-        return true;
+    List<?> stop() {
+        for (NodeInfo ninfo: nodes.values())
+            ninfo.node.stop();
+        nodes.clear();
+        return emptyList();
     }
 
-    boolean history() {
-        for (String l: history)
-            out.println(l);
-        return true;
+    List<?> restart(final String name, final String curi, final String cloc) {
+        stop(name);
+        return start(name, curi, cloc);
     }
 
-    static boolean bye() {
-        return false;
+    List<?> status() {
+        return new ArrayList(nodes.values());
+    }
+
+    List<?> history() {
+        return history;
+    }
+
+    List<?> bye() {
+        return null;
     }
 
     List<String> read(final BufferedReader r) throws IOException {
-        out.print("=> ");
         final String l = r.readLine();
         history.add(l);
-        return Arrays.asList(l != null? l.split(" ") : "bye".split(" "));
+        return l != null? Arrays.asList(l.split(" ")) : singletonList("bye");
     }
        
-    Callable<Boolean> eval(final List<String> toks) {
+    Callable<List<?>> eval(final List<String> toks) {
         final String op = toks.get(0);
-        if (op.equals("start")) return new Callable<Boolean>() { public Boolean call() {
+        if(op.equals("start")) return new Callable<List<?>>() { public List<?> call() {
             return start(toks.get(1), toks.get(2), toks.get(3));
         }};
-        if (op.equals("stop")) return new Callable<Boolean>() { public Boolean call() {
+        if(op.equals("stop")) return new Callable<List<?>>() { public List<?> call() {
+            if (toks.size() == 1)
+                return stop();
             return stop(toks.get(1));
         }};
-        if (op.equals("status")) return new Callable<Boolean>() { public Boolean call() {
+        if(op.equals("restart")) return new Callable<List<?>>() { public List<?> call() {
+            return restart(toks.get(1), toks.get(2), toks.get(3));
+        }};
+        if(op.equals("status")) return new Callable<List<?>>() { public List<?> call() {
             return status();
         }};
-        if (op.equals("history")) return new Callable<Boolean>() { public Boolean call() {
+        if(op.equals("history")) return new Callable<List<?>>() { public List<?> call() {
             return history();
         }};
-        if (op.equals("bye")) return new Callable<Boolean>() { public Boolean call() {
+        if(op.equals("bye")) return new Callable<List<?>>() { public List<?> call() {
             return bye();
         }};
-        return new Callable<Boolean>() { public Boolean call() {
-            return true;
+        return new Callable<List<?>>() { public List<?> call() {
+            return emptyList();
         }};
     }
 
-    boolean apply(final Callable<Boolean> func) {
+    List<?> apply(final Callable<List<?>> func) {
         try {
             return func.call();
         } catch (Exception e) {
-            e.printStackTrace();
-            return true;
+            return singletonList(e);
         }
     }
 
-    public Map<String, NodeInfo> run() throws IOException {
-        final BufferedReader r = new BufferedReader(new InputStreamReader(in));
-        while(apply(eval(read(r))));
+    boolean print(final List<?> l, PrintWriter w) {
+        if(l == null)
+            return false;
+        for(Object o: l)
+            w.println(o);
+        return true;
+    }
+
+    public Map<String, NodeInfo> run(final BufferedReader r, final PrintWriter w) throws IOException {
+        while(print(apply(eval(read(r))), w));
+        r.close();
         return nodes;
+    }
+
+    public static void main(final String[] args) throws Exception {
+        new Shell(NodeFactory.newInstance()).run(new BufferedReader(new InputStreamReader(in)), new PrintWriter(out, true));
     }
 }
