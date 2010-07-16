apache/tuscany-sca-2.x

commit 2366e9ad5e8b74cf3fb57004390265edf2139d63

TUSCANY-3616 - Add code to check that that interface contracts a reference and reference binding and at service and service binding match. Motivated by BWS_2007. TO do this properly we have to test that the interfaces are described using the same IDL and if not convert to WSDL1.1 are required by the SCA specifications. There are a lot of changes here as doing this upset quite a few tests. Further work is required to look at the details of our WSDL generation process which looks a little suspect around wrapper namespaces.
git-svn-id: https://svn.apache.org/repos/asf/tuscany/sca-java-2.x/trunk@963624 13f79535-47bb-0310-9956-ffa450edef68

modules/core/src/main/java/org/apache/tuscany/sca/core/assembly/impl/RuntimeEndpointImpl.java
@@ -187,6 +187,8 @@ public class RuntimeEndpointImpl extends EndpointImpl implements RuntimeEndpoint
         this.phaseManager = utilities.getUtility(PhaseManager.class);
         this.serializer = utilities.getUtility(EndpointSerializer.class);
         this.providerFactories = registry.getExtensionPoint(ProviderFactoryExtensionPoint.class);
+        this.builders = registry.getExtensionPoint(BuilderExtensionPoint.class);
+        this.contractBuilder = builders.getContractBuilder();
     }
 
     public void unbind() {
@@ -224,7 +226,11 @@ public class RuntimeEndpointImpl extends EndpointImpl implements RuntimeEndpoint
             for (InvocationChain chain : getInvocationChains()) {
                 Operation op = chain.getTargetOperation();
 
-                if (interfaceContractMapper.isCompatible(operation, op, Compatibility.SUBSET)) {
+                // We used to check compatibility here but this is now validated when the 
+                // chain is created. As the chain operations are the real interface types 
+                // they may be incompatible just because they are described in different 
+                // IDLs
+                if (operation.getName().equals(op.getName())) {
                     invocationChainMap.put(operation, chain);
                     return chain;
                 }
@@ -304,6 +310,7 @@ public class RuntimeEndpointImpl extends EndpointImpl implements RuntimeEndpoint
         // TODO - EPR - why is this looking at the component types. The endpoint should have the right interface contract by this time
         InterfaceContract targetContract = getComponentTypeServiceInterfaceContract();
         // setInterfaceContract(targetContract);
+        validateServiceInterfaceCompatibility();
         for (Operation operation : sourceContract.getInterface().getOperations()) {
             Operation targetOperation = interfaceContractMapper.map(targetContract.getInterface(), operation);
             if (targetOperation == null) {
@@ -463,6 +470,47 @@ public class RuntimeEndpointImpl extends EndpointImpl implements RuntimeEndpoint
         FactoryExtensionPoint modelFactories = registry.getExtensionPoint(FactoryExtensionPoint.class);
         return (RuntimeAssemblyFactory)modelFactories.getFactory(AssemblyFactory.class);
     } // end method RuntimeAssemblyFactory
+    
+    /**
+     * Check that endpoint  has compatible interface at the component and binding ends. 
+     * The user can specify the interfaces at both ends so there is a danger that they won't be compatible.
+     */
+    public void validateServiceInterfaceCompatibility() {
+        
+        InterfaceContract serviceContract = getComponentServiceInterfaceContract();
+        InterfaceContract bindingContract = getBindingInterfaceContract();
+
+        if ((serviceContract != null) &&
+            (bindingContract != null)){
+            try {
+                if ((serviceContract.getClass() != bindingContract.getClass()) &&
+                    (serviceContract instanceof JavaInterfaceContract)) {
+                        interfaceContractMapper.checkCompatibility(getGeneratedWSDLContract(serviceContract), 
+                                                                   bindingContract, 
+                                                                   Compatibility.SUBSET, 
+                                                                   true, // we ignore callbacks as binding iface won't have one 
+                                                                   false);
+                    } else {
+                        interfaceContractMapper.checkCompatibility(serviceContract, 
+                                                                   bindingContract, 
+                                                                   Compatibility.SUBSET, 
+                                                                   true, // we ignore callbacks as binding iface won't have one
+                                                                   false);                   
+                    }                 
+            } catch (Exception ex){
+                throw new ServiceRuntimeException("Component " +
+                                                  this.getComponent().getName() +
+                                                  " Service " +
+                                                  getService().getName() +
+                                                  " interface is incompatible with the interface of the reference binding  - " + 
+                                                  getBinding().getName() +
+                                                  " - " + 
+                                                  ex.getMessage() +
+                                                  " - [" + this.toString() + "]");
+            }
+        }
+                
+    }    
 
     private void initServiceBindingInvocationChains() {
 
@@ -744,5 +792,17 @@ public class RuntimeEndpointImpl extends EndpointImpl implements RuntimeEndpoint
             }
         }
     }
+    public InterfaceContract getGeneratedWSDLContract(InterfaceContract interfaceContract) {
 
+        if ( interfaceContract.getNormalizedWSDLContract() == null){
+            if (getComponentServiceInterfaceContract() instanceof JavaInterfaceContract){
+                if (contractBuilder == null){
+                    throw new ServiceRuntimeException("Contract builder not found while calculating WSDL contract for " + this.toString());
+                }
+                contractBuilder.build(getComponentServiceInterfaceContract(), null);
+            }
+        }
+        
+        return interfaceContract.getNormalizedWSDLContract();      
+    }    
 }

compliance-tests/assembly/src/test/resources/tuscany-oasis-sca-tests-errors.properties
@@ -80,8 +80,8 @@ ASM_9005=org.apache.tuscany.sca.monitor.ValidationException: [Composite: {http:/
 ASM_10001=org.apache.tuscany.sca.monitor.ValidationException: [Contribution: ASM_10001, Artifact: META-INF/definitions.xml, Definitions: jar:file:***ASM_10001***.zip!/META-INF/definitions.xml] - [ASM10001,POL30002] Duplicate intent {http://docs.oasis-open.org/ns/opencsa/scatests/200903}Fred found in domain
 ASM_10003=org.apache.tuscany.sca.monitor.ValidationException: [Contribution: ASM_10003, Artifact: META-INF/definitions.xml, Definitions: jar:file:***ASM_10003***.zip!/META-INF/definitions.xml] - XMLSchema validation error occured in: Test_ASM_10003.composite ,line = ***, column = ***, Message = cvc-complex-type.2.4.a: Invalid content was found starting with element 'component'. One of '{"http://docs.oasis-open.org/ns/opencsa/sca/200912":documentation, "http://docs.oasis-open.org/ns/opencsa/sca/200912":intent, "http://docs.oasis-open.org/ns/opencsa/sca/200912":policySet, "http://docs.oasis-open.org/ns/opencsa/sca/200912":bindingType, "http://docs.oasis-open.org/ns/opencsa/sca/200912":implementationType, WC[##other:"http://docs.oasis-open.org/ns/opencsa/sca/200912"]}' is expected.
 ASM_12003=org.apache.tuscany.sca.contribution.processor.ContributionResolveException: Invalid interface when resolving
-ASM_12007=org.apache.tuscany.sca.monitor.ValidationException: [Composite: {http://docs.oasis-open.org/ns/opencsa/sca/200912}, Component: TestComponent1] - [ASM50004,JCA30002,JCI80001] Component service interface incompatible with implementation service interface: Component = TestComponent1 Service = Service1 Operation operation1 not found on target
-ASM_12008=org.apache.tuscany.sca.monitor.ValidationException: [Composite: {http://docs.oasis-open.org/ns/opencsa/sca/200912}, Component: TEST_ASM_12008TestComponent1] - [ASM50004,JCA30002,JCI80001] Component service interface incompatible with implementation service interface: Component = TEST_ASM_12008TestComponent1 Service = Service1 Operation operation1 not found on target
+ASM_12007=org.apache.tuscany.sca.monitor.ValidationException: [Composite: {http://docs.oasis-open.org/ns/opencsa/sca/200912}, Component: TestComponent1, Composite: {http://docs.oasis-open.org/ns/opencsa/scatests/2009032}TestComposite5, Component: TestComposite5TestComponent1] - [ASM50004,JCA30002,JCI80001] Component service interface incompatible with implementation service interface: Component = TestComposite5TestComponent1 Service = Service1 Operations called operation2 are not compatible Operation argument types source = {http://www.w3.org/2001/XMLSchema}int target = {http://www.w3.org/2001/XMLSchema}string don't match for output types
+ASM_12008=org.apache.tuscany.sca.monitor.ValidationException: [Composite: {http://docs.oasis-open.org/ns/opencsa/sca/200912}, Component: TEST_ASM_12008TestComponent1, Composite: {http://docs.oasis-open.org/ns/opencsa/scatests/2009032}TestComposite5, Component: TestComposite5TestComponent1] - [ASM50004,JCA30002,JCI80001] Component service interface incompatible with implementation service interface: Component = TestComposite5TestComponent1 Service = Service1 Operations called operation2 are not compatible Operation argument types source = {http://www.w3.org/2001/XMLSchema}int target = {http://www.w3.org/2001/XMLSchema}string don't match for output types
 ASM_12011=org.apache.tuscany.sca.monitor.ValidationException: [Composite: {http://docs.oasis-open.org/ns/opencsa/sca/200912}, Component: TestComponent1] - [ASM60030,JCI50004,JCI80001] Component implementation not resolved: Component = TestComponent1 Uri = TestComponent1
 ASM_12012=org.apache.tuscany.sca.monitor.ValidationException: [Contribution: ASM_12012, Artifact: META-INF/sca-contribution.xml] - XMLSchema validation error occured in: jar:file:***ASM_12012***.zip!/META-INF/sca-contribution.xml ,line = ***, column = ***, Message = cvc-complex-type.2.4.a: Invalid content was found starting with element 'component'. One of '{"http://docs.oasis-open.org/ns/opencsa/sca/200912":documentation, "http://docs.oasis-open.org/ns/opencsa/sca/200912":deployable, "http://docs.oasis-open.org/ns/opencsa/sca/200912":importBase, "http://docs.oasis-open.org/ns/opencsa/sca/200912":exportBase, WC[##other:"http://docs.oasis-open.org/ns/opencsa/sca/200912"]}' is expected.
 ASM_13001=org.apache.tuscany.sca.monitor.ValidationException: [Contribution: ASM_13001, Artifact: Test_ASM_13001.composite] - XMLSchema validation error occured in: Test_ASM_13001.composite ,line = ***, column = ***, Message = cvc-complex-type.2.4.a: Invalid content was found starting with element 'sevrice'. One of '{"http://docs.oasis-open.org/ns/opencsa/sca/200912":service, "http://docs.oasis-open.org/ns/opencsa/sca/200912":reference, "http://docs.oasis-open.org/ns/opencsa/sca/200912":property, "http://docs.oasis-open.org/ns/opencsa/sca/200912":requires, "http://docs.oasis-open.org/ns/opencsa/sca/200912":policySetAttachment, WC[##other:"http://docs.oasis-open.org/ns/opencsa/sca/200912"]}' is expected.

itest/bpel/helloworld/pom.xml
@@ -56,6 +56,12 @@
 			<version>2.0-SNAPSHOT</version>
 			<scope>runtime</scope>
 		</dependency>
+		
+        <dependency>
+            <groupId>org.apache.tuscany.sca</groupId>
+            <artifactId>tuscany-binding-ws-wsdlgen</artifactId>
+            <version>2.0-SNAPSHOT</version>
+        </dependency> 		
 
 		<dependency>
 			<groupId>xerces</groupId>

itest/databindings/jaxb-top-down/src/main/resources/wsdl/wrapped/hello-service.wsdl
@@ -28,7 +28,8 @@
     xmlns:soap="http://schemas.xmlsoap.org/wsdl/soap/"
     xmlns:wsdl="http://schemas.xmlsoap.org/wsdl/">
     <wsdl:types>
-        <xs:schema targetNamespace="http://util.java/"
+        <!-- <xs:schema targetNamespace="http://util.java/" -->
+        <xs:schema targetNamespace=""
             version="1.0" xmlns:xs="http://www.w3.org/2001/XMLSchema">
             <xs:complexType name="arrayList">
                 <xs:complexContent>

itest/interfaces/src/main/java/org/apache/tuscany/sca/itest/interfaces/LocalClientComponent.java
@@ -28,11 +28,11 @@ public interface LocalClientComponent {
 
     String foo1(ParameterObject po);
 
-    String foo1(String str) throws Exception;
+    String foo2(String str) throws Exception;
 
-    String foo2(String str, int i);
+    String foo3(String str, int i);
 
-    String foo2(int i, String str) throws Exception;
+    String foo4(int i, String str) throws Exception;
 
     void callback(String str);
 

itest/interfaces/src/main/java/org/apache/tuscany/sca/itest/interfaces/LocalClientComponentImpl.java
@@ -35,15 +35,15 @@ public class LocalClientComponentImpl implements LocalClientComponent, LocalCall
         return "AComponent";
     }
 
-    public String foo1(String str) throws Exception {
+    public String foo2(String str) throws Exception {
         return str + "AComponent";
     }
 
-    public String foo2(String str, int i) {
+    public String foo3(String str, int i) {
         return str + "AComponent" + i;
     }
 
-    public String foo2(int i, String str) throws Exception {
+    public String foo4(int i, String str) throws Exception {
         return str + "AComponent" + i;
     }
 

itest/interfaces/src/test/java/org/apache/tuscany/sca/itest/interfaces/InterfacesTestCase.java
@@ -60,10 +60,10 @@ public class InterfacesTestCase {
             assertEquals("AComponent", local.foo1(po));
             assertEquals("AComponent", po.field1);
 
-            assertEquals("AAComponent", local.foo1("A"));
+            assertEquals("AAComponent", local.foo2("A"));
 
-            assertEquals("AAComponent1", local.foo2("A", 1));
-            assertEquals("AAComponent1", local.foo2(1, "A"));
+            assertEquals("AAComponent1", local.foo3("A", 1));
+            assertEquals("AAComponent1", local.foo4(1, "A"));
         } catch (Exception e) {
             fail();
         }

itest/jms/format-jmsobject/src/main/resources/jmsobject/helloworld.composite
@@ -42,7 +42,6 @@
     <component name="HelloWorldServiceComponent1">
         <implementation.java class="org.apache.tuscany.sca.binding.jms.format.jmsobject.helloworld.HelloWorldServiceImpl" />
         <service name="HelloWorldService">
-            <interface.wsdl interface="http://helloworld.jmsobject.format.jms.binding.sca.tuscany.apache.org/#wsdl.interface(HelloWorldService)"/>
             <binding.jms>
                 <tuscany:wireFormat.jmsObject wrapSingle="true"/>
                 <destination jndiName="HelloWorldService1"/>

commit ce3a736c95514ca9564da8f868840bd36aab3485

Fix the regression caused by r963624 for overloaded local methods
git-svn-id: https://svn.apache.org/repos/asf/tuscany/sca-java-2.x/trunk@964980 13f79535-47bb-0310-9956-ffa450edef68

modules/core/src/main/java/org/apache/tuscany/sca/core/assembly/impl/RuntimeEndpointImpl.java
@@ -230,9 +230,18 @@ public class RuntimeEndpointImpl extends EndpointImpl implements RuntimeEndpoint
                 // chain is created. As the chain operations are the real interface types 
                 // they may be incompatible just because they are described in different 
                 // IDLs
-                if (operation.getName().equals(op.getName())) {
-                    invocationChainMap.put(operation, chain);
-                    return chain;
+                if (operation.getInterface().isRemotable()) {
+                    if (operation.getName().equals(op.getName())) {
+                        invocationChainMap.put(operation, chain);
+                        return chain;
+                    }
+                } else {
+                    // [rfeng] We need to run the compatibility check for local operations as they 
+                    // can be overloaded
+                    if (interfaceContractMapper.isCompatible(operation, op, Compatibility.SUBSET)) {
+                        invocationChainMap.put(operation, chain);
+                        return chain;
+                    }
                 }
             }
             // Cache it with the NULL_CHAIN to avoid NPE