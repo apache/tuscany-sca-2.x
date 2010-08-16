fe6ce4e4bcabdbf706cd83be7a99d59aae6475b8
Simon Laws <slaws@apache.org>
2010-08-16 17:39:38 +0000

    Extend the JSONRPC binding sample to be hosted in a webapp and include a DOJO based client. This is for looking at Fabiano's latest problem and doesn't work yet. Checking in so I have a backup while I get it to work.
    git-svn-id: https://svn.apache.org/repos/asf/tuscany/sca-java-2.x/trunk@986072 13f79535-47bb-0310-9956-ffa450edef68

---
+++ samples/binding-jsonrpc/contribution-calculator-webapp/src/main/java/calculator/AddBean.java
@@ -0,0 +1,88 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.    
+ */
+
+package calculator;
+
+public class AddBean {
+
+    protected double n1;
+    protected double n2;
+    protected double n3;
+    
+    protected Boolean b;
+    protected String s;
+    protected int x;
+    protected Integer y;
+    
+    public double getN1() {
+        return n1;
+    }
+    
+    public void setN1(double n1) {
+        this.n1 = n1;
+    }
+    
+    public double getN2() {
+        return n2;
+    }
+    
+    public void setN2(double n2) {
+        this.n2 = n2;
+    }
+    
+    public double getN3() {
+        return n3;
+    }
+    
+    public void setN3(double n3) {
+        this.n3 = n3;
+    }
+    
+    public Boolean getB() {
+        return b;
+    }
+    
+    public void setB(Boolean b) {
+        this.b = b;
+    }
+    
+    public String getS() {
+        return s;
+    }
+    
+    public void setS(String s) {
+        this.s = s;
+    }
+    
+    public int getX() {
+        return x;
+    }
+    
+    public void setX(int x) {
+        this.x = x;
+    }
+    
+    public Integer getY() {
+        return y;
+    }
+
+    public void setY(Integer y) {
+        this.y = y;
+    }
+}
