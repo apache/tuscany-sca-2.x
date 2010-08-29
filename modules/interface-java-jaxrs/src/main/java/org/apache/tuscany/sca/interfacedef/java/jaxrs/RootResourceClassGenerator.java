7ecf1a5f42f82f3fa0808f2bf2c1d925b9fdd146
Luciano Resende <lresende@apache.org>
2010-08-29 08:45:47 +0000

    TUSCANY-3595 - Minor tweaks to code that generates JAX-RS resource interface
    git-svn-id: https://svn.apache.org/repos/asf/tuscany/sca-java-2.x/trunk@990503 13f79535-47bb-0310-9956-ffa450edef68

--- modules/interface-java-jaxrs/src/main/java/org/apache/tuscany/sca/interfacedef/java/jaxrs/RootResourceClassGenerator.java
+++ modules/interface-java-jaxrs/src/main/java/org/apache/tuscany/sca/interfacedef/java/jaxrs/RootResourceClassGenerator.java
@@ -6,15 +6,15 @@
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
  * with the License.  You may obtain a copy of the License at
- * 
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
  * KIND, either express or implied.  See the License for the
  * specific language governing permissions and limitations
- * under the License.    
+ * under the License.
  */
 
 package org.apache.tuscany.sca.interfacedef.java.jaxrs;
@@ -45,7 +45,7 @@ public class RootResourceClassGenerator implements Opcodes {
         String className =
             interfaceName.substring(0, index) + ".Generated" + interfaceName.substring(index + 1) + "Impl";
 
-        byte[] content = generate(interfaze, path, consumes, produces);
+        final byte[] content = generate(interfaze, path, consumes, produces);
         Class<?> cls = classLoader.getGeneratedClass(className, content);
         return cls;
     }
