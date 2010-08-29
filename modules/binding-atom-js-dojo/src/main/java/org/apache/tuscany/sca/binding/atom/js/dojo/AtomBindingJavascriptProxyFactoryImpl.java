90ab6bac6374e0f3b8530ae2370659fefb1765af
Luciano Resende <lresende@apache.org>
2010-08-29 08:53:41 +0000

    Minor update to reuse Binding QName
    git-svn-id: https://svn.apache.org/repos/asf/tuscany/sca-java-2.x/trunk@990514 13f79535-47bb-0310-9956-ffa450edef68

--- modules/binding-atom-js-dojo/src/main/java/org/apache/tuscany/sca/binding/atom/js/dojo/AtomBindingJavascriptProxyFactoryImpl.java
+++ modules/binding-atom-js-dojo/src/main/java/org/apache/tuscany/sca/binding/atom/js/dojo/AtomBindingJavascriptProxyFactoryImpl.java
@@ -6,15 +6,15 @@
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
  * with the License.  You may obtain a copy of the License at
- * 
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
  * KIND, either express or implied.  See the License for the
  * specific language governing permissions and limitations
- * under the License.    
+ * under the License.
  */
 
 package org.apache.tuscany.sca.binding.atom.js.dojo;
@@ -34,8 +34,8 @@ import org.apache.tuscany.sca.runtime.RuntimeEndpointReference;
 import org.apache.tuscany.sca.web.javascript.JavascriptProxyFactory;
 
 public class AtomBindingJavascriptProxyFactoryImpl implements JavascriptProxyFactory {
-    private static final QName NAME = new QName("http://tuscany.apache.org/xmlns/sca/1.1", "binding.atom");
-        
+    private static final QName NAME = AtomBinding.TYPE;
+
     public Class<?> getModelType() {
         return AtomBinding.class;
     }
@@ -43,7 +43,7 @@ public class AtomBindingJavascriptProxyFactoryImpl implements JavascriptProxyFac
     public QName getQName() {
         return NAME;
     }
-    
+
     public String getJavascriptProxyFile() {
         return null;
     }
@@ -65,13 +65,13 @@ public class AtomBindingJavascriptProxyFactoryImpl implements JavascriptProxyFac
             ((RuntimeEndpointReference) epr).getInvocationChains();
             targetEndpoint = epr.getTargetEndpoint();
         }
-        
+
         Binding binding = targetEndpoint.getBinding();
-        
+
         URI targetURI = URI.create(binding.getURI());
         String targetPath = targetURI.getPath();
-        
-        return "tuscany.AtomService(\"" + targetPath + "\")";        
+
+        return "tuscany.AtomService(\"" + targetPath + "\")";
     }
 
 }
