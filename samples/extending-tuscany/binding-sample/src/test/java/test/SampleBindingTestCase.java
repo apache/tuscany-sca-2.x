5aca734dbdc2835fdd2775715924629011419b75
Anthony Elder <antelder@apache.org>
2010-10-01 07:56:18 +0000

    Add a binding extension sample
    git-svn-id: https://svn.apache.org/repos/asf/tuscany/sca-java-2.x/trunk@1003421 13f79535-47bb-0310-9956-ffa450edef68

---
+++ samples/extending-tuscany/binding-sample/src/test/java/test/SampleBindingTestCase.java
@@ -0,0 +1,52 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.    
+ */
+package test;
+
+import helloworld.HelloWorldService;
+import junit.framework.Assert;
+
+import org.apache.tuscany.sca.node.Node;
+import org.apache.tuscany.sca.node.NodeFactory;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class SampleBindingTestCase {
+
+    private static Node node;
+    
+    @Test
+    public void testSayHello() {
+        HelloWorldService service = node.getService(HelloWorldService.class, "HelloWorldClient/HelloWorldService");
+        Assert.assertEquals("Hello boo", service.sayHello("boo"));
+    }
+
+    @BeforeClass
+    public static void init() throws Exception {
+        node = NodeFactory.newInstance().createNode("helloworld.composite").start();
+    }
+    
+    @AfterClass
+    public static void destroy() throws Exception {
+        if (node != null) {
+            node.stop();
+        }
+    }
+
+}
