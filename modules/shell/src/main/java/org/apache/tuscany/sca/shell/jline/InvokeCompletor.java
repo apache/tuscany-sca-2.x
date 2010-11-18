e52aae46a5b7ccb74d7362ab4103515af6d3be7d
Anthony Elder <antelder@apache.org>
2010-11-18 11:09:46 +0000

    Add a command completor for the invoke command
    git-svn-id: https://svn.apache.org/repos/asf/tuscany/sca-java-2.x/trunk@1036406 13f79535-47bb-0310-9956-ffa450edef68

---
+++ modules/shell/src/main/java/org/apache/tuscany/sca/shell/jline/InvokeCompletor.java
@@ -0,0 +1,57 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.    
+ */
+
+package org.apache.tuscany.sca.shell.jline;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import jline.SimpleCompletor;
+
+import org.apache.tuscany.sca.assembly.Endpoint;
+import org.apache.tuscany.sca.node2.impl.NodeImpl;
+import org.apache.tuscany.sca.runtime.EndpointRegistry;
+import org.apache.tuscany.sca.shell.Shell;
+
+/**
+ * An Invoke command Completor
+ */
+public class InvokeCompletor extends SimpleCompletor {
+
+    private Shell shell;
+
+    public InvokeCompletor(Shell shell) {
+        super("");
+        this.shell = shell;
+    }
+    
+    @Override
+    public int complete(final String buffer, final int cursor, final List clist) {
+        if (shell.getNode() != null) {
+            List<String> services = new ArrayList<String>();
+            EndpointRegistry reg = ((NodeImpl)shell.getNode()).getEndpointRegistry();
+            for (Endpoint endpoint : reg.getEndpoints()) {
+                services.add(endpoint.getComponent().getURI() + "/" + endpoint.getService().getName());
+            }
+            setCandidateStrings(services.toArray(new String[services.size()]));
+        }
+       return super.complete(buffer, cursor, clist);   
+    }
+    
+}
