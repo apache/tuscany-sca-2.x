95cc02d3033ec454a94c8317da77ee084df89762
Anthony Elder <antelder@apache.org>
2010-12-03 08:01:37 +0000

    Clean up the getting started helloworld webapp. Update it to use a jsp to invoke the component
    git-svn-id: https://svn.apache.org/repos/asf/tuscany/sca-java-2.x/trunk@1041721 13f79535-47bb-0310-9956-ffa450edef68

--- samples/getting-started/helloworld-webapp/src/main/java/sample/Helloworld.java
+++ samples/getting-started/helloworld-webapp/src/main/java/sample/Helloworld.java
@@ -1,4 +1,4 @@
-<!--
+/*
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements.  See the NOTICE file
  * distributed with this work for additional information
@@ -6,27 +6,23 @@
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
  * with the License.  You may obtain a copy of the License at
- *
+ * 
  *   http://www.apache.org/licenses/LICENSE-2.0
- *
+ * 
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
  * KIND, either express or implied.  See the License for the
  * specific language governing permissions and limitations
- * under the License.
--->
-<html>
+ * under the License.    
+ */
+package sample;
 
-<head>
-<title>Apache Tuscany Helloworld Servlet Sample</title>
-</head>
+import org.oasisopen.sca.annotation.Remotable;
 
-<body>
+@Remotable
+public interface Helloworld {
 
-<h2>Apache Tuscany Helloworld Servlet Sample</h2>
+    String sayHello(String name);
 
-<a href="http://localhost:8085/HelloworldWSComponent/Helloworld?wsdl">WSDL for binding.ws of the Helloworld service of the HelloWorldWS component</a>
-
-</body>
-</html>
+}
