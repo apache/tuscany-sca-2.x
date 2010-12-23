6c91fd3165eb4d56bd9e7145980ff58deb805b9f
Simon Laws <slaws@apache.org>
2010-12-23 13:06:07 +0000

    Throw systematic errors as ServiceRuntimeExceptions
    git-svn-id: https://svn.apache.org/repos/asf/tuscany/sca-java-2.x/trunk@1052256 13f79535-47bb-0310-9956-ffa450edef68

--- modules/core/src/main/java/org/apache/tuscany/sca/core/invocation/RuntimeInvoker.java
+++ modules/core/src/main/java/org/apache/tuscany/sca/core/invocation/RuntimeInvoker.java
@@ -178,10 +178,7 @@ public class RuntimeInvoker implements Invoker, InvokerAsyncRequest {
                 // temporary fix to swallow the dummy exception that's
                 // thrown back to get past the response chain processing. 
                 if (!(ex instanceof AsyncResponseException)){
-                  // send the exception in through the 
-                  // async response processing path
-                  msg.setFaultBody(ex);
-                  invokeAsyncResponse(msg);
+                    throw new ServiceRuntimeException(ex);
                 }
             }
         } finally {
@@ -200,11 +197,15 @@ public class RuntimeInvoker implements Invoker, InvokerAsyncRequest {
     public void invokeAsyncResponse(Message msg) {  
     	InvocationChain chain = invocable.getInvocationChain(msg.getOperation());
         Invoker tailInvoker = chain.getTailInvoker();
-        ((InvokerAsyncResponse)tailInvoker).invokeAsyncResponse(msg);       
+        try {
+            ((InvokerAsyncResponse)tailInvoker).invokeAsyncResponse(msg);   
+        } catch (Throwable ex) {
+            throw new ServiceRuntimeException(ex);
+        }    
     } // end method invokeAsyncResponse
 
-	@Override
-	public void invokeAsyncRequest(Message msg) throws Throwable {
-		invokeAsync(msg);
-	} // end method invokeAsyncRequest
+    @Override
+    public void invokeAsyncRequest(Message msg) throws Throwable {
+        invokeAsync(msg);
+    } // end method invokeAsyncRequest
 }
