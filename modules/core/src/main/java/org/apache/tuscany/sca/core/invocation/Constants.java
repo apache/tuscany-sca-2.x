e2840fb52afea1de25921d483fed236028a4f65e
Simon Laws <slaws@apache.org>
2011-01-12 16:55:09 +0000

    Add some more constants for use in the async processing
    git-svn-id: https://svn.apache.org/repos/asf/tuscany/sca-java-2.x/trunk@1058230 13f79535-47bb-0310-9956-ffa450edef68

--- modules/core/src/main/java/org/apache/tuscany/sca/core/invocation/Constants.java
+++ modules/core/src/main/java/org/apache/tuscany/sca/core/invocation/Constants.java
@@ -26,5 +26,6 @@ package org.apache.tuscany.sca.core.invocation;
  */
 public interface Constants {
     String MESSAGE_ID = "MESSAGE_ID";
-
+    String RELATES_TO = "RELATES_TO";
+    String ASYNC_RESPONSE_INVOKER = "ASYNC_RESPONSE_INVOKER";
 }
