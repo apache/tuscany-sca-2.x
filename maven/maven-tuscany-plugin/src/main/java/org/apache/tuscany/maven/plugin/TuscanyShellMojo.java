635d8539f646b27b5698241114fcf34eddb26760
Anthony Elder <antelder@apache.org>
2011-02-06 13:02:10 +0000

    Rename the tuscany plugin 'launch' mojo to be named 'shell' and update it to run the tuscany shell. So now you can run the shell without needing to get any Tuscany distribution by using: mvn org.apache.tuscany.maven.plugins:maven-tuscany-plugin:2.0-SNAPSHOT:shell
    git-svn-id: https://svn.apache.org/repos/asf/tuscany/sca-java-2.x/trunk@1067654 13f79535-47bb-0310-9956-ffa450edef68

---
+++ maven/maven-tuscany-plugin/src/main/java/org/apache/tuscany/maven/plugin/TuscanyShellMojo.java
@@ -0,0 +1,79 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.tuscany.maven.plugin;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.apache.tuscany.sca.shell.Shell;
+
+/**
+ * Maven Mojo to run the Tuscany Shell
+ * 
+ * Invoked with mvn org.apache.tuscany.maven.plugin:maven-tuscany-plugins:shell [-Ddomain=uri:myDomain] -Dcontributions=path\to\scacontribution,...
+ * 
+ * @goal shell
+ * @requiresProject false
+ * @requiresDependencyResolution runtime
+ */
+public class TuscanyShellMojo extends AbstractMojo {
+
+    /**
+     * @parameter expression="${domain}" default-value="default"
+     */
+    private String domain;
+    
+    /**
+     * @parameter expression="${contributions}" 
+     */
+    private String contributions;
+
+    public void execute() throws MojoExecutionException, MojoFailureException {
+        getLog().info("Starting Tuscany Shell...");
+        
+        String[] args;
+        if (contributions != null) {
+            List<String> cs = new ArrayList<String>();
+            StringTokenizer st = new StringTokenizer(contributions, ",");
+            while (st.hasMoreTokens()) {
+                cs.add(st.nextToken());
+            }
+            cs.add(0, domain);
+            args = cs.toArray(new String[cs.size()]);
+        } else {
+            if ("default".equals(domain)) {
+                args = new String[]{};
+            } else {
+                args = new String[]{domain};
+            }
+        }
+
+        try {
+            Shell.main(args);
+        } catch (Exception e) {
+            throw new MojoExecutionException("Exception in Shell", e);
+        }
+
+        getLog().info("Tuscany Shell stopped.");
+    }
+}
