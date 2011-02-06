15319b11eb4e61be59bcfb44efbe21afa923a921
Anthony Elder <antelder@apache.org>
2011-02-06 09:07:24 +0000

    Clean up contribution jar archetype and update to create a standalone project that doesn't use a tuscany parent pom
    git-svn-id: https://svn.apache.org/repos/asf/tuscany/sca-java-2.x/trunk@1067618 13f79535-47bb-0310-9956-ffa450edef68

--- maven/archetypes/contribution-jar/src/main/resources/archetype-resources/src/main/java/Helloworld.java
+++ maven/archetypes/contribution-jar/src/main/resources/archetype-resources/src/main/java/Helloworld.java
@@ -21,7 +21,7 @@ package ${package};
 import org.oasisopen.sca.annotation.Remotable;
 
 @Remotable
-public interface HelloworldService {
+public interface Helloworld {
 
     String sayHello(String name);
 
