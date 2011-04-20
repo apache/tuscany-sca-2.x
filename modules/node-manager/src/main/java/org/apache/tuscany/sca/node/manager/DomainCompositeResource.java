674a2eea493ba188357a3c58e04aba53b185f9b0
Luciano Resende <lresende@apache.org>
2011-04-20 06:43:53 +0000

    Minor cleanup on the node-manager test ui
    git-svn-id: https://svn.apache.org/repos/asf/tuscany/sca-java-2.x/trunk@1095293 13f79535-47bb-0310-9956-ffa450edef68

--- modules/node-manager/src/main/java/org/apache/tuscany/sca/node/manager/DomainCompositeResource.java
+++ modules/node-manager/src/main/java/org/apache/tuscany/sca/node/manager/DomainCompositeResource.java
@@ -23,8 +23,6 @@ import javax.ws.rs.DefaultValue;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
 
 import org.oasisopen.sca.annotation.Remotable;
 
