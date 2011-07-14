c28aedea05cfcfb8075ef7526eb2e4e2d33d5dd2
Simon Laws <slaws@apache.org>
2011-07-14 13:55:02 +0000

    TUSCANY-3855 - start of test for contexts. Not in build yet.
    git-svn-id: https://svn.apache.org/repos/asf/tuscany/sca-java-2.x/trunk@1146715 13f79535-47bb-0310-9956-ffa450edef68

---
+++ testing/itest/context/src/test/java/itest/helloworld/ContextTestCase.java
@@ -0,0 +1,54 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package itest.helloworld;
+
+import java.io.IOException;
+
+import org.apache.tuscany.sca.node.Node;
+import org.apache.tuscany.sca.node.NodeFactory;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ContextTestCase {
+
+    static {
+        org.apache.tuscany.sca.http.jetty.JettyServer.portDefault = 8085;
+    }
+
+    Node node;
+    
+    @Test
+    public void testContext() throws IOException, InterruptedException {
+        node = NodeFactory.getInstance().createNode("context.composite", new String[]{"target/classes"}) ;
+        node.start();
+        
+        // test the service invocations work
+        Helloworld helloworld = node.getService(Helloworld.class, "Client1");
+        Assert.assertEquals("Hello Petra", helloworld.sayHello("Petra"));
+
+       // helloworld = node.getService(Helloworld.class, "Client2");
+       // Assert.assertEquals("Hello Petra", helloworld.sayHello("Petra"));
+    }
+
+    @After
+    public void shutdown() throws InterruptedException {
+        node.stop();
+    }
+}
