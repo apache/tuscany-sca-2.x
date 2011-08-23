7d462418e553719f93544a694f471fab432d6d31
Anthony Elder <antelder@apache.org>
2011-08-23 10:34:44 +0000

    Add itests for jms wireformats textXml, bytes, and bytesXml. These don't work presently due to some databinding issue so taken out of the build with @Ignore
    git-svn-id: https://svn.apache.org/repos/asf/tuscany/sca-java-2.x/trunk@1160608 13f79535-47bb-0310-9956-ffa450edef68

---
+++ testing/itest/async-services/src/test/java/org/apache/tuscany/sca/itest/Test1JmsBytesXmlTestCase.java
@@ -0,0 +1,56 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.tuscany.sca.itest;
+
+import static org.junit.Assert.assertEquals;
+
+import org.apache.tuscany.sca.Node;
+import org.apache.tuscany.sca.TuscanyRuntime;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.oasisopen.sca.NoSuchServiceException;
+
+@Ignore
+public class Test1JmsBytesXmlTestCase {
+
+    protected TuscanyRuntime runtime;
+    protected Node node;
+
+    @Before
+    public void setUp() throws Exception {
+        node = TuscanyRuntime.runComposite("test1jmsbytesxml.composite", "target/classes");
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        node.stop();
+    }
+
+    @Test
+    public void testReference() throws NoSuchServiceException {
+        Service1 test = node.getService(Service1.class, "Client");
+        assertEquals("Service1AsyncServerImpl2.operation1Async foo" + 
+                     "Service1AsyncServerImpl2.operation1Async fooTypeOne" + 
+                     "Service1AsyncServerImpl2.operation1Async fooTypeTwo", 
+                     test.operation1("foo"));
+    }
+
+}
