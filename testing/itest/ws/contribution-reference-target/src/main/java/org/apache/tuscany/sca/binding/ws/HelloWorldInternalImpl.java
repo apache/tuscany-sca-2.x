4b44526c8f7c2555d307844999b2f95ac10721bc
Simon Laws <slaws@apache.org>
2011-09-15 13:37:28 +0000

    Add a test case to show that reference/target gets the right ws binding configuration when the target service binding is configured via a WSDL element.
    git-svn-id: https://svn.apache.org/repos/asf/tuscany/sca-java-2.x/trunk@1171094 13f79535-47bb-0310-9956-ffa450edef68

---
+++ testing/itest/ws/contribution-reference-target/src/main/java/org/apache/tuscany/sca/binding/ws/HelloWorldInternalImpl.java
@@ -0,0 +1,60 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.    
+ */
+
+package org.apache.tuscany.sca.binding.ws;
+
+import org.apache.tuscany.sca.binding.ws.jaxws.external.service.iface.Foo;
+import org.apache.tuscany.sca.binding.ws.jaxws.external.service.iface.HelloWorldService;
+import org.oasisopen.sca.ServiceRuntimeException;
+import org.oasisopen.sca.annotation.Reference;
+
+public class HelloWorldInternalImpl implements HelloWorld {
+
+    @Reference
+    public HelloWorldService helloWorldExternal;
+    
+    public String getGreetings(String s) {
+        System.out.println("Entering SCA HelloWorldInternal.getGreetings: " + s);
+        String response = helloWorldExternal.getGreetings(s);
+        System.out.println("Leaving SCA HelloWorldInternal.getGreetings: " + response);
+        return response;
+    }
+    
+    public String getGreetingsException(String s) throws ServiceRuntimeException {
+        System.out.println("Entering SCA HelloWorldInternal.getGreetingsException: " + s);
+        String response = helloWorldExternal.getGreetings(s);
+        System.out.println("Leaving SCA HelloWorldInternal.getGreetings: " + response);
+        throw new ServiceRuntimeException(response);
+    }  
+
+    public Foo getGreetingsComplex(Foo foo){
+        System.out.println("Entering SCA HelloWorldInternal.getGreetingsComplex: " + foo.getBars().get(0).getS());
+        Foo response = helloWorldExternal.getGreetingsComplex(foo);
+        System.out.println("Leaving SCA HelloWorldInternal.getGreetingsComplex: " + foo.getBars().get(0).getS());
+        return response;
+    } 
+    
+    public String getGreetingsCaps(String s) {
+        System.out.println("Entering SCA HelloWorldInternal.getGreetingsCaps: " + s);
+        String response = helloWorldExternal.getGreetingsCaps(s);
+        System.out.println("Leaving SCA HelloWorldInternal.getGreetingsCaps: " + response);
+        return response;
+    }
+   
+}
