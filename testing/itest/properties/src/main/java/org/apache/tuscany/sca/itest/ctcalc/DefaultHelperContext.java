4cc08bdaaeb88e48cbf4cdfcd19bf96bcbbbab20
Anthony Elder <antelder@apache.org>
2011-12-05 12:23:45 +0000

    TUSCANY_3965: Add a field thats annotated but not with an SCA annotation to match the scenario as described in TUSCANY_3965
    git-svn-id: https://svn.apache.org/repos/asf/tuscany/sca-java-2.x/trunk@1210425 13f79535-47bb-0310-9956-ffa450edef68

--- testing/itest/properties/src/main/java/org/apache/tuscany/sca/itest/ctcalc/DefaultHelperContext.java
+++ testing/itest/properties/src/main/java/org/apache/tuscany/sca/itest/ctcalc/DefaultHelperContext.java
@@ -16,23 +16,20 @@
  * specific language governing permissions and limitations
  * under the License.    
  */
+
 package org.apache.tuscany.sca.itest.ctcalc;
 
-import org.oasisopen.sca.annotation.Service;
+import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.ElementType.PARAMETER;
+import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
-import commonj.sdo.helper.HelperContext;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
 
-@Service(CTCalcComponent.class)
-public class AnnotatedCTCalcComponentImpl extends BaseCTCalcComponentImpl {
-    
-    private String aPropertyPrivate;
-    protected String aPropertyProtected;
-    public String aPropertyPublic;
-    
-    @DefaultHelperContext 
-    protected HelperContext hc;
-    
-    public String test(){
-        return "Annotated";
-    }
+@Target({TYPE, METHOD, FIELD, PARAMETER})
+@Retention(RUNTIME)
+public @interface DefaultHelperContext {
+
 }
