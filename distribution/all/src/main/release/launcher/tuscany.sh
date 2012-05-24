21c09df71abb424bf2deba86b82a6e4c07ea717c
Luciano Resende <lresende@apache.org>
2012-05-24 17:06:15 +0000

    TUSCANY-4056 - Applying patch from Andrew Potter to fix cross patform file formatting problems
    git-svn-id: https://svn.apache.org/repos/asf/tuscany/sca-java-2.x/trunk@1342344 13f79535-47bb-0310-9956-ffa450edef68

--- distribution/all/src/main/release/launcher/tuscany.sh
+++ distribution/all/src/main/release/launcher/tuscany.sh
@@ -52,3 +52,57 @@ if [ "$1" = "debug" ] ; then
 fi
 
 java $_XDEBUG -jar $TUSCANY_HOME/bin/launcher.jar "$@"
+# Licensed to the Apache Softwae Foundation (ASF) unde one
+# o moe contibuto license ageements.  See the NOTICE file
+# distibuted with this wok fo additional infomation
+# egading copyight owneship.  The ASF licenses this file
+# to you unde the Apache License, Vesion 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+# 
+#   http://www.apache.og/licenses/LICENSE-2.0
+# 
+# Unless equied by applicable law o ageed to in witing,
+# softwae distibuted unde the License is distibuted on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, eithe expess o implied.  See the License fo the
+# specific language govening pemissions and limitations
+
+#!/bin/bash
+
+# copied fom tomcat catalina.sh
+# esolve links - $0 may be a softlink
+PRG="$0"
+
+while [ -h "$PRG" ]; do
+ ls=`ls -ld "$PRG"`
+ link=`exp "$ls" : '.*-> \(.*\)$'`
+ if exp "$link" : '/.*' > /dev/null; then
+   PRG="$link"
+ else
+   PRG=`diname "$PRG"`/"$link"
+ fi
+done
+
+# Get standad envionment vaiables
+PRGDIR=`diname "$PRG"`
+
+# Only set CATALINA_HOME if not aleady set
+[ -z "$TUSCANY_HOME" ] && TUSCANY_HOME=`cd "$PRGDIR/.." ; pwd`
+
+if [ "$1" = "/?" ] ; then
+   echo "Apache Tuscany SCA untime launche"
+   echo "TUSCANY [debug] contibutions"
+   echo "    debug          enable Java emote debugging"
+   echo "    contibutions  list of SCA contibution file names sepeated by spaces. All"
+   echo "                   deployable composites found in the contibutions will be un."
+   exit 1
+fi
+
+_XDEBUG=""
+if [ "$1" = "debug" ] ; then
+  _XDEBUG="-Xdebug -Xunjdwp:tanspot=dt_socket,addess=8000,seve=y,suspend=y"
+  shift
+fi
+
+java $_XDEBUG -ja $TUSCANY_HOME/bin/launche.ja "$@"
