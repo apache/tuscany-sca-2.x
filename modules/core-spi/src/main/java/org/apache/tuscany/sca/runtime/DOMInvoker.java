c924a17a665ce11f3eb4e12894bdfbfb72225ba6
Anthony Elder <antelder@apache.org>
2012-08-16 14:39:41 +0000

    Make DOMInvoker interface remotable
    git-svn-id: https://svn.apache.org/repos/asf/tuscany/sca-java-2.x/trunk@1373867 13f79535-47bb-0310-9956-ffa450edef68

--- modules/core-spi/src/main/java/org/apache/tuscany/sca/runtime/DOMInvoker.java
+++ modules/core-spi/src/main/java/org/apache/tuscany/sca/runtime/DOMInvoker.java
@@ -1,7 +1,9 @@
 package org.apache.tuscany.sca.runtime;
 
+import org.oasisopen.sca.annotation.Remotable;
 import org.w3c.dom.Node;
 
+@Remotable
 public interface DOMInvoker {
 
     Node invoke(String operation, Node args);
