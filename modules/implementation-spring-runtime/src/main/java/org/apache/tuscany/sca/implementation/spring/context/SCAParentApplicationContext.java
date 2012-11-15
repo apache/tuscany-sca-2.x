7768ed9c95759d3b089fafeeebf6c5a56c6e687d
Zhaohui Feng <rfeng@apache.org>
2012-11-15 19:31:00 +0000

    Fix a regression caused by the base class
    git-svn-id: https://svn.apache.org/repos/asf/tuscany/sca-java-2.x/trunk@1409943 13f79535-47bb-0310-9956-ffa450edef68

--- modules/implementation-spring-runtime/src/main/java/org/apache/tuscany/sca/implementation/spring/context/SCAParentApplicationContext.java
+++ modules/implementation-spring-runtime/src/main/java/org/apache/tuscany/sca/implementation/spring/context/SCAParentApplicationContext.java
@@ -61,6 +61,11 @@ public class SCAParentApplicationContext extends GenericApplicationContext imple
     } // end constructor
 
 
+    protected BeanFactory getInternalParentBeanFactory() {
+        // Disable the parent delegation as we'll handle it inside the getBean()
+        return null;
+    }
+
     public Object getBean(String name) throws BeansException {
         return getBean(name, (Class)null);
     }
